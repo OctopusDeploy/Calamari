557399f5d937506d6694c8eb4d54a30419f26bc4
Michael Richardson <michael.james.richardson@gmail.com>
2015-03-25 09:42:23 +1000

    init

--- source/Calamari/Deployment/Conventions/SubstituteInFilesConvention.cs
+++ source/Calamari/Deployment/Conventions/SubstituteInFilesConvention.cs
@@ -1,6 +1,19 @@
+using Calamari.Integration.FileSystem;
+
 namespace Calamari.Deployment.Conventions
 {
-    public class SubstituteInFilesConvention : IConvention
+    public class SubstituteInFilesConvention : IInstallConvention
     {
+        private readonly ICalamariFileSystem fileSystem;
+
+        public SubstituteInFilesConvention(ICalamariFileSystem fileSystem)
+        {
+            this.fileSystem = fileSystem;
+        }
+
+        public void Install(RunningDeployment deployment)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
