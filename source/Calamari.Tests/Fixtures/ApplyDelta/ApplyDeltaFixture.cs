73b9fb45b83f0036a2e88b68cf3ba7290469856a
Henrik Andersson <henrik.a.andersson@outlook.com>
2015-04-08 09:43:24 +1000

    Add test for applying delta file to a package

--- source/Calamari.Tests/Fixtures/ApplyDelta/ApplyDeltaFixture.cs
+++ source/Calamari.Tests/Fixtures/ApplyDelta/ApplyDeltaFixture.cs
@@ -4,11 +4,12 @@ using Calamari.Integration.FileSystem;
 using Calamari.Tests.Fixtures.Deployment.Packages;
 using Calamari.Tests.Helpers;
 using NUnit.Framework;
+using Octodiff.Core;
 
 namespace Calamari.Tests.Fixtures.ApplyDelta
 {
     [TestFixture]
-    public class ApplyDeltaFixture  : CalamariFixture
+    public class ApplyDeltaFixture : CalamariFixture
     {
         readonly string downloadPath = Path.Combine(GetPackageDownloadFolder("ApplyDelta"), "Files");
         readonly string newFileName = "Acme.Web.1.0.0.1.nupkg";
@@ -145,11 +146,48 @@ namespace Calamari.Tests.Fixtures.ApplyDelta
                 var result = ApplyDelta(basisFile.FilePath, basisFile.Hash, deltaFilePath, newFileName);
 
                 result.AssertNonZero();
-                result.AssertOutput("Applying delta to {0} with hash {1} and storing as {2}", 
+                result.AssertOutput("Applying delta to {0} with hash {1} and storing as {2}",
                     basisFile.FilePath,
-                    basisFile.Hash, 
+                    basisFile.Hash,
                     Path.Combine(downloadPath, newFileName));
                 result.AssertErrorOutput("The delta file appears to be corrupt.");
+            }
+        }
+
+        [Test]
+        public void ShouldApplyDeltaToPreviousPackageToCreateNewPackage()
+        {
+            var signatureBuilder = new SignatureBuilder();
+            var deltaBuilder = new DeltaBuilder();
+
+            using (var basisFile = new TemporaryFile(PackageBuilder.BuildSamplePackage("Acme.Web", "1.0.0.0")))
+            using (var signatureFile = new TemporaryFile(basisFile.FilePath + ".octosig"))
+            {
+                using (var basisFileStream = File.OpenRead(basisFile.FilePath))
+                using (var signatureFileStream = File.OpenWrite(signatureFile.FilePath))
+                {
+                    signatureBuilder.Build(basisFileStream, new SignatureWriter(signatureFileStream));
+                    Assert.That(File.Exists(signatureFile.FilePath));
+                }
+
+                using (var newFile = new TemporaryFile(PackageBuilder.BuildSamplePackage("Acme.Web", "1.0.0.1", true)))
+                using (var deltaFile = new TemporaryFile(basisFile.FilePath +
+                        "_to_" + newFileName + ".octodelta"))
+                {
+                    using (var newFileStream = File.OpenRead(newFile.FilePath))
+                    using (var signatureFileStream = File.OpenRead(signatureFile.FilePath))
+                    using (var deltaFileStream = File.Open(deltaFile.FilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.Read))
+                    {
+                        deltaBuilder.BuildDelta(newFileStream,
+                            new SignatureReader(signatureFileStream, deltaBuilder.ProgressReporter),
+                            new AggregateCopyOperationsDecorator(new BinaryDeltaWriter(deltaFileStream)));
+                    }
+                    var result = ApplyDelta(basisFile.FilePath, basisFile.Hash, deltaFile.FilePath, newFileName);
+                    result.AssertZero();
+                    result.AssertOutput("Applying delta to {0} with hash {1} and storing as {2}", basisFile.FilePath,
+                        basisFile.Hash, Path.Combine(downloadPath, newFileName));
+                    result.AssertOutput("##octopus[deltaVerification");
+                }
             }
         }
     }
