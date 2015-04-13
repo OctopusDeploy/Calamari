298468c743aad24423e59762c3d1df4025ec8b30
Damian Brady <dev@damianbrady.com.au>
2015-04-13 16:10:31 +1000

    XML Transform step will now fail on warnings unless the Octopus.Action.Package.IgnoreConfigTransformationErrors variable is set to True. Part of #1225.

--- source/Calamari/Integration/ConfigurationTransforms/VerboseTransformLogger.cs
+++ source/Calamari/Integration/ConfigurationTransforms/VerboseTransformLogger.cs
@@ -6,12 +6,16 @@ namespace Calamari.Integration.ConfigurationTransforms
 {
     public class VerboseTransformLogger : IXmlTransformationLogger
     {
+        public event LogDelegate Warning;
+
+        private readonly bool suppressWarnings;
         private string stdOutMode;
         private readonly IndentedTextWriter stdOut;
         private readonly IndentedTextWriter stdErr;
 
-        public VerboseTransformLogger()
+        public VerboseTransformLogger(bool suppressWarnings = false)
         {
+            this.suppressWarnings = suppressWarnings;
             stdOut = new IndentedTextWriter(Console.Out, "  ");
             stdErr = new IndentedTextWriter(Console.Error, "  ");
         }
@@ -29,20 +33,27 @@ namespace Calamari.Integration.ConfigurationTransforms
 
         public void LogWarning(string message, params object[] messageArgs)
         {
-            EnsureStdOutMode("warning");
+            if (Warning != null) { Warning(this, new ErrorDelegateArgs(string.Format(message, messageArgs))); }
+
+            EnsureStdOutMode(suppressWarnings ? "verbose" : "warning");
             stdOut.WriteLine(message, messageArgs);
         }
 
         public void LogWarning(string file, string message, params object[] messageArgs)
         {
-            EnsureStdOutMode("warning");
+            if (Warning != null) { Warning(this, new ErrorDelegateArgs(string.Format("{0}: {1}", file, string.Format(message, messageArgs)))); }
+
+            EnsureStdOutMode(suppressWarnings ? "verbose" : "warning");
             stdOut.Write("File {0}: ", file);
             stdOut.WriteLine(message, messageArgs);
+
         }
 
         public void LogWarning(string file, int lineNumber, int linePosition, string message, params object[] messageArgs)
         {
-            EnsureStdOutMode("warning");
+            if (Warning != null) { Warning(this, new ErrorDelegateArgs(string.Format("{0}({1},{2}): {3}", file, lineNumber, linePosition, string.Format(message, messageArgs)))); }
+
+            EnsureStdOutMode(suppressWarnings ? "verbose" : "warning");
             stdOut.Write("File {0}, line {1}, position {2}: ", file, lineNumber, linePosition);
             stdOut.WriteLine(message, messageArgs);
         }
@@ -106,12 +117,24 @@ namespace Calamari.Integration.ConfigurationTransforms
         {
             EndSection(message, messageArgs);
         }
-        
+
         void EnsureStdOutMode(string mode)
         {
             if (stdOutMode != mode)
                 stdOut.WriteLine("##octopus[stdout-" + mode + "]");
             stdOutMode = mode;
+        }
+    }
+
+    public delegate void LogDelegate(object sender, ErrorDelegateArgs args);
+
+    public class ErrorDelegateArgs
+    {
+        public string Message { get; set; }
+
+        public ErrorDelegateArgs(string message)
+        {
+            Message = message;
         }
     }
 }
