0a08a1ecb69688794d26898a776e73c3325b3a9d
Damian Brady <dev@damianbrady.com.au>
2015-04-14 12:33:33 +1000

    Refactorings suggested by @PaulStovell

---
+++ source/Calamari/Integration/ConfigurationTransforms/WarningDelegateArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace Calamari.Integration.ConfigurationTransforms
+{
+    public delegate void LogDelegate(object sender, WarningDelegateArgs args);
+
+    public class WarningDelegateArgs
+    {
+        public string Message { get; set; }
+
+        public WarningDelegateArgs(string message)
+        {
+            Message = message;
+        }
+    }
+}
