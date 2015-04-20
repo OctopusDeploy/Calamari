a857953c1dbb3cc8aa363a693808111785e33b57
Paul Stovell <paul@paulstovell.com>
2015-04-20 16:02:39 +1000

    OCD

--- source/Calamari.Tests/Fixtures/PackageDownload/AuthenticatedTest.cs
+++ source/Calamari.Tests/Fixtures/PackageDownload/AuthenticatedTest.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using NUnit.Framework;
 
 namespace Calamari.Tests.Fixtures.PackageDownload
@@ -9,24 +6,24 @@ namespace Calamari.Tests.Fixtures.PackageDownload
     [AttributeUsage(AttributeTargets.Method)]
     public class AuthenticatedTest : Attribute, ITestAction
     {
-        readonly string _feedUri;
-        readonly string _feedUsernameVariable;
-        readonly string _feedPasswordVariable;
+        readonly string feedUri;
+        readonly string feedUsernameVariable;
+        readonly string feedPasswordVariable;
 
         public AuthenticatedTest(string feedUri, string feedUsernameVariable, string feedPasswordVariable)
         {
-            _feedUri = feedUri;
-            _feedUsernameVariable = feedUsernameVariable;
-            _feedPasswordVariable = feedPasswordVariable;
+            this.feedUri = feedUri;
+            this.feedUsernameVariable = feedUsernameVariable;
+            this.feedPasswordVariable = feedPasswordVariable;
         }
 
         public void BeforeTest(TestDetails testDetails)
         {
-            if (String.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable(_feedUri)) ||
-                String.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable(_feedUsernameVariable)) ||
-                String.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable(_feedPasswordVariable)))
+            if (String.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable(feedUri)) ||
+                String.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable(feedUsernameVariable)) ||
+                String.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable(feedPasswordVariable)))
             {
-                Assert.Ignore("The authenticated feed tests were skipped because the " + _feedUri + ", " + _feedUsernameVariable + " and " +_feedPasswordVariable + " environment variables are not set.");
+                Assert.Ignore("The authenticated feed tests were skipped because the " + feedUri + ", " + feedUsernameVariable + " and " +feedPasswordVariable + " environment variables are not set.");
             }
         }
 
