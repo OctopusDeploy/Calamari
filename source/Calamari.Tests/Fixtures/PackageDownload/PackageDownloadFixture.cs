2e1411c4f880e30994f9e786ff8fcde9ff773de5
Robert Erez <robert.erez@gmail.com>
2015-05-04 10:37:56 +1000

    ensure ExtractPackageToApplicationDirectoryConvention tests run on Linux

--- source/Calamari.Tests/Fixtures/PackageDownload/PackageDownloadFixture.cs
+++ source/Calamari.Tests/Fixtures/PackageDownload/PackageDownloadFixture.cs
@@ -53,17 +53,17 @@ namespace Calamari.Tests.Fixtures.PackageDownload
         [Test]
         public void ShouldDownloadPackage()
         {
-            var result = DownloadPackage(AuthFeed.PackageId, PublicFeed.Version, PublicFeed.Id, PublicFeedUri);
+            var result = DownloadPackage(PublicFeed.PackageId, PublicFeed.Version, PublicFeed.Id, PublicFeedUri);
 
             result.AssertZero();
 
-            result.AssertOutput("Downloading NuGet package {0} {1} from feed: '{2}'", AuthFeed.PackageId, PublicFeed.Version, PublicFeedUri);
+            result.AssertOutput("Downloading NuGet package {0} {1} from feed: '{2}'", PublicFeed.PackageId, PublicFeed.Version, PublicFeedUri);
             result.AssertOutput("Downloaded package will be stored in: '{0}'", PublicFeed.DownloadFolder);
-            result.AssertOutput("Found package {0} version {1}", AuthFeed.PackageId, PublicFeed.Version);
+            result.AssertOutput("Found package {0} version {1}", PublicFeed.PackageId, PublicFeed.Version);
             AssertPackageHashMatchesExpected(result, ExpectedPackageHash);
             AssertPackageSizeMatchesExpected(result, ExpectedPackageSize);
             AssertStagePackageOutputVariableSet(result, PublicFeed.File);
-            result.AssertOutput("Package {0} {1} successfully downloaded from feed: '{2}'", AuthFeed.PackageId, PublicFeed.Version, PublicFeedUri);
+            result.AssertOutput("Package {0} {1} successfully downloaded from feed: '{2}'", PublicFeed.PackageId, PublicFeed.Version, PublicFeedUri);
         }
 
         [Test]
