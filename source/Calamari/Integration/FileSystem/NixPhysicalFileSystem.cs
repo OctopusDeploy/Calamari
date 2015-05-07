5a7d0853f8c5d405c1006e550b6b3c8ad0d61754
Robert Erez <robert.erez@gmail.com>
2015-05-08 00:10:53 +1000

    Allow DiskFreespace for Nix

--- source/Calamari/Integration/FileSystem/NixPhysicalFileSystem.cs
+++ source/Calamari/Integration/FileSystem/NixPhysicalFileSystem.cs
@@ -1,9 +1,18 @@
-﻿namespace Calamari.Integration.FileSystem
+﻿using System.IO;
+
+namespace Calamari.Integration.FileSystem
 {
     public class NixCalamariPhysicalFileSystem : CalamariPhysicalFileSystem
     {
         protected override bool GetFiskFreeSpace(string directoryPath, out ulong totalNumberOfFreeBytes)
         {
+            var pathRoot = Path.GetPathRoot(directoryPath);
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                if (!drive.Name.Equals(pathRoot)) continue;
+                totalNumberOfFreeBytes = (ulong)drive.TotalFreeSpace;
+                return true;
+            }
             totalNumberOfFreeBytes = 0;
             return false;
         }
