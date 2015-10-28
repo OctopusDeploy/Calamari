a433ac08826d17f199c91ac4823168bb8e8ef454
michaelnoonan <michaelnoonan@gmail.com>
2015-10-28 13:29:51 +1000

    Log the informational version at startup.
    This will enable us to persist/view the version of Calamari being used to run any Task.

---
+++ source/Calamari/Util/AssemblyExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+// ReSharper disable once CheckNamespace
+namespace Calamari
+{
+    public static class AssemblyExtensions
+    {
+        public static string FullLocalPath(this Assembly assembly)
+        {
+            var codeBase = assembly.CodeBase;
+            var uri = new UriBuilder(codeBase);
+            var root = Uri.UnescapeDataString(uri.Path);
+            root = root.Replace("/", "\\");
+            return root;
+        }
+
+        public static string GetInformationalVersion(this Assembly assembly)
+        {
+            var fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.FullLocalPath());
+            return fileVersionInfo.ProductVersion;
+        }
+    }
+}
