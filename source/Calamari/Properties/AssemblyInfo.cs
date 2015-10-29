60990585ccf28544f3c28c90aefefc3cdbd7e814
michaelnoonan <michaelnoonan@gmail.com>
2015-10-29 16:22:30 +1000

    Version with GitVersionTask.
    Moved common attributes to SolutionInfo.cs and VersionInfo.cs.
    Updated Package.targets to use timestamp NuGet version for DEBUG and GitVersion version for RELEASE.

--- source/Calamari/Properties/AssemblyInfo.cs
+++ source/Calamari/Properties/AssemblyInfo.cs
@@ -2,38 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Calamari")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Calamari")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: InternalsVisibleTo("Calamari.Tests")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2c948d59-4d8c-4c9e-a736-21c82e8a47c4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0-local")]
