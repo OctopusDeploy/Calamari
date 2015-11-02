7856a780674667716f5021b1f8289473c180b9aa
michaelnoonan <michaelnoonan@gmail.com>
2015-11-02 12:54:43 +1000

    Ensure we are testing with the .config file present.
    https://github.com/OctopusDeploy/Issues/issues/2107

--- source/Calamari.Tests/Helpers/CalamariFixture.cs
+++ source/Calamari.Tests/Helpers/CalamariFixture.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.Linq;
 using Calamari.Commands;
 using Calamari.Integration.Processes;
 using Calamari.Integration.ServiceMessages;
@@ -12,14 +11,19 @@ namespace Calamari.Tests.Helpers
     {
         protected CommandLine Calamari()
         {
-            return CommandLine.Execute(typeof (DeployPackageCommand).Assembly.FullLocalPath());
+            var calamariFullPath = typeof (DeployPackageCommand).Assembly.FullLocalPath();
+            var calamariConfigFilePath = calamariFullPath + ".config";
+            if (!File.Exists(calamariConfigFilePath))
+                throw new FileNotFoundException($"Unable to find {calamariConfigFilePath} which means the config file would not have been included in testing {calamariFullPath}");
+
+            return CommandLine.Execute(calamariFullPath);
         }
 
         protected CommandLine OctoDiff()
         {
             var octoDiffExe = Path.Combine(TestEnvironment.CurrentWorkingDirectory, "Octodiff.exe");
             if (!File.Exists(octoDiffExe))
-                throw new FileNotFoundException("Unable to find Octodiff.exe");
+                throw new FileNotFoundException($"Unable to find {octoDiffExe}");
 
             return CommandLine.Execute(octoDiffExe);
         }
