626c4d919f56f1c85e7f26ea83c1cc65f41aa382
Robert Erez <robert.erez@gmail.com>
2015-11-29 16:18:35 +1000

    Change package version parser to account for hash suffix

---
+++ source/Calamari.Tests/Fixtures/Integration/Packages/SimplePackageExtractorFixture.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using Calamari.Integration.Packages;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Calamari.Tests.Fixtures.Integration.Packages
+{
+    [TestFixture]
+    public class SimplePackageExtractorFixture
+    {
+        SimplePackageExtractor extractor;
+
+        [SetUp]
+        public void SetUp()
+        {
+            extractor = Substitute.For<SimplePackageExtractor>();
+            extractor.Extensions.Returns(new string[] { ".tar.gz" });
+        }
+
+        [Test]
+        public void MetadataExtractedWhenHashPresent()
+        {
+            var metaData = extractor.GetMetadata("octofxjs.2.3.41.tar.gz-e7e75d07-6b62-4219-81c6-121698876868");
+
+            Assert.AreEqual("octofxjs", metaData.Id);
+            Assert.AreEqual("2.3.41", metaData.Version);
+            Assert.AreEqual(".tar.gz", metaData.FileExtension);
+        }
+
+        [Test]
+        public void MetadataExtractedWhenHashAbsent()
+        {
+            var metaData = extractor.GetMetadata("octofxjs.2.3.41.tar.gz");
+
+            Assert.AreEqual("octofxjs", metaData.Id);
+            Assert.AreEqual("2.3.41", metaData.Version);
+            Assert.AreEqual(".tar.gz", metaData.FileExtension);
+        }
+
+        [Test]
+        [ExpectedException(typeof(FileFormatException))]
+        public void ThrowsWhenUnknownExtension()
+        {
+            extractor.GetMetadata("octofxjs.2.3.41.doc");
+        }
+
+        [Test]
+        [ExpectedException(typeof(FileFormatException))]
+        public void ThrowsWhenInvalidSeverVersion()
+        {
+            extractor.GetMetadata("octofxjs.2.3.other.tar.gz");
+        }
+
+        [Test]
+        [ExpectedException(typeof(FileFormatException))]
+        public void ThrowsWhenMissingVersion()
+        {
+            extractor.GetMetadata("octofxjs.tar.gz");
+        }
+
+        [Test]
+        [ExpectedException(typeof(FileFormatException))]
+        public void ThrowsWhenMissingPackageId()
+        {
+            extractor.GetMetadata("2.3.41.doc");
+        }
+
+    }
+}
--- source/Calamari/Integration/Packages/SimplePackageExtractor.cs
+++ source/Calamari/Integration/Packages/SimplePackageExtractor.cs
@@ -1,5 +1,7 @@
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
+using NuGet;
 
 namespace Calamari.Integration.Packages
 {
@@ -8,15 +10,34 @@ namespace Calamari.Integration.Packages
         public PackageMetadata GetMetadata(string packageFile)
         {
             var pkg = new PackageMetadata();
-            var fileName = Path.GetFileName(packageFile);
 
-            var matchingExtension = Extensions.FirstOrDefault(ext => fileName.EndsWith(ext));
+            var metaDataSection = ExtractMatchingExtension(packageFile, pkg);
 
-            if (string.IsNullOrEmpty(matchingExtension))
-                return pkg;
+            if (string.IsNullOrEmpty(pkg.FileExtension))
+            {
+                throw new FileFormatException(string.Format("Unable to determine filetype of file \"{0}\"", packageFile));
+            }
 
-            var metaData = fileName.Substring(0, fileName.Length - matchingExtension.Length);
-            var nameParts = metaData.Split('.');
+            ExtractIdAndVersion(metaDataSection, pkg);
+
+
+            SemanticVersion version;
+            if (string.IsNullOrEmpty(pkg.Version) || !SemanticVersion.TryParse(pkg.Version, out version))
+            {
+                throw new FileFormatException(string.Format("Unable to extract the package version from file \"{0}\"", packageFile));
+            }
+
+            if (string.IsNullOrEmpty(pkg.Id))
+            {
+                throw new FileFormatException(string.Format("Unable to extract the package Id from file \"{0}\"", packageFile));
+            }
+
+            return pkg;
+        }
+
+        private static void ExtractIdAndVersion(string metaDataSection, PackageMetadata pkg)
+        {
+            var nameParts = metaDataSection.Split('.');
             for (var i = 0; i < nameParts.Length; i++)
             {
                 int num;
@@ -27,11 +48,39 @@ namespace Calamari.Integration.Packages
                     break;
                 }
             }
-
-            return pkg;
         }
+
+        private string ExtractMatchingExtension(string packageFile, PackageMetadata pkg)
+        {
+            var fileName = Path.GetFileName(packageFile);
+            var matchingExtension = Extensions.FirstOrDefault(fileName.EndsWith);
+            var metaDataSection = string.Empty;
+            if (matchingExtension != null)
+            {
+                metaDataSection = fileName.Substring(0, fileName.Length - matchingExtension.Length);
+            }
+            else
+            {
+                foreach (var ext in Extensions)
+                {
+                    var match = new Regex("(?<extension>" + ext.Replace(".", "\\.") + ")-[a-z0-9\\-]*$").Match(fileName);
+                    if (match.Success)
+                    {
+                        matchingExtension = match.Groups["extension"].Value;
+                        metaDataSection = fileName.Substring(0, match.Index);
+                        break;
+                    }
+                }
+            }
+            
+            pkg.FileExtension = matchingExtension;
+            return metaDataSection;
+        }
+
 
         public abstract int Extract(string packageFile, string directory, bool suppressNestedScriptWarning);
         public abstract string[] Extensions { get; }
+
+        
     }
 }
