10ccdcd08ecad02ad6ee931cc818234411dd316f
Shane Gill <tothegills@gmail.com>
2015-12-01 14:38:14 +1000

    Fix wildcard trimming to prevent iterating all files when an extension is provided

--- source/Calamari/Integration/ConfigurationTransforms/XmlConfigTransformDefinition.cs
+++ source/Calamari/Integration/ConfigurationTransforms/XmlConfigTransformDefinition.cs
@@ -1,45 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 
 namespace Calamari.Integration.ConfigurationTransforms
 {
-        public class XmlConfigTransformDefinition
+    public class XmlConfigTransformDefinition
+    {
+        public XmlConfigTransformDefinition(string definition)
         {
-            public XmlConfigTransformDefinition(string definition)
+            if (definition.Contains("=>"))
             {
-                if (definition.Contains("=>"))
-                {
-                    Advanced = true;
-                    var separators = new[] {"=>"};
-                    var parts = definition.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-                    TransformPattern = parts[0].Trim();
-                    SourcePattern = parts[1].Trim();
+                Advanced = true;
+                var separators = new[] {"=>"};
+                var parts = definition.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                TransformPattern = parts[0].Trim();
+                SourcePattern = parts[1].Trim();
 
-                    if (TransformPattern.StartsWith("*."))
-                    {
-                        Wildcard = true;
-                        TransformPattern = TransformPattern.Remove(0, 2);
-                    }
-
-                    if (SourcePattern.StartsWith("*."))
-                    {
-                        Wildcard = true;
-                        SourcePattern = SourcePattern.Remove(0, 2);
-                    }
-                }
-                else
+                if (TransformPattern.StartsWith("*."))
                 {
-                    TransformPattern = definition;
+                    Wildcard = true;
+                    TransformPattern = TrimWildcardPattern(TransformPattern);
                 }
 
+                if (SourcePattern.StartsWith("*."))
+                {
+                    Wildcard = true;
+                    SourcePattern = TrimWildcardPattern(SourcePattern);
+                }
             }
-
-            public string TransformPattern { get; private set; }
-            public string SourcePattern { get; private set; }
-            public bool Wildcard { get; private set; }
-            public bool Advanced { get; private set; }
-
-
+            else
+            {
+                TransformPattern = definition;
+            }
         }
+
+        string TrimWildcardPattern(string pattern)
+        {
+            return (pattern.LastIndexOf('.') > 2)
+                ? pattern.Remove(0, 2)
+                : pattern.Remove(0, 1);
+        }
+
+        public string TransformPattern { get; private set; }
+        public string SourcePattern { get; private set; }
+        public bool Wildcard { get; private set; }
+        public bool Advanced { get; private set; }
+    }
 }
