f8d7cb8e5f16d18a8e7e37fe9db669911efbbf26
Michael Richardson <michael.james.richardson@gmail.com>
2015-12-23 10:32:00 +1000

    Minor issue caused by merge

--- source/Calamari.Azure/Commands/DeployAzureResourceGroupCommand.cs
+++ source/Calamari.Azure/Commands/DeployAzureResourceGroupCommand.cs
@@ -51,7 +51,7 @@ namespace Calamari.Azure.Commands
             {
                 new ContributeEnvironmentVariablesConvention(),
                 new LogVariablesConvention(),
-                new ExtractPackageToStagingDirectoryConvention(new LightweightPackageExtractor(), fileSystem),
+                new ExtractPackageToStagingDirectoryConvention(new GenericPackageExtractor(), fileSystem),
                 new DeployAzureResourceGroupConvention(templateFile, templateParameterFile, fileSystem, new ResourceGroupTemplateParameterParser())
             };
 
