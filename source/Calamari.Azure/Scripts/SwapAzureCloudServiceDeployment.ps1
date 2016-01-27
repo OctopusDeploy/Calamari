f5e213dc3cee3a5031807933920c45e3aed5b4b2
Michael Richardson <michael.james.richardson@gmail.com>
2016-01-27 12:10:34 +1000

    SwapAzureCloudServiceDeployment.ps1 no longer sets the storage-account, as it is set by the AzureContext bootstrap script

--- source/Calamari.Azure/Scripts/SwapAzureCloudServiceDeployment.ps1
+++ source/Calamari.Azure/Scripts/SwapAzureCloudServiceDeployment.ps1
@@ -2,16 +2,11 @@
 ## The script will be passed the following parameters in addition to the normal Octopus 
 ## variables passed to any PowerShell script. 
 ## 
-##   $OctopusAzureSubscriptionId           // The subscription ID GUID
-##   $OctopusAzureSubscriptionName         // The name of the subscription as stored in the subscription data file. This will be the same as the Account name in Octopus Deploy. 
 ##   $OctopusAzureServiceName              // The name of your cloud service
 ##   $OctopusAzureStorageAccountName       // The name of your storage account
 ##   $OctopusAzureSlot                     // The name of the slot to deploy to (Staging or Production)
 ##   $OctopusAzureDeploymentLabel          // The label to use for deployment
 ##   $OctopusAzureSwapIfPossible           // "True" if we should attempt to "swap" deployments rather than a new deployment
-##
-
-Set-AzureSubscription -SubscriptionName $OctopusAzureSubscriptionName -CurrentStorageAccount $OctopusAzureStorageAccountName
  
 function SwapDeployment()
 {
