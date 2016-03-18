4197853bd9da0420480479dee73d31fba2452846
Michael Noonan <michaelnoonan@gmail.com>
2016-03-18 13:25:42 +1000

    Fixed help messages.
    Not just NuGet packages any more.
    Not just PowerShell nor ScriptCS any more.

--- source/Calamari/Commands/RunScriptCommand.cs
+++ source/Calamari/Commands/RunScriptCommand.cs
@@ -25,8 +25,8 @@ namespace Calamari.Commands
         public RunScriptCommand()
         {
             Options.Add("variables=", "Path to a JSON file containing variables.", v => variablesFile = Path.GetFullPath(v));
-            Options.Add("package=", "Path to the NuGet package to extract that contains the package.", v => packageFile = Path.GetFullPath(v));
-            Options.Add("script=", "Path to the script (PowerShell or ScriptCS) script to execute. If --package is used, it can be a script inside the package.", v => scriptFile = Path.GetFullPath(v));
+            Options.Add("package=", "Path to the package to extract that contains the package.", v => packageFile = Path.GetFullPath(v));
+            Options.Add("script=", "Path to the script to execute. If --package is used, it can be a script inside the package.", v => scriptFile = Path.GetFullPath(v));
             Options.Add("sensitiveVariables=", "Password protected JSON file containing sensitive-variables.", v => sensitiveVariablesFile = v);
             Options.Add("sensitiveVariablesPassword=", "Password used to decrypt sensitive-variables.", v => sensitiveVariablesPassword = v);
             Options.Add("substituteVariables", "Perform variable substitution on the script body before executing it.", v => substituteVariables = true);
