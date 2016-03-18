bdf4566cb5c75d415589293baa541b030546415f
Michael Noonan <michaelnoonan@gmail.com>
2016-03-18 13:25:10 +1000

    Add option to substituteVariables so packaged scripts work as expected.
    Taking this approach means people currently using `run-script` without this option will get the existing behavior.

--- source/Calamari/Commands/RunScriptCommand.cs
+++ source/Calamari/Commands/RunScriptCommand.cs
@@ -2,10 +2,12 @@
 using System.IO;
 using Calamari.Commands.Support;
 using Calamari.Deployment;
+using Calamari.Integration.FileSystem;
 using Calamari.Integration.Packages;
 using Calamari.Integration.Processes;
 using Calamari.Integration.Scripting;
 using Calamari.Integration.ServiceMessages;
+using Calamari.Integration.Substitutions;
 using Octostache;
 
 namespace Calamari.Commands
@@ -18,6 +20,7 @@ namespace Calamari.Commands
         private string sensitiveVariablesFile;
         private string sensitiveVariablesPassword;
         private string packageFile;
+        private bool substituteVariables;
 
         public RunScriptCommand()
         {
@@ -26,6 +29,7 @@ namespace Calamari.Commands
             Options.Add("script=", "Path to the script (PowerShell or ScriptCS) script to execute. If --package is used, it can be a script inside the package.", v => scriptFile = Path.GetFullPath(v));
             Options.Add("sensitiveVariables=", "Password protected JSON file containing sensitive-variables.", v => sensitiveVariablesFile = v);
             Options.Add("sensitiveVariablesPassword=", "Password used to decrypt sensitive-variables.", v => sensitiveVariablesPassword = v);
+            Options.Add("substituteVariables", "Perform variable substitution on the script body before executing it.", v => substituteVariables = true);
         }
 
         public override int Execute(string[] commandLineArguments)
@@ -37,7 +41,7 @@ namespace Calamari.Commands
             variables.LogVariables();
 
             ExtractPackage(variables);
-
+            SubstituteVariablesInScript(variables);
             return InvokeScript(variables);
         }
 
@@ -45,6 +49,8 @@ namespace Calamari.Commands
         {
             if (string.IsNullOrWhiteSpace(packageFile))
                 return;
+
+            Log.Info("Extracting package: " + packageFile);
 
             if (!File.Exists(packageFile))
                 throw new CommandException("Could not find package file: " + packageFile);
@@ -54,17 +60,35 @@ namespace Calamari.Commands
 
             variables.Set(SpecialVariables.OriginalPackageDirectoryPath, Environment.CurrentDirectory);
         }
-        
+
+        private void SubstituteVariablesInScript(CalamariVariableDictionary variables)
+        {
+            if (!substituteVariables) return;
+
+            Log.Info("Substituting variables in: " + scriptFile);
+
+            var validatedScriptFilePath = AssertScriptFileExists();
+            var substituter = new FileSubstituter(CalamariPhysicalFileSystem.GetPhysicalFileSystem());
+            substituter.PerformSubstitution(validatedScriptFilePath, variables);
+        }
+
         private int InvokeScript(CalamariVariableDictionary variables)
         {
-            if (!File.Exists(scriptFile))
-                throw new CommandException("Could not find script file: " + scriptFile);
+            var validatedScriptFilePath = AssertScriptFileExists();
 
             var scriptEngine = new CombinedScriptEngine();
             var runner = new CommandLineRunner(
                 new SplitCommandOutput(new ConsoleCommandOutput(), new ServiceMessageCommandOutput(variables)));
-            var result = scriptEngine.Execute(scriptFile, variables, runner);
+            var result = scriptEngine.Execute(validatedScriptFilePath, variables, runner);
             return result.ExitCode;
+        }
+
+        private string AssertScriptFileExists()
+        {
+            if (!File.Exists(scriptFile))
+                throw new CommandException("Could not find script file: " + scriptFile);
+
+            return scriptFile;
         }
     }
 }
