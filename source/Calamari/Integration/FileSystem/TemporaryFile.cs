bddc0b72a2c718cd9369e6d89309d149611bdfac
Pawel Pabich <pawel@pabich.eu>
2016-06-13 16:15:13 +1000

    That was meant to be removed

--- source/Calamari/Integration/FileSystem/TemporaryFile.cs
+++ source/Calamari/Integration/FileSystem/TemporaryFile.cs
@@ -12,7 +12,6 @@ namespace Calamari.Integration.FileSystem
         public TemporaryFile(string filePath)
         {
             this.filePath = filePath;
-            Console.WriteLine(filePath);
         }
 
         public string DirectoryPath
