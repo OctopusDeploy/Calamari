aa26be4d1a4ce6642c3565e489d734f22230c5d6
Robert Wagner <robert.wagner@octopus.com>
2016-06-29 13:33:56 +1000

    Reverted accidental checkin

--- source/Calamari/Integration/Proxies/ProxyInitializer.cs
+++ source/Calamari/Integration/Proxies/ProxyInitializer.cs
@@ -8,24 +8,11 @@ namespace Calamari.Integration.Proxies
         public static void InitializeDefaultProxy()
         {
             var proxyUsername = Environment.GetEnvironmentVariable("TentacleProxyUsername");
-            Log.Info($"TentacleProxyUsername: {proxyUsername}");
             var proxyPassword = Environment.GetEnvironmentVariable("TentacleProxyPassword");
-            Log.Info($"TentacleProxyPassword: {proxyPassword}");
 
-            Log.Info($"WebRequest.DefaultWebProxy: {WebRequest.DefaultWebProxy}");
-            Log.Info($"Custom Creds: {string.IsNullOrWhiteSpace(proxyUsername)}");
-            Log.Info($"CredentialCache.DefaultCredentials: {CredentialCache.DefaultCredentials}");
-            Log.Info($"NetworkCredential: {new NetworkCredential(proxyUsername, proxyPassword)}");
-
-
-            if (string.IsNullOrWhiteSpace(proxyUsername))
-            {
-                WebRequest.DefaultWebProxy.Credentials = CredentialCache.DefaultCredentials;
-            }
-            else
-            {
-                WebRequest.DefaultWebProxy.Credentials = new NetworkCredential(proxyUsername, proxyPassword);
-            }
+            WebRequest.DefaultWebProxy.Credentials = string.IsNullOrWhiteSpace(proxyUsername) 
+                ? CredentialCache.DefaultCredentials 
+                : new NetworkCredential(proxyUsername, proxyPassword);
         }
     }
 }
