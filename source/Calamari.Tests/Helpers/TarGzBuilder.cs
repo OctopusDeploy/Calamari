f8f882749fc5276b965ea106d2066ca119f94a10
robert <robert.erez@gmail.com>
2016-08-17 16:50:42 +1000

    Update SharpCompress to `0.12.4`

--- source/Calamari.Tests/Helpers/TarGzBuilder.cs
+++ source/Calamari.Tests/Helpers/TarGzBuilder.cs
@@ -1,6 +1,6 @@
 ﻿using System.IO;
-using ICSharpCode.SharpZipLib.GZip;
-using ICSharpCode.SharpZipLib.Tar;
+using SharpCompress.Common;
+using SharpCompress.Writer;
 
 namespace Calamari.Tests.Helpers
 {
@@ -9,49 +9,19 @@ namespace Calamari.Tests.Helpers
         public static string BuildSamplePackage(string name, string version)
         {
             var sourceDirectory = TestEnvironment.GetTestPath("Fixtures", "Deployment", "Packages", name);
-            
+
             var outputTarFilename = Path.Combine(Path.GetTempPath(), name + "." + version + ".tar.gz");
             if (File.Exists(outputTarFilename))
                 File.Delete(outputTarFilename);
 
 
-            using (FileStream fs = new FileStream(outputTarFilename, FileMode.Create, FileAccess.Write, FileShare.None))
-            using (Stream gzipStream = new GZipOutputStream(fs))
-            using (TarArchive tarArchive = TarArchive.CreateOutputTarArchive(gzipStream))
+            using (Stream stream = File.OpenWrite(outputTarFilename))
+            using (var writer = WriterFactory.Open(stream, ArchiveType.Tar, CompressionType.GZip))
             {
-                var rootPath = sourceDirectory
-                    .Replace(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar, "")
-                    .Replace('\\', '/');
-                AddDirectoryFilesToTar2(tarArchive, sourceDirectory, true, rootPath);
+                writer.WriteAll(sourceDirectory, "*", SearchOption.AllDirectories);
             }
+            
             return outputTarFilename;
-        }
-
-        
-        private static void AddDirectoryFilesToTar2(TarArchive tarArchive, string sourceDirectory, bool recurse, string rootDirectory)
-        {
-            // Recursively add sub-folders
-            if (recurse)
-            {
-                string[] directories = Directory.GetDirectories(sourceDirectory);
-                foreach (string directory in directories)
-                    AddDirectoryFilesToTar2(tarArchive, directory, recurse, rootDirectory);
-            }
-
-            // Add files
-            string[] filenames = Directory.GetFiles(sourceDirectory);
-            foreach (string filename in filenames)
-            {
-                TarEntry tarEntry = TarEntry.CreateEntryFromFile(filename);
-
-                if (tarEntry.Name.StartsWith(rootDirectory))
-                {
-                    tarEntry.Name = tarEntry.Name.Substring(rootDirectory.Length+1);
-                }
-
-
-                tarArchive.WriteEntry(tarEntry, true);
-            }
         }
     }
 }
