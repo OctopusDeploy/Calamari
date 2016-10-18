0781372ae34981e5055a02f33d8b2a6b7890ad22
Matt Richardson <matt.richardson@octopus.com>
2016-10-19 09:45:20 +1100

    Ignore test if run on incorrect platform

--- source/Calamari.Tests/Fixtures/Integration/Process/Semaphores/SemaphoreFactoryFixture.cs
+++ source/Calamari.Tests/Fixtures/Integration/Process/Semaphores/SemaphoreFactoryFixture.cs
@@ -11,6 +11,8 @@ namespace Calamari.Tests.Fixtures.Integration.Process.Semaphores
         [Category(TestEnvironment.CompatibleOS.Nix)] //todo: add macOS here
         public void ReturnsFileBasedSemaphoreManagerForLinux()
         {
+            if (!CalamariEnvironment.IsRunningOnNix)
+                Assert.Ignore("This test is designed to run on *nix");
             var result = SemaphoreFactory.Get();
             Assert.That(result, Is.InstanceOf<FileBasedSempahoreManager>());
         }
@@ -19,6 +21,8 @@ namespace Calamari.Tests.Fixtures.Integration.Process.Semaphores
         [Category(TestEnvironment.CompatibleOS.Windows)]
         public void ReturnsSystemSemaphoreManagerForWindows()
         {
+            if (!CalamariEnvironment.IsRunningOnWindows)
+                Assert.Ignore("This test is designed to run on windows");
             var result = SemaphoreFactory.Get();
             Assert.That(result, Is.InstanceOf<SystemSemaphoreManager>());
         }
