f7cf94ddf2b904a467bfe7a0459cddf4ed26464e
Matt Richardson <matt.richardson@octopus.com>
2016-10-21 17:00:10 +1100

    Fix mac detection logic
    Was detecting as *nix under mono on mac

--- source/Calamari/CalamariEnvironment.cs
+++ source/Calamari/CalamariEnvironment.cs
@@ -22,7 +22,7 @@ namespace Calamari
             get
             {
 #if NET40
-                return Environment.OSVersion.Platform == PlatformID.Unix;
+                return (Environment.OSVersion.Platform == PlatformID.Unix) && !IsRunningOnMac;
 #else
                 return RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
 #endif
@@ -44,16 +44,55 @@ namespace Calamari
             }
         }
 
+#if NET40
+        //from https://github.com/jpobst/Pinta/blob/master/Pinta.Core/Managers/SystemManager.cs#L162
+        //(MIT license)
+        [DllImport("libc")]//From Managed.Windows.Forms/XplatUI
+        static extern int uname(IntPtr buf);
+
         public static bool IsRunningOnMac
         {
             get
             {
-#if NET40
-                return Environment.OSVersion.Platform == PlatformID.MacOSX;
-#else
-                return RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
-#endif
+                if (Environment.OSVersion.Platform == PlatformID.MacOSX)
+                    return true;
+                if (Environment.OSVersion.Platform != PlatformID.Unix)
+                    return false;
+
+                var buf = IntPtr.Zero;
+                try
+                {
+                    buf = Marshal.AllocHGlobal(8192);
+                    // Get sysname from uname ()
+                    if (uname(buf) == 0)
+                    {
+                        var os = Marshal.PtrToStringAnsi(buf);
+                        if (os == "Darwin")
+                            return true;
+                    }
+                }
+                catch
+                {
+                    // ignored
+                }
+                finally
+                {
+                    if (buf != IntPtr.Zero)
+                        Marshal.FreeHGlobal(buf);
+                }
+                return false;
             }
         }
+#else
+
+        public static bool IsRunningOnMac
+        {
+            get
+            {
+
+                return RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+            }
+        }
+#endif
     }
 }
