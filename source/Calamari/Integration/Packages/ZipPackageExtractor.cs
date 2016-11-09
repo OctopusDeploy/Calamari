e7613b9390fa75646a96a4f0edcdf0c1830492ae
Matt Richardson <matt.richardson@octopus.com>
2016-11-09 17:10:07 +1100

    Change to use ZipArchive rather than ZipReader
    To better handle extraction of embedded zip files.
    Fixes OctopusDeploy/Issues#2888.

--- source/Calamari/Integration/Packages/ZipPackageExtractor.cs
+++ source/Calamari/Integration/Packages/ZipPackageExtractor.cs
@@ -1,5 +1,8 @@
 using System;
 using System.IO;
+using System.Linq;
+using SharpCompress.Archive;
+using SharpCompress.Archive.Zip;
 using SharpCompress.Common;
 using SharpCompress.Reader;
 using SharpCompress.Reader.Zip;
@@ -14,12 +17,12 @@ namespace Calamari.Integration.Packages
         {
             var filesExtracted = 0;
             using (var inStream = new FileStream(packageFile, FileMode.Open, FileAccess.Read))
-            using (var reader = ZipReader.Open(inStream))
-            {               
-                while (reader.MoveToNextEntry())
+            using (var archive = ZipArchive.Open(inStream))
+            {
+                foreach (var entry in archive.Entries.Where(entry => !entry.IsDirectory))
                 {
-                    ProcessEvent(ref filesExtracted, reader.Entry, suppressNestedScriptWarning);
-                    reader.WriteEntryToDirectory(directory,ExtractOptions.ExtractFullPath | ExtractOptions.Overwrite | ExtractOptions.PreserveFileTime);
+                    ProcessEvent(ref filesExtracted, entry, suppressNestedScriptWarning);
+                    entry.WriteToDirectory(directory, ExtractOptions.ExtractFullPath | ExtractOptions.Overwrite | ExtractOptions.PreserveFileTime);
                 }
             }
             return filesExtracted;
