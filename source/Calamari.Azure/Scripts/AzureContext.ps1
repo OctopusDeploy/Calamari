b8ad9c387e3957e105d5420cc37a4329ba7fa5e0
robert <robert.erez@gmail.com>
2016-11-23 15:17:29 +1000

    Change Logging

--- source/Calamari.Azure/Scripts/AzureContext.ps1
+++ source/Calamari.Azure/Scripts/AzureContext.ps1
@@ -32,30 +32,30 @@ If ([System.Convert]::ToBoolean($OctopusUseServicePrincipal)) {
 	# Authenticate via Service Principal
 	$securePassword = ConvertTo-SecureString $OctopusAzureADPassword -AsPlainText -Force
 	$creds = New-Object System.Management.Automation.PSCredential ($OctopusAzureADClientId, $securePassword)
-	$azureEnvironment = Get-AzureEnvironment | Where-Object {$_.Name -eq $OctopusAzureEnvrionment}
+	$AzureEnvironment = Get-AzureEnvironment | Where-Object {$_.Name -eq $OctopusAzureEnvrionment}
 
-	if (!$azureEnvironment)
+	if (!$AzureEnvironment)
 	{
 		Write-Error "No Azure environment could be matched given name $OctopusAzureEnvrionment"
 		exit -2
 	}
-	Write-Verbose "Looking for $OctopusAzureEnvrionment"
-	Write-Verbose "Found Environment $azureEnvironment"
+	Write-Verbose "Found Environment $AzureEnvironment.Name from $OctopusAzureEnvrionment"
 	Write-Verbose "Authenticating with Service Principal"
-	Login-AzureRmAccount -Credential $creds -TenantId $OctopusAzureADTenantId -SubscriptionId $OctopusAzureSubscriptionId -ServicePrincipal -Environment $azureEnvironment
+	Login-AzureRmAccount -Credential $creds -TenantId $OctopusAzureADTenantId -SubscriptionId $OctopusAzureSubscriptionId -Environment $AzureEnvironment -ServicePrincipal
 } Else {
 	# Authenticate via Management Certificate
 	Write-Verbose "Loading the management certificate"
 	Add-Type -AssemblyName "System"
 	$certificate = new-object System.Security.Cryptography.X509Certificates.X509Certificate2 -ArgumentList @($OctopusAzureCertificateFileName, $OctopusAzureCertificatePassword, ([System.Security.Cryptography.X509Certificates.X509KeyStorageFlags] "PersistKeySet", "Exportable"))
-	$azureEnvironment = Get-AzureEnvironment | Where-Object {$_.Name -eq $OctopusAzureEnvrionment}
+	$AzureEnvironment = Get-AzureEnvironment | Where-Object {$_.Name -eq $OctopusAzureEnvrionment}
 
-	if (!$azureEnvironment)
+	if (!$AzureEnvironment)
 	{
 		Write-Error "No Azure environment could be matched given name $OctopusAzureEnvrionment"
 		exit -2
 	}
-	$azureProfile = New-AzureProfile -SubscriptionId $OctopusAzureSubscriptionId -StorageAccount $OctopusAzureStorageAccountName -Certificate $certificate -Environment $azureEnvironment
+	Write-Verbose "Found Environment $AzureEnvironment.Name from $OctopusAzureEnvrionment"
+	$azureProfile = New-AzureProfile -SubscriptionId $OctopusAzureSubscriptionId -StorageAccount $OctopusAzureStorageAccountName -Certificate $certificate -Environment $AzureEnvironment
 	$azureProfile.Save(".\AzureProfile.json")
 	Select-AzureProfile -Profile $azureProfile | Out-Null
 } 
