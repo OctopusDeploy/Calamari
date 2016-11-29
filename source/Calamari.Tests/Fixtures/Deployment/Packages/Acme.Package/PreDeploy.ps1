530d4648bd59b984de41789ddcf902ac7cda43cd
Mark Rydstrom <mark.rydstrom@octopus.com>
2016-11-29 13:34:55 +1000

    Fix up variable set syntax in test

--- source/Calamari.Tests/Fixtures/Deployment/Packages/Acme.Package/PreDeploy.ps1
+++ source/Calamari.Tests/Fixtures/Deployment/Packages/Acme.Package/PreDeploy.ps1
@@ -2,4 +2,4 @@
 # PreDeploy.ps1
 #
 write-host "hello from pre-deploy, skipping remaining conventions"
-Set-OctopusVariable('Octopus.Action.SkipRemainingConventions', 'True')
+Set-OctopusVariable 'Octopus.Action.SkipRemainingConventions' 'True'
