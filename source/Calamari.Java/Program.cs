4d11dd97fed79bd81f1afe5748a714d3988b58b1
Michael Richardson <michael.james.richardson@gmail.com>
2017-07-03 14:38:02 +1000

    Created Calamari.Java project!

---
+++ source/Calamari.Java/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calamari.Java
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
