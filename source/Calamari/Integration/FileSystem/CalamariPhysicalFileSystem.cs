OctopusDeploy/Calamari

commit 77d153639776a2df35e6344b62e3de6c3b8b1e09

Don't hard-code recursive in Directory.Delete wrapper

source/Calamari/Integration/FileSystem/CalamariPhysicalFileSystem.cs
@@ -117,7 +117,7 @@ namespace Calamari.Integration.FileSystem
 
         public void DeleteDirectory(string path)
         {
-            Directory.Delete(path);
+            Directory.Delete(path, true);
         }
 
         public void DeleteDirectory(string path, FailureOptions options)
@@ -413,7 +413,7 @@ namespace Calamari.Integration.FileSystem
 
                 if ((info.Attributes & FileAttributes.ReparsePoint) == FileAttributes.ReparsePoint)
                 {
-                    Directory.Delete(directory);
+                    Directory.Delete(directory, true);
                 }
                 else
                 {

source/Calamari/Integration/FileSystem/FileOperations.cs
@@ -21,7 +21,7 @@ namespace Calamari.Integration.FileSystem
     public interface IDirectory
     {
         void CreateDirectory(string path);
-        void Delete(string path);
+        void Delete(string path, bool recursive);
         bool Exists(string path);
         string[] GetFileSystemEntries(string path);
         IEnumerable<string> EnumerateDirectories(string path);
@@ -47,7 +47,7 @@ namespace Calamari.Integration.FileSystem
     public class StandardDirectory : IDirectory
     {
         public void CreateDirectory(string path) => Directory.CreateDirectory(path);
-        public void Delete(string path) => Directory.Delete(path, true);
+        public void Delete(string path, bool recursive) => Directory.Delete(path, recursive);
         public bool Exists(string path) => Directory.Exists(path);
         public string[] GetFileSystemEntries(string path) => Directory.GetFileSystemEntries(path);
         public IEnumerable<string> EnumerateDirectories(string path) => Directory.EnumerateDirectories(path);
@@ -74,7 +74,7 @@ namespace Calamari.Integration.FileSystem
     public class LongPathsDirectory : IDirectory
     {
         public void CreateDirectory(string path) => Alphaleonis.Win32.Filesystem.Directory.CreateDirectory(path);
-        public void Delete(string path) => Alphaleonis.Win32.Filesystem.Directory.Delete(path, true);
+        public void Delete(string path, bool recursive) => Alphaleonis.Win32.Filesystem.Directory.Delete(path, recursive);
         public bool Exists(string path) => Alphaleonis.Win32.Filesystem.Directory.Exists(path);
         public string[] GetFileSystemEntries(string path) => Alphaleonis.Win32.Filesystem.Directory.GetFileSystemEntries(path);
         public IEnumerable<string> EnumerateDirectories(string path) => Alphaleonis.Win32.Filesystem.Directory.EnumerateDirectories(path);

commit 2b2dbc90d4cfeaf84e942390745853365d0eebb9

Do not recursively delete symlinked directories
When the retention policy is applied, just delete the symlink
instead of recursing through the directory so that important
(potentially system) files are not deleted.

Bug was introduced in 77d153639776a2df35e6344b62e3de6c3b8b1e09

source/Calamari/Integration/FileSystem/CalamariPhysicalFileSystem.cs
@@ -436,7 +436,7 @@ namespace Calamari.Integration.FileSystem
 
                 if ((info.Attributes & FileAttributes.ReparsePoint) == FileAttributes.ReparsePoint)
                 {
-                    Directory.Delete(directory, true);
+                    Directory.Delete(directory, false);
                 }
                 else
                 {