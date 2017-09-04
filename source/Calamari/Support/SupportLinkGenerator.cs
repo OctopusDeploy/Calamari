4a00af739dd62b4e0c73af8139e857be04b9c67f
Matthew Casperson <matthewcasperson@gmail.com>
2017-09-04 14:18:48 +1000

    Build errors were related to the agent, not the depednencyt, so added preconditions back in

--- source/Calamari/Support/SupportLinkGenerator.cs
+++ source/Calamari/Support/SupportLinkGenerator.cs
@@ -1,21 +1,29 @@
-﻿namespace Calamari.Support
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EnsureThat;
+
+namespace Calamari.Support
 {
     public class SupportLinkGenerator : ISupportLinkGenerator
     {
-        private const string JavaErrorCodePrefix = "JAVA";
-        /// <summary>
-        /// The shortended link to the Java documentation
-        /// </summary>
-        private const string JavaSupportLink = "http://g.octopushq.com/JavaAppDeploy";
+        private readonly Dictionary<string, string> SupportLinks = new Dictionary<string, string>()
+        {
+            {
+                "JAVA", "http://g.octopushq.com/JavaAppDeploy"
+            }
+        };
+       
         
         public string GenerateSupportMessage(string baseMessage, string errorCode)
         {
-            if (errorCode.StartsWith(JavaErrorCodePrefix))
-            {
-                return $"{errorCode}: {baseMessage} {JavaSupportLink + "#" + errorCode.ToLower()}";
-            }
-
-            return $"{errorCode}: {baseMessage}";
+            EnsureArg.IsNotNullOrWhiteSpace(baseMessage);
+            EnsureArg.IsNotNullOrWhiteSpace(errorCode);
+            
+            return SupportLinks
+                .Where(entry => errorCode.StartsWith(entry.Key))
+                .Select(entry => $"{errorCode}: {baseMessage} {entry.Value + "#" + errorCode.ToLower()}")
+                .FirstOrDefault() ?? $"{errorCode}: {baseMessage}";
         }
     }
 }
