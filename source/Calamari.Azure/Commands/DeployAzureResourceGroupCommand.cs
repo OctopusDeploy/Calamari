982d5a835798ab20ef45596cb148490cb3fd3b95
Mark Siedle <mark.siedle@octopus.com>
2018-02-14 15:12:26 +1000

    Adding condition to check for templateParameters, as these don't need to be provided and our options parser can't deal with empty params apparently

--- source/Calamari.Azure/Commands/DeployAzureResourceGroupCommand.cs
+++ source/Calamari.Azure/Commands/DeployAzureResourceGroupCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Calamari.Azure.Deployment.Conventions;
 using Calamari.Azure.Deployment.Integration.ResourceGroups;
 using Calamari.Commands.Support;
@@ -14,22 +15,19 @@ namespace Calamari.Azure.Commands
     [Command("deploy-azure-resource-group", Description = "Creates a new Azure Resource Group deployment")]
     public class DeployAzureResourceGroupCommand : Command
     {
-        string packageFile;
-        string variablesFile;
-        string sensitiveVariablesFile;
-        string sensitiveVariablesPassword;
-        string templateFile;
-        string templateParameterFile;
+        private string variablesFile;
+        private string packageFile;
+        private string sensitiveVariablesFile;
+        private string sensitiveVariablesPassword;
+        private string templateFile;
+        private string templateParameterFile;
 
         public DeployAzureResourceGroupCommand()
         {
-            Options.Add("variables=", "Path to a JSON file containing variables.",
-                v => variablesFile = Path.GetFullPath(v));
-            Options.Add("sensitiveVariables=", "Password protected JSON file containing sensitive-variables.",
-                v => sensitiveVariablesFile = v);
-            Options.Add("sensitiveVariablesPassword=", "Password used to decrypt sensitive-variables.",
-                v => sensitiveVariablesPassword = v);
-            Options.Add("package=", "Path to the NuGet package to install.", v => packageFile = Path.GetFullPath(v));
+            Options.Add("variables=", "Path to a JSON file containing variables.", v => variablesFile = Path.GetFullPath(v));
+            Options.Add("package=", "Path to the deployment package to install.", v => packageFile = Path.GetFullPath(v));
+            Options.Add("sensitiveVariables=", "Password protected JSON file containing sensitive-variables.", v => sensitiveVariablesFile = v);
+            Options.Add("sensitiveVariablesPassword=", "Password used to decrypt sensitive-variables.", v => sensitiveVariablesPassword = v);
             Options.Add("template=", "Path to the JSON template file.", v => templateFile = v);
             Options.Add("templateParameters=", "Path to the JSON template parameters file.", v => templateParameterFile = v);
         }
@@ -41,13 +39,10 @@ namespace Calamari.Azure.Commands
             if (variablesFile != null && !File.Exists(variablesFile))
                 throw new CommandException("Could not find variables file: " + variablesFile);
 
-            var variables = new CalamariVariableDictionary(variablesFile, sensitiveVariablesFile,
-                sensitiveVariablesPassword);
+            var variables = new CalamariVariableDictionary(variablesFile, sensitiveVariablesFile, sensitiveVariablesPassword);
 
             var fileSystem = new WindowsPhysicalFileSystem();
-
             var filesInPackage = !string.IsNullOrWhiteSpace(packageFile);
-
             var conventions = new List<IConvention>
             {
                 new ContributeEnvironmentVariablesConvention(),
--- source/Calamari.Azure/Deployment/Conventions/DeployAzureResourceGroupConvention.cs
+++ source/Calamari.Azure/Deployment/Conventions/DeployAzureResourceGroupConvention.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
