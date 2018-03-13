7fe2c550ff0108ab9cb39310be9c456aed14ea64
Mark Siedle <mark.siedle@octopus.com>
2018-03-13 12:02:08 +1000

    Progress on SF and automatic installation of client certificate to server's cert store

--- source/Calamari.Azure/Integration/AzureServiceFabricPowerShellContext.cs
+++ source/Calamari.Azure/Integration/AzureServiceFabricPowerShellContext.cs
@@ -41,7 +41,10 @@ namespace Calamari.Azure.Integration
             if (securityMode == AzureServiceFabricSecurityMode.SecureClientCertificate.ToString())
             {
                 var certificateVariable = GetMandatoryVariable(variables, SpecialVariables.Action.ServiceFabric.ClientCertVariable);
+                Log.Info($"markse: certificateVariable={certificateVariable}");
                 clientCertThumbprint = variables.Get($"{certificateVariable}.{SpecialVariables.Certificate.Properties.Thumbprint}");
+                Log.Info($"{certificateVariable}.{SpecialVariables.Certificate.Properties.Thumbprint}");
+                Log.Info($"markse: clientCertThumbprint={clientCertThumbprint}");
             }
 
             // Set output variables for our script to access.
