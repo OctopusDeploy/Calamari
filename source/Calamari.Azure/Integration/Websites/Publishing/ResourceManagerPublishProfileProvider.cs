b64eb09152fced76a1f750fbdeaa07922dfc7b35
Matthew Casperson <matthewcasperson@gmail.com>
2018-03-21 09:49:20 +1000

    Added some additional logging of task cancalled exceptions

--- source/Calamari.Azure/Integration/Websites/Publishing/ResourceManagerPublishProfileProvider.cs
+++ source/Calamari.Azure/Integration/Websites/Publishing/ResourceManagerPublishProfileProvider.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Calamari.Azure.Integration.Security;
+using Calamari.Azure.Util;
 using Calamari.Commands.Support;
 using Microsoft.Azure;
 using Microsoft.Azure.Management.Resources;
@@ -11,6 +12,7 @@ using Microsoft.Azure.Management.Resources.Models;
 using Microsoft.Azure.Management.WebSites;
 using Microsoft.Rest;
 using Newtonsoft.Json.Linq;
+using Octopus.CoreUtilities.Extensions;
 
 namespace Calamari.Azure.Integration.Websites.Publishing
 {
@@ -18,57 +20,82 @@ namespace Calamari.Azure.Integration.Websites.Publishing
     {
         public static SitePublishProfile GetPublishProperties(string subscriptionId, string resourceGroupName, string siteName, string tenantId, string applicationId, string password,string resourceManagementEndpoint, string activeDirectoryEndPoint)
         {
-            var token = ServicePrincipal.GetAuthorizationToken(tenantId, applicationId, password, resourceManagementEndpoint, activeDirectoryEndPoint);
-
-            using (var resourcesClient = new ResourceManagementClient(new TokenCloudCredentials(subscriptionId, token), new Uri(resourceManagementEndpoint)))
-            using (var webSiteClient = new WebSiteManagementClient(new Uri(resourceManagementEndpoint), new TokenCredentials(token)) { SubscriptionId = subscriptionId})
+            try
             {
-                // We may need to search all ResourceGroups, if one isn't specified.  New Step template will always provide the Resource Group, it is currently treated as optional here
-                // for backward compatibility.
-                var resourceGroups = resourcesClient.ResourceGroups.List(new ResourceGroupListParameters()).ResourceGroups.Where(rg => string.IsNullOrWhiteSpace(resourceGroupName) || string.Equals(rg.Name, resourceGroupName, StringComparison.InvariantCultureIgnoreCase)).Select(rg => rg.Name).ToList();
+                var token = ServicePrincipal.GetAuthorizationToken(tenantId, applicationId, password,
+                    resourceManagementEndpoint, activeDirectoryEndPoint);
 
-                foreach (var resourceGroup in resourceGroups)
+                using (var resourcesClient =
+                    new ResourceManagementClient(new TokenCloudCredentials(subscriptionId, token),
+                        new Uri(resourceManagementEndpoint)))
+                using (var webSiteClient =
+                    new WebSiteManagementClient(new Uri(resourceManagementEndpoint), new TokenCredentials(token))
+                        {SubscriptionId = subscriptionId})
                 {
-                    var sites = webSiteClient.Sites.GetSites(resourceGroup, null, null, true).Value;
-                    var matchingSite = sites.FirstOrDefault(x => x.SiteName.Equals(siteName, StringComparison.OrdinalIgnoreCase));
+                    // We may need to search all ResourceGroups, if one isn't specified.  New Step template will always provide the Resource Group, it is currently treated as optional here
+                    // for backward compatibility.
+                    var resourceGroups = resourcesClient.ResourceGroups.List(new ResourceGroupListParameters())
+                        .ResourceGroups
+                        .Where(rg => string.IsNullOrWhiteSpace(resourceGroupName) || string.Equals(rg.Name,
+                                         resourceGroupName, StringComparison.InvariantCultureIgnoreCase))
+                        .Select(rg => rg.Name).ToList();
 
-                    if (matchingSite == null)
-                        continue;
+                    foreach (var resourceGroup in resourceGroups)
+                    {
+                        var sites = webSiteClient.Sites.GetSites(resourceGroup, null, null, true).Value;
+                        var matchingSite = sites.FirstOrDefault(x =>
+                            x.SiteName.Equals(siteName, StringComparison.OrdinalIgnoreCase));
 
-                    // Once we know the Resource Group, we have to POST a request to the URI below to retrieve the publishing credentials
-                    var publishSettingsUri = new Uri(resourcesClient.BaseUri,
-                        $"/subscriptions/{subscriptionId}/resourceGroups/{resourceGroup}/providers/Microsoft.Web/sites/{matchingSite.Name.Replace("/", "/slots/")}/config/publishingCredentials/list?api-version=2015-08-01");
-                    Log.Verbose($"Retrieving publishing profile from {publishSettingsUri}");
+                        if (matchingSite == null)
+                            continue;
 
-                    SitePublishProfile publishProperties = null;
-                    var request = new HttpRequestMessage {Method = HttpMethod.Post, RequestUri = publishSettingsUri};
-                    // Add the authentication headers
-                    var requestTask = resourcesClient.Credentials.ProcessHttpRequestAsync(request, new CancellationToken())
-                        .ContinueWith(authResult => resourcesClient.HttpClient.SendAsync(request), TaskContinuationOptions.NotOnFaulted)
-                        .ContinueWith(publishSettingsResponse =>
-                        {
-                            var result = publishSettingsResponse.Result.Result; 
+                        // Once we know the Resource Group, we have to POST a request to the URI below to retrieve the publishing credentials
+                        var publishSettingsUri = new Uri(resourcesClient.BaseUri,
+                            $"/subscriptions/{subscriptionId}/resourceGroups/{resourceGroup}/providers/Microsoft.Web/sites/{matchingSite.Name.Replace("/", "/slots/")}/config/publishingCredentials/list?api-version=2015-08-01");
+                        Log.Verbose($"Retrieving publishing profile from {publishSettingsUri}");
 
-                            if (!result.IsSuccessStatusCode)
+                        SitePublishProfile publishProperties = null;
+                        var request =
+                            new HttpRequestMessage {Method = HttpMethod.Post, RequestUri = publishSettingsUri};
+                        // Add the authentication headers
+                        var requestTask = resourcesClient.Credentials
+                            .ProcessHttpRequestAsync(request, new CancellationToken())
+                            .ContinueWith(authResult => resourcesClient.HttpClient.SendAsync(request),
+                                TaskContinuationOptions.NotOnFaulted)
+                            .ContinueWith(publishSettingsResponse =>
                             {
-                                Log.Error($"Retrieving publishing credentials failed. Publish-settings URI: {publishSettingsUri}");
-                                throw new Exception($"Retrieving publishing credentials failed with HTTP status {(int)result.StatusCode} - {result.ReasonPhrase}");
-                            }
+                                var result = publishSettingsResponse.Result.Result;
 
-                            dynamic response = JObject.Parse(result.Content.AsString());
-                            string publishUserName = response.properties.publishingUserName;
-                            string publishPassword = response.properties.publishingPassword;
-                            string scmUri = response.properties.scmUri;
-                            Log.Verbose($"Retrieved publishing profile. URI: {scmUri}  UserName: {publishUserName}");
-                            publishProperties = new SitePublishProfile(publishUserName, publishPassword, new Uri(scmUri));
-                        }, TaskContinuationOptions.NotOnFaulted);
+                                if (!result.IsSuccessStatusCode)
+                                {
+                                    Log.Error(
+                                        $"Retrieving publishing credentials failed. Publish-settings URI: {publishSettingsUri}");
+                                    throw new Exception(
+                                        $"Retrieving publishing credentials failed with HTTP status {(int) result.StatusCode} - {result.ReasonPhrase}");
+                                }
 
-                    requestTask.Wait();
-                    return publishProperties;
+                                dynamic response = JObject.Parse(result.Content.AsString());
+                                string publishUserName = response.properties.publishingUserName;
+                                string publishPassword = response.properties.publishingPassword;
+                                string scmUri = response.properties.scmUri;
+                                Log.Verbose(
+                                    $"Retrieved publishing profile. URI: {scmUri}  UserName: {publishUserName}");
+                                publishProperties =
+                                    new SitePublishProfile(publishUserName, publishPassword, new Uri(scmUri));
+                            }, TaskContinuationOptions.NotOnFaulted);
+
+                        requestTask.Wait();
+                        return publishProperties;
+                    }
+
+                    throw new CommandException(
+                        $"Could not find Azure WebSite '{siteName}' in subscription '{subscriptionId}'");
                 }
-
-                throw new CommandException(
-                    $"Could not find Azure WebSite '{siteName}' in subscription '{subscriptionId}'");
+            }
+            catch (TaskCanceledException ex)
+            {
+                new TaskCanceledExceptionHandler().HandleException(ex);
+                throw ex;
             }
         }
     }
