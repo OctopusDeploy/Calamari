86b7b205eb70e64902ec653421f44a54581e1e81
Shaun Marx <shaun.m4rx@gmail.com>
2018-04-11 14:26:18 +1000

    Skip content hash checking for s3 step when md5 hash algorithm is not available

--- source/Calamari/Util/HashCalculator.cs
+++ source/Calamari/Util/HashCalculator.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using System.Security.Cryptography;
+using Octopus.CoreUtilities;
 
 namespace Calamari.Util
 {
@@ -36,5 +38,25 @@ namespace Calamari.Util
         {
             return SHA1.Create();
         }
+
+        /// <summary>
+        /// When FIPS mode is enabled certain algorithms such as MD5 may not be available.  
+        /// </summary>
+        /// <param name="factory">A callback function used to create the associated algorithm i.e. MD5.Create</param>
+        /// <returns></returns>
+        public static bool IsAvailableHashingAlgorithm(Func<HashAlgorithm> factory)
+        {
+            Guard.NotNull(factory, "Factory method is required");
+
+            try
+            {
+                var result = factory();
+                return result != null; 
+            }
+            catch (TargetInvocationException)
+            {
+                return false;
+            }
+        } 
     }
 }
