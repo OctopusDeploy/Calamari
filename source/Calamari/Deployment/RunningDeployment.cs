d109cedcbd8ee682f56749f30603eedd1a1acbd3
Matthew Casperson <matthewcasperson@gmail.com>
2018-05-01 08:05:18 +1000

    Removed change to getter

--- source/Calamari/Deployment/RunningDeployment.cs
+++ source/Calamari/Deployment/RunningDeployment.cs
@@ -24,7 +24,7 @@ namespace Calamari.Deployment
         /// </summary>
         public string StagingDirectory
         {
-            get { return Variables.Get(SpecialVariables.OriginalPackageDirectoryPath) ?? Environment.CurrentDirectory; }
+            get { return Variables.Get(SpecialVariables.OriginalPackageDirectoryPath); }
             set { Variables.Set(SpecialVariables.OriginalPackageDirectoryPath, value); }
         }
 
