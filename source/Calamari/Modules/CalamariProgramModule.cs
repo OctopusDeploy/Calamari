3b2cdeda8957e81c072e2939703fdc973ce34395
Matthew Casperson <matthewcasperson@gmail.com>
2018-05-03 09:13:40 +1000

    Cleaned up some code

--- source/Calamari/Modules/CalamariProgramModule.cs
+++ source/Calamari/Modules/CalamariProgramModule.cs
@@ -10,6 +10,7 @@ namespace Calamari.Modules
 {
     class CalamariProgramModule : Module
     {
+        private static readonly ICommandLocator CommandLocator = new CommandLocator();
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterType<Program>()
@@ -19,22 +20,10 @@ namespace Calamari.Modules
                 .WithParameter(
                     new ResolvedParameter(
                         (pi, ctx) => pi.ParameterType == typeof(ICommand),
-                        (pi, ctx) => GetOptionalNamedCommand(ctx, CalamariCommandsModule.RunCommand)))
+                        (pi, ctx) => CommandLocator.GetOptionalNamedCommand(ctx, CalamariCommandsModule.RunCommand)))
                 .SingleInstance();
             builder.RegisterType<CombinedScriptEngine>().AsSelf();
             builder.RegisterType<HelpCommand>().AsSelf();
-        }
-
-        private ICommand GetOptionalNamedCommand(IComponentContext ctx, string named)
-        {
-            try
-            {
-                return ctx.ResolveNamed<ICommand>(named);
-            }
-            catch
-            {
-                return null;
-            }
         }
     }
 }
