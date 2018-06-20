3e14f525cd53925f4c474e26002d97229349805b
Rob <robert.erez@gmail.com>
2018-06-20 18:34:54 +1000

    Fixes https://github.com/OctopusDeploy/Issues/issues/4656 Prevent double variable substitution

--- source/Calamari/Commands/RunScriptCommand.cs
+++ source/Calamari/Commands/RunScriptCommand.cs
@@ -104,7 +104,6 @@ namespace Calamari.Commands
             using (new TemporaryFile(scriptFilePath))
             {
                 File.WriteAllBytes(scriptFilePath, scriptBody.EncodeInUtf8Bom());
-                SubstituteVariablesInScript(scriptFilePath, this.variables);
                 return InvokeScript(scriptFilePath, variables);
             }
         }
