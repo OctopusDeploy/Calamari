5d5ef759460d76141aae1f7e8823a7f54fc25360
Henrik Andersson <henrik.andersson@octopus.com>
2018-11-08 23:03:06 +1000

    Make sure user has sudo access without password to required commands

--- source/Calamari/Scripts/Octopus.Features.Nginx_BeforePreDeploy.sh
+++ source/Calamari/Scripts/Octopus.Features.Nginx_BeforePreDeploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+function check_user_has_sudo_access_without_password_prompt {
+    sudo -n cp --help > /dev/null 2>&1 && sudo -n rm --help > /dev/null 2>&1 && sudo -n nginx -h > /dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        echo >&2 "User requires 'sudo' access without password to the 'cp', 'rm' and 'nginx' commands."
+        echo >&2 "See https://g.octopushq.com/NginxPermissions for more information."
+        exit 1
+    fi
+}
+
 function check_app_exists {
 	command -v $1 > /dev/null 2>&1
 	if [[ $? -ne 0 ]]; then
@@ -7,4 +16,5 @@ function check_app_exists {
 	fi
 }
 
+check_user_has_sudo_access_without_password_prompt
 check_app_exists nginx
