0ce14d8a8fa5a77fdda2560a4953f5675becf72e
Henrik Andersson <henrik.andersson@octopus.com>
2018-11-12 12:20:02 +1000

    Refactor nginx pre-deploy script check for required commands

--- source/Calamari/Scripts/Octopus.Features.Nginx_BeforePreDeploy.sh
+++ source/Calamari/Scripts/Octopus.Features.Nginx_BeforePreDeploy.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-function check_user_has_sudo_access_without_password_prompt {
-    sudo -n cp --help > /dev/null 2>&1 && sudo -n rm --help > /dev/null 2>&1 && sudo -n nginx -h > /dev/null 2>&1
+# Enter any commands that are required by the NGINX feature in a way the program doesn't do anything, like displaying the commands help
+requiredCommandsToCheck="cp --help
+mv --help
+rm --help
+nginx -h"
+
+function check_user_has_sudo_access_without_password_to_command {
+    sudo -n $1 $2 > /dev/null 2>&1
     if [[ $? -ne 0 ]]; then
-        echo >&2 "User requires 'sudo' access without password to the 'cp', 'mv', 'rm' and 'nginx' commands."
-        echo >&2 "See https://g.octopushq.com/NginxUserPermissions for more information."
+        echo >&2 "User does not have 'sudo' access without password to command '$1'"
+        failedSudoCheck=1
+    fi
+}
+
+function check_user_has_sudo_access_without_password_to_required_commands {
+    IFS=$'\n' read -rd '' -a cmdArr <<< "$requiredCommandsToCheck"
+    
+    failedSudoCheck=0
+    for cmd in "${cmdArr[@]}"
+    do
+        check_user_has_sudo_access_to_command ${cmd}
+    done
+    
+    if [[ $failedSudoCheck -ne 0 ]]; then
+        echo >&2 "User does not have the required 'sudo' access without password."
+        echo >&2 "See https://g.octopus.hq.com/NginxInstall from more information"
         exit 1
     fi
 }
@@ -13,10 +34,9 @@ function check_app_exists {
 	command -v $1 > /dev/null 2>&1
 	if [[ $? -ne 0 ]]; then
 		echo >&2 "The executable $1 does not exist, or is not on the path"
-		echo >&2 "See https://g.octopus.hq.com/NginxInstall from more information"
 		exit 1
 	fi
 }
 
-check_user_has_sudo_access_without_password_prompt
 check_app_exists nginx
+check_user_has_sudo_access_without_password_to_required_commands
