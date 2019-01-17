dc8a0704e4c63ec70725722d064d0d00ab59371b
Shane Gill <tothegills@users.noreply.github.com>
2019-01-17 10:21:38 +1000

    Only run PowerShell version test on OSes that support PowerShell 2 (#354)
    * Only run PowerShell version test on OSes that support PowerShell 2
    
    * Add Windows 10 to test platform
    
    * Comment explaining omission of Windows 2016

--- source/Calamari.Tests/Fixtures/PowerShell/PowerShellFixture.cs
+++ source/Calamari.Tests/Fixtures/PowerShell/PowerShellFixture.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using Assent;
-using Assent.Namers;
 using Calamari.Deployment;
 using Calamari.Integration.FileSystem;
-using Calamari.Integration.Processes;
 using Calamari.Integration.Scripting;
 using Calamari.Tests.Helpers;
 using Calamari.Util.Environments;
@@ -28,9 +24,11 @@ namespace Calamari.Tests.Fixtures.PowerShell
         }
 
         [Test]
+        [Platform]
         [Category(TestCategory.CompatibleOS.Windows)]
-        [TestCase("2", "PSVersion                      2.0")]
-        [TestCase("2.0", "PSVersion                      2.0")]
+        // Windows 2016 (has PowerShell 2) will also match Windows 2019 (no PowerShell 2) so have omitted it.
+        [TestCase("2", "PSVersion                      2.0", IncludePlatform = "Win2008Server,Win2008ServerR2,Win2012Server,Win2012ServerR2,Windows10")]
+        [TestCase("2.0", "PSVersion                      2.0", IncludePlatform = "Win2008Server,Win2008ServerR2,Win2012Server,Win2012ServerR2,Windows10")]
         public void ShouldCustomizePowerShellVersionIfRequested(string customPowerShellVersion, string expectedLogMessage)
         {
             var variablesFile = Path.GetTempFileName();
