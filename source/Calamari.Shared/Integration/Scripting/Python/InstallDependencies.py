db806100c783de390dbf8b4621e40c92360c23ff
Shane Gill <tothegills@gmail.com>
2019-01-24 09:16:22 +1000

    Handle the case where pip can not install a package

--- source/Calamari.Shared/Integration/Scripting/Python/InstallDependencies.py
+++ source/Calamari.Shared/Integration/Scripting/Python/InstallDependencies.py
@@ -2,14 +2,21 @@ import importlib
 import subprocess
 import sys
 
+def install_package_using_pip(package):
+    printverbose("Did not find dependency, attempting to install {} for the current user using pip".format(package))
+    exitcode = subprocess.call([sys.executable, "-m", "pip", "install", package, "--user"])
+    if exitcode != 0:
+        print("Unable to install package {} using pip.".format(package), file=sys.stderr)
+        print("If you do not have pip you can install {} using your favorite python package manager.".format(package), file=sys.stderr)
+        exit(exitcode)
+
 def install_missing_dependency(module, package):
     try:
         printverbose("Checking for dependency {}".format(package))
         importlib.import_module(module)
         printverbose("{} was found".format(package))
     except:
-        printverbose("Did not find dependency, attempting to install {} for the current user using pip".format(package))
-        subprocess.call([sys.executable, "-m", "pip", "install", package, "--user"])
+        install_package_using_pip(package)
 
 def printverbose(message):
     print("##octopus[stdout-verbose]")
