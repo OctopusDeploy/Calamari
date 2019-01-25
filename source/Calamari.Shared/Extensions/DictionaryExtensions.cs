3bd46709940d5ed932d5342b833c93559451ce84
johnsimons <john.simons@octopus.com>
2019-01-25 15:26:35 +1000

    Ignore null dictionary

--- source/Calamari.Shared/Extensions/DictionaryExtensions.cs
+++ source/Calamari.Shared/Extensions/DictionaryExtensions.cs
@@ -7,14 +7,11 @@ namespace Calamari.Extensions
     {
         public static void MergeDictionaries(this IDictionary<string, string> collection, IDictionary<string, string> items)
         {
-<<<<<<< HEAD
             if (items == null)
             {
                 return;
             }
 
-=======
->>>>>>> Removing usages of StringDictionary
             foreach (var obj in items)
             {
                 collection[obj.Key] = obj.Value;
