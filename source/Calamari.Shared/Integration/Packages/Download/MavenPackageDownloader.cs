41269923d7a88df203243a726d5b4018de501e01
Roy Wallace <waldo000000@gmail.com>
2019-05-31 15:55:17 +1000

    Fix flaky Maven tests (HashSet is not thread-safe)

--- source/Calamari.Shared/Integration/Packages/Download/MavenPackageDownloader.cs
+++ source/Calamari.Shared/Integration/Packages/Download/MavenPackageDownloader.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -205,7 +204,7 @@ namespace Calamari.Integration.Packages.Download
             Guard.NotNull(mavenPackageId, "mavenPackageId can not be null");
             Guard.NotNull(feedUri, "feedUri can not be null");
 
-            var errors = new HashSet<string>();
+            var errors = new ConcurrentDictionary<string, string>();
             var fileChecks = JarExtractor.EXTENSIONS
                 .Union(AdditionalExtensions)
                 .AsParallel()
@@ -214,7 +213,7 @@ namespace Calamari.Integration.Packages.Download
                     var packageId = new MavenPackageID(mavenPackageId.Group, mavenPackageId.Artifact,
                         mavenPackageId.Version, Regex.Replace(extension, "^\\.", ""));
                     var result = MavenPackageExists(packageId, feedUri, feedCredentials, snapshotMetadata);
-                    errors.Add(result.ErrorMsg);
+                    errors[result.ErrorMsg] = result.ErrorMsg;
                     return new
                     {
                         result.Found,
@@ -229,7 +228,7 @@ namespace Calamari.Integration.Packages.Download
                 return firstFound.MavenPackageId;
             }
 
-            throw new MavenDownloadException($"Failed to find the Maven artifact.\r\nRecieved Error(s):\r\n{string.Join("\r\n", errors.ToList())}");
+            throw new MavenDownloadException($"Failed to find the Maven artifact.\r\nReceived Error(s):\r\n{string.Join("\r\n", errors.Values.ToList())}");
         }
 
         /// <summary>
