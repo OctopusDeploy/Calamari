faec3902a45a99ffd90bdd3ed1004de9967e5d5e
David Young <thedewi@users.noreply.github.com>
2019-09-16 16:36:45 +1000

    use sudo path when checking nginx is installed

--- source/Calamari/Scripts/Octopus.Features.Nginx_BeforePreDeploy.sh
+++ source/Calamari/Scripts/Octopus.Features.Nginx_BeforePreDeploy.sh
@@ -32,14 +32,15 @@ function check_user_has_sudo_access_without_password_to_required_commands {
     fi
 }
 
-function check_app_exists {
-	command -v $1 > /dev/null 2>&1
+function check_nginx_exists {
+	sudo -n bash -c 'command -v nginx' > /dev/null 2>&1
 	if [[ $? -ne 0 ]]; then
-		echo >&2 "The executable $1 does not exist, or is not on the path"
-        echo >&2 "See https://g.octopushq.com/NginxInstall from more information"
+		echo >&2 "The executable 'nginx' does not exist, or is not on the sudo path,"
+		echo >&2 "or the user does not have 'sudo' access to it without a password."
+		echo >&2 "See https://g.octopushq.com/NginxInstall from more information."
 		exit 1
 	fi
 }
 
-check_app_exists nginx
+check_nginx_exists
 check_user_has_sudo_access_without_password_to_required_commands
