789580d6713d8b5bc3d81a2c62664586dc1a314b
Tom Peters <tom.peters@octopus.com>
2019-09-26 16:06:18 +1000

    Get args from all processes when searching for pwsh cmd line params in tests. This is required for macOS

--- source/Calamari.Tests/Fixtures/PowerShell/Scripts/Profile.Nix.ps1
+++ source/Calamari.Tests/Fixtures/PowerShell/Scripts/Profile.Nix.ps1
@@ -1,2 +1,2 @@
 ﻿ # returns the commandline powershell was called with which lets us check for -NoProfile in our test
-ps -o args | grep -i pwsh | grep -v grep
+ps -Ao args | grep -i pwsh | grep -v grep
