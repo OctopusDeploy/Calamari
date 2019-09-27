9e04c4e867df824488a065e2112f391fbe89b7da
Tom Peters <tom.peters@octopus.com>
2019-09-27 10:17:07 +1000

    Construct test packages that only contain .ps1 or .sh files, based on the platform we are deploying to.

--- source/Calamari.Tests/Helpers/TarGzBuilder.cs
+++ source/Calamari.Tests/Helpers/TarGzBuilder.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 using SharpCompress.Archives;
 using SharpCompress.Archives.Tar;
 using SharpCompress.Common;
@@ -22,10 +24,28 @@ namespace Calamari.Tests.Helpers
             using (Stream stream = File.OpenWrite(outputTarFilename))
             using (var writer = WriterFactory.Open(stream, ArchiveType.Tar, new WriterOptions(CompressionType.GZip) {LeaveStreamOpen = false}))
             {
-                writer.WriteAll(sourceDirectory, "*", SearchOption.AllDirectories);
+                var isRunningOnWindows = CalamariEnvironment.IsRunningOnWindows;
+
+                var files = Directory.EnumerateFiles(sourceDirectory, "*.*", SearchOption.AllDirectories)
+                    .Select(f => new FileInfo(f))
+                    .Where(f => isRunningOnWindows
+                        ? !f.Name.EndsWith(".Nix.nuspec")
+                        : !f.Name.EndsWith(".nuspec") || f.Name.EndsWith(".Nix.nuspec"))
+                    .Where(f => isRunningOnWindows ? f.Extension != ".sh" : f.Extension != ".ps1");
+                foreach(var file in files)
+                    writer.Write(GetFilePathRelativeToRoot(sourceDirectory, file), file);
             }
             
             return outputTarFilename;
+        }
+        
+        static string GetFilePathRelativeToRoot(string root, FileInfo file)
+        {
+            var directory = new DirectoryInfo(root);
+            var rootDirectoryUri = new Uri(directory.FullName + Path.DirectorySeparatorChar);
+            var fileUri = new Uri(file.FullName);
+
+            return rootDirectoryUri.MakeRelativeUri(fileUri).ToString();
         }
     }
 }
