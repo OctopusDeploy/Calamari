8451d4f0227a3f349a226d6d3e345601f93df5c0
Matt Richardson <matt.richardson@octopus.com>
2019-11-27 16:32:55 +1100

    Add tests for script tracing and strict mode

--- source/Calamari.Tests/Fixtures/Integration/Scripting/ScriptEngineFixture.cs
+++ source/Calamari.Tests/Fixtures/Integration/Scripting/ScriptEngineFixture.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Calamari.Integration.FileSystem;
 using Calamari.Integration.Processes;
 using Calamari.Integration.Scripting;
@@ -23,6 +24,95 @@ namespace Calamari.Tests.Fixtures.Integration.Scripting
                 File.WriteAllText(scriptFile.FilePath, "Write-Host $mysecrect");
                 var result = ExecuteScript(new PowerShellScriptEngine(), scriptFile.FilePath, GetDictionaryWithSecret());
                 result.AssertOutput("KingKong");
+            }
+        }
+        
+        [Test]
+        [Category(TestCategory.CompatibleOS.OnlyWindows)]
+        [TestCase("true")]
+        [TestCase("True")]
+        [TestCase("1")]
+        [TestCase("2")]
+        public void PowerShellCanSetTraceMode(string variableValue)
+        {
+            using (var scriptFile = new TemporaryFile(Path.ChangeExtension(Path.GetTempFileName(), "ps1")))
+            {
+                File.WriteAllText(scriptFile.FilePath, "Write-Host $mysecrect");
+                var calamariVariableDictionary = GetDictionaryWithSecret();
+                calamariVariableDictionary.Set("Octopus.Action.PowerShell.PSDebug.Trace", variableValue);
+
+                var result = ExecuteScript(new PowerShellScriptEngine(), scriptFile.FilePath, calamariVariableDictionary);
+                
+                result.AssertOutput("KingKong");
+                result.AssertOutput("DEBUG:    1+  >>>> Write-Host $mysecrect");
+
+                if (variableValue != "1")
+                {
+                    //see https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/set-psdebug?view=powershell-6#description
+                    //When the Trace parameter has a value of 1, each line of script is traced as it runs.
+                    //When the parameter has a value of 2, variable assignments, function calls, and script calls are also traced
+                    //we translate "true" to "2"
+                    result.AssertOutput("! CALL function 'Import-CalamariModules'");
+                }
+            }
+        }
+        
+        [Test]
+        [Category(TestCategory.CompatibleOS.OnlyWindows)]
+        [TestCase("0")]
+        [TestCase("False")]
+        [TestCase("false")]
+        [TestCase("")]
+        public void PowerShellDoesntForceTraceMode(string variableValue)
+        {
+            using (var scriptFile = new TemporaryFile(Path.ChangeExtension(Path.GetTempFileName(), "ps1")))
+            {
+                File.WriteAllText(scriptFile.FilePath, "Write-Host $mysecrect");
+                var calamariVariableDictionary = GetDictionaryWithSecret();
+                if (!string.IsNullOrEmpty(variableValue))
+                    calamariVariableDictionary.Set("Octopus.Action.PowerShell.PSDebug.Trace", variableValue);
+
+                var result = ExecuteScript(new PowerShellScriptEngine(), scriptFile.FilePath, calamariVariableDictionary);
+                
+                result.AssertOutput("KingKong");
+                result.AssertNoOutput("DEBUG:    1+  >>>> Write-Host $mysecrect");
+            }
+        }
+        
+        [Test]
+        [Category(TestCategory.CompatibleOS.OnlyWindows)]
+        public void PowerShellWorksWithStrictMode()
+        {
+            using (var scriptFile = new TemporaryFile(Path.ChangeExtension(Path.GetTempFileName(), "ps1")))
+            {
+                File.WriteAllText(scriptFile.FilePath, "$newVar = $nonExistentVar" + Environment.NewLine + "write-host \"newVar = '$newVar'\"");
+                var calamariVariableDictionary = GetDictionaryWithSecret();
+                calamariVariableDictionary.Set("Octopus.Action.PowerShell.PSDebug.Strict", "true");
+
+                var result = ExecuteScript(new PowerShellScriptEngine(), scriptFile.FilePath, calamariVariableDictionary);
+                
+                result.AssertErrorOutput(" cannot be retrieved because it has not been set.");
+                result.AssertFailure();
+            }
+        }
+        
+        [Test]
+        [Category(TestCategory.CompatibleOS.OnlyWindows)]
+        [TestCase("false")]
+        [TestCase("")]
+        public void PowerShellDoesntForceStrictMode(string variableValue)
+        {
+            using (var scriptFile = new TemporaryFile(Path.ChangeExtension(Path.GetTempFileName(), "ps1")))
+            {
+                File.WriteAllText(scriptFile.FilePath, "$newVar = $nonExistentVar" + Environment.NewLine + "write-host \"newVar = '$newVar'\"");
+                var calamariVariableDictionary = GetDictionaryWithSecret();
+                if (!string.IsNullOrEmpty(variableValue))
+                    calamariVariableDictionary.Set("Octopus.Action.PowerShell.PSDebug.Strict", variableValue);
+
+                var result = ExecuteScript(new PowerShellScriptEngine(), scriptFile.FilePath, calamariVariableDictionary);
+                
+                result.AssertOutput("newVar = ''");
+                result.AssertNoOutput(" cannot be retrieved because it has not been set.");
             }
         }
 
