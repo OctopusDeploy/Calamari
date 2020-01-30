95dffe495a0db4b7a107f715388b785cb6f5a0f1
Tom Williams <twilliams1711@gmail.com>
2020-01-30 11:00:14 +1100

    Added proxy tests for PSCore

---
+++ source/Calamari.Tests/Fixtures/PowerShell/PowerShellCoreProxyFixture.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Calamari.Deployment;
+using Calamari.Tests.Fixtures.Integration.Proxies;
+using Calamari.Tests.Helpers;
+using NUnit.Framework;
+
+namespace Calamari.Tests.Fixtures.PowerShell
+{
+    [TestFixture]
+    [Category(TestCategory.CompatibleOS.OnlyWindows)]
+    public class PowerShellCoreProxyFixture : WindowsScriptProxyFixtureBase
+    {
+        protected override CalamariResult RunScript()
+        {
+            var variables = new Dictionary<string,string>()
+            {
+                {SpecialVariables.Action.PowerShell.Edition, "Core"}
+            };
+
+            return RunScript("Proxy.ps1", variables).result;
+        }
+
+        [Test]
+        [Category(TestCategory.CompatibleOS.OnlyWindows)]
+        public override void Initialize_HasSystemProxy_UseSystemProxyWithCredentials()
+        {
+            Assert.Inconclusive("This test currently fails on PSCore");
+        }
+
+        [Test]
+        [Category(TestCategory.CompatibleOS.OnlyWindows)]
+        public override void Initialize_HasSystemProxy_UseSystemProxyWithExceptions()
+        {
+            Assert.Inconclusive("This test currently fails on PSCore");
+        }
+
+        protected override bool TestWebRequestDefaultProxy => true;
+    }
+}
