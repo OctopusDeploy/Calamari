6e4d3e64ad84e8320d3b0ce44803075247bef732
Shaun Marx <shaun.m4rx@gmail.com>
2020-01-31 13:12:46 +1000

    Add helm test for invalid URL

--- source/Calamari.Tests/Fixtures/Integration/Packages/HelmChartPackageDownloaderFixture.cs
+++ source/Calamari.Tests/Fixtures/Integration/Packages/HelmChartPackageDownloaderFixture.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using Calamari.Commands.Support;
 using Calamari.Integration.FileSystem;
 using Calamari.Integration.Packages.Download;
 using Calamari.Integration.Packages.Download.Helm;
@@ -46,6 +47,16 @@ namespace Calamari.Tests.Fixtures.Integration.Packages
                 TimeSpan.FromSeconds(3));
             pkg.PackageId.Should().Be("mychart");
             pkg.Version.Should().Be(new SemanticVersion("0.3.7"));
+        }
+        
+        [Test]
+        [RequiresMonoVersion480OrAbove(Description = "This test requires TLS 1.2, which doesn't work with mono prior to 4.8")]
+        public void PackageWithInvalidUrl_Throws()
+        {
+            var badUrl = new Uri($"https://octopusdeploy.jfrog.io/gobbelygook/{Guid.NewGuid().ToString("N")}");
+            var badEndpointDownloader = new HelmChartPackageDownloader(CalamariPhysicalFileSystem.GetPhysicalFileSystem(), new HelmEndpointProxy(new HttpClient(), badUrl, FeedUsername, FeedPassword), new HttpClient());
+            Action action = () => badEndpointDownloader.DownloadPackage("something", new SemanticVersion("99.9.7"), "gobbely", new Uri(badUrl, "something.99.9.7"), new NetworkCredential(FeedUsername, FeedPassword), true, 1, TimeSpan.FromSeconds(3));
+            action.Should().Throw<Exception>().And.Message.Should().Contain("Unable to read Helm index file").And.Contain("404");
         }
     }
 }
