3ac5d1643f36cc530ca3bbd9326b1652052ef9bc
Frank Lin <frank.lin@octopus.com>
2020-01-31 16:49:32 +1000

    Add Helm3 Support - detect Helm version and use right command line arguments

---
+++ source/Calamari.Tests/KubernetesFixtures/HelmInstalledVersionUpgradeFixture.cs
@@ -0,0 +1,27 @@
+using Calamari.Tests.Fixtures;
+using Calamari.Tests.Helpers;
+using NUnit.Framework;
+
+namespace Calamari.Tests.KubernetesFixtures
+{
+    [TestFixture]
+    public class HelmInstalledVersionUpgradeFixture : HelmUpgradeFixture
+    {
+        [Test]
+        [RequiresNonFreeBSDPlatform]
+        [RequiresNon32BitWindows]
+        [RequiresNonMac]
+        [Category(TestCategory.PlatformAgnostic)]
+        public void Upgrade_Succeeds()
+        {
+            var result = DeployPackage();
+
+            result.AssertSuccess();
+            result.AssertNoOutput("Using custom helm executable at");
+
+            Assert.AreEqual(ReleaseName.ToLower(), result.CapturedOutput.OutputVariables["ReleaseName"]);
+        }
+
+        protected override string ExplicitExeVersion => null;
+    }
+}
