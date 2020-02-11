a93820bd419656986a7b95e7d60df693119fee39
Shaun Marx <shaun.m4rx@gmail.com>
2020-02-11 13:15:29 +1000

    Drop unused class

--- source/Calamari.Aws/Integration/S3/IHaveBucketKeyBehaviour.cs
+++ source/Calamari.Aws/Integration/S3/IHaveBucketKeyBehaviour.cs
@@ -6,18 +6,4 @@
         string BucketKey { get; }
         string BucketKeyPrefix { get; }
     }
-
-    public class S3MultiFileSelectionBucketKeyAdapter: IHaveBucketKeyBehaviour
-    {
-        private readonly S3MultiFileSelectionProperties properties;
-
-        public S3MultiFileSelectionBucketKeyAdapter(S3MultiFileSelectionProperties properties)
-        {
-            this.properties = properties;
-        }
-
-        public BucketKeyBehaviourType BucketKeyBehaviour => BucketKeyBehaviourType.Filename;
-        public string BucketKey => BucketKeyPrefix;
-        public string BucketKeyPrefix => properties.BucketKeyPrefix;
-    }
 }
