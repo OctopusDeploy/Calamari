b51d96d8909cfdb2652597b24a75cd3317eb9365
Robert Wagner <robert@wagner.id.au>
2020-03-06 10:53:28 +1000

    Re-added proxy setup and CalamariWorkingDirectory env variable

--- source/Calamari/Program.cs
+++ source/Calamari/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Calamari.Commands;
+using Calamari.Deployment;
 using Calamari.Deployment.Journal;
 using Calamari.HealthChecks;
 using Calamari.Hooks;
