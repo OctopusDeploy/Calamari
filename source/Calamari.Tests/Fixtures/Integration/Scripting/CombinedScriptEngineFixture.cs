9ad0878de2c4aa73ea18ecb21c9f204d28f008ba
Adam McCoy <adam.mccoy@octopus.com>
2020-03-24 22:09:14 +1100

    Add tests to validate the preferred script type order on each OS

---
+++ source/Calamari.Tests/Fixtures/Integration/Scripting/CombinedScriptEngineFixture.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using Calamari.Integration.Scripting;
+using Calamari.Tests.Helpers;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Calamari.Tests.Fixtures.Integration.Scripting
+{
+    [TestFixture]
+    public class CombinedScriptEngineFixture
+    {
+        private static readonly ScriptSyntax[] ScriptPreferencesNonWindows = new[]
+        {
+            ScriptSyntax.Bash,
+            ScriptSyntax.PowerShell,
+            ScriptSyntax.CSharp,
+            ScriptSyntax.FSharp,
+            ScriptSyntax.Python
+        };
+
+        private static readonly ScriptSyntax[] ScriptPreferencesWindows = new[]
+        {
+            ScriptSyntax.PowerShell,
+            ScriptSyntax.CSharp,
+            ScriptSyntax.FSharp,
+            ScriptSyntax.Python,
+            ScriptSyntax.Bash
+        };
+
+        [Category(TestCategory.CompatibleOS.OnlyWindows)]
+        public void DeterminesCorrectScriptTypePreferenceOrderWindows()
+            => DeterminesCorrectScriptTypePreferenceOrder(ScriptPreferencesWindows);
+
+        [Test]
+        [Category(TestCategory.CompatibleOS.OnlyNixOrMac)]
+        public void DeterminesCorrectScriptTypePreferencesOrderNonWindows()
+            => DeterminesCorrectScriptTypePreferenceOrder(ScriptPreferencesNonWindows);
+
+        private void DeterminesCorrectScriptTypePreferenceOrder(IEnumerable<ScriptSyntax> expected)
+        {
+            var engine = new CombinedScriptEngine(null);
+            var supportedTypes = engine.GetSupportedTypes();
+
+            supportedTypes.Should().BeEquivalentTo(expected);
+        }
+    }
+}
