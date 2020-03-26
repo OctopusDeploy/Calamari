d09e8799dec1b0e1555946bd043e09d2169865fd
Robert Wagner <robert@wagner.id.au>
2020-03-26 16:14:10 +1000

    Fixed up injection of the script engine

--- source/Calamari/Commands/Java/DeployJavaArchiveCommand.cs
+++ source/Calamari/Commands/Java/DeployJavaArchiveCommand.cs
@@ -24,13 +24,13 @@ namespace Calamari.Commands.Java
     public class DeployJavaArchiveCommand : Command
     {
         string archiveFile;
-        private readonly ScriptEngine scriptEngine;
+        private readonly IScriptEngine scriptEngine;
         readonly IVariables variables;
         readonly ICalamariFileSystem fileSystem;
         readonly ICommandLineRunner commandLineRunner;
         readonly ILog log;
 
-        public DeployJavaArchiveCommand(ScriptEngine scriptEngine, IVariables variables, ICalamariFileSystem fileSystem, ICommandLineRunner commandLineRunner, ILog log)
+        public DeployJavaArchiveCommand(IScriptEngine scriptEngine, IVariables variables, ICalamariFileSystem fileSystem, ICommandLineRunner commandLineRunner, ILog log)
         {
             Options.Add("archive=", "Path to the Java archive to deploy.", v => archiveFile = Path.GetFullPath(v));
 
