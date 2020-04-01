041a9be0745de5d4259ad781db77f36df05622bf
Robert Wagner <robert@wagner.id.au>
2020-04-01 20:11:25 +1000

    Moved ILog into it's own file

--- source/Calamari.Shared/Log.cs
+++ source/Calamari.Shared/Log.cs
@@ -7,29 +7,6 @@ using Octostache;
 
 namespace Calamari
 {
-    public interface ILog
-    {
-        void Verbose(string message);
-        void VerboseFormat(string messageFormat, params object[] args);
-        void Info(string message);
-        void InfoFormat(string messageFormat, params object[] args);
-        void Warn(string message);
-        void WarnFormat(string messageFormat, params object[] args);
-        void Error(string message);
-        void ErrorFormat(string messageFormat, params object[] args);
-        void SetOutputVariableButDoNotAddToVariables(string name, string value, bool isSensitive = false);
-        void SetOutputVariable(string name, string value, IVariables variables, bool isSensitive = false);
-        void NewOctopusArtifact(string fullPath, string name, long fileLength);
-
-        void PackageFound(string packageId, IVersion packageVersion, string packageHash,
-            string packageFileExtension, string packageFullPath, bool exactMatchExists = false);
-
-        void Progress(int percentage, string message);
-        void DeltaVerification(string remotePath, string hash, long size);
-        void DeltaVerificationError(string error);
-        string FormatLink(string uri, string description = null);
-    }
-    
     public static class Log
     {
         public static void Verbose(string message)
