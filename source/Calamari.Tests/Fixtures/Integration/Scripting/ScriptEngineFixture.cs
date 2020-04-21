ab35a62cf4281f8ccd71d33c42b71c29add36e37
Andrew Best <andrew.best@octopus.com>
2020-04-21 17:10:05 +0930

    Creates a test demonstrating the current mutable-variable behaviour with script wrappers

--- source/Calamari.Tests/Fixtures/Integration/Scripting/ScriptEngineFixture.cs
+++ source/Calamari.Tests/Fixtures/Integration/Scripting/ScriptEngineFixture.cs
@@ -1,11 +1,74 @@
 using System.Collections.Generic;
+using Calamari.Hooks;
+using Calamari.Integration.Processes;
 using Calamari.Integration.Scripting;
 using Calamari.Tests.Helpers;
+using Calamari.Variables;
 using NUnit.Framework;
 using FluentAssertions;
+using NSubstitute;
 
 namespace Calamari.Tests.Fixtures.Integration.Scripting
 {
+    public class ScriptEngineVariableHandlingFixture
+    {
+        class ScriptWrapperOne : IScriptWrapper
+        {
+            readonly IVariables variables;
+
+            public ScriptWrapperOne(IVariables variables)
+            {
+                this.variables = variables;
+            }
+
+            public int Priority => 1;
+            public bool IsEnabled(ScriptSyntax syntax) => true;
+            public IScriptWrapper NextWrapper { get; set; }
+            public CommandResult ExecuteScript(Script script, ScriptSyntax scriptSyntax, ICommandLineRunner commandLineRunner,
+                Dictionary<string, string> environmentVars)
+            {
+                variables.Set("OctopusAzureTargetScript", "ValueOne");
+                return NextWrapper.ExecuteScript(new Script("Script-One.ps1"), scriptSyntax, commandLineRunner, environmentVars);
+            }
+        }
+        
+        class ScriptWrapperTwo : IScriptWrapper
+        {
+            readonly IVariables variables;
+
+            public ScriptWrapperTwo(IVariables variables)
+            {
+                this.variables = variables;
+            }
+
+            public int Priority => 1;
+            public bool IsEnabled(ScriptSyntax syntax) => true;
+            public IScriptWrapper NextWrapper { get; set; }
+            public CommandResult ExecuteScript(Script script, ScriptSyntax scriptSyntax, ICommandLineRunner commandLineRunner,
+                Dictionary<string, string> environmentVars)
+            {
+                variables.Set("OctopusAzureTargetScript", "ValueTwo");
+                return NextWrapper.ExecuteScript(new Script("Script-Two.ps1"), scriptSyntax, commandLineRunner, environmentVars);
+            }
+        }
+
+        [Test]
+        public void ScriptExecution_ShouldNotAllowScriptWrappersToOverwriteVariables()
+        {
+            var variables = new CalamariVariables();
+            variables.Set("OctopusAzureTargetScript", "ExpectedValue");
+            
+            var runner = Substitute.For<ICommandLineRunner>();
+            runner.Execute(Arg.Any<CommandLineInvocation>()).Returns(new CommandResult("command", 0));
+            
+            var sut = new ScriptEngine(new IScriptWrapper[] { new ScriptWrapperOne(variables), new ScriptWrapperTwo(variables) });
+            
+            sut.Execute(new Script("My-Script.ps1", "Parameters"), variables, runner);
+
+            variables.Get("OctopusAzureTargetScript").Should().Be("ExpectedValue");
+        }
+    }
+    
     [TestFixture]
     public class ScriptEngineFixture
     {
@@ -44,5 +107,6 @@ namespace Calamari.Tests.Fixtures.Integration.Scripting
 
             supportedTypes.Should().Equal(expected);
         }
+        
     }
 }
