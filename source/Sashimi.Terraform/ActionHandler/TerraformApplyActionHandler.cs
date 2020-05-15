d4d2173df3a4247e7c0b21d096489446e7e8036c
benpearce1 <ben.pearce@octopus.com>
2020-05-15 12:30:08 +1000

    refactored terraform ioc module

---
+++ source/Sashimi.Terraform/ActionHandler/TerraformApplyActionHandler.cs
@@ -0,0 +1,17 @@
+﻿using Sashimi.Server.Contracts.CloudTemplates;
+
+namespace Sashimi.Terraform.ActionHandler
+{
+    public class TerraformApplyActionHandler : TerraformActionHandler
+    {
+        public override string Id => TerraformActionTypes.Apply;
+        public override string Name => "Apply a Terraform template";
+        public override string Description => "Applies a Terraform template";
+        public override string ToolCommand => "apply-terraform";
+
+        public TerraformApplyActionHandler(ICloudTemplateHandlerFactory cloudTemplateHandlerFactory)
+            : base(cloudTemplateHandlerFactory)
+        {
+        }
+    }
+}
---
+++ source/Sashimi.Terraform/ActionHandler/TerraformDestroyActionHandler.cs
@@ -0,0 +1,17 @@
+﻿using Sashimi.Server.Contracts.CloudTemplates;
+
+namespace Sashimi.Terraform.ActionHandler
+{
+    public class TerraformDestroyActionHandler : TerraformActionHandler
+    {
+        public TerraformDestroyActionHandler(ICloudTemplateHandlerFactory cloudTemplateHandlerFactory)
+            : base(cloudTemplateHandlerFactory)
+        {
+        }
+
+        public override string Id => TerraformActionTypes.Destroy;
+        public override string Name => "Destroy Terraform resources";
+        public override string Description => "Destroys Terraform resources";
+        public override string ToolCommand => "destroy-terraform";
+    }
+}
