2f19f7469cb7b6a37378865b51cb7d62d825e315
John Simons <john.simons@octopus.com>
2020-05-18 09:45:20 +1000

    A few improvements based on feedback

--- source/Sashimi.Terraform.Tests/TerraformValidatorFixture.cs
+++ source/Sashimi.Terraform.Tests/TerraformValidatorFixture.cs
@@ -18,16 +18,22 @@ namespace Sashimi.Terraform.Tests
     public class TerraformValidatorFixture
     {
         TerraformValidator? validator;
-        
+        IContainer? container;
+
         [SetUp]
-        public void Setup() {
+        public void Setup()
+        {
             var builder = new ContainerBuilder();
             builder.RegisterModule<TerraformModule>();
             builder.RegisterModule<ServerModule>();
-            using (var container = builder.Build())
-            {
-                validator = new TerraformValidator(container.Resolve<ICloudTemplateHandlerFactory>());
-            }
+            container = builder.Build();
+            validator = new TerraformValidator(container.Resolve<ICloudTemplateHandlerFactory>());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            container?.Dispose();
         }
 
         [Test]
@@ -35,29 +41,35 @@ namespace Sashimi.Terraform.Tests
         [TestCase(TerraformActionTypes.Destroy)]
         public void Should_have_error_when_empty_inline_template(string actionType)
         {
-            var result = validator.TestValidate(new DeploymentActionValidationContext(actionType,
+            var context = new DeploymentActionValidationContext(actionType,
                 new Dictionary<string, string>
                 {
                     {KnownVariables.Action.Script.ScriptSource, KnownVariableValues.Action.Script.ScriptSource.Inline}
-                }, new List<PackageReference>()));
+                }, new List<PackageReference>());
+            
+            var result = validator.TestValidate(context);
+            
             result.IsValid.Should().BeFalse();
-            result.Errors.Single().PropertyName.Should().Be(TerraformSpecialVariables.Action.Terraform.Template);
+            result.Errors.Should().ContainSingle().Which.PropertyName.Should().Be(TerraformSpecialVariables.Action.Terraform.Template);
         }
-        
+
         [Test]
         [TestCase(TerraformActionTypes.Apply)]
         [TestCase(TerraformActionTypes.Destroy)]
         public void Should_not_have_error_when_has_inline_template(string actionType)
         {
-            var result = validator.TestValidate(new DeploymentActionValidationContext(actionType,
+            var context = new DeploymentActionValidationContext(actionType,
                 new Dictionary<string, string>
                 {
                     {KnownVariables.Action.Script.ScriptSource, KnownVariableValues.Action.Script.ScriptSource.Inline},
                     {TerraformSpecialVariables.Action.Terraform.Template, "{}"}
-                }, new List<PackageReference>()));
+                }, new List<PackageReference>());
+            
+            var result = validator.TestValidate(context);
+            
             result.IsValid.Should().BeTrue();
         }
-        
+
         [Test]
         [TestCase(TerraformActionTypes.Apply)]
         [TestCase(TerraformActionTypes.Destroy)]
@@ -74,17 +86,23 @@ namespace Sashimi.Terraform.Tests
             variable ""map"" {
                 type = ""map""
             }";
-            var result = validator.TestValidate(new DeploymentActionValidationContext(actionType,
+            var context = new DeploymentActionValidationContext(actionType,
                 new Dictionary<string, string>
                 {
                     {KnownVariables.Action.Script.ScriptSource, KnownVariableValues.Action.Script.ScriptSource.Inline},
                     {TerraformSpecialVariables.Action.Terraform.Template, template},
-                    {TerraformSpecialVariables.Action.Terraform.TemplateParameters, @"{""list"": ""invalid list"", ""map"": ""invalid map""}"}
-                }, new List<PackageReference>()));
+                    {
+                        TerraformSpecialVariables.Action.Terraform.TemplateParameters,
+                        @"{""list"": ""invalid list"", ""map"": ""invalid map""}"
+                    }
+                }, new List<PackageReference>());
+            
+            var result = validator.TestValidate(context);
+            
             result.IsValid.Should().BeFalse();
-            result.Errors.Single().PropertyName.Should().Be("Properties");
+            result.Errors.Should().ContainSingle().Which.PropertyName.Should().Be("Properties");
         }
-        
+
         [Test]
         [TestCase(TerraformActionTypes.Apply)]
         [TestCase(TerraformActionTypes.Destroy)]
@@ -101,85 +119,109 @@ namespace Sashimi.Terraform.Tests
             variable ""map"" {
                 type = ""map""
             }";
-            var result = validator.TestValidate(new DeploymentActionValidationContext(actionType,
+            var context = new DeploymentActionValidationContext(actionType,
                 new Dictionary<string, string>
                 {
                     {KnownVariables.Action.Script.ScriptSource, KnownVariableValues.Action.Script.ScriptSource.Inline},
                     {TerraformSpecialVariables.Action.Terraform.Template, template},
-                    {TerraformSpecialVariables.Action.Terraform.TemplateParameters, "{\"map\":\"{foo = \\\"bar\\\", baz = \\\"qux\\\"}\"}"}
-                }, new List<PackageReference>()));
+                    {
+                        TerraformSpecialVariables.Action.Terraform.TemplateParameters,
+                        "{\"map\":\"{foo = \\\"bar\\\", baz = \\\"qux\\\"}\"}"
+                    }
+                }, new List<PackageReference>());
+            
+            var result = validator.TestValidate(context);
+            
             result.IsValid.Should().BeTrue();
         }
-        
+
         [Test]
         [TestCase(TerraformActionTypes.Apply)]
         [TestCase(TerraformActionTypes.Destroy)]
         public void Should_have_error_when_package_template_not_specified(string actionType)
         {
-            var result = validator.TestValidate(new DeploymentActionValidationContext(actionType,
+            var context = new DeploymentActionValidationContext(actionType,
                 new Dictionary<string, string>
                 {
                     {KnownVariables.Action.Script.ScriptSource, KnownVariableValues.Action.Script.ScriptSource.Package},
-                }, new List<PackageReference>()));
+                }, new List<PackageReference>());
+            
+            var result = validator.TestValidate(context);
+            
             result.IsValid.Should().BeFalse();
-            result.Errors.Single().PropertyName.Should().Be("Packages");
+            result.Errors.Should().ContainSingle().Which.PropertyName.Should().Be("Packages");
         }
-        
+
         [Test]
         [TestCase(TerraformActionTypes.Apply)]
         [TestCase(TerraformActionTypes.Destroy)]
         public void Should_have_no_error_when_package_template_is_specified(string actionType)
         {
-            var result = validator.TestValidate(new DeploymentActionValidationContext(actionType,
+            var context = new DeploymentActionValidationContext(actionType,
                 new Dictionary<string, string>
                 {
                     {KnownVariables.Action.Script.ScriptSource, KnownVariableValues.Action.Script.ScriptSource.Package},
-                }, new List<PackageReference> { new PackageReference("packageId", "feedId")}));
+                }, new List<PackageReference> {new PackageReference("packageId", "feedId")});
+            
+            var result = validator.TestValidate(context);
+            
             result.IsValid.Should().BeTrue();
         }
-        
+
         [Test]
         public void Should_have_error_when_azure_account_not_specified()
         {
-            var result = validator.TestValidate(new DeploymentActionValidationContext(TerraformActionTypes.Plan,
+            var context = new DeploymentActionValidationContext(TerraformActionTypes.Plan,
                 new Dictionary<string, string>
                 {
                     {TerraformSpecialVariables.Action.Terraform.AzureAccount, Boolean.TrueString},
-                }, new List<PackageReference>()));
+                }, new List<PackageReference>());
+            
+            var result = validator.TestValidate(context);
+            
             result.IsValid.Should().BeFalse();
-            result.Errors.Single().PropertyName.Should().Be("Octopus.Action.AzureAccount.Variable");
+            result.Errors.Should().ContainSingle().Which.PropertyName.Should().Be("Octopus.Action.AzureAccount.Variable");
         }
-        
+
         [Test]
         public void Should_have_no_error_when_azure_account_is_specified()
         {
-            var result = validator.TestValidate(new DeploymentActionValidationContext(TerraformActionTypes.Plan,
+            var context = new DeploymentActionValidationContext(TerraformActionTypes.Plan,
                 new Dictionary<string, string>
                 {
                     {TerraformSpecialVariables.Action.Terraform.AzureAccount, Boolean.TrueString},
                     {"Octopus.Action.AzureAccount.Variable", "Myvariable"},
-                }, new List<PackageReference>()));
+                }, new List<PackageReference>());
+            
+            var result = validator.TestValidate(context);
+            
             result.IsValid.Should().BeTrue();
         }
-        
+
         [Test]
         public void Should_have_error_when_aws_account_has_no_roles_specified()
         {
-            var result = validator.TestValidate(new DeploymentActionValidationContext(TerraformActionTypes.Plan,
+            var context = new DeploymentActionValidationContext(TerraformActionTypes.Plan,
                 new Dictionary<string, string>
                 {
                     {TerraformSpecialVariables.Action.Terraform.ManagedAccount, TerraformSpecialVariables.AwsAccount},
                     {TerraformSpecialVariables.Action.Aws.AssumeRole, Boolean.TrueString},
                     {TerraformSpecialVariables.Action.Aws.AwsRegion, "MyRegion"},
-                    {TerraformSpecialVariables.Action.Aws.AccountVariable, "MyVariable"},}, new List<PackageReference>()));
+                    {TerraformSpecialVariables.Action.Aws.AccountVariable, "MyVariable"},
+                }, new List<PackageReference>());
+            
+            var result = validator.TestValidate(context);
+            
             result.IsValid.Should().BeFalse();
-            result.Errors.Select(_ => _.PropertyName).Should().Equal(TerraformSpecialVariables.Action.Aws.AssumedRoleArn, TerraformSpecialVariables.Action.Aws.AssumedRoleSession);
+            result.Errors.Select(_ => _.PropertyName).Should().Equal(
+                TerraformSpecialVariables.Action.Aws.AssumedRoleArn,
+                TerraformSpecialVariables.Action.Aws.AssumedRoleSession);
         }
-        
+
         [Test]
         public void Should_have_no_error_when_aws_account_has_roles_specified()
         {
-            var result = validator.TestValidate(new DeploymentActionValidationContext(TerraformActionTypes.Plan,
+            var context = new DeploymentActionValidationContext(TerraformActionTypes.Plan,
                 new Dictionary<string, string>
                 {
                     {TerraformSpecialVariables.Action.Terraform.ManagedAccount, TerraformSpecialVariables.AwsAccount},
@@ -188,63 +230,78 @@ namespace Sashimi.Terraform.Tests
                     {TerraformSpecialVariables.Action.Aws.AccountVariable, "MyVariable"},
                     {TerraformSpecialVariables.Action.Aws.AssumedRoleArn, "RoleArn"},
                     {TerraformSpecialVariables.Action.Aws.AssumedRoleSession, "RoleSession"},
-                }, new List<PackageReference>()));
+                }, new List<PackageReference>());
+            
+            var result = validator.TestValidate(context);
+            
             result.IsValid.Should().BeTrue();
         }
-        
+
         [Test]
         public void Should_have_error_when_aws_account_with_no_region_specified()
         {
-            var result = validator.TestValidate(new DeploymentActionValidationContext(TerraformActionTypes.Plan,
+            var context = new DeploymentActionValidationContext(TerraformActionTypes.Plan,
                 new Dictionary<string, string>
                 {
                     {TerraformSpecialVariables.Action.Terraform.ManagedAccount, TerraformSpecialVariables.AwsAccount},
                     {TerraformSpecialVariables.Action.Aws.AssumeRole, Boolean.FalseString},
                     {TerraformSpecialVariables.Action.Aws.AccountVariable, "MyVariable"},
-                }, new List<PackageReference>()));
+                }, new List<PackageReference>());
+            
+            var result = validator.TestValidate(context);
+            
             result.IsValid.Should().BeFalse();
-            result.Errors.Single().PropertyName.Should().Be(TerraformSpecialVariables.Action.Aws.AwsRegion);
+            result.Errors.Should().ContainSingle().Which.PropertyName.Should().Be(TerraformSpecialVariables.Action.Aws.AwsRegion);
         }
-        
+
         [Test]
         public void Should_have_no_error_when_aws_account_has_region_specified()
         {
-            var result = validator.TestValidate(new DeploymentActionValidationContext(TerraformActionTypes.Plan,
+            var context = new DeploymentActionValidationContext(TerraformActionTypes.Plan,
                 new Dictionary<string, string>
                 {
                     {TerraformSpecialVariables.Action.Terraform.ManagedAccount, TerraformSpecialVariables.AwsAccount},
                     {TerraformSpecialVariables.Action.Aws.AssumeRole, Boolean.FalseString},
                     {TerraformSpecialVariables.Action.Aws.AwsRegion, "MyRegion"},
                     {TerraformSpecialVariables.Action.Aws.AccountVariable, "MyVariable"},
-                }, new List<PackageReference>()));
+                }, new List<PackageReference>());
+            
+            var result = validator.TestValidate(context);
+            
             result.IsValid.Should().BeTrue();
         }
-        
+
         [Test]
         public void Should_have_error_when_aws_account_with_no_account_variable_specified()
         {
-            var result = validator.TestValidate(new DeploymentActionValidationContext(TerraformActionTypes.Plan,
+            var context = new DeploymentActionValidationContext(TerraformActionTypes.Plan,
                 new Dictionary<string, string>
                 {
                     {TerraformSpecialVariables.Action.Terraform.ManagedAccount, TerraformSpecialVariables.AwsAccount},
                     {TerraformSpecialVariables.Action.Aws.AssumeRole, Boolean.FalseString},
                     {TerraformSpecialVariables.Action.Aws.AwsRegion, "MyRegion"},
-                }, new List<PackageReference>()));
+                }, new List<PackageReference>());
+            
+            var result = validator.TestValidate(context);
+            
             result.IsValid.Should().BeFalse();
-            result.Errors.Single().PropertyName.Should().Be(TerraformSpecialVariables.Action.Aws.AccountVariable);
+            result.Errors.Should().ContainSingle().Which.PropertyName.Should().Be(TerraformSpecialVariables.Action.Aws.AccountVariable);
         }
-        
+
         [Test]
         public void Should_have_no_error_when_aws_account_with_account_variable_specified()
         {
-            var result = validator.TestValidate(new DeploymentActionValidationContext(TerraformActionTypes.Plan,
+            var context = new DeploymentActionValidationContext(TerraformActionTypes.Plan,
                 new Dictionary<string, string>
                 {
                     {TerraformSpecialVariables.Action.Terraform.ManagedAccount, TerraformSpecialVariables.AwsAccount},
                     {TerraformSpecialVariables.Action.Aws.AssumeRole, Boolean.FalseString},
                     {TerraformSpecialVariables.Action.Aws.AwsRegion, "MyRegion"},
                     {TerraformSpecialVariables.Action.Aws.AccountVariable, "MyVariable"},
-                }, new List<PackageReference>()));
+                }, new List<PackageReference>());
+            
+            var result = validator.TestValidate(context);
+            
             result.IsValid.Should().BeTrue();
         }
     }
