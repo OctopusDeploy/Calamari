78fe10e5f08117633fdbbceac0994d6d516cf4b2
John Simons <john.simons@octopus.com>
2020-05-18 12:57:41 +1000

    Moved E2E tests from Server to here

--- source/Sashimi.Terraform.Tests/ActionHandlersFixture.cs
+++ source/Sashimi.Terraform.Tests/ActionHandlersFixture.cs
@@ -422,21 +422,157 @@ namespace Sashimi.Terraform.Tests
         }
 
         [Test]
-        public void InlineTemplateAndVariables()
+        public void InlineHclTemplateAndVariables()
         {
-            ExecuteAndReturnLogOutput<TerraformPlanActionHandler>(_ =>
+            const string variables =
+                "{\"stringvar\":\"default string\",\"images\":\"\",\"test2\":\"\",\"test3\":\"\",\"test4\":\"\"}";
+            const string template = @"variable stringvar {
+  type = ""string""
+  default = ""default string""
+}
+variable ""images"" {
+  type = ""map""
+  default = {
+    us-east-1 = ""image-1234""
+    us-west-2 = ""image-4567""
+  }
+}
+variable ""test2"" {
+  type    = ""map""
+  default = {
+    val1 = [""hi""]
+  }
+}
+variable ""test3"" {
+  type    = ""map""
+  default = {
+    val1 = {
+      val2 = ""#{RandomNumber}""
+    }
+  }
+}
+variable ""test4"" {
+  type    = ""map""
+  default = {
+    val1 = {
+      val2 = [""hi""]
+    }                        
+  }
+}
+# Example of getting an element from a list in a map
+output ""nestedlist"" {
+  value = ""${element(var.test2[""val1""], 0)}""
+}
+# Example of getting an element from a nested map
+output ""nestedmap"" {
+  value = ""${lookup(var.test3[""val1""], ""val2"")}""
+}";                                        
+
+            ExecuteAndReturnLogOutput<TerraformApplyActionHandler>(_ =>
             {
-                var template = $@"output ""my_output"" {{
-  value = ""boo""
-}}";
+                _.Variables.Add("RandomNumber", new Random().Next().ToString());
                 _.Variables.Add(TerraformSpecialVariables.Action.Terraform.Template, template);
-                _.Variables.Add(TerraformSpecialVariables.Action.Terraform.TemplateParameters, "{}");
-                _.Variables.Add(KnownVariables.Action.Script.ScriptSource, KnownVariables.Action.Script.ScriptSourceOptions.Inline);
+                _.Variables.Add(TerraformSpecialVariables.Action.Terraform.TemplateParameters, variables);
+                _.Variables.Add(KnownVariables.Action.Script.ScriptSource,
+                    KnownVariables.Action.Script.ScriptSourceOptions.Inline);
             }, String.Empty, _ =>
             {
-                _.WasSuccessful.Should().BeTrue();
+                _.OutputVariables.ContainsKey("TerraformValueOutputs[nestedlist]").Should().BeTrue();
+                _.OutputVariables.ContainsKey("TerraformValueOutputs[nestedmap]").Should().BeTrue();
             });
         }
+        
+        [Test]
+        public void InlineHclTemplateWithMultilineOutput()
+        {
+            const string expected = @"apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: arbitrary text
+      username: system:node:username
+      groups:
+        - system:bootstrappers
+        - system:nodes";
+            string template = String.Format(@"locals {{
+  config-map-aws-auth = <<CONFIGMAPAWSAUTH
+{0}
+CONFIGMAPAWSAUTH
+}}
+
+output ""config-map-aws-auth"" {{
+    value = ""${{local.config-map-aws-auth}}""
+}}", expected);                                     
+
+            ExecuteAndReturnLogOutput<TerraformApplyActionHandler>(_ =>
+            {
+                _.Variables.Add(TerraformSpecialVariables.Action.Terraform.Template, template);
+                _.Variables.Add(TerraformSpecialVariables.Action.Terraform.TemplateParameters, "{}");
+                _.Variables.Add(KnownVariables.Action.Script.ScriptSource,
+                    KnownVariables.Action.Script.ScriptSourceOptions.Inline);
+            }, String.Empty, _ =>
+            {
+                _.OutputVariables.ContainsKey("TerraformValueOutputs[config-map-aws-auth]").Should().BeTrue();
+                _.OutputVariables["TerraformValueOutputs[config-map-aws-auth]"].Value.Should().Be(
+                    $"{expected}{Environment.NewLine}");
+            });
+        }
+
+        [Test]
+        public void InlineJsonTemplateAndVariables()
+        {
+            const string variables =
+                "{\"ami\":\"new ami value\"}";
+            const string template = @"{
+    ""variable"":{
+      ""ami"":{
+         ""type"":""string"",
+         ""description"":""the AMI to use"",
+         ""default"":""1234567890""
+      }
+    },
+    ""output"":{
+      ""test"":{
+         ""value"":""hi there""
+      },
+      ""test2"":{
+         ""value"":[
+            ""hi there"",
+            ""hi again""
+         ]
+      },
+      ""test3"":{
+         ""value"":""${map(\""a\"", \""hi\"")}""
+      },
+      ""ami"":{
+         ""value"":""${var.ami}""
+      },
+      ""random"":{
+         ""value"":""#{RandomNumber}""
+      }
+    }
+}";                          
+
+            var randomNumber = new Random().Next().ToString();
+
+            ExecuteAndReturnLogOutput<TerraformApplyActionHandler>(_ =>
+            {
+                _.Variables.Add("RandomNumber", randomNumber);
+                _.Variables.Add(TerraformSpecialVariables.Action.Terraform.Template, template);
+                _.Variables.Add(TerraformSpecialVariables.Action.Terraform.TemplateParameters, variables);
+                _.Variables.Add(KnownVariables.Action.Script.ScriptSource,
+                    KnownVariables.Action.Script.ScriptSourceOptions.Inline);
+            }, String.Empty, _ =>
+            {
+                _.OutputVariables.ContainsKey("TerraformValueOutputs[ami]").Should().BeTrue();
+                _.OutputVariables["TerraformValueOutputs[ami]"].Value.Should().Be("new ami value");
+                _.OutputVariables.ContainsKey("TerraformValueOutputs[random]").Should().BeTrue();
+                _.OutputVariables["TerraformValueOutputs[random]"].Value.Should().Be(randomNumber);
+            });
+        }        
 
         string ExecuteAndReturnLogOutput(Type commandType, Action<TestActionHandlerContext<Program>> populateVariables,
             string folderName, Action<TestActionHandlerResult>? assert = null)
