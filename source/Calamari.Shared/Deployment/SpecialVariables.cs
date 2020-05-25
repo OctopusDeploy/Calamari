b158a41d63084ce28fabfd4f4267c6a1fc583f89
Matthew Casperson <matthewcasperson@gmail.com>
2020-05-26 09:09:18 +1000

    Added support for external id when assuming aws role (#563)

--- source/Calamari.Shared/Deployment/SpecialVariables.cs
+++ source/Calamari.Shared/Deployment/SpecialVariables.cs
@@ -169,6 +169,7 @@ namespace Calamari.Deployment
                 public static readonly string CloudFormationTemplate =  "Octopus.Action.Aws.CloudFormationTemplate";
                 public static readonly string CloudFormationProperties = "Octopus.Action.Aws.CloudFormationProperties";
                 public static readonly string AssumeRoleARN = "Octopus.Action.Aws.AssumedRoleArn";
+                public static readonly string AssumeRoleExternalId = "Octopus.Action.Aws.AssumeRoleExternalId";
                 public static readonly string AccountId = "Octopus.Action.AwsAccount.Variable";
                 public static readonly string CloudFormationAction = "Octopus.Action.Aws.CloudFormationAction";
             }
