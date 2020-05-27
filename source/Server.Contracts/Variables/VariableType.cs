de62b05dfbaaadcdb62ff4267730900f0d6e7630
Frank Lin <frank.lin@octopus.com>
2020-05-27 17:30:13 +1000

    Fix bug where VariableType types were properties and not static fields (#31)

--- source/Server.Contracts/Variables/VariableType.cs
+++ source/Server.Contracts/Variables/VariableType.cs
@@ -5,14 +5,14 @@ namespace Sashimi.Server.Contracts.Variables
 {
     public class VariableType : CaseInsensitiveTypedString
     {
-        public static VariableType String => new VariableType("String");
-        public static VariableType Sensitive => new VariableType("Sensitive");
-        public static VariableType Certificate => new VariableType("Certificate");
-        public static VariableType WorkerPool => new VariableType("WorkerPool");
+        public static readonly VariableType String = new VariableType("String");
+        public static readonly VariableType Sensitive = new VariableType("Sensitive");
+        public static readonly VariableType Certificate = new VariableType("Certificate");
+        public static readonly VariableType WorkerPool = new VariableType("WorkerPool");
 
         //TODO: Move out to respective projects
-        public static VariableType AmazonWebServicesAccount => new VariableType("AmazonWebServicesAccount");
-        public static VariableType AzureAccount => new VariableType("AzureAccount");
+        public static readonly VariableType AmazonWebServicesAccount = new VariableType("AmazonWebServicesAccount");
+        public static readonly VariableType AzureAccount = new VariableType("AzureAccount");
 
         public VariableType(string value) : base(value)
         {
