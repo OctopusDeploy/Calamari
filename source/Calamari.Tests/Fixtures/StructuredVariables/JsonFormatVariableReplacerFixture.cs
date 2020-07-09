1371a9536b660fe07bb7e52059dc4001f0fcb9db
Andrew Moore <andrew.moore@octopus.com>
2020-07-09 13:31:59 +1000

    Refactor tests.

--- source/Calamari.Tests/Fixtures/StructuredVariables/JsonFormatVariableReplacerFixture.cs
+++ source/Calamari.Tests/Fixtures/StructuredVariables/JsonFormatVariableReplacerFixture.cs
@@ -5,21 +5,21 @@ using Calamari.Integration.FileSystem;
 using Calamari.Features.StructuredVariables;
 using Calamari.Tests.Helpers;
 using Calamari.Variables;
+using FluentAssertions;
+using NSubstitute;
 using NUnit.Framework;
 
-namespace Calamari.Tests.Fixtures.JsonVariables
+namespace Calamari.Tests.Fixtures.StructuredVariables
 {
     [TestFixture]
-    public class JsonConfigurationVariableReplacerFixture : CalamariFixture
+    public class JsonFormatVariableReplacerFixture : CalamariFixture
     {
-        StructuredConfigVariableReplacer configVariableReplacer;
+        JsonFormatVariableReplacer replacer;
 
         [SetUp]
         public void SetUp()
         {
-            configVariableReplacer = new StructuredConfigVariableReplacer(
-                new JsonFormatVariableReplacer(), 
-                new YamlFormatVariableReplacer());
+            replacer = new JsonFormatVariableReplacer();
         }
 
         [Test]
@@ -164,7 +164,6 @@ namespace Calamari.Tests.Fixtures.JsonVariables
             this.Assent(replaced, TestEnvironment.AssentJsonDeepCompareConfiguration);
         }
 
-
         [Test]
         public void ShouldReplaceDecimal()
         {
@@ -196,7 +195,8 @@ namespace Calamari.Tests.Fixtures.JsonVariables
 
             using (new TemporaryFile(temp))
             {
-                configVariableReplacer.ModifyFile(temp, variables);
+                var success = replacer.TryModifyFile(temp, variables);
+                success.Should().BeTrue();
                 return File.ReadAllText(temp);
             }
         }
---
+++ source/Calamari.Tests/Fixtures/StructuredVariables/StructuredVariableReplacerFixture.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Calamari.Features.StructuredVariables;
+using Calamari.Variables;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Calamari.Tests.Fixtures.StructuredVariables
+{
+    public class StructuredVariableReplacerFixture
+    {
+        private void RunExceptionTest(
+            bool canParseAsJson,
+            bool canParseAsYaml,
+            Action<Action> invocationAssertions
+        )
+        {
+            var jsonReplacer = Substitute.For<IJsonFormatVariableReplacer>();
+            var yamlReplacer = Substitute.For<IYamlFormatVariableReplacer>();
+
+            jsonReplacer.TryModifyFile(Arg.Any<string>(), Arg.Any<IVariables>()).Returns(canParseAsJson);
+            yamlReplacer.TryModifyFile(Arg.Any<string>(), Arg.Any<IVariables>()).Returns(canParseAsYaml);
+            
+            var replacer = new StructuredConfigVariableReplacer(jsonReplacer, yamlReplacer);
+            var variables = new CalamariVariables();
+
+            invocationAssertions(replacer.Invoking(r => r.ModifyFile("path", variables)));
+        }
+        
+        [Test]
+        public void ShouldNotThrowIfTheFileCanBeParsedAsJson()
+        {
+            RunExceptionTest(
+                canParseAsJson: true,
+                canParseAsYaml: false,
+                invocationResult => invocationResult
+                    .Should()
+                    .NotThrow()
+            );
+        }
+        
+        [Test]
+        public void ShouldNotThrowIfTheFileCanBeParsedAsYaml()
+        {
+            RunExceptionTest(
+                canParseAsJson: false,
+                canParseAsYaml: true,
+                invocationResult => invocationResult
+                    .Should()
+                    .NotThrow()
+            );
+        }
+        
+        [Test]
+        public void ShouldThrowIfTheFileCantBeParsedWithAllReplacers()
+        {
+            RunExceptionTest(
+                canParseAsJson: false,
+                canParseAsYaml: false,
+                invocationResult => invocationResult
+                    .Should()
+                    .ThrowExactly<Exception>()
+                    .WithMessage("The config file at 'path' couldn't be parsed.")
+            );
+        }
+
+        [Test]
+        public void ShouldOnlyTryYamlIfFileIsNotJson()
+        {
+            var jsonReplacer = Substitute.For<IJsonFormatVariableReplacer>();
+            var yamlReplacer = Substitute.For<IYamlFormatVariableReplacer>();
+
+            jsonReplacer.TryModifyFile(Arg.Any<string>(), Arg.Any<IVariables>()).Returns(true);
+            
+            var replacer = new StructuredConfigVariableReplacer(jsonReplacer, yamlReplacer);
+            var variables = new CalamariVariables();
+
+            replacer.ModifyFile("path", variables);
+            
+            yamlReplacer.DidNotReceiveWithAnyArgs().TryModifyFile("", null);
+        }
+    }
+}
