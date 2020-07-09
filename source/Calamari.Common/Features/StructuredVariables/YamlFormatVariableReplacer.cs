e726f7fca8e5318ec37985aa98a7a66e65d50bca
David Young <thedewi@users.noreply.github.com>
2020-07-09 23:15:07 +1000

    Implement path-based scalar value YamlFileFormatVariableReplacer

--- source/Calamari.Common/Features/StructuredVariables/YamlFormatVariableReplacer.cs
+++ source/Calamari.Common/Features/StructuredVariables/YamlFormatVariableReplacer.cs
@@ -1,4 +1,10 @@
-﻿namespace Calamari.Common.Features.StructuredVariables
+﻿using System.IO;
+using System.Linq;
+using Calamari.Integration.FileSystem;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+
+namespace Calamari.Common.Features.StructuredVariables
 {
     public interface IYamlFormatVariableReplacer : IFileFormatVariableReplacer
     {
@@ -10,8 +16,46 @@
         
         public bool TryModifyFile(string filePath, IVariables variables)
         {
-            // Yaml not yet supported.
-            return false;
+            var variablesByKey = variables
+                .DistinctBy(v => v.Key)
+                .ToDictionary(v => v.Key, v => v.Value);
+
+            try
+            {
+                string updatedText;
+                using (var writer = new StringWriter())
+                using (var reader = new StreamReader(filePath))
+                {
+                    var parser = new Parser(reader);
+                    var classifier = new YamlEventStreamClassifier();
+                    var emitter = new Emitter(writer);
+                    while (parser.MoveNext())
+                    {
+                        var ev = parser.Current;
+                        if (ev == null)
+                            continue;
+
+                        var found = classifier.Process(ev);
+                        if (found is YamlScalarValueNode
+                            && variablesByKey.TryGetValue(found.Path, out string newValue))
+                        {
+                            ev = new Scalar(newValue);
+                        }
+
+                        emitter.Emit(ev);
+                    }
+
+                    writer.Close();
+                    updatedText = writer.ToString();
+                }
+
+                File.WriteAllText(filePath, updatedText);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
---
+++ source/Calamari.Tests/Fixtures/StructuredVariables/YamlVariableReplacerFixture.cs
@@ -0,0 +1,38 @@
+﻿using Calamari.Common.Features.StructuredVariables;
+using Calamari.Variables;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Calamari.Tests.Fixtures.StructuredVariables
+{
+    [TestFixture]
+    public class YamlVariableReplacerFixture : VariableReplacerFixture
+    {
+        public YamlVariableReplacerFixture() : base(new YamlFormatVariableReplacer())
+        {
+        }
+
+        [Test]
+        public void ShouldReplaceSimpleStringYamlVariables()
+        {
+            const string expected = @"environment: production
+include:
+- prod-system
+- monitoring
+";
+
+            var variables = new CalamariVariables();
+            variables.Set("environment", "production");
+            variables.Set("include:0", "prod-system");
+
+            var replaced = Replace(variables, "application-simple-string.yaml");
+
+            AssertYamlEquivalent(replaced, expected);
+        }
+
+        void AssertYamlEquivalent(string replaced, string expected)
+        {
+            replaced.Should().Be(expected);
+        }
+    }
+}
