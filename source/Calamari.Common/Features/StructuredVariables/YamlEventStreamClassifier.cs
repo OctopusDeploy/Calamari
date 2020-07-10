4c7be42a4ba4589b2ef498fbb465a8f6f069e103
David Young <thedewi@users.noreply.github.com>
2020-07-10 14:40:32 +1000

    Implement path-based scalar value YamlFileFormatVariableReplacer (#579)
    * Add YamlTransformApproachComparisonDemoFixture
    
    * Add YamlEventStreamClassifier and test
    
    * Extract VariableReplacerFixture base class
    
    * Implement path-based scalar value YamlFileFormatVariableReplacer
    
    * Preserve YAML input style more faithfully
    
    * Expand ShouldReplaceSimpleStringYamlVariables to use Assent, bigger example
    
    * Untangle YAML file reading and writing
    
    * Catch syntax errors but let others bubble up, like Json replacer

---
+++ source/Calamari.Common/Features/StructuredVariables/YamlEventStreamClassifier.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using YamlDotNet.Core.Events;
+
+namespace Calamari.Common.Features.StructuredVariables
+{
+    enum YamlStructure
+    {
+        Mapping,
+        Sequence
+    }
+
+    class YamlPathComponent
+    {
+        public YamlStructure Type { get; }
+        public string MappingKey { get; set; }
+        public int SequenceIndex { get; set; } = -1;
+
+        public YamlPathComponent(YamlStructure structure)
+        {
+            Type = structure;
+        }
+    }
+
+    class YamlPathStack
+    {
+        readonly Stack<YamlPathComponent> stack = new Stack<YamlPathComponent>();
+
+        public void Push(YamlStructure structure) => stack.Push(new YamlPathComponent(structure));
+
+        public void Pop() => stack.Pop();
+
+        IEnumerable<string> GetPathComponents()
+        {
+            foreach (var stackItem in stack.Reverse())
+            {
+                if (stackItem.MappingKey != null)
+                    yield return stackItem.MappingKey;
+                else if (stackItem.Type == YamlStructure.Sequence)
+                    yield return stackItem.SequenceIndex.ToString();
+            }
+        }
+
+        public string GetPath() => string.Join(":", GetPathComponents());
+
+        public bool TopIsSequence => stack.Count > 0
+                                     && stack.Peek().Type == YamlStructure.Sequence;
+
+        public void TopSequenceIncrementIndex()
+        {
+            if (TopIsSequence)
+                stack.Peek().SequenceIndex++;
+        }
+
+        public bool TopIsMappingExpectingKey => stack.Count > 0
+                                                && stack.Peek().Type == YamlStructure.Mapping
+                                                && stack.Peek().MappingKey == null;
+
+        public void TopMappingKeyStart(string key)
+        {
+            if (TopIsMappingExpectingKey)
+                stack.Peek().MappingKey = key;
+        }
+
+        public void TopMappingKeyEnd()
+        {
+            if (stack.Count > 0)
+                stack.Peek().MappingKey = null;
+        }
+    }
+
+    public abstract class YamlNode
+    {
+        public IList<ParsingEvent> ParsingEvents { get; }
+        public string Path { get; }
+
+        protected YamlNode(IList<ParsingEvent> parsingEvents, string path)
+        {
+            ParsingEvents = parsingEvents;
+            Path = path;
+        }
+    }
+
+    public class YamlScalarValueNode : YamlNode
+    {
+        readonly Scalar scalar;
+
+        public string Value { get; }
+
+        public YamlScalarValueNode(Scalar scalar, string path, string value)
+            : base(new List<ParsingEvent>
+            {
+                scalar
+            }, path)
+        {
+            this.scalar = scalar;
+            Value = value;
+        }
+
+        public Scalar ReplaceValue(string newValue)
+        {
+            return new Scalar(scalar.Anchor, scalar.Tag, newValue, scalar.Style, scalar.IsPlainImplicit,
+                scalar.IsQuotedImplicit, scalar.Start, scalar.End);
+        }
+    }
+
+    public class YamlEventStreamClassifier
+    {
+        readonly YamlPathStack stack = new YamlPathStack();
+
+        public YamlNode Process(ParsingEvent ev)
+        {
+            YamlNode result = null;
+
+            if (stack.TopIsSequence && (ev is MappingStart || ev is SequenceStart || ev is Scalar))
+            {
+                stack.TopSequenceIncrementIndex();
+            }
+
+            switch (ev)
+            {
+                case MappingStart _:
+                    stack.Push(YamlStructure.Mapping);
+                    break;
+                case MappingEnd _:
+                    stack.Pop();
+                    stack.TopMappingKeyEnd();
+                    break;
+                case SequenceStart _:
+                    stack.Push(YamlStructure.Sequence);
+                    break;
+                case SequenceEnd _:
+                    stack.Pop();
+                    stack.TopMappingKeyEnd();
+                    break;
+                case Scalar sc:
+                    if (stack.TopIsMappingExpectingKey)
+                    {
+                        // This is a map key
+                        stack.TopMappingKeyStart(sc.Value);
+                    }
+                    else
+                    {
+                        // This is a value in a map or sequence
+                        result = new YamlScalarValueNode(sc, stack.GetPath(), sc.Value);
+                        stack.TopMappingKeyEnd();
+                    }
+
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
--- source/Calamari.Common/Features/StructuredVariables/YamlFormatVariableReplacer.cs
+++ source/Calamari.Common/Features/StructuredVariables/YamlFormatVariableReplacer.cs
@@ -1,4 +1,11 @@
-﻿namespace Calamari.Common.Features.StructuredVariables
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Calamari.Integration.FileSystem;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+
+namespace Calamari.Common.Features.StructuredVariables
 {
     public interface IYamlFormatVariableReplacer : IFileFormatVariableReplacer
     {
@@ -7,11 +14,60 @@
     public class YamlFormatVariableReplacer : IYamlFormatVariableReplacer
     {
         public string FileFormatName => "YAML";
-        
+
         public bool TryModifyFile(string filePath, IVariables variables)
         {
-            // Yaml not yet supported.
-            return false;
+            var variablesByKey = variables
+                .DistinctBy(v => v.Key)
+                .ToDictionary(v => v.Key, v => v.Value);
+
+            // Read and transform the input file
+            var outputEvents = new List<ParsingEvent>();
+            try
+            {
+                using (var reader = new StreamReader(filePath))
+                {
+                    var parser = new Parser(reader);
+                    var classifier = new YamlEventStreamClassifier();
+                    while (parser.MoveNext())
+                    {
+                        var ev = parser.Current;
+                        if (ev == null)
+                            continue;
+
+                        var found = classifier.Process(ev);
+                        if (found is YamlScalarValueNode scalar
+                            && variablesByKey.TryGetValue(scalar.Path, out string newValue))
+                        {
+                            ev = scalar.ReplaceValue(newValue);
+                        }
+
+                        outputEvents.Add(ev);
+                    }
+                }
+            }
+            catch (SyntaxErrorException)
+            {
+                // TODO: Report where the problem was in the input file
+                return false;
+            }
+
+            // Write the replacement file
+            string outputText;
+            using (var writer = new StringWriter())
+            {
+                var emitter = new Emitter(writer);
+                foreach (var outputEvent in outputEvents)
+                {
+                    emitter.Emit(outputEvent);
+                }
+
+                writer.Close();
+                outputText = writer.ToString();
+            }
+
+            File.WriteAllText(filePath, outputText);
+            return true;
         }
     }
 }
