3ca3fcb7306c1bed4047c73c6bffb6d026ae8397
David Young <thedewi@users.noreply.github.com>
2020-07-10 16:00:35 +1000

    Implement feature toggle Octopus.Action.Package.StructuredConfigurationFeatureFlag (#580)
    * Implement feature toggle Octopus.Action.Package.StructuredConfigurationFeatureFlag
    
    * Implement feature toggle Octopus.Action.Package.StructuredConfigurationFeatureFlag

--- source/Calamari.Common/Features/StructuredVariables/StructuredConfigVariableReplacer.cs
+++ source/Calamari.Common/Features/StructuredVariables/StructuredConfigVariableReplacer.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Linq;
 
 namespace Calamari.Common.Features.StructuredVariables
 {
     public class StructuredConfigVariableReplacer : IStructuredConfigVariableReplacer
     {
+        readonly string featureToggleVariableName = "Octopus.Action.Package.StructuredConfigurationFeatureFlag";
+
         readonly IFileFormatVariableReplacer[] replacers;
 
         public StructuredConfigVariableReplacer(
@@ -25,7 +28,19 @@ namespace Calamari.Common.Features.StructuredVariables
         {
             Log.Info($"Attempting to push variables into structured config file at '{filePath}'");
 
-            foreach (var replacer in replacers)
+            // Toggle set of replacers based on feature flag
+            IFileFormatVariableReplacer[] replacersToTry;
+            if (variables.GetFlag(featureToggleVariableName))
+            {
+                Log.Info($"Feature toggle flag {featureToggleVariableName} detected. Trying replacers for all supported file formats.");
+                replacersToTry = replacers;
+            }
+            else
+            {
+                replacersToTry = replacers.OfType<IJsonFormatVariableReplacer>().ToArray<IFileFormatVariableReplacer>();
+            }
+
+            foreach (var replacer in replacersToTry)
             {
                 var fileUpdated = replacer.TryModifyFile(filePath, variables);
                 if (fileUpdated)
