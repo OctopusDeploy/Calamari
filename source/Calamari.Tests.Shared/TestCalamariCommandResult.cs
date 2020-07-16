ce11bedf656e42b9477266f873117d860554df7e
Shannon Lewis <slewis74@gmail.com>
2020-07-17 08:23:36 +1000

    package updates for merge

--- source/Calamari.Tests.Shared/TestCalamariCommandResult.cs
+++ source/Calamari.Tests.Shared/TestCalamariCommandResult.cs
@@ -8,7 +8,7 @@ namespace Calamari.Tests.Shared
     {
         public TestCalamariCommandResult(int exitCode, IReadOnlyDictionary<string, TestOutputVariable> outputVariables,
             IReadOnlyList<TestScriptOutputAction> outputActions, IReadOnlyList<ServiceMessage> serviceMessages,
-            string resultMessage, IReadOnlyList<CollectedArtifact> artifacts, string fullLog)
+            string? resultMessage, IReadOnlyList<CollectedArtifact> artifacts, string fullLog)
         {
             ExitCode = exitCode;
             OutputVariables = outputVariables;
@@ -27,7 +27,7 @@ namespace Calamari.Tests.Shared
         public IReadOnlyList<ServiceMessage> ServiceMessages { get; }
         public TestExecutionOutcome Outcome => WasSuccessful ? TestExecutionOutcome.Successful : TestExecutionOutcome.Unsuccessful;
         public bool WasSuccessful => ExitCode == 0;
-        public string ResultMessage { get; }
+        public string? ResultMessage { get; }
         public int ExitCode { get; }
     }
 }
