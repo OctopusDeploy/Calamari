265533b089bdd89267866fc8860ecd9254b76ace
David Young <thedewi@users.noreply.github.com>
2020-07-16 14:39:57 +1000

    Improve alignment

--- source/Calamari.Tests/Fixtures/StructuredVariables/YamlVariableReplacerFixture.cs
+++ source/Calamari.Tests/Fixtures/StructuredVariables/YamlVariableReplacerFixture.cs
@@ -19,66 +19,66 @@ namespace Calamari.Tests.Fixtures.StructuredVariables
         public void CanReplaceStringWithString()
         {
             this.Assent(Replace(new CalamariVariables
-                    {
-                        { "server:ports:0", "8080" },
-                        { "Spring:H2:Console:Enabled", "false" },
-                        { "environment", "production" }
-                    },
-                    "application.yaml"),
-                TestEnvironment.AssentConfiguration);
+                                {
+                                    { "server:ports:0", "8080" },
+                                    { "Spring:H2:Console:Enabled", "false" },
+                                    { "environment", "production" }
+                                },
+                                "application.yaml"),
+                        TestEnvironment.AssentConfiguration);
         }
 
         [Test]
         public void CanReplaceMappingWithString()
         {
             this.Assent(Replace(new CalamariVariables
-                    {
-                        { "server", "local" },
-                        { "spring:datasource", "none" }
-                    },
-                    "application.yaml"),
-                TestEnvironment.AssentConfiguration);
+                                {
+                                    { "server", "local" },
+                                    { "spring:datasource", "none" }
+                                },
+                                "application.yaml"),
+                        TestEnvironment.AssentConfiguration);
         }
 
         [Test]
         public void CanReplaceSequenceWithString()
         {
             this.Assent(Replace(new CalamariVariables
-                    {
-                        { "server:ports", "none" }
-                    },
-                    "application.yaml"),
-                TestEnvironment.AssentConfiguration);
+                                {
+                                    { "server:ports", "none" }
+                                },
+                                "application.yaml"),
+                        TestEnvironment.AssentConfiguration);
         }
 
         [Test]
         public void CanReplaceMappingDespiteReplacementInsideMapping()
         {
             this.Assent(Replace(new CalamariVariables
-                    {
-                        { "spring:datasource:dbcp2", "none" },
-                        { "spring:datasource", "none" }
-                    },
-                    "application.yaml"),
-                TestEnvironment.AssentConfiguration);
+                                {
+                                    { "spring:datasource:dbcp2", "none" },
+                                    { "spring:datasource", "none" }
+                                },
+                                "application.yaml"),
+                        TestEnvironment.AssentConfiguration);
         }
 
         [Test]
         public void TypesAreInfluencedByThePositionInTheInputDocument()
         {
             this.Assent(Replace(new CalamariVariables
-                    {
-                        { "null1", "~" },
-                        { "bool1", "true" },
-                        { "int1", "99" },
-                        { "float1", "1.99" },
-                        { "str1", "true" },
-                        { "str2", "~" },
-                        { "obj1", $"fruit: apple{Environment.NewLine}animal: sloth" },
-                        { "seq1", $"- scissors{Environment.NewLine}- paper{Environment.NewLine}- rock" }
-                    },
-                    "types.yaml"),
-                TestEnvironment.AssentConfiguration);
+                                {
+                                    { "null1", "~" },
+                                    { "bool1", "true" },
+                                    { "int1", "99" },
+                                    { "float1", "1.99" },
+                                    { "str1", "true" },
+                                    { "str2", "~" },
+                                    { "obj1", $"fruit: apple{Environment.NewLine}animal: sloth" },
+                                    { "seq1", $"- scissors{Environment.NewLine}- paper{Environment.NewLine}- rock" }
+                                },
+                                "types.yaml"),
+                        TestEnvironment.AssentConfiguration);
         }
     }
 }
