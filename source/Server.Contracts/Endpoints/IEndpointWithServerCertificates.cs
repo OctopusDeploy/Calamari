9d9e8c05eed2902c0dbdec227d2af9dd93f5bfc5
Ali Almaktoum <ali.almaktoum@octopus.com>
2020-08-05 10:34:28 +1000

    added todos

--- source/Server.Contracts/Endpoints/IEndpointWithServerCertificates.cs
+++ source/Server.Contracts/Endpoints/IEndpointWithServerCertificates.cs
@@ -4,6 +4,7 @@ namespace Sashimi.Server.Contracts.Endpoints
 {
     public interface IEndpointWithServerCertificates
     {
+        // todo https://github.com/OctopusDeploy/Sashimi/pull/76#discussion_r465399149
         IEnumerable<string> ServerCertificateIds { get; }
     }
 }
