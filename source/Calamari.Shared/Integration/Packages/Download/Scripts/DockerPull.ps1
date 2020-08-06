6ab70b3857fd0a842317db4e256698889a91889c
Ben <ben.pearce@octopus.com>
2020-08-06 10:49:46 +1000

    improve checks for docker

--- source/Calamari.Shared/Integration/Packages/Download/Scripts/DockerPull.ps1
+++ source/Calamari.Shared/Integration/Packages/Download/Scripts/DockerPull.ps1
@@ -1,13 +1,25 @@
  function IsDockerAvailable() {
     $command = $(Get-Command 'docker' -ErrorAction SilentlyContinue)
     if ($command -eq $null) {
-        Write-Error 'docker command not available'
+        Write-Host 'docker command not available'
         return $false
     }
 
-    & docker ps 2>$null 
+    try {
+        & docker ps 1>$null 2>$null | Out-Null
+        if ($LASTEXITCODE -ne 0) {
+            Write-Host 'Docker was unable to connect to a docker service'
+            return $false
+        }
+    }
+    catch {
+        Write-Host 'Unable to connect to docker service'
+        return $false
+    }
+
     return $LASTEXITCODE -eq 0
 } 
+
 
 $IMAGE=$OctopusParameters["Image"]
 $dockerUsername=$OctopusParameters["DockerUsername"]
