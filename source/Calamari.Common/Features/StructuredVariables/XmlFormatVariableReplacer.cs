74be4e3cca29e03369fec41ea5dd360d411d09a1
David Young <thedewi@users.noreply.github.com>
2020-08-10 14:45:56 +1000

    XML replacer: upgrade output encoding if necessary to represent output

--- source/Calamari.Common/Features/StructuredVariables/XmlFormatVariableReplacer.cs
+++ source/Calamari.Common/Features/StructuredVariables/XmlFormatVariableReplacer.cs
@@ -97,17 +97,19 @@ namespace Calamari.Common.Features.StructuredVariables
                         }
                 }
 
-            using (var stream = new MemoryStream())
-            using (var textWriter = new StreamWriter(stream, encoding))
-            using (var writer = new XmlTextWriter(textWriter))
-            {
-                textWriter.NewLine = lineEnding == StringExtensions.LineEnding.Dos ? "\r\n" : "\n";
-                writer.Formatting = Formatting.Indented;
+            fileSystem.OverwriteFile(filePath,
+                                     textWriter =>
+                                     {
+                                         using (var writer = new XmlTextWriter(textWriter))
+                                         {
+                                             textWriter.NewLine = lineEnding == StringExtensions.LineEnding.Dos ? "\r\n" : "\n";
+                                             writer.Formatting = Formatting.Indented;
 
-                doc.WriteTo(writer);
-                writer.Close();
-                fileSystem.OverwriteFile(filePath, stream.ToArray());
-            }
+                                             doc.WriteTo(writer);
+                                             writer.Close();
+                                         }
+                                     },
+                                     encoding);
         }
 
         void TrySetInnerXml(XmlElement element, string xpathExpression, string variableValue)
