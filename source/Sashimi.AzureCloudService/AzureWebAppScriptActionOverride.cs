191b1672d97ec16c88f80e2a132894acd2621ad2
John Simons <john.simons@octopus.com>
2020-08-11 13:53:22 +1000

    We need the same for CloudService

---
+++ source/Sashimi.AzureCloudService/AzureWebAppScriptActionOverride.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Sashimi.AzureCloudService.Endpoints;
+using Sashimi.AzureScripting;
+using Sashimi.Server.Contracts;
+using Sashimi.Server.Contracts.ActionHandlers;
+
+namespace Sashimi.AzureCloudService
+{
+    class AzureCloudServicesScriptActionOverride : IScriptActionOverride
+    {
+        public ScriptActionOverrideResult ShouldOverride(DeploymentTargetType deploymentTargetType, IActionHandlerContext context)
+        {
+            return deploymentTargetType == AzureCloudServiceEndpoint.AzureCloudServiceDeploymentTargetType
+                ? ScriptActionOverrideResult.RedirectToHandler<AzurePowerShellActionHandler>()
+                :  ScriptActionOverrideResult.RunDefaultAction();
+        }
+    }
+}
