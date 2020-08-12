fdb8322bfd686c239cd543e0c311663ed86726f2
John Simons <john.simons@octopus.com>
2020-08-12 11:04:40 +1000

    Singular name

--- source/Sashimi/AzureConstants.cs
+++ source/Sashimi/AzureConstants.cs
@@ -7,6 +7,6 @@ namespace Sashimi.AzureCloudService
     static class AzureConstants
     {
         public static readonly ActionHandlerCategory AzureActionHandlerCategory = new ActionHandlerCategory("Azure", "Azure", 600);
-        public static CalamariFlavour CalamariAzure = new CalamariFlavour("Calamari.AzureCloudServices");
+        public static CalamariFlavour CalamariAzure = new CalamariFlavour("Calamari.AzureCloudService");
     }
 }
