cae0b1787545feac6e1d574f2b4776fdb560b345
John Simons <john.simons@octopus.com>
2020-08-13 15:55:45 +1000

    Repro test

---
+++ source/Calamari.Scripting.Tests/RunScriptCommandFixture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Calamari.Tests.Shared;
+using FluentAssertions;
+using NUnit.Framework;
+using Sashimi.Server.Contracts;
+using KnownVariables = Sashimi.Server.Contracts.KnownVariables;
+using ScriptSyntax = Calamari.Common.Features.Scripts.ScriptSyntax;
+
+namespace Calamari.Scripting.Tests
+{
+    [TestFixture]
+    public class RunScriptCommandFixture
+    {
+        [Test]
+        public Task ExecuteInlineScript()
+        {
+            var psScript = "echo 'Hello #{Name}'";
+            return CommandTestBuilder.CreateAsync<RunScriptCommand, Program>()
+                                     .WithArrange(context =>
+                                                  {
+                                                      context.Variables.Add(KnownVariables.Action.Script.ScriptSource, KnownVariableValues.Action.Script.ScriptSource.Inline);
+                                                      context.Variables.Add(KnownVariables.Action.Script.Syntax, ScriptSyntax.PowerShell.ToString());
+                                                      context.Variables.Add(KnownVariables.Action.Script.ScriptBody, psScript);
+                                                      context.Variables.Add("Name", "World");
+                                                  })
+                                     .WithAssert(result => result.FullLog.Should().Contain("Hello World"))
+                                     .Execute();
+        }
+    }
+}
