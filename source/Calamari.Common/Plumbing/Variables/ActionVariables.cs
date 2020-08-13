051fd090849f329ccf9cae1f604080fbdaee3b53
Corey Underdown <corey.underdown@octopus.com>
2020-08-13 14:12:51 +1000

    Added comment to Structured Config Fallback flag about removal date and who to contact

--- source/Calamari.Common/Plumbing/Variables/ActionVariables.cs
+++ source/Calamari.Common/Plumbing/Variables/ActionVariables.cs
@@ -14,6 +14,9 @@ namespace Calamari.Common.Plumbing.Variables
          the old Octopus Variable names. */
         public static readonly string StructuredConfigurationVariablesEnabled = "Octopus.Action.Package.JsonConfigurationVariablesEnabled";
         public static readonly string StructuredConfigurationVariablesTargets = "Octopus.Action.Package.JsonConfigurationVariablesTargets";
+        /* If this flag still exists after 2020.5.0 releases, please reach out to those involved with adding the fallback flag for
+         Structured Configuration in this PR (https://github.com/OctopusDeploy/Calamari/pull/629) so we can assess if the feature 
+         has been stable for long enough to tidy up the fallback flag. */
         public static readonly string StructuredConfigurationFallbackFlag = "Octopus.Action.StructuredConfigurationFallbackFlag";	
 
         public static string GetOutputVariableName(string actionName, string variableName)
