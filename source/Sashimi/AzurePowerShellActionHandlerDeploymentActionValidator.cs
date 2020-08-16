abd668f34af8ac5c619d1cf0f21861a79f1a1448
Frank Lin <frank.lin@octopus.com>
2020-08-17 08:59:55 +1000

    With all files

---
+++ source/Sashimi/AzurePowerShellActionHandlerDeploymentActionValidator.cs
@@ -0,0 +1,14 @@
+﻿﻿using System;
+using FluentValidation;
+using Sashimi.Server.Contracts.ActionHandlers.Validation;
+
+namespace Sashimi.AzureScripting
+{
+    class AzurePowerShellActionHandlerDeploymentActionValidator : IDeploymentActionValidator
+    {
+        public virtual void AddDeploymentValidationRule(AbstractValidator<DeploymentActionValidationContext> validator)
+        {
+            validator.Include(new AzurePowerShellActionHandlerValidator());
+        }
+    }
+}
