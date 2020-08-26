bc3055ac3b81eed29069684c18bd0b57be77400e
John Simons <john.simons@octopus.com>
2020-08-27 08:30:17 +1000

    One more test

--- source/Templates/Sashimi.Template.Wrangler.Tests/WranglerTests.cs
+++ source/Templates/Sashimi.Template.Wrangler.Tests/WranglerTests.cs
@@ -8,12 +8,15 @@ using NUnit.Framework;
 
 namespace Sashimi.Template.Wrangler.Tests
 {
+    [TestFixture]
     public class WranglerTests
     {
-        [Test]
-        public async Task CreatesSolution()
+        TemporaryDirectory installFolder;
+
+        [OneTimeSetUp]
+        public async Task Setup()
         {
-            using var installFolder = TemporaryDirectory.Create();
+            installFolder = TemporaryDirectory.Create();
             var currentPath = Path.GetDirectoryName(Uri.UnescapeDataString(new UriBuilder(Assembly.GetExecutingAssembly().CodeBase).Path).Replace('/', Path.DirectorySeparatorChar));
 
             var templatePath = Path.GetFullPath(Path.Combine(currentPath, "../../../../NewStep/Sashimi.NamingIsHard"));
@@ -21,10 +24,31 @@ namespace Sashimi.Template.Wrangler.Tests
             Copy(templatePath, installFolder.DirectoryPath);
 
             await Program.Main(new[] { "1.0.0", installFolder.DirectoryPath });
+        }
 
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            installFolder?.Dispose();
+        }
+
+        [Test]
+        public void CreatesSolution()
+        {
             var slnFile = Path.Combine(installFolder.DirectoryPath, "source/Sashimi.NamingIsHard.sln");
 
             File.Exists(slnFile).Should().BeTrue();
+        }
+
+        [Test]
+        public void UpdatedCsprojToIncludeOnlyProjectReferencesBetweenEachProject()
+        {
+            var csProjFiles = Directory.EnumerateFiles(Path.Combine(installFolder.DirectoryPath, "source"), "*.csproj", SearchOption.AllDirectories);
+            foreach (var projFile in csProjFiles)
+            {
+                var content = File.ReadAllText(projFile);
+                content.Should().NotContain("<ProjectReference Include=\"..\\..\\");
+            }
         }
 
         void Copy(string sourcePath, string destinationPath)
