470be9c4a583e0378b8a8ef1eb3757afa91330eb
Andy Hsu <andy.hsu@octopus.com>
2020-09-07 16:58:41 +1000

    Add AmazonWebServicesAccountVerifierFixture

---
+++ source/Sashimi.Aws.Accounts.Tests/AmazonWebServicesAccountVerifierFixture.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using Octopus.Server.Extensibility.Extensions.Infrastructure.Web.Api;
+using System;
+using System.Net.Http;
+using NSubstitute;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using FluentAssertions;
+using Calamari.Tests.Shared;
+using Octopus.Data.Model;
+
+namespace Sashimi.Aws.Accounts.Tests
+{
+    [TestFixture]
+    public class AmazonWebServicesAccountVerifierFixture
+    {
+        [Test]
+        public void Verify_ShouldUseTheHttpClientProvidedFromTheOctopusHttpClientFactory()
+        {
+            var httpMessageHandler = new TestHttpClientHandler();
+            var awsHttpClientFactory = new AwsHttpClientFactory(new Lazy<IOctopusHttpClientFactory>(
+            () => GetOctopusHttpClientFactory(httpMessageHandler)));
+            var verifier = new AmazonWebServicesAccountVerifier(awsHttpClientFactory);
+
+            verifier.Verify(new AmazonWebServicesAccountDetails
+            {
+                AccessKey = ExternalVariables.Get(ExternalVariable.AwsAcessKey),
+                SecretKey = ExternalVariables.Get(ExternalVariable.AwsSecretKey).ToSensitiveString()
+            });
+
+            httpMessageHandler.RequestLog.Should().ContainSingle(r => r.RequestUri.AbsoluteUri == "https://sts.amazonaws.com/");
+        }
+
+        static IOctopusHttpClientFactory GetOctopusHttpClientFactory(HttpMessageHandler httpMessageHandler)
+        {
+            var httpClientFactory = Substitute.For<IOctopusHttpClientFactory>();
+            httpClientFactory.CreateClient().Returns(new HttpClient(httpMessageHandler));
+
+            return httpClientFactory;
+        }
+
+        class TestHttpClientHandler : HttpClientHandler
+        {
+            public TestHttpClientHandler()
+            {
+                RequestLog = new List<HttpRequestMessage>();
+            }
+
+            public IList<HttpRequestMessage> RequestLog { get; }
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                RequestLog.Add(request);
+
+                return base.SendAsync(request, cancellationToken);
+            }
+        }
+    }
+}
