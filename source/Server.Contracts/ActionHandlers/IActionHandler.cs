ff71eb854ba89621c66208698b689ed94d750454
Mark Siedle <msiedle@gmail.com>
2020-09-21 13:25:43 +1000

    Toying with an idea of the named-properties lookup existing on the action handler for a given action type (similar to the keywords)

--- source/Server.Contracts/ActionHandlers/IActionHandler.cs
+++ source/Server.Contracts/ActionHandlers/IActionHandler.cs
@@ -1,4 +1,8 @@
-﻿namespace Sashimi.Server.Contracts.ActionHandlers
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Sashimi.Server.Contracts.ActionHandlers
 {
     /// <summary>
     /// Implementors of this interface must not keep state so that they can be reusable between steps and deployments
@@ -13,6 +17,12 @@
         bool WhenInAChildStepRunInTheContextOfTheTargetMachine { get; }
         bool CanRunOnDeploymentTarget { get; }
         ActionHandlerCategory[] Categories { get; }
+
+        /// <summary>
+        /// For config-as-code projects, various ID properties can be replaced with their name equivalent. This lookup allows us to inspect potential IdOrName properties.
+        /// E.g. Octopus.Action.Email.ToTeamIds, Octopus.Action.Azure.AccountId
+        /// </summary>
+        IEnumerable<string>? NamedPropertiesLookup { get; }
 
         IActionHandlerResult Execute(IActionHandlerContext context);
     }
