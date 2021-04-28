a70e3d772206b03ad72e09de6f4473ccea56453e
Michael Richardson <michael.richardson@hey.com>
2021-04-28 12:02:26 +1000

    Made AzureWebAppEndpoint internal to correct failing test

--- source/Sashimi/AzureWebAppEndpoint.cs
+++ source/Sashimi/AzureWebAppEndpoint.cs
@@ -2,7 +2,7 @@ using Sashimi.Server.Contracts;
 
 namespace Sashimi.AzureWebApp
 {
-    public class AzureWebAppEndpoint
+    class AzureWebAppEndpoint
     {
         public static readonly DeploymentTargetType AzureWebAppDeploymentTargetType = new DeploymentTargetType("AzureWebApp", "Azure Web Application");
     }
