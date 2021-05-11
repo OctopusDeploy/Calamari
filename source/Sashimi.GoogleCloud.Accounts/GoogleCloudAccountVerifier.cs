19bea8c8d2721e443f89e2adf8532d0bc0422c68
Ray Nham <ray.nham@octopus.com>
2021-05-11 13:52:38 +1000

    Added logic to verify GCP account

--- source/Sashimi.GoogleCloud.Accounts/GoogleCloudAccountVerifier.cs
+++ source/Sashimi.GoogleCloud.Accounts/GoogleCloudAccountVerifier.cs
@@ -1,3 +1,8 @@
+using System.Net;
+using Google;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Iam.v1;
+using Google.Apis.Services;
 using Sashimi.Server.Contracts.Accounts;
 
 namespace Sashimi.GoogleCloud.Accounts
@@ -7,6 +12,21 @@ namespace Sashimi.GoogleCloud.Accounts
         public void Verify(AccountDetails account)
         {
             var accountTyped = (GoogleCloudAccountDetails) account;
+            var credential = GoogleCredential.FromJson(accountTyped.JsonKey?.ToString());
+            var service = new IamService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential
+            });
+
+            var dummyRequest = service.Projects.ServiceAccounts.List("projects/" + "invalidProjectName");
+            try
+            {
+                dummyRequest.Execute();
+            }
+            catch (GoogleApiException exception)
+            {
+                if (exception.HttpStatusCode == HttpStatusCode.Unauthorized) throw;
+            }
         }
     }
 }
