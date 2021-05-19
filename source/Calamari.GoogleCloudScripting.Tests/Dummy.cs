41854e5e2e0ff79f40d1c23da503866cf5c5800b
John Simons <john.simons@octopus.com>
2021-05-20 09:56:36 +1000

    Adding Calamari.*.Tests project to make build happy

---
+++ source/Calamari.GoogleCloudScripting.Tests/Dummy.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Calamari.GoogleCloudScripting.Tests
+{
+    [TestFixture]
+    public class Dummy
+    {
+        [Test]
+        public void DummyTest()
+        {
+        }
+    }
+}
