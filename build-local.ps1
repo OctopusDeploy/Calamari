822330178925519cad20b741ffdbf508ae92ae59
Geoff Lamrock <geoff.lamrock@gmail.com>
2021-06-22 13:51:41 +1000

    Adds a local build option to improve local dev experience (#741)

---
+++ build-local.ps1
@@ -0,0 +1,29 @@
+Write-Host "
+##################################################################################################
+#                                                                                                #
+#  This is a helper script for running local Calamari builds, it's going to do the following:    #
+#    * Append a timestamp to the NuGet package versions to give you a unique version number      #
+#      without needing to commit your changes locally                                            #
+#    * Set the msbuild verbosity to minimal to reduce noise                                      #
+#    * Create NuGet packages for the various runtimes in parallel                                #
+#    * Skip creating .Tests NuGet packages                                                       #
+#    * Set the CalamariVersion property in Octopus.Server.csproj                                 #
+#                                                                                                # 
+#  This script is intended to only be run locally and not in CI.                                 #
+#                                                                                                #
+#  If something unexpected is happening in your build or Calamari changes you may want to run    #
+#  the full build by running ./build.ps1 and check again as something in the optimizations here  #
+#  might have caused an issue.                                                                   #
+#                                                                                                #
+##################################################################################################
+" -ForegroundColor Cyan
+
+./build.ps1 -Target Local -BuildVerbosity Minimal -PackInParallel -Timestamp -SetOctopusServerVersion
+
+Write-Host "
+########################################################################################
+#                                                                                      #
+#  Local build complete, restart your Octopus Server to test your Calamari changes :)  #
+#                                                                                      #
+########################################################################################
+" -ForegroundColor Cyan
