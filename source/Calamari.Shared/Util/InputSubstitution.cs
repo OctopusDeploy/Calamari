1cf03eda471d1281a2c924867496dc792b0e8514
Egor Pavlikhin <egor.pavlikhin@octopus.com>
2021-08-11 17:29:47 +1000

    Correctly parse NodeExecutor inputs (#759)
    * Parse inputs as Octostache template and then string replace evaluated variables

--- source/Calamari.Shared/Util/InputSubstitution.cs
+++ source/Calamari.Shared/Util/InputSubstitution.cs
@@ -1,5 +1,8 @@
 ﻿using System.Linq;
+using Calamari.Common.Commands;
+using Calamari.Common.Plumbing.Logging;
 using Calamari.Common.Plumbing.Variables;
+using Newtonsoft.Json;
 using Octostache;
 using Octostache.Templates;
 
@@ -7,24 +10,23 @@ namespace Calamari.Util
 {
     public static class InputSubstitution
     {
-        public static string SubstituteAndEscapeAllVariablesInJson(string jsonInputs, IVariables variables)
+        public static string SubstituteAndEscapeAllVariablesInJson(string jsonInputs, IVariables variables, ILog log)
         {
-            var tempVariableDictionaryToUseForExpandedVariables = new VariableDictionary();
-            var template = TemplateParser.ParseTemplate(jsonInputs);
+            if (!TemplateParser.TryParseTemplate(jsonInputs, out var template, out string error))
+            {
+                throw new CommandException($"Variable expression could not be parsed. Error: {error}");
+            }
+            
+            jsonInputs = template.ToString(); // we parse the template back to string to have a consistent representation of Octostache expressions
             foreach (var templateToken in template.Tokens)
             {
-                // TODO: we need change this to have a better way of escaping json string here
-                var arguments = templateToken.GetArguments();
-                if (!arguments.Any()) continue; // to avoid TextToken, which doesn't need to be evaluated
-                var variableName = templateToken.ToString()
-                                                .Replace("#{", string.Empty)
-                                                .Replace("}", string.Empty);
-                var expanded = variables.Evaluate($"#{{{variableName} | JsonEscape}}");
-                tempVariableDictionaryToUseForExpandedVariables.Add(variableName, expanded);
+                string evaluated = variables.Evaluate(templateToken.ToString());
+                if (templateToken.GetArguments().Any() && evaluated == templateToken.ToString())
+                    log.Warn($"Expression {templateToken.ToString()} could not be evaluated, check that the referenced variables exist.");
+                jsonInputs = jsonInputs.Replace($"\"{templateToken.ToString()}\"", JsonConvert.ToString(evaluated));
             }
 
-            var evaluatedJson = tempVariableDictionaryToUseForExpandedVariables.Evaluate(jsonInputs);
-            return evaluatedJson;
+            return jsonInputs;
         }
     }
 }
--- source/Calamari.Tests/Fixtures/Util/InputSubstitutionFixture.cs
+++ source/Calamari.Tests/Fixtures/Util/InputSubstitutionFixture.cs
@@ -1,8 +1,11 @@
 ﻿using Calamari.Util;
 using NUnit.Framework;
-using System.Collections.Generic;
 using System.Linq;
+using Calamari.Common.Commands;
+using Calamari.Common.Plumbing.Logging;
 using Calamari.Common.Plumbing.Variables;
+using FluentAssertions;
+using NSubstitute;
 
 namespace Calamari.Tests.Fixtures.Util
 {
@@ -10,17 +13,108 @@ namespace Calamari.Tests.Fixtures.Util
     public class InputSubstitutionFixture
     {
         [Test]
+        [TestCase("#{Octopus.Action.Package[package].ExtractedPath}", "Octopus.Action.Package[package].ExtractedPath=C:\\OctopusTest\\Api Test\\1\\Octopus-Primary\\Work\\20210804020317-7-11\\package",
+                     "C:\\\\OctopusTest\\\\Api Test\\\\1\\\\Octopus-Primary\\\\Work\\\\20210804020317-7-11\\\\package")]
+        [TestCase("#{MyPassword[#{UserName}]}", "MyPassword[username]=C:\\OctopusTest;UserName=username",
+                     "C:\\\\OctopusTest")]
+        [TestCase("#{if BoolVariable}#{TestVariable}#{/if}", "BoolVariable=True;TestVariable=C:\\OctopusTest",
+                     "C:\\\\OctopusTest")]
+        [TestCase("#{if BoolVariable}#{TestVariable}#{else}#{OtherVariable}#{/if}", "BoolVariable=False;TestVariable=C:\\OctopusTest;OtherVariable=D:\\TestFolder",
+                     "D:\\\\TestFolder")]
+        [TestCase("#{if Octopus.Environment.Name == \"Production\"}#{TestVariable}#{/if}", "Octopus.Environment.Name=Production;TestVariable=C:\\OctopusTest",
+                     "C:\\\\OctopusTest")]
+        [TestCase("#{each w in MyWidgets}'#{w.Value.WidgetId}': #{if w.Value.WidgetId == WidgetIdSelector}This is my Widget!#{else}No widget matched :(#{/if}#{/each}",
+                     "WidgetIdSelector=Widget-2;MyWidgets={\"One\":{\"WidgetId\":\"Widget-1\",\"Name\":\"Widget-One\"},\"Two\":{\"WidgetId\":\"Widget-2\",\"Name\":\"Widget-Two\"}}",
+                     @"'Widget-1': No widget matched :('Widget-2': This is my Widget!")]
+        [TestCase("#{each endpoint in Endpoints}#{endpoint},#{/each}", 
+                     "Endpoints=C:\\A,D:\\B",
+                     "C:\\\\A,D:\\\\B,")]
+        [TestCase("#{FilterVariable | ToUpper}", "FilterVariable=C:\\somelowercase",
+                     "C:\\\\SOMELOWERCASE")]
+        [TestCase("#{MyVar | Match \"a b\"}", "MyVar=a b c",
+                     "true")]
+        [TestCase("#{MyVar | StartsWith \"Ab\"}", "MyVar=Abc",
+                     "true")]
+        [TestCase("#{MyVar | Match #{pattern}}", "MyVar=a b c;pattern=a b",
+                     "true")]
+        [TestCase("#{MyVar | Substring \"8\" \"6\"}", "MyVar=Octopus Deploy",
+                     "Deploy")]
+        public void CommonExpressionsAreEvaluated(string expression, string variables, string expectedResult)
+        {
+            var variableDictionary = ParseVariables(variables);
+            string jsonInputs = "{\"testValue\":\"" + expression + "\"}";
+            var log = Substitute.For<ILog>();
+            string evaluatedInputs = InputSubstitution.SubstituteAndEscapeAllVariablesInJson(jsonInputs, variableDictionary, log);
+
+            string expectedEvaluatedInputs = "{\"testValue\":\"" + expectedResult + "\"}";
+            Assert.AreEqual(expectedEvaluatedInputs, evaluatedInputs);
+            log.DidNotReceive().Warn(Arg.Any<string>());
+        }
+        
+        [Test]
+        public void SimpleExpressionsAreEvaluated()
+        {
+            var variableDictionary = new CalamariVariables();
+            string jsonInputs = "{\"testValue\":\"#{ | NowDateUtc}\"}";
+            var log = Substitute.For<ILog>();
+            string evaluatedInputs = InputSubstitution.SubstituteAndEscapeAllVariablesInJson(jsonInputs, variableDictionary, log);
+
+            evaluatedInputs.Should().NotBeEmpty();
+            evaluatedInputs.Should().NotBeEquivalentTo(jsonInputs);
+            
+            log.DidNotReceive().Warn(Arg.Any<string>());
+        }
+        
+        [Test]
         public void VariablesInJsonInputsShouldBeEvaluated()
         {
             var variables = new CalamariVariables
             {
                 { "Octopus.Action.Package[package].ExtractedPath", "C:\\OctopusTest\\Api Test\\1\\Octopus-Primary\\Work\\20210804020317-7-11\\package" },
             };
-            var jsonInputs = "{\"containerNameOverride\":\"payload\",\"package\":{\"extractedToPath\":\"#{Octopus.Action.Package[package].ExtractedPath}\"},\"target\":{\"files\":[]}}";
-            var evaluatedInputs = InputSubstitution.SubstituteAndEscapeAllVariablesInJson(jsonInputs, variables);
+            string jsonInputs = "{\"containerNameOverride\":\"payload\",\"package\":{\"extractedToPath\":\"#{Octopus.Action.Package[package].ExtractedPath}\"},\"target\":{\"files\":[{\"path\":\"azure-blob-container-target.0.0.0.zip\",\"fileName\":{\"type\":\"original file name\"}}]}}";
+            string evaluatedInputs = InputSubstitution.SubstituteAndEscapeAllVariablesInJson(jsonInputs, variables, Substitute.For<ILog>());
 
-            var expectedEvaluatedInputs = "{\"containerNameOverride\":\"payload\",\"package\":{\"extractedToPath\":\"C:\\\\OctopusTest\\\\Api Test\\\\1\\\\Octopus-Primary\\\\Work\\\\20210804020317-7-11\\\\package\"},\"target\":{\"files\":[]}}";
-            Assert.AreEqual(evaluatedInputs, expectedEvaluatedInputs);
+            string expectedEvaluatedInputs = "{\"containerNameOverride\":\"payload\",\"package\":{\"extractedToPath\":\"C:\\\\OctopusTest\\\\Api Test\\\\1\\\\Octopus-Primary\\\\Work\\\\20210804020317-7-11\\\\package\"},\"target\":{\"files\":[{\"path\":\"azure-blob-container-target.0.0.0.zip\",\"fileName\":{\"type\":\"original file name\"}}]}}";
+            Assert.AreEqual(expectedEvaluatedInputs, evaluatedInputs);
+        }
+            
+        [Test]
+        public void MissingVariableValue_LogsAWarning()
+        {
+            var variables = new CalamariVariables
+            {
+            };
+            string jsonInputs = "{\"containerNameOverride\":\"payload\",\"package\":{\"extractedToPath\":\"#{Octopus.Action.Package[package].ExtractedPath}\"},\"target\":{\"files\":[]}}";
+            var log = Substitute.For<ILog>();
+            InputSubstitution.SubstituteAndEscapeAllVariablesInJson(jsonInputs, variables, log);
+            log.Received().Warn(Arg.Any<string>());
+        }
+        
+        [Test]
+        public void InvalidVariableExpressionFails()
+        {
+            var variables = new CalamariVariables
+            {
+            };
+            string jsonInputs = "{\"package\":{\"extractedToPath\":\"#{Octopus.Action.Package[package]...ExtractedPath}\"}}";
+            Assert.Throws<CommandException>(() => InputSubstitution.SubstituteAndEscapeAllVariablesInJson(jsonInputs, variables, Substitute.For<ILog>()));
+        }
+
+        CalamariVariables ParseVariables(string variableDefinitions)
+        {
+            var variables = new CalamariVariables();
+
+            var items = variableDefinitions.Split(';');
+            foreach (var item in items)
+            {
+                var pair = item.Split('=');
+                var key = pair.First();
+                var value = pair.Last();
+                variables[key] = value;
+            }
+
+            return variables;
         }
     }
 }
