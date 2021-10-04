b677f5ae82171bd7709aafb905e185d1b3754bae
Shaun Hevey <shaun.hevey@octopus.com>
2021-10-04 17:09:13 +1100

    adding the ability to fallback to registration entries to try and find NuGet download uri

--- source/Calamari.Shared/Integration/Packages/NuGet/NuGetV3Downloader.cs
+++ source/Calamari.Shared/Integration/Packages/NuGet/NuGetV3Downloader.cs
@@ -9,23 +9,51 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using Calamari.Common.Commands;
-using Calamari.Common.Plumbing.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Octopus.Versioning;
+using Octopus.Versioning.Semver;
 
 namespace Calamari.Integration.Packages.NuGet
 {
     internal static class NuGetV3Downloader
     {
+        public static bool CanHandle(Uri feedUri, ICredentials feedCredentials, TimeSpan httpTimeout)
+        {
+            if (feedUri.ToString().EndsWith(".json", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            
+            return IsJsonEndpoint(feedUri, feedCredentials, httpTimeout);
+        }
+
+        static bool IsJsonEndpoint(Uri feedUri, ICredentials feedCredentials, TimeSpan httpTimeout)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, feedUri);
+
+            using (var httpClient = CreateHttpClient(feedCredentials, httpTimeout))
+            {
+                var sending = httpClient.SendAsync(request);
+                sending.Wait();
+                
+                using (var response = sending.Result)
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    return response.Content.Headers.ContentType.MediaType == "application/json";
+                }
+            }
+        }
+
         public static void DownloadPackage(string packageId, IVersion version, Uri feedUri, ICredentials feedCredentials, string targetFilePath, TimeSpan httpTimeout)
         {
-            var normalizedId = packageId.ToLowerInvariant();
-            var normalizedVersion = version.ToString().ToLowerInvariant();
-            var packageBaseUri = GetPackageBaseUri(feedUri, feedCredentials, httpTimeout).AbsoluteUri.TrimEnd('/');
-            var downloadUri = new Uri($"{packageBaseUri}/{normalizedId}/{normalizedVersion}/{normalizedId}.{normalizedVersion}.nupkg");
+            var downloadUri = DownloadUri(packageId, version, feedUri, feedCredentials, httpTimeout);
 
-            Log.Verbose($"Downloading package from '{downloadUri}'");
+            if (downloadUri == null)
+            {
+                throw new InvalidOperationException($"Unable to find url to download package: {version} with version: {version} from feed: {feedUri}");
+            }
 
             using (var nupkgFile = new FileStream(targetFilePath, FileMode.Create, FileAccess.ReadWrite, FileShare.None))
             {
@@ -34,6 +62,106 @@ namespace Calamari.Integration.Packages.NuGet
                     pkgStream.CopyTo(nupkgFile);
                 });
             }
+        }
+
+        static Uri? DownloadUri(string packageId, IVersion version, Uri feedUri, ICredentials feedCredentials, TimeSpan httpTimeout)
+        {
+            var normalizedId = packageId.ToLowerInvariant();
+            var normalizedVersion = version.ToString().ToLowerInvariant();
+            
+            var packageBaseDownloadUri = GetPackageBaseDownloadUri(feedUri, feedCredentials, httpTimeout, normalizedId, normalizedVersion, out IDictionary<string, List<Uri>> resources);
+            if (packageBaseDownloadUri != null) return packageBaseDownloadUri;
+
+            return GetPackageRegistrationDownloadUri(version, feedCredentials, httpTimeout, resources, normalizedId, normalizedVersion);
+        }
+
+        static Uri? GetPackageRegistrationDownloadUri(IVersion version, ICredentials feedCredentials, TimeSpan httpTimeout, IDictionary<string, List<Uri>> resources, string normalizedId, string normalizedVersion)
+        {
+            var packageRegistrationUri = GetPackageRegistrationUri(resources, normalizedId);
+
+            var packageRegistrationResponse = GetJsonResponse(packageRegistrationUri, feedCredentials, httpTimeout);
+            var packageRegistrationId = packageRegistrationResponse["@id"];
+
+            if (packageRegistrationId.Type == JTokenType.String)
+            {
+                var packageRegistrationIdResponse = GetJsonResponse(new Uri(packageRegistrationId.ToString()), feedCredentials, httpTimeout);
+
+                if (packageRegistrationIdResponse.TryGetValue("items", out JToken entries))
+                {
+                    foreach (var entry in entries)
+                    {
+                        var entryObject = entry.ToObject<JObject>();
+                        var entryObjectId = entryObject["@id"];
+
+                        if (entryObjectId == null)
+                        {
+                            continue;
+                        }
+
+                        var entryObjectIdResponse = GetJsonResponse(new Uri(entryObjectId.ToString()), feedCredentials, httpTimeout);
+
+                        if (entryObjectIdResponse.TryGetValue("items", out JToken catalogItems))
+                        {
+                            foreach (var catalogItem in catalogItems)
+                            {
+                                var catalogEntry = catalogItem["catalogEntry"];
+
+                                if (catalogEntry?["version"] != null)
+                                {
+                                    var packageVersion = new SemanticVersion(catalogEntry["version"].ToString());
+
+                                    if (VersionComparer.Default.Compare(packageVersion, version) == 0)
+                                    {
+                                        var url = catalogEntry["packageContent"]?.ToString();
+                                        if (url == null) continue;
+                                        {
+                                            return new Uri($"{url.Remove(url.LastIndexOfAny("/".ToCharArray()) + 1)}{normalizedVersion}");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        static Uri? GetPackageBaseDownloadUri(Uri feedUri, ICredentials feedCredentials, TimeSpan httpTimeout, string normalizedId, string normalizedVersion, out IDictionary<string, List<Uri>>? resources)
+        {
+            
+            var json = GetServiceIndexJson(feedUri, feedCredentials, httpTimeout);
+
+            if (json == null)
+            {
+                throw new CommandException($"'{feedUri}' is not a valid NuGet v3 feed");
+            }
+
+            resources = GetServiceResources(json);
+
+            var packageBaseUri = GetPackageBaseUri(resources);
+
+            if (packageBaseUri?.AbsoluteUri.TrimEnd('/') != null)
+            {
+                {
+                    return new Uri($"{packageBaseUri}/{normalizedId}/{normalizedVersion}/{normalizedId}.{normalizedVersion}.nupkg");
+                }
+            }
+
+            return null;
+        }
+
+        static Uri GetPackageRegistrationUri(IDictionary<string, List<Uri>> resources, string normalizedId)
+        {
+            var registrationUrl = NuGetServiceTypes.RegistrationsBaseUrl
+                                                   .Where(serviceType => resources.ContainsKey(serviceType))
+                                                   .SelectMany(serviceType => resources[serviceType])
+                                                   .First()
+                                                   .OriginalString.TrimEnd('/');
+
+            var packageRegistrationUri = new Uri($"{registrationUrl}/{normalizedId}/index.json");
+            return packageRegistrationUri;
         }
 
         static HttpClient CreateHttpClient(ICredentials credentials, TimeSpan httpTimeout)
@@ -72,33 +200,40 @@ namespace Calamari.Integration.Packages.NuGet
             }
         }
 
-        static Uri GetPackageBaseUri(Uri feedUri, ICredentials feedCredentials, TimeSpan httpTimeout)
+        static Uri? GetPackageBaseUri(IDictionary<string, List<Uri>> resources)
         {
-            // Parse JSON for package base URL
-            JObject json = null;
-            GetHttp(feedUri, feedCredentials, httpTimeout, stream =>
-            {
-                using (var streamReader = new StreamReader(stream))
-                using (var jsonReader = new JsonTextReader(streamReader))
-                {
-                    json = JObject.Load(jsonReader);
-                }
-            });
-
-            if (!IsValidV3Json(json))
-                throw new CommandException($"'{feedUri}' is not a valid NuGet v3 feed");
-
-            var resources = GetServiceResources(json);
-
             // If index.json contains a flat container resource use that to directly
             // construct package download urls.
             if (resources.ContainsKey(NuGetServiceTypes.PackageBaseAddress))
                 return resources[NuGetServiceTypes.PackageBaseAddress].FirstOrDefault();
+            return null;
+        }
 
-            return NuGetServiceTypes.RegistrationsBaseUrl
-                .Where(serviceType => resources.ContainsKey(serviceType))
-                .SelectMany(serviceType => resources[serviceType])
-                .First();
+        static JObject? GetServiceIndexJson(Uri feedUri, ICredentials feedCredentials, TimeSpan httpTimeout)
+        {
+            var json = GetJsonResponse(feedUri, feedCredentials, httpTimeout);
+
+            if (!IsValidV3Json(json)) return null;
+            
+            return json;
+        }
+
+        static JObject GetJsonResponse(Uri feedUri, ICredentials feedCredentials, TimeSpan httpTimeout)
+        {
+            // Parse JSON for package base URL
+            JObject json = null;
+            GetHttp(feedUri,
+                    feedCredentials,
+                    httpTimeout,
+                    stream =>
+                    {
+                        using (var streamReader = new StreamReader(stream))
+                        using (var jsonReader = new JsonTextReader(streamReader))
+                        {
+                            json = JObject.Load(jsonReader);
+                        }
+                    });
+            return json;
         }
 
         static bool IsValidV3Json(JObject json)
--- source/Calamari.Tests/Fixtures/PackageDownload/NuGetFeedSupportFixture.cs
+++ source/Calamari.Tests/Fixtures/PackageDownload/NuGetFeedSupportFixture.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
+using System.Linq;
 using Calamari.Common.Features.Packages;
 using Calamari.Tests.Helpers;
 using NUnit.Framework;
@@ -9,16 +11,21 @@ using Octopus.Versioning;
 namespace Calamari.Tests.Fixtures.PackageDownload
 {
     [TestFixture]
+    [Category(TestCategory.CompatibleOS.OnlyWindows)]
+    
     public class NuGetFeedVersionSupportFixture : CalamariFixture
     {
-        const string PackageId = "Calamari.Tests.Fixtures.PackageDownload.NuGetFeedSupport";
+        const string FeedzPackageId = "Calamari.Tests.Fixtures.PackageDownload.NuGetFeedSupport";
+        const string ArtifactoryPackageId = "Artifactory.Test.NuGet";
         
         // TODO: Packages here were generated using the nuspec file in the .\NuGetFeedSupport folder
         // Right now, they have been manually uploaded to the feedz.io repository below.
         // In future, we should ensure this test fixture sets its own data up from scratch before running
         // and tears it down on completion, rather than relying on external state as it currently does.
-        const string NuGetV2FeedUrl = "https://f.feedz.io/octopus-deploy/integration-tests/nuget";
-        const string NuGetV3FeedUrl = "https://f.feedz.io/octopus-deploy/integration-tests/nuget/index.json";
+        const string FeedzNuGetV2FeedUrl = "https://f.feedz.io/octopus-deploy/integration-tests/nuget";
+        const string FeedzNuGetV3FeedUrl = "https://f.feedz.io/octopus-deploy/integration-tests/nuget/index.json";
+        const string ArtifactoryNuGetV2FeedUrl = "https://nuget.packages.octopushq.com/";
+        const string ArtifactoryNuGetV3FeedUrl = "https://packages.octopushq.com/artifactory/api/nuget/v3/nuget";
         
         static readonly string TentacleHome = TestEnvironment.GetTestPath("Fixtures", "PackageDownload");
 
@@ -45,66 +52,81 @@ namespace Calamari.Tests.Fixtures.PackageDownload
         }
         
         [Test]
-        [TestCaseSource(nameof(NuGet2SupportedVersionStrings))]
-        public void ShouldSupportNuGetVersion2Feeds(string versionString)
+        [TestCaseSource(nameof(FeedzNuGet2SupportedVersionStrings))]
+        public void ShouldSupportFeedzNuGetVersion2Feeds(string versionString)
         {
-            var calamariResult = DownloadPackage(PackageId, versionString, "nuget-local", NuGetV2FeedUrl);
+            var calamariResult = DownloadPackage(FeedzPackageId, versionString, "nuget-local", FeedzNuGetV2FeedUrl);
+            calamariResult.AssertSuccess();
+        }
+        
+        [Test]
+        [TestCaseSource(nameof(FeedzNuGet2SupportedVersionStrings))]
+        [TestCaseSource(nameof(FeedzNuGet3SupportedVersionStrings))]
+        public void ShouldSupportFeedzNuGetVersion3Feeds(string versionString)
+        {
+            var calamariResult = DownloadPackage(FeedzPackageId, versionString, "nuget-local", FeedzNuGetV3FeedUrl);
             
             calamariResult.AssertSuccess();
         }
         
         [Test]
-        [TestCaseSource(nameof(NuGet3SupportedVersionStrings))]
-        public void ShouldNotSupportSemVer2OnNuGetVersion2Feeds(string versionString)
+        [TestCaseSource(nameof(ArtifactoryNuGet3SupportedVersionStrings))]
+        [TestCaseSource(nameof(ArtifactoryNuGet2SupportedVersionStrings))]
+        [Platform("Net-4.5")]
+        public void ArtifactoryShouldSupportNuGetVersion3Feeds(string versionString)
         {
-            var calamariResult = DownloadPackage(PackageId, versionString, "nuget-local", NuGetV2FeedUrl);
-            
-            calamariResult.AssertFailure();
-            calamariResult.AssertOutput($"Could not find package Calamari.Tests.Fixtures.PackageDownload.NuGetFeedSupport {versionString} in feed: '{NuGetV2FeedUrl}'");
+            var calamariResult = DownloadPackage(ArtifactoryPackageId,  versionString, "nuget-local", ArtifactoryNuGetV3FeedUrl);
+            calamariResult.AssertSuccess();
         }
-
+        
         [Test]
-        [TestCaseSource(nameof(NuGet2SupportedVersionStrings))]
-        [TestCaseSource(nameof(NuGet3SupportedVersionStrings))]
-        public void ShouldSupportNuGetVersion3Feeds(string versionString)
+        [TestCaseSource(nameof(ArtifactoryNuGet2SupportedVersionStrings))]
+        public void ArtifactoryShouldSupportNuGetVersion2Feeds(string versionString)
         {
-            var calamariResult = DownloadPackage(PackageId, versionString, "nuget-local", NuGetV3FeedUrl);
-            
+            var calamariResult = DownloadPackage(ArtifactoryPackageId,  versionString, "nuget-local", ArtifactoryNuGetV2FeedUrl);
             calamariResult.AssertSuccess();
         }
 
-        public static IEnumerable<TestCaseData> NuGet2SupportedVersionStrings
+       
+
+        public static IEnumerable<TestCaseData> FeedzNuGet2SupportedVersionStrings
         {
             get
             {
-                yield return new TestCaseData("0.0.1").SetDescription("Pre-v1 Patch Version");
-                yield return new TestCaseData("0.5.1").SetDescription("Pre-v1 Minor Version");
                 yield return new TestCaseData("1.0.0");
                 yield return new TestCaseData("1.4.92").SetDescription("Multi-digit Patch Version");
-                yield return new TestCaseData("1.101.0").SetDescription("Multi-digit Minor Version");
-                yield return new TestCaseData("101.0.0").SetDescription("Multi-digit Major Version");
-                yield return new TestCaseData("2.0.0-alpha").SetDescription("SemVer 1.0 alpha pre-release");
                 yield return new TestCaseData("2.0.0-beta").SetDescription("SemVer 1.0 beta pre-release");
             }
         }
 
-        public static IEnumerable<TestCaseData> NuGet3SupportedVersionStrings
+        public static IEnumerable<TestCaseData> FeedzNuGet3SupportedVersionStrings
         {
             get
             {
-                yield return new TestCaseData("2.0.0-beta.2").SetDescription("Pre-release version with dot suffix");
-                yield return new TestCaseData("2.0.0-beta.2.1").SetDescription("Pre-release version with double-dot suffix");
+
                 yield return new TestCaseData("2.0.0-beta+abcd16bd").SetDescription("Pre-release version with metadata");
                 yield return new TestCaseData("2.0.0-beta.1+abcd16bd").SetDescription("Pre-release version with dot suffix and metadata");
+                yield return new TestCaseData("2.0.0-beta.2").SetDescription("Pre-release version with dot suffix");
+                yield return new TestCaseData("2.0.0-beta.2.1").SetDescription("Pre-release version with double-dot suffix");
+            }
+        }
+        public static IEnumerable<TestCaseData> ArtifactoryNuGet2SupportedVersionStrings
+        {
+            get
+            {
+                yield return new TestCaseData("1.0.0").SetDescription("Pre-release version with metadata");
+            }
+        }
+
+        public static IEnumerable<TestCaseData> ArtifactoryNuGet3SupportedVersionStrings
+        {
+            get
+            {
+                yield return new TestCaseData("1.0.0-alpha.3+metadata").SetDescription("Pre-release version with dot suffix and metadata");
             }
         }
         
-        CalamariResult DownloadPackage(string packageId,
-                                       string packageVersion,
-                                       string feedId,
-                                       string feedUri,
-                                       string feedUsername = "",
-                                       string feedPassword = "")
+        CalamariResult DownloadPackage(string packageId, string packageVersion, string feedId, string feedUri, string feedUsername = "", string feedPassword = "")
         {
             var calamari = Calamari()
                            .Action("download-package")
@@ -122,7 +144,8 @@ namespace Calamari.Tests.Fixtures.PackageDownload
 
             if (!string.IsNullOrWhiteSpace(feedPassword))
                 calamari.Argument("feedPassword", feedPassword);
-
+            
+            
             return Invoke(calamari);
         }
     }
