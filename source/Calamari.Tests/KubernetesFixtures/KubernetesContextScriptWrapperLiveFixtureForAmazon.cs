95a3c329edb75f8babf24dc2024f12636604a9ca
Matt Hilton <matt.hilton@octopus.com>
2021-10-20 15:01:16 +1100

    Reverts incorrect change to KubernetesContextScriptWrapperLiveFixtureForAmazon

--- source/Calamari.Tests/KubernetesFixtures/KubernetesContextScriptWrapperLiveFixtureForAmazon.cs
+++ source/Calamari.Tests/KubernetesFixtures/KubernetesContextScriptWrapperLiveFixtureForAmazon.cs
@@ -13,18 +13,18 @@ namespace Calamari.Tests.KubernetesFixtures
         [Test]
         public void AuthoriseWithAmazonEC2Role()
         {
-            // This fixture is marked Explicit, so it's always ignored unless you explicitly run it.
-            // Please BYO EKS Cluster and fill in the variables below if you want to run it.
-            var eksClusterUrl = "";
-            var eksClusterName = "";
-            var eksClusterRegion = "";
-
+            // This is a special test fixture, that gets remotely executed on the cluster created by the test 
+            // Calamari.Tests.KubernetesFixtures.KubernetesContextScriptWrapperLiveFixture.UsingEc2Instance
+            // (see Terraform/EC2/ec2.kubernetes.tf and Terraform/EC2/test.sh)
+            //
+            // It's allowed to access environment variables directly because of this specialness.
+            // It's ignored from direct runs locally or on CI using the [Explicit] attribute.
             variables.Set(Deployment.SpecialVariables.Account.AccountType, "AmazonWebServicesAccount");
-            variables.Set(SpecialVariables.ClusterUrl, eksClusterUrl);
-            variables.Set(SpecialVariables.EksClusterName, eksClusterName);
+            variables.Set(SpecialVariables.ClusterUrl, Environment.GetEnvironmentVariable("AWS_CLUSTER_URL"));
+            variables.Set(SpecialVariables.EksClusterName, Environment.GetEnvironmentVariable("AWS_CLUSTER_NAME"));
             variables.Set(SpecialVariables.SkipTlsVerification, Boolean.TrueString);
             variables.Set("Octopus.Action.Aws.AssumeRole", Boolean.FalseString);
-            variables.Set("Octopus.Action.Aws.Region", eksClusterRegion);
+            variables.Set("Octopus.Action.Aws.Region", Environment.GetEnvironmentVariable("AWS_REGION"));
 
             var wrapper = CreateWrapper();
             TestScript(wrapper, "Test-Script");
