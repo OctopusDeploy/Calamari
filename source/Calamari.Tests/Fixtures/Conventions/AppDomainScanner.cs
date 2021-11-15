dc47407fb6ab635f1db6b37eb3f8e037a7e031d7
Nelson Susanto <nelson.susanto@octopus.com>
2021-11-15 11:58:55 +1100

    Add convention test to prevent anyone from newing up a repository without going via the factory

---
+++ source/Calamari.Tests/Fixtures/Conventions/AppDomainScanner.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Calamari.Tests.Fixtures.Conventions
+{
+    public static class AppDomainScanner
+    {
+        static readonly Lazy<Type[]> CalamariTypesLazy;
+
+        static AppDomainScanner()
+        {
+
+            CalamariTypesLazy = new Lazy<Type[]>(ScanForCalamariTypes);
+        }
+
+        public static IReadOnlyCollection<Type> CalamariTypes => CalamariTypesLazy.Value;
+
+        static Type[] ScanForCalamariTypes()
+        {
+            var calamariAssemblies = AppDomain.CurrentDomain.GetAssemblies().Where(IsCalamariAssembly);
+            var types = calamariAssemblies
+                .SelectMany(a =>
+                    {
+                        try
+                        {
+                            return a.DefinedTypes.ToArray();
+                        }
+                        catch (ReflectionTypeLoadException)
+                        {
+                            return Array.Empty<Type>();
+                        }
+                        catch (TypeLoadException)
+                        {
+                            return Array.Empty<Type>();
+                        }
+                    })
+                .ToArray();
+
+            return types;
+        }
+
+        static bool IsCalamariAssembly(Assembly assembly)
+        {
+            if (assembly.IsDynamic) return false;
+            return assembly.GetName().Name?.StartsWith("Calamari") ?? false;
+        }
+    }
+}
---
+++ source/Calamari.Tests/Fixtures/Conventions/ConventionSpecifications/DecompilationCache.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Conventional.Conventions.Cecil;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+// ReSharper disable ReplaceWithSingleCallToFirstOrDefault
+// ReSharper disable ReplaceWithSingleCallToFirst
+
+namespace Calamari.Tests.Fixtures.Conventions.ConventionSpecifications
+{
+    // Taken from Octopus.Tests.Common.Conventions.ConventionSpecifications
+    static class DecompilationCache
+    {
+        static readonly ConcurrentDictionary<string, ModuleDefinition> ModuleDefinitionsByAssemblyLocation;
+        static readonly ConcurrentDictionary<Type, TypeDefinition> TypeDefinitionsByType;
+        static readonly ConcurrentDictionary<Type, IReadOnlyCollection<Instruction>> InstructionsByType;
+        static readonly object CecilMutex; // Cecil is not internally thread-safe in some places
+
+        static DecompilationCache()
+        {
+            ModuleDefinitionsByAssemblyLocation = new ConcurrentDictionary<string, ModuleDefinition>();
+            TypeDefinitionsByType = new ConcurrentDictionary<Type, TypeDefinition>();
+            InstructionsByType = new ConcurrentDictionary<Type, IReadOnlyCollection<Instruction>>();
+            CecilMutex = new object();
+        }
+
+        public static IReadOnlyCollection<Instruction> InstructionsFor(Type type)
+        {
+            return InstructionsByType.GetOrAdd(type, GetInstructionsFor);
+        }
+
+        static IReadOnlyCollection<Instruction> GetInstructionsFor(Type type)
+        {
+            var typeDefinition = TypeDefinitionsByType.GetOrAdd(type, GetTypeDefinitionFor);
+
+            lock (CecilMutex)
+            {
+                var typeInstructions =
+                    typeDefinition
+                        .Methods
+                        .Where(method => method.HasBody)
+                        .SelectMany(method => method.Body.Instructions)
+                        .Union(
+                            typeDefinition
+                                .Methods
+                                .Where(x => x.HasAttribute<AsyncStateMachineAttribute>())
+                                .SelectMany(x => x.GetAsyncStateMachineType().Methods.Where(method => method.HasBody))
+                                .SelectMany(method => method.Body.Instructions))
+                        .Union(
+                            typeDefinition
+                                .Methods
+                                .Where(x => x.HasAttribute<IteratorStateMachineAttribute>())
+                                .SelectMany(x => x.GetIteratorStateMachineType().Methods.Where(method => method.HasBody))
+                                .SelectMany(method => method.Body.Instructions))
+                        .Distinct()
+                        .ToArray();
+
+                return typeInstructions;
+            }
+        }
+
+        public static MethodDefinition GetMethodDefinitionFor(MethodInfo method)
+        {
+            var typeDefinition = GetTypeDefinitionFor(method.DeclaringType);
+
+            var methodDefinition = typeDefinition.Methods
+                .Where(m => m.Name == method.Name)
+                .Where(m => m.Parameters.Count == method.GetParameters().Length)
+                .First();
+            return methodDefinition;
+        }
+
+        public static TypeDefinition GetTypeDefinitionFor(Type type)
+        {
+            if (type.IsNested && type.DeclaringType != null)
+            {
+                var parentTypeDefinition = GetTypeDefinitionFor(type.DeclaringType);
+
+                // ReSharper disable once ReplaceWithSingleCallToSingleOrDefault
+                var typeDefinition = parentTypeDefinition.NestedTypes.Where(td => td.Name == type.Name).SingleOrDefault()
+                    ?? throw new Exception($"Could not find nested type {type.Name} in declaring type {parentTypeDefinition.FullName}");
+
+                return typeDefinition;
+            }
+
+            var result = TypeDefinitionsByType.GetOrAdd(type,
+                t =>
+                {
+                    var moduleDefinition = ModuleDefinitionsByAssemblyLocation.GetOrAdd(t.Assembly.Location, GetModuleDefinitionByLocation);
+                    var typeDefinition = (TypeDefinition) moduleDefinition.GetType(type.FullName, true);
+                    if (typeDefinition is null)
+                    {
+                        var additionalMessage = string.Join(Environment.NewLine, moduleDefinition.Types
+                            .Select(td => $" - {td.FullName}"));
+                        throw new InvalidOperationException($"Could not find type {t.FullName} in {t.Assembly.Location}.{Environment.NewLine}Types we can see: {additionalMessage}");
+                    }
+
+                    return typeDefinition;
+                });
+
+            return result;
+        }
+
+        static ModuleDefinition GetModuleDefinitionByLocation(string location)
+        {
+            var moduleDefinition = ModuleDefinition.ReadModule(location,
+                new ReaderParameters
+                {
+                    AssemblyResolver = new ConventionalAssemblyResolver()
+                });
+
+            return moduleDefinition;
+        }
+    }
+}
