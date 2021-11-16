4601feb24f6cd88613ea6988724ec25e0c8c08e1
Nelson Susanto <nelson.susanto@octopus.com>
2021-11-16 13:33:46 +1100

    Add semaphore factory to factory

--- source/Calamari/Deployment/PackageRetention/Repositories/JsonJournalRepositoryFactory.cs
+++ source/Calamari/Deployment/PackageRetention/Repositories/JsonJournalRepositoryFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Calamari.Common.Features.Processes.Semaphores;
 using Calamari.Common.Plumbing.FileSystem;
 
 namespace Calamari.Deployment.PackageRetention.Repositories
@@ -8,18 +9,20 @@ namespace Calamari.Deployment.PackageRetention.Repositories
         internal const string DefaultJournalName = "PackageRetentionJournal.json";
 
         readonly ICalamariFileSystem fileSystem;
+        readonly ISemaphoreFactory semaphoreFactory;
         readonly string journalPath;
 
-        public JsonJournalRepositoryFactory(ICalamariFileSystem fileSystem)
+        public JsonJournalRepositoryFactory(ICalamariFileSystem fileSystem, ISemaphoreFactory semaphoreFactory)
         {
             this.fileSystem = fileSystem;
+            this.semaphoreFactory = semaphoreFactory;
 
             this.journalPath = @"C:\Octopus\PackageJournal.json";//journalPath;
         }
 
         public IJournalRepository CreateJournalRepository()
         {
-            return new JsonJournalRepository(fileSystem, journalPath);
+            return new JsonJournalRepository(fileSystem, semaphoreFactory, journalPath);
         }
     }
 }
