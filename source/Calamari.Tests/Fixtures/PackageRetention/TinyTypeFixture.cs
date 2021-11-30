9aa4e468e7924587c58153867b6d604b56c29ea0
MissedTheMark <mark.coafield@octopus.com>
2021-11-30 14:38:58 +1100

    Added tests, fixed couple of things the tests found

---
+++ source/Calamari.Tests/Fixtures/PackageRetention/TinyTypeFixture.cs
@@ -0,0 +1,39 @@
+﻿using Calamari.Deployment.PackageRetention;
+using NUnit.Framework;
+
+namespace Calamari.Tests.Fixtures.PackageRetention
+{
+    [TestFixture]
+    public class TinyTypeFixture
+    {
+        [Test]
+        public void EquivalentValuesAreEqualTinyTypes()
+        {
+            var tt1 = ATinyType.Create<ATinyType>("Value");
+            var tt2 = AnotherTinyType.Create<AnotherTinyType>("Value");
+            Assert.AreEqual(tt1, tt2);
+        }
+
+        [Test]
+        public void EquivalentValuesHaveEqualHashCodes()
+        {
+            var tt1 = ATinyType.Create<ATinyType>("Value");
+            var tt2 = AnotherTinyType.Create<AnotherTinyType>("Value");
+            Assert.AreEqual(tt1.GetHashCode(), tt2.GetHashCode());
+        }
+
+        class ATinyType : TinyType<string>
+        {
+            public ATinyType(string value) : base(value)
+            {
+            }
+        }
+
+        class AnotherTinyType : TinyType<string>
+        {
+            public AnotherTinyType(string value) : base(value)
+            {
+            }
+        }
+    }
+}
