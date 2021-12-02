17c68890fa7e748d5da2fc53d093d4c2c986c229
Mark Coafield <46470837+MissedTheMark@users.noreply.github.com>
2021-12-02 11:34:21 +1100

    Created TinyType class (#801)
    Added TinyType class and moved some details to that from CaseInsensitiveTinyType

--- source/Calamari.Common/Plumbing/Deployment/PackageRetention/CaseInsensitiveTinyType.cs
+++ source/Calamari.Common/Plumbing/Deployment/PackageRetention/CaseInsensitiveTinyType.cs
@@ -4,63 +4,39 @@ using YamlDotNet.Core.Tokens;
 
 namespace Calamari.Deployment.PackageRetention
 {
-    //TODO: Replace this basic tiny types implementation with the Octopus one.
-    public abstract class CaseInsensitiveTinyType
+    public abstract class CaseInsensitiveTinyType : TinyType<string>
     {
-        public readonly string Value;
-
-        public CaseInsensitiveTinyType(string value)
+        protected CaseInsensitiveTinyType(string value)
+            : base(value)
         {
-            this.Value = value;
-        }
-        protected bool Equals(CaseInsensitiveTinyType other)
-        {
-            return Value == other.Value;
         }
 
-        static object Create(Type type, string value)
+        public override bool Equals(object? obj)
         {
-            return Activator.CreateInstance(type, value);
-        }
+            if (obj == null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
 
-        public static T Create<T>(string value) where T : CaseInsensitiveTinyType
-        {
-            return (T)Create(typeof(T), value);
-        }
+            if (obj is CaseInsensitiveTinyType ciTinyT)
+                return string.Equals(ciTinyT.Value, Value, StringComparison.OrdinalIgnoreCase);;
 
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj))
-                return false;
-            if (ReferenceEquals(this, obj))
-                return true;
-            if (obj.GetType() != this.GetType())
-                return false;
-
-            var other = (CaseInsensitiveTinyType)obj;
-
-            return this == other;
+            return false;
         }
 
         public override int GetHashCode()
         {
-            return (Value != null ? Value.GetHashCode() : 0);
+            return GetType().GetHashCode() ^ (Value != null ? Value.ToLowerInvariant().GetHashCode() : 0);
         }
 
-        public static bool operator == (CaseInsensitiveTinyType first, CaseInsensitiveTinyType second)
+        public static bool operator ==(CaseInsensitiveTinyType? first, CaseInsensitiveTinyType? second)
         {
             if (first is null || second is null) return false;
-            return string.Equals(first.Value, second.Value, StringComparison.OrdinalIgnoreCase);
+            return first.Equals(second);
         }
 
-        public static bool operator !=(CaseInsensitiveTinyType first, CaseInsensitiveTinyType second)
+        public static bool operator !=(CaseInsensitiveTinyType? first, CaseInsensitiveTinyType? second)
         {
             return !(first == second);
-        }
-
-        public override string ToString()
-        {
-            return Value;
         }
     }
 }
---
+++ source/Calamari.Tests/Fixtures/PackageRetention/CaseInsensitiveTinyTypeFixture.cs
@@ -0,0 +1,63 @@
+﻿using Calamari.Deployment.PackageRetention;
+using NUnit.Framework;
+
+namespace Calamari.Tests.Fixtures.PackageRetention
+{
+    [TestFixture]
+    public class CaseInsensitiveTinyTypeFixture
+    {
+        [Test]
+        public void EquivalentValuesOfSameTypesAreEqual()
+        {
+            var tt1 = ATinyType.Create<ATinyType>("value");
+            var tt2 = ATinyType.Create<ATinyType>("value");
+            Assert.IsTrue(tt1.Equals(tt2));
+        }
+
+        [Test]
+        public void EquivalentValuesOfDifferentTypesAreNotEqual()
+        {
+            var tt1 = ATinyType.Create<ATinyType>("value");
+            var tt2 = AnotherTinyType.Create<AnotherTinyType>("value");
+            Assert.IsFalse(tt1.Equals(tt2));
+        }
+
+        [Test]
+        public void EquivalentValuesWithDifferentCasesOfSameTypesAreEqual()
+        {
+            var tt1 = ATinyType.Create<ATinyType>("VALUE");
+            var tt2 = ATinyType.Create<ATinyType>("value");
+            Assert.IsTrue(tt1.Equals(tt2));
+        }
+
+        [Test]
+        public void EquivalentValuesWithDifferentCasesOfSameTypesHaveEqualHashCodes()
+        {
+            var tt1 = ATinyType.Create<ATinyType>("VALUE");
+            var tt2 = ATinyType.Create<ATinyType>("value");
+            Assert.AreEqual(tt1.GetHashCode(), tt2.GetHashCode());
+        }
+
+        [Test]
+        public void EquivalentValuesOfDifferentTypesHaveUnequalHashCodes()
+        {
+            var tt1 = ATinyType.Create<ATinyType>("value");
+            var tt2 = AnotherTinyType.Create<AnotherTinyType>("value");
+            Assert.AreNotEqual(tt1.GetHashCode(), tt2.GetHashCode());
+        }
+        
+        class ATinyType : CaseInsensitiveTinyType
+        {
+            public ATinyType(string value) : base(value)
+            {
+            }
+        }
+
+        class AnotherTinyType : CaseInsensitiveTinyType
+        {
+            public AnotherTinyType(string value) : base(value)
+            {
+            }
+        }
+    }
+}
