36d98a1303f22e8214397f7be8eea2c65d9da508
Geoff Lamrock <geoff.lamrock@gmail.com>
2022-01-18 13:35:16 +1100

    Adds test discovery handler

--- source/Sashimi/AzureWebAppHealthCheckActionHandler.cs
+++ source/Sashimi/AzureWebAppHealthCheckActionHandler.cs
@@ -26,4 +26,25 @@ namespace Sashimi.AzureAppService
                           .Execute(taskLog);
         }
     }
+
+    class AzureWebAppDiscoveryActionHandler : IActionHandlerWithAccount
+    {
+        static readonly CalamariFlavour CalamariAzureAppService = new CalamariFlavour("Calamari.AzureAppService");
+
+        public string Id => SpecialVariables.Action.Azure.WebAppHealthCheckActionTypeName;
+        public string Name => "Discover Azure Web Apps";
+        public string Description => "Discover available Azure Web Apps.";
+        public string? Keywords => null;
+        public bool ShowInStepTemplatePickerUI => false;
+        public bool WhenInAChildStepRunInTheContextOfTheTargetMachine => false;
+        public bool CanRunOnDeploymentTarget => false;
+        public ActionHandlerCategory[] Categories => new[] { ActionHandlerCategory.BuiltInStep, ActionHandlerCategory.Azure };
+        public string[] StepBasedVariableNameForAccountIds { get; } = { SpecialVariables.Action.Azure.AccountId };
+
+        public IActionHandlerResult Execute(IActionHandlerContext context, ITaskLog taskLog)
+        {
+            taskLog.Info("Yo I'm finding some web apps here");
+            return ActionHandlerResult.FromSuccess();
+        }
+    }
 }
