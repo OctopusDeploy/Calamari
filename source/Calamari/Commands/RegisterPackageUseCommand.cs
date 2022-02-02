9cb2c8c046f57ea291e8f845eb9bb673c8948452
Shane Gill <tothegills@gmail.com>
2022-02-02 12:51:13 +1000

    Add clean and register package commands

---
+++ source/Calamari/Commands/RegisterPackageUseCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Calamari.Commands.Support;
+using Calamari.Common.Commands;
+using Calamari.Common.Plumbing.Deployment.PackageRetention;
+using Calamari.Common.Plumbing.Logging;
+using Calamari.Common.Plumbing.Variables;
+using Octopus.Versioning;
+
+namespace Calamari.Commands
+{
+    [Command("register-package", Description = "Register the use of a package in the package journal")]
+    public class RegisterPackageUseCommand : Command
+    {
+        string packageId;
+        string packageVersion;
+        readonly ILog log;
+        readonly IVariables variables;
+        readonly IManagePackageUse journal;
+        readonly PackageIdentityFactory packageIdentityFactory;
+
+        public RegisterPackageUseCommand(ILog log, IManagePackageUse journal, PackageIdentityFactory packageIdentityFactory, IVariables variables)
+        {
+            this.log = log;
+            this.journal = journal;
+            this.packageIdentityFactory = packageIdentityFactory;
+            this.variables = variables;
+            Options.Add("packageId=", "Package ID of the used package", v => packageId = v);
+            Options.Add("packageVersion=", "Package version of the used package", v => packageVersion = v);
+        }
+
+        public override int Execute(string[] commandLineArguments)
+        {
+            try
+            {
+                RegisterPackageUse(commandLineArguments);
+            }
+            catch (Exception ex)
+            {
+                log.Info($"Unable to register package use.{Environment.NewLine}{ex.ToString()}");
+                return ConsoleFormatter.PrintError(log, ex); // todo: what should the impact of failure here be?
+            }
+
+            return 0;
+        }
+
+        void RegisterPackageUse(string[] commandLineArguments)
+        {
+            var deploymentTaskId = ServerTaskId.FromVariables(variables);
+            var package = packageIdentityFactory.CreatePackageIdentity(journal,
+                                                                       variables,
+                                                                       commandLineArguments,
+                                                                       VersionFormat.Semver,
+                                                                       packageId,
+                                                                       packageVersion);
+
+            journal.RegisterPackageUse(package, deploymentTaskId);
+        }
+    }
+}
