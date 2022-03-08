e7482746af9bae3acd0641efe4f21f97b2bf2e26
Scott Merchant <scott.merchant@octopus.com>
2022-03-08 13:16:10 +1030

    Applying target discovery changes
    Using patch created via diff between:
    2708dcfcb732b7aa01f7fe2eb9f554e5227ba54d (master)
    f5fa34f545f9488525e0793d20e618761d102ea0 (geoffb/step-package-target-discovery)

---
+++ source/Calamari.Tests/LaunchTools/NodeExecutorFixture.cs
@@ -0,0 +1,106 @@
+﻿using Calamari.Common.Features.Processes;
+using Calamari.Common.Plumbing.Commands;
+using Calamari.Common.Plumbing.Variables;
+using Calamari.LaunchTools;
+using Calamari.Testing.Helpers;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Calamari.Tests.LaunchTools
+{
+    [TestFixture]
+    public class NodeExecutorFixture
+    {
+        [Test]
+        public void Execute_UsesCorrectCommandLineInvocation_ForExecution()
+        {
+            // Arrange
+            var instructions = BuildInstructions("Execute");
+            var variables = BuildVariables();
+            var options = BuildOptions();
+            var commandLineRunner = Substitute.For<ICommandLineRunner>();
+            var sut = new NodeExecutor(options, variables, commandLineRunner, new InMemoryLog());
+            CommandLineInvocation capturedInvocation = null;
+            var fakeResult = new CommandResult("fakeCommand", 0);
+            commandLineRunner.Execute(Arg.Do<CommandLineInvocation>(arg => capturedInvocation = arg)).Returns(fakeResult);
+
+            // Act
+            sut.Execute(instructions);
+
+            // Assert
+            var arguments = capturedInvocation.Arguments.Split(' ');
+            arguments.Should().HaveCount(8)
+                .And.HaveElementAt(0, "\"expectedBootstrapperPath\\bootstrapper.js\"")
+                .And.HaveElementAt(1, "\"Execute\"")
+                .And.HaveElementAt(2, "\"expectedTargetPath\"")
+                .And.HaveElementAt(4, "\"encryptionPassword\"")
+                .And.HaveElementAt(5, "\"Octopuss\"")
+                .And.HaveElementAt(6, "\"inputsKey\"")
+                .And.HaveElementAt(7, "\"targetInputsKey\"");
+        }
+
+        [Test]
+        public void Execute_UsesCorrectCommandLineInvocation_ForTargetDiscovery()
+        {
+            // Arrange
+            var instructions = BuildInstructions("Discovery");
+            var variables = BuildVariables();
+            var options = BuildOptions();
+            var commandLineRunner = Substitute.For<ICommandLineRunner>();
+            var sut = new NodeExecutor(options, variables, commandLineRunner, new InMemoryLog());
+            CommandLineInvocation capturedInvocation = null;
+            var fakeResult = new CommandResult("fakeCommand", 0);
+            commandLineRunner.Execute(Arg.Do<CommandLineInvocation>(arg => capturedInvocation = arg)).Returns(fakeResult);
+
+            // Act
+            sut.Execute(instructions);
+
+            // Assert
+            var arguments = capturedInvocation.Arguments.Split(' ');
+            arguments.Should().HaveCount(7)
+                .And.HaveElementAt(0, "\"expectedBootstrapperPath\\bootstrapper.js\"")
+                .And.HaveElementAt(1, "\"Discovery\"")
+                .And.HaveElementAt(2, "\"expectedTargetPath\"")
+                .And.HaveElementAt(4, "\"encryptionPassword\"")
+                .And.HaveElementAt(5, "\"Octopuss\"")
+                .And.HaveElementAt(6, "\"discoveryContextKey\"");
+        }
+
+        private string BuildInstructions(string command) => $@"{{
+    ""nodePathVariable"": ""nodePathKey"",
+    ""targetPathVariable"": ""targetPathKey"",
+    ""bootstrapperPathVariable"": ""bootstrapperVarKey"",
+    ""bootstrapperInvocationCommand"": ""{command}"",
+    ""inputsVariable"": ""inputsKey"",
+    ""deploymentTargetInputsVariable"": ""targetInputsKey"",
+    ""targetDiscoveryContextVariable"": ""discoveryContextKey""
+}}";
+
+        private CommonOptions BuildOptions() => CommonOptions.Parse(new[]
+            {
+                "Test",
+                "--variables",
+                "firstInsensitiveVariablesFileName",
+                "--sensitiveVariables",
+                "firstSensitiveVariablesFileName",
+                "--sensitiveVariables",
+                "secondSensitiveVariablesFileName",
+                "--sensitiveVariablesPassword",
+                "encryptionPassword"
+            });
+
+        private CalamariVariables BuildVariables()
+        {
+            var variables = new CalamariVariables();
+            variables.Set("nodePathKey", "expectedNodePath");
+            variables.Set("targetPathKey", "expectedTargetPath");
+            variables.Set("bootstrapperVarKey", "expectedBootstrapperPath");
+            variables.Set("inputsKey", @"{ input: ""foo"" }");
+            variables.Set("targetInputsKey", @"{ targetInput: ""bar"" }");
+            variables.Set("foo", "AwsAccount");
+            variables.Set("discoveryContextKey", @"{ ""blah"": ""baz"" }");
+            return variables;
+        }
+    }
+}
