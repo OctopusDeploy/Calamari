a99d1ccf141d5e562b14771833285c6f872333bd
Scott Merchant <scott.merchant@octopus.com>
2022-04-29 10:50:05 +0930

    Add TargetTags extension method

---
+++ source/Calamari.Common/Features/Discovery/TargetTagsExtensions.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Calamari.Common.Features.Discovery
+{
+    public static class TargetTagsExtensions
+    {
+        public static TargetTags ToTargetTags(this IReadOnlyDictionary<string, string> tags)
+        {
+            var caseInsensitiveTagDictionary = tags.ToDictionary(kvp => kvp.Key, kvp => kvp.Value, StringComparer.OrdinalIgnoreCase);
+            caseInsensitiveTagDictionary.TryGetValue(TargetTags.EnvironmentTagName, out var environment);
+            caseInsensitiveTagDictionary.TryGetValue(TargetTags.RoleTagName, out var role);
+            caseInsensitiveTagDictionary.TryGetValue(TargetTags.ProjectTagName, out var project);
+            caseInsensitiveTagDictionary.TryGetValue(TargetTags.SpaceTagName, out var space);
+            caseInsensitiveTagDictionary.TryGetValue(TargetTags.TenantTagName, out var tenant);
+            return new TargetTags(
+                environment: environment,
+                role: role,
+                project: project,
+                space: space,
+                tenant: tenant);
+        }
+    }
+}
