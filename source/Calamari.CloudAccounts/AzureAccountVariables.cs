89d00d27fdfcb2e55f60f3157b678dfe77d6609f
Scott Merchant <scott.merchant@octopus.com>
2022-05-04 15:27:56 +0930

    Revert adding Azure Client classes to CloudAccounts
    In the end I don't think it's worth the trouble as we only use a small subset of these classes

--- source/Calamari.CloudAccounts/AzureAccountVariables.cs
+++ source/Calamari.CloudAccounts/AzureAccountVariables.cs
@@ -1,20 +1,14 @@
-using Calamari.CloudAccounts.Azure;
-
 namespace Calamari.CloudAccounts
 {
-    /// <remarks>
-    /// This class is being kept to maintain backwards compatibility
-    /// as it's a public interface for the Calamari.CloudAccounts Nuget package. - Scott M 29/04/22
-    /// </remarks>
     public static class AzureAccountVariables
     {
-        public static readonly string Environment = AccountVariables.Environment;
+        public static readonly string Environment = "Octopus.Action.Azure.Environment";
         public static readonly string AccountVariable = "Octopus.Action.AzureAccount.Variable";
-        public static readonly string SubscriptionId = AccountVariables.SubscriptionId;
-        public static readonly string ClientId = AccountVariables.ClientId;   
-        public static readonly string TenantId = AccountVariables.TenantId;
-        public static readonly string Password = AccountVariables.Password;
-        public static readonly string ResourceManagementEndPoint = AccountVariables.ResourceManagementEndPoint;
-        public static readonly string ActiveDirectoryEndPoint = AccountVariables.ActiveDirectoryEndPoint;
+        public static readonly string SubscriptionId = "Octopus.Action.Azure.SubscriptionId";
+        public static readonly string ClientId = "Octopus.Action.Azure.ClientId";   
+        public static readonly string TenantId = "Octopus.Action.Azure.TenantId";
+        public static readonly string Password = "Octopus.Action.Azure.Password";
+        public static readonly string ResourceManagementEndPoint = "Octopus.Action.Azure.ResourceManagementEndPoint";
+        public static readonly string ActiveDirectoryEndPoint = "Octopus.Action.Azure.ActiveDirectoryEndPoint";
     }
 }
