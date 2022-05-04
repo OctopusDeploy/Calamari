06708b33be4427bd00589aa1ad0108839b93e2ef
Scott Merchant <scott.merchant@octopus.com>
2022-05-04 15:39:04 +0930

    Add required AzureClient related classes to Calamari.Azure

--- source/Calamari.Azure/Kubernetes/Discovery/AzureKubernetesDiscovery.cs
+++ source/Calamari.Azure/Kubernetes/Discovery/AzureKubernetesDiscovery.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Calamari.CloudAccounts.Azure;
 using Calamari.Common.Features.Discovery;
 using Calamari.Common.Plumbing.Logging;
 using JetBrains.Annotations;
