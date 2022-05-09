151fcc81aca6deb54d7b8b3edae5265955d85d4d
geoff.lamrock <geoff.lamrock@gmail.com>
2022-05-09 16:43:05 +1000

    TeamCity change in 'Octopus Deploy / Libraries / Sashimi Group / Sashimi.AzureAppService' project: snapshot dependency was added in 'Chain: Build and Test and Publish to Feedz.io' build configuration

--- .teamcity/patches/buildTypes/PublishToFeedzIo.kts
+++ .teamcity/patches/buildTypes/PublishToFeedzIo.kts
@@ -225,7 +225,6 @@ changeBuildType(RelativeId("PublishToFeedzIo")) {
         }
         update(RelativeId("WindowsNetFxTesting_2012r2")) {
             snapshot {
-                reuseBuilds = ReuseBuilds.ANY
                 onDependencyFailure = FailureAction.CANCEL
                 onDependencyCancel = FailureAction.CANCEL
             }
