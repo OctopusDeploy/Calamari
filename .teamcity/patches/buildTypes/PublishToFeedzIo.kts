002a478c7635be248704985e45ecc8e18dc7c478
matt.hilton <matt.hilton@octopus.com>
2022-05-09 13:10:47 +1000

    TeamCity change in 'Octopus Deploy / Libraries / Sashimi Group / Sashimi.AzureAppService' project: build features of 'Chain: Build and Test and Publish to Feedz.io' build configuration were updated

--- .teamcity/patches/buildTypes/PublishToFeedzIo.kts
+++ .teamcity/patches/buildTypes/PublishToFeedzIo.kts
@@ -2,7 +2,9 @@ package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.CommitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.PullRequests
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.pullRequests
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.NuGetPublishStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.nuGetPublish
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.VcsTrigger
@@ -78,6 +80,16 @@ changeBuildType(RelativeId("PublishToFeedzIo")) {
                 githubUrl = "https://api.github.com"
                 authType = personalToken {
                     token = "credentialsJSON:d2d6ff31-56f1-4893-a448-f7a517da6c88"
+                }
+            }
+        }
+        add {
+            pullRequests {
+                provider = github {
+                    authType = token {
+                        token = "credentialsJSON:e3abf97f-cad5-4d88-9a7a-f588c55c53ed"
+                    }
+                    filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
                 }
             }
         }
