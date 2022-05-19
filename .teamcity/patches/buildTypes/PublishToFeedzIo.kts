e4402c088729ec860f578b6a9c131ffb157b798d
liam.mackie <liam.mackie@octopus.com>
2022-05-19 11:27:20 +1000

    Build feature parameter added

--- .teamcity/patches/buildTypes/PublishToFeedzIo.kts
+++ .teamcity/patches/buildTypes/PublishToFeedzIo.kts
@@ -1,6 +1,8 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.CommitStatusPublisher
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.commitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.NuGetPublishStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.nuGetPublish
 import jetbrains.buildServer.configs.kotlin.v2019_2.ui.*
@@ -32,6 +34,27 @@ changeBuildType(RelativeId("PublishToFeedzIo")) {
             param("org.jfrog.artifactory.selectedDeployableServer.downloadSpecSource", "Job configuration")
             param("org.jfrog.artifactory.selectedDeployableServer.useSpecs", "false")
             param("org.jfrog.artifactory.selectedDeployableServer.uploadSpecSource", "Job configuration")
+        }
+    }
+
+    features {
+        val feature1 = find<CommitStatusPublisher> {
+            commitStatusPublisher {
+                publisher = github {
+                    githubUrl = "https://api.github.com"
+                    authType = personalToken {
+                        token = "credentialsJSON:d2d6ff31-56f1-4893-a448-f7a517da6c88"
+                    }
+                }
+            }
+        }
+        feature1.apply {
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = personalToken {
+                    token = "credentialsJSON:7416c240-5c67-48ed-97a3-f5fe49d0e744"
+                }
+            }
         }
     }
 }
