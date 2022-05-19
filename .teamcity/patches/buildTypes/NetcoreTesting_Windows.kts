c261741b106868a5b48b44a53c51711d31290fbd
liam.mackie <liam.mackie@octopus.com>
2022-05-19 11:31:22 +1000

    Build feature parameter added

--- .teamcity/patches/buildTypes/NetcoreTesting_Windows.kts
+++ .teamcity/patches/buildTypes/NetcoreTesting_Windows.kts
@@ -14,4 +14,25 @@ changeBuildType(RelativeId("NetcoreTesting_Windows")) {
     vcs {
         add(DslContext.settingsRoot.id!!)
     }
+
+    features {
+        val feature1 = find<CommitStatusPublisher> {
+            commitStatusPublisher {
+                publisher = github {
+                    githubUrl = "https://api.github.com"
+                    authType = personalToken {
+                        token = "credentialsJSON:d2d6ff31-56f1-4893-a448-f7a517da6c88"
+                    }
+                }
+            }
+        }
+        feature1.apply {
+            publisher = github {
+                githubUrl = "https://api.github.com"
+                authType = personalToken {
+                    token = "credentialsJSON:7416c240-5c67-48ed-97a3-f5fe49d0e744"
+                }
+            }
+        }
+    }
 }
