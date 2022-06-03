6ea45caf35059ed535005372df7592aaac0d1a7c
Scott Merchant <scott.merchant@octopus.com>
2022-06-03 14:48:23 +0930

    Reduce number of regions to choose from

--- source/Calamari.Tests/AWS/RegionRandomiser.cs
+++ source/Calamari.Tests/AWS/RegionRandomiser.cs
@@ -6,25 +6,29 @@ namespace Calamari.Tests.AWS
     {
         static Random random = new Random();
 
-        static string[] regions = new string[]
+        // Only a subset of regions are currently
+        // allowed to make it easier to manage
+        // if we need to do cleanups or allocate
+        // higher quotas.
+        static string[] regions =
         {
-            "ap-northeast-1",
-            "ap-northeast-2",
-            "ap-northeast-3",
-            "ap-south-1",
+            // "ap-northeast-1",
+            // "ap-northeast-2",
+            // "ap-northeast-3",
+            // "ap-south-1",
             "ap-southeast-1",
             "ap-southeast-2",
-            "ca-central-1",
-            "eu-central-1",
-            "eu-north-1",
-            "eu-west-1",
-            "eu-west-2",
-            "eu-west-3",
-            "sa-east-1",
+            // "ca-central-1",
+            // "eu-central-1",
+            // "eu-north-1",
+            // "eu-west-1",
+            // "eu-west-2",
+            // "eu-west-3",
+            // "sa-east-1",
             "us-east-1",
-            "us-east-2",
-            "us-west-1",
-            "us-west-2"
+            // "us-east-2",
+            // "us-west-1",
+            // "us-west-2"
         };
 
         public static string GetARegion()
