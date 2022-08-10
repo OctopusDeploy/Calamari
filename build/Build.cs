c2339dae1b0a30bb070c25d2cc2fe175dc7abc28
isaac <isaac.calligeros@octopus.com>
2022-08-10 12:27:47 +0930

    remove net5.0 dependency in AzureCloudService

--- build/Build.cs
+++ build/Build.cs
@@ -105,7 +105,7 @@ class Build : NukeBuild
                         File.Copy(RootDirectory / "global.json", PublishDirectory / calamariFlavour / platform / "global.json");
                     }
 
-                    if(framework == "net5.0")
+                    if(framework == "net6.0")
                     {
                         var runtimes = XmlTasks.XmlPeekSingle(project, "Project/PropertyGroup/RuntimeIdentifiers")?.Split(';');
                         foreach (var runtime in runtimes)
