468758d9dd564b8e95f3cbb9bfabc1050e2e5779
Chris Kim <chris.kim@octopus.com>
2022-08-18 16:26:48 +1200

    Prepare for consolidation into Calamari repository.

--- source/Sashimi/AzureWebAppEndpoint.cs
+++
@@ -1,9 +0,0 @@
-using Sashimi.Server.Contracts;
-
-namespace Sashimi.AzureWebApp
-{
-    class AzureWebAppEndpoint
-    {
-        public static readonly DeploymentTargetType AzureWebAppDeploymentTargetType = new DeploymentTargetType("AzureWebApp", "Azure Web Application");
-    }
-}
