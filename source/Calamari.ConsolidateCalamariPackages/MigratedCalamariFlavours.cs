7db8de1c791bb6f3a6371b34b95861436d9bab73
Yihao <yihao.wang@octopus.com>
2022-08-25 18:21:28 +1200

    Update consolidate package to .net6; update migrated and unmigrated packages

--- source/Calamari.ConsolidateCalamariPackages/MigratedCalamariFlavours.cs
+++ source/Calamari.ConsolidateCalamariPackages/MigratedCalamariFlavours.cs
@@ -5,6 +5,12 @@ namespace Calamari.ConsolidateCalamariPackages
 {
     public class MigratedCalamariFlavours
     {
-        public static List<string> Flavours = new List<string> { "Calamari.AzureAppService", "Calamari.AzureResourceGroup", "Calamari.AzureWebApp" };
+        public static List<string> Flavours = new()
+        {
+            "Calamari.AzureAppService", 
+            "Calamari.AzureResourceGroup", 
+            "Calamari.AzureWebApp",
+            "Calamari.AzureServiceFabric",
+        };
     }
 }
