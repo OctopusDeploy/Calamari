b0327f4a5bd7879330d02cb5043bff393d374304
isaac <isaac.calligeros@octopus.com>
2022-08-29 10:14:26 +0930

    Include imported .csproj files into solution.

--- source/Calamari.AzureCloudService.Tests/HealthCheckCommandFixture.cs
+++ source/Calamari.AzureCloudService.Tests/HealthCheckCommandFixture.cs
@@ -2,7 +2,7 @@ using System;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
-using Calamari.Tests.Shared;
+using Calamari.Testing;
 using FluentAssertions;
 using Microsoft.Azure;
 using Microsoft.WindowsAzure.Management.Compute;
