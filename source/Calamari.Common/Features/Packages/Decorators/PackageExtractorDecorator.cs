10cdee5870b9f8df5198063788f8fb1c859cb3ee
Matt Hilton <matt@hiltons.id>
2022-12-19 16:17:21 +1100

    Enforce Archive size and compression ratio limits
    Add archive limits
    
    Cleans up archive limit decorators
    
    Renames and tests
    
    Removes LimitingArchiveEntry and co, which don't work and need a rethink
    
    Refactor tests to cover feature flags

---
+++ source/Calamari.Common/Features/Packages/Decorators/PackageExtractorDecorator.cs
@@ -0,0 +1,21 @@
+﻿namespace Calamari.Common.Features.Packages.Decorators
+{
+    /// <summary>
+    /// A base Decorator which allows addition to the behaviour of an IPackageExtractor 
+    /// </summary>
+    public class PackageExtractorDecorator : IPackageExtractor
+    {
+        readonly IPackageExtractor concreteExtractor;
+
+        protected PackageExtractorDecorator(IPackageExtractor concreteExtractor)
+        {
+            this.concreteExtractor = concreteExtractor;
+        }
+
+        public string[] Extensions => concreteExtractor.Extensions;
+        public virtual int Extract(string packageFile, string directory)
+        {
+            return concreteExtractor.Extract(packageFile, directory);
+        }
+    }
+}
