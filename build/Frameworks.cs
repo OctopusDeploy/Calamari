12135cb561b61123270e87c277a3bc54fe6c9785
Matt Hilton <matt.hilton@octopus.com>
2023-01-30 11:46:05 +1100

    Revert "Remove net452 and net462 constants"
    This reverts commit 4f2ad3843a9e72cef7daa7d4439c43f31adc288a.

--- build/Frameworks.cs
+++ build/Frameworks.cs
@@ -5,6 +5,8 @@ namespace Calamari.Build
     public static class Frameworks
     {
         public const string Net40 = "net40";
+        public const string net452 = "net452";
+        public const string Net461 = "net461";
         public const string Net48 = "net48";
         public const string Net60 = "net6.0";
     }
