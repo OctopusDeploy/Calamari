30dd454a252fefd2fe6a8b557e1608bd6bc002c8
Matt Hilton <matt.hilton@octopus.com>
2023-01-30 11:46:40 +1100

    Revert "Bump dotnet 4.6.1 to 4.8.1"
    This reverts commit a9fa5241794084962160a6ccf90f2124de1f8c81.

--- build/Build.cs
+++ build/Build.cs
@@ -380,7 +380,7 @@ namespace Calamari.Build
                   .Executes(async () =>
                   {
                       var nugetVersion = NugetVersion.Value;
-                      var defaultTarget = OperatingSystem.IsWindows() ? Frameworks.Net481 : Frameworks.Net60;
+                      var defaultTarget = OperatingSystem.IsWindows() ? Frameworks.Net461 : Frameworks.Net60;
                       AbsolutePath binFolder = SourceDirectory / "Calamari.Tests" / "bin" / Configuration / defaultTarget;
                       Directory.Exists(binFolder);
                       var actions = new List<Action>
