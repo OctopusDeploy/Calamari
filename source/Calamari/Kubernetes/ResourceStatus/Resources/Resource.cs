27dcf1003c0a6d754e85c5fb03c384240dbf0c3d
John Simons <john.simons@octopus.com>
2023-03-07 14:19:40 +1000

    Clean up

--- source/Calamari/Kubernetes/ResourceStatus/Resources/Resource.cs
+++ source/Calamari/Kubernetes/ResourceStatus/Resources/Resource.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
 
 namespace Calamari.Kubernetes.ResourceStatus.Resources
