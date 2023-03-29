939d3ded9bcd35202755303b89c8542c063dc27f
Yihao <yihao.wang@octopus.com>
2023-03-29 16:15:53 +1300

    Override HasUpdate for PersistentVolumeClaim

--- source/Calamari/Kubernetes/ResourceStatus/Resources/PersistentVolumeClaim.cs
+++ source/Calamari/Kubernetes/ResourceStatus/Resources/PersistentVolumeClaim.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json.Linq;
 
 namespace Calamari.Kubernetes.ResourceStatus.Resources
@@ -18,6 +19,16 @@ namespace Calamari.Kubernetes.ResourceStatus.Resources
             Capacity = Field("$.status.capacity.storage");
             AccessModes = data.SelectToken("$.status.accessModes")?.Values<string>() ?? new string[] { };
             StorageClass = Field("$.spec.storageClassName");
+        }
+
+        public override bool HasUpdate(Resource lastStatus)
+        {
+            var last = CastOrThrow<PersistentVolumeClaim>(lastStatus);
+            return last.Status != Status
+                   || last.Volume != Volume
+                   || last.Capacity != Capacity
+                   || !last.AccessModes.SequenceEqual(AccessModes)
+                   || last.StorageClass != StorageClass;
         }
     }
 }
