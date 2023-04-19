342b94f7743948e52e8d2488c1aa8047aafe3146
Scott Merchant <scott.merchant@octopus.com>
2023-04-19 11:58:43 +0930

    Fix more build errors

--- source/Calamari/Kubernetes/Integration/CaptureCommandOutput.cs
+++ source/Calamari/Kubernetes/Integration/CaptureCommandOutput.cs
@@ -5,12 +5,13 @@ namespace Calamari.Kubernetes.Integration
 {
     public interface ICommandOutput
     {
-        public IReadOnlyList<Message> Messages { get; }
+        Message[] Messages { get; }
     }
     public class CaptureCommandOutput : ICommandInvocationOutputSink, ICommandOutput
     {
         private readonly List<Message> messages = new List<Message>();
-        public IReadOnlyList<Message> Messages => messages;
+        public Message[] Messages => messages.ToArray();
+
         public void WriteInfo(string line)
         {
             messages.Add(new Message(Level.Info, line));
