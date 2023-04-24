91a9d54066425da2f9d60b21c5f36760e2c6a6b0
Scott Merchant <scott.merchant@octopus.com>
2023-04-24 11:43:40 +0930

    Add some basic unit tests for AzureResource around IsSlot, SlotName and ParentName

--- source/Calamari.AzureAppService/Azure/AzureResource.cs
+++ source/Calamari.AzureAppService/Azure/AzureResource.cs
@@ -31,7 +31,7 @@ namespace Calamari.AzureAppService.Azure
                     return null;
                 }
                 var indexOfSlash = Name.LastIndexOf("/", StringComparison.InvariantCulture);
-                return indexOfSlash < 0 ? null : Name.Substring(indexOfSlash + 1);
+                return indexOfSlash < 0 ? "" : Name.Substring(indexOfSlash + 1);
             }
         }
 
@@ -45,7 +45,7 @@ namespace Calamari.AzureAppService.Azure
                     return null;
                 }
                 var indexOfSlash = Name.LastIndexOf("/", StringComparison.InvariantCulture);
-                return indexOfSlash < 0 ? null : Name.Substring(0, indexOfSlash);
+                return indexOfSlash < 0 ? Name : Name.Substring(0, indexOfSlash);
             }
         }
     }
