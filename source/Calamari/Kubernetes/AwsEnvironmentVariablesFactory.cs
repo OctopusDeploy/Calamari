122b9e4b6423182ef6413590c2fee8e4c4eab8b0
Scott Merchant <scott.merchant@octopus.com>
2023-05-16 11:38:54 +0930

    Fix build

--- source/Calamari/Kubernetes/AwsEnvironmentVariablesFactory.cs
+++ source/Calamari/Kubernetes/AwsEnvironmentVariablesFactory.cs
@@ -3,19 +3,20 @@ using Calamari.Common.Aws;
 using Calamari.Common.Plumbing.Logging;
 using Calamari.Common.Plumbing.Variables;
 
-namespace Calamari.Kubernetes;
-
-public class AwsEnvironmentVariablesFactory
+namespace Calamari.Kubernetes
 {
-    private readonly Lazy<IAwsEnvironmentVariablesGenerator> awsEnvironmentVariablesGeneratorLazy;
-
-    public AwsEnvironmentVariablesFactory(Lazy<IAwsEnvironmentVariablesGenerator> awsEnvironmentVariablesGeneratorLazy)
+    public class AwsEnvironmentVariablesFactory
     {
-        this.awsEnvironmentVariablesGeneratorLazy = awsEnvironmentVariablesGeneratorLazy;
-    }
+        private readonly Lazy<IAwsEnvironmentVariablesGenerator> awsEnvironmentVariablesGeneratorLazy;
 
-    public IAwsEnvironmentVariables Create(ILog log, IVariables variables)
-    {
-        return awsEnvironmentVariablesGeneratorLazy.Value.Create(log, variables);
+        public AwsEnvironmentVariablesFactory(Lazy<IAwsEnvironmentVariablesGenerator> awsEnvironmentVariablesGeneratorLazy)
+        {
+            this.awsEnvironmentVariablesGeneratorLazy = awsEnvironmentVariablesGeneratorLazy;
+        }
+
+        public IAwsEnvironmentVariables Create(ILog log, IVariables variables)
+        {
+            return awsEnvironmentVariablesGeneratorLazy.Value.Create(log, variables);
+        }
     }
 }
