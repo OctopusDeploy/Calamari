bf419d9d01bc326a7d34383c255b4a48b5bc0031
Scott Merchant <scott.merchant@octopus.com>
2023-05-17 11:11:04 +0930

    Fix scrubber to take a predicate and fix predicate.

--- source/Calamari.Tests/Helpers/KubernetesJsonResourceScrubber.cs
+++ source/Calamari.Tests/Helpers/KubernetesJsonResourceScrubber.cs
@@ -6,17 +6,14 @@ namespace Calamari.Tests.Helpers
 {
     public static class KubernetesJsonResourceScrubber
     {
-        public static string ScrubRawJson(string json)
+        public static string ScrubRawJson(string json, Func<JProperty, bool> propertiesToRemovePredicate)
         {
             var jObject = JObject.Parse(json);
 
             jObject.Descendants()
                    .OfType<JProperty>()
-                   .Where(p => p.Name.Contains("Time") ||
-                       p.Name == "annotations" ||
-                       p.Name == "uid" ||
-                       p.Name == "conditions")
-                .ToList()
+                   .Where(propertiesToRemovePredicate)
+                   .ToList()
                    .ForEach(p => p.Remove());
 
             return jObject.ToString();
--- source/Calamari.Tests/KubernetesFixtures/KubernetesContextScriptWrapperLiveFixtureEks.cs
+++ source/Calamari.Tests/KubernetesFixtures/KubernetesContextScriptWrapperLiveFixtureEks.cs
@@ -137,7 +137,14 @@ namespace Calamari.Tests.KubernetesFixtures
                 variableMessages.Should().ContainSingle(m => m.Properties["name"] == "CustomResources(nginx-deployment)")
                                 .Subject;
 
-            this.Assent(KubernetesJsonResourceScrubber.ScrubRawJson(variableMessage.Properties["value"]), configuration: AssentConfiguration.Default);
+            var scrubbedJson = KubernetesJsonResourceScrubber.ScrubRawJson(variableMessage.Properties["value"], p =>
+                p.Name.Contains("Time") ||
+                p.Name == "annotations" ||
+                p.Name == "uid" ||
+                p.Name == "conditions" ||
+                p.Name == "resourceVersion");
+
+            this.Assent(scrubbedJson, configuration: AssentConfiguration.Default);
 
             var idx = Array.IndexOf(rawLogs, "Performing resource status checks on the following resources:");
             rawLogs[idx + 1].Should().Be(" - Deployment/nginx-deployment in namespace calamari-testing");
