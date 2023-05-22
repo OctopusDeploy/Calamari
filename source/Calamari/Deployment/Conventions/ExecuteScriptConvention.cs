a31c7ac5740cf0418088fe3272065be65a8d8881
Scott Merchant <scott.merchant@octopus.com>
2023-05-22 12:34:49 +0930

    Ensure EnvironmentVariables are passed to ScriptEngine

--- source/Calamari/Deployment/Conventions/ExecuteScriptConvention.cs
+++ source/Calamari/Deployment/Conventions/ExecuteScriptConvention.cs
@@ -17,20 +17,20 @@ namespace Calamari.Deployment.Conventions
             this.scriptEngine = scriptEngine;
             this.commandLineRunner = commandLineRunner;
         }
-        
+
         public void Install(RunningDeployment deployment)
         {
             var variables = deployment.Variables;
             var scriptFile = Path.Combine(deployment.CurrentDirectory, variables.Get(ScriptVariables.ScriptFileName));
             var scriptParameters = variables.Get(SpecialVariables.Action.Script.ScriptParameters);
             Log.VerboseFormat("Executing '{0}'", scriptFile);
-            var result = scriptEngine.Execute(new Script(scriptFile, scriptParameters), variables, commandLineRunner);
+            var result = scriptEngine.Execute(new Script(scriptFile, scriptParameters), variables, commandLineRunner, deployment.EnvironmentVariables);
 
-            var exitCode = 
+            var exitCode =
                 result.ExitCode == 0 && result.HasErrors && variables.GetFlag(SpecialVariables.Action.FailScriptOnErrorOutput)
                     ? -1
                     : result.ExitCode;
-            
+
             Log.SetOutputVariable(SpecialVariables.Action.Script.ExitCode, exitCode.ToString(), variables);
         }
     }
