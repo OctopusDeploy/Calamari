9349ddd569784e1c466c1ed942c3a115e95cc8b5
veo.chen <veo.chen@octopus.com>
2023-05-25 10:28:23 +1200

    unmanaged

--- source/Calamari.Tests/KubernetesFixtures/Terraform/Clusters/AKS/aks.tf
+++ source/Calamari.Tests/KubernetesFixtures/Terraform/Clusters/AKS/aks.tf
@@ -8,6 +8,7 @@ resource "azurerm_kubernetes_cluster" "default" {
   resource_group_name = azurerm_resource_group.default.name
   location            = "Australia East"
   dns_prefix          = "${random_pet.prefix.id}-k8s"
+  local_account_disabled = true
   
   tags = {
     octopus-environment = "Staging"
@@ -22,6 +23,14 @@ resource "azurerm_kubernetes_cluster" "default" {
   }
 
   role_based_access_control_enabled = true
+
+  azure_active_directory_role_based_access_control {
+    managed                = false
+
+    client_app_id = var.aks_client_id
+    server_app_id = var.aks_client_id
+    server_app_secret = var.aks_client_secret
+  }
 
   service_principal {
     client_id     = var.aks_client_id
