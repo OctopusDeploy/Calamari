2dc6951670b207ebd4f7772bf51958b7abae2344
Scott Merchant <97430840+scottmerchant@users.noreply.github.com>
2023-05-30 08:10:15 +0930

    Add extra check to ensure that the AWSScriptWrapper runs for aws script steps (#1046)

--- source/Calamari.Aws/Deployment/AwsSpecialVariables.cs
+++ source/Calamari.Aws/Deployment/AwsSpecialVariables.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Calamari.Aws.Deployment
+﻿namespace Calamari.Aws.Deployment
 {
     public static class AwsSpecialVariables
     {
@@ -13,6 +7,7 @@ namespace Calamari.Aws.Deployment
         public static class Authentication
         {
             public static readonly string UseInstanceRole = "Octopus.Action.AwsAccount.UseInstanceRole";
+            public static readonly string AwsAccountVariable = "Octopus.Action.AwsAccount.Variable";
         }
 
         public static class S3
