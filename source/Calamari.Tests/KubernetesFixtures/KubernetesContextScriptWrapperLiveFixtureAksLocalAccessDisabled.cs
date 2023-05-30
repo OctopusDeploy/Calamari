99e0215b4c67728d0ffe58870241444f5da1656d
veo.chen <veo.chen@octopus.com>
2023-05-30 13:05:57 +1200

    missing test

---
+++ source/Calamari.Tests/KubernetesFixtures/KubernetesContextScriptWrapperLiveFixtureAksLocalAccessDisabled.cs
@@ -0,0 +1,173 @@
+#if NETCORE
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Calamari.Azure;
+using Calamari.Common.Features.Discovery;
+using Calamari.Common.Plumbing.ServiceMessages;
+using Calamari.Deployment;
+using Calamari.Kubernetes.Commands;
+using Calamari.Testing;
+using Calamari.Testing.Helpers;
+using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace Calamari.Tests.KubernetesFixtures
+{
+    [TestFixture]
+    [Category(TestCategory.RunOnceOnWindowsAndLinux)]
+    public class KubernetesContextScriptWrapperLiveFixtureAksLocalAccessDisabled: KubernetesContextScriptWrapperLiveFixture
+    {
+        string aksClusterName;
+        string azurermResourceGroup;
+        string azureSubscriptionId;
+
+        protected override string KubernetesCloudProvider => "AKS-local-access-disabled";
+
+        protected override IEnumerable<string> ToolsToAddToPath(InstallTools tools)
+        {
+            yield return tools.KubeloginExecutable;
+        }
+        
+        protected override async Task InstallOptionalTools(InstallTools tools)
+        {
+            await tools.InstallKubelogin();
+        }
+
+        protected override void ExtractVariablesFromTerraformOutput(JObject jsonOutput)
+        {
+            aksClusterName = jsonOutput.Get<string>("aks_cluster_name", "value");
+            azurermResourceGroup = jsonOutput.Get<string>("aks_rg_name", "value");
+        }
+
+        protected override Dictionary<string, string> GetEnvironmentVars()
+        {
+            azureSubscriptionId = ExternalVariables.Get(ExternalVariable.AzureSubscriptionId);
+            return new Dictionary<string, string>()
+            {
+                { "ARM_SUBSCRIPTION_ID", azureSubscriptionId},
+                { "ARM_CLIENT_ID", ExternalVariables.Get(ExternalVariable.AzureSubscriptionClientId) },
+                { "ARM_CLIENT_SECRET", ExternalVariables.Get(ExternalVariable.AzureSubscriptionPassword) },
+                { "ARM_TENANT_ID", ExternalVariables.Get(ExternalVariable.AzureSubscriptionTenantId) },
+                { "TF_VAR_aks_client_id", ExternalVariables.Get(ExternalVariable.AzureSubscriptionClientId) },
+                { "TF_VAR_aks_client_secret", ExternalVariables.Get(ExternalVariable.AzureSubscriptionPassword) },
+                { "TF_VAR_test_namespace", TestNamespace },
+            };
+        }
+
+        [Test]
+        public void AuthorisingWithAzureServicePrincipal()
+        {
+            variables.Set(SpecialVariables.Account.AccountType, "AzureServicePrincipal");
+            variables.Set("Octopus.Action.Kubernetes.AksClusterResourceGroup", azurermResourceGroup);
+            variables.Set(Kubernetes.SpecialVariables.AksClusterName, aksClusterName);
+            variables.Set("Octopus.Action.Kubernetes.AksAdminLogin", Boolean.FalseString);
+            variables.Set("Octopus.Action.Azure.SubscriptionId", ExternalVariables.Get(ExternalVariable.AzureSubscriptionId));
+            variables.Set("Octopus.Action.Azure.TenantId", ExternalVariables.Get(ExternalVariable.AzureSubscriptionTenantId));
+            variables.Set("Octopus.Action.Azure.Password", ExternalVariables.Get(ExternalVariable.AzureSubscriptionPassword));
+            variables.Set("Octopus.Action.Azure.ClientId", ExternalVariables.Get(ExternalVariable.AzureSubscriptionClientId));
+            var wrapper = CreateWrapper();
+            
+            // Actually do a dummy deployment to ensure we don't just have read permissions.
+            TestKubectlCommands(
+                                wrapper,
+                                "create deployment kubernetes-bootcamp --image=gcr.io/google-samples/kubernetes-bootcamp:v1",
+                                "wait deployment kubernetes-bootcamp --for condition=Available=True --timeout=90s",
+                                "get deployments"
+                               );
+        }
+
+        [Test]
+        public void UnreachableK8Cluster_ShouldExecuteTargetScript()
+        {
+            const string unreachableClusterUrl = "https://example.kubernetes.com";
+
+            variables.Set(Kubernetes.SpecialVariables.ClusterUrl, unreachableClusterUrl);
+            variables.Set(SpecialVariables.Account.AccountType, "AzureServicePrincipal");
+            variables.Set("Octopus.Action.Kubernetes.AksClusterResourceGroup", azurermResourceGroup);
+            variables.Set(Kubernetes.SpecialVariables.AksClusterName, aksClusterName);
+            variables.Set("Octopus.Action.Kubernetes.AksAdminLogin", Boolean.FalseString);
+            variables.Set("Octopus.Action.Azure.SubscriptionId", ExternalVariables.Get(ExternalVariable.AzureSubscriptionId));
+            variables.Set("Octopus.Action.Azure.TenantId", ExternalVariables.Get(ExternalVariable.AzureSubscriptionTenantId));
+            variables.Set("Octopus.Action.Azure.Password", ExternalVariables.Get(ExternalVariable.AzureSubscriptionPassword));
+            variables.Set("Octopus.Action.Azure.ClientId", ExternalVariables.Get(ExternalVariable.AzureSubscriptionClientId));
+
+            var wrapper = CreateWrapper();
+
+            TestScript(wrapper, "Test-Script");
+        }
+
+        [Test]
+        [TestCase(false)]
+        [TestCase(true)]
+        public void DiscoverKubernetesClusterWithAzureServicePrincipalAccount(bool setHealthCheckContainer)
+        {
+            var scope = new TargetDiscoveryScope("TestSpace",
+                "Staging",
+                "testProject",
+                null,
+                new[] { "discovery-role" },
+                "WorkerPool-1",
+                setHealthCheckContainer ? new FeedImage("MyImage:with-tag", "Feeds-123") : null);
+
+            var account = new ServicePrincipalAccount(
+                ExternalVariables.Get(ExternalVariable.AzureSubscriptionId),
+                ExternalVariables.Get(ExternalVariable.AzureSubscriptionClientId),
+                ExternalVariables.Get(ExternalVariable.AzureSubscriptionTenantId),
+                ExternalVariables.Get(ExternalVariable.AzureSubscriptionPassword),
+                null,
+                null,
+                null);
+
+            var authenticationDetails =
+                new AccountAuthenticationDetails<ServicePrincipalAccount>(
+                    "Azure",
+                    "Accounts-1",
+                    account);
+
+            var targetDiscoveryContext =
+                new TargetDiscoveryContext<AccountAuthenticationDetails<ServicePrincipalAccount>>(scope,
+                    authenticationDetails);
+
+            var result =
+                ExecuteDiscoveryCommand(targetDiscoveryContext,
+                    new[]{"Calamari.Azure"}
+                );
+
+            result.AssertSuccess();
+
+            var targetName = $"aks/{azureSubscriptionId}/{azurermResourceGroup}/{aksClusterName}";
+            var serviceMessageProperties = new Dictionary<string, string>
+            {
+                { "name", targetName },
+                { "clusterName", aksClusterName },
+                { "clusterResourceGroup", azurermResourceGroup },
+                { "skipTlsVerification", bool.TrueString },
+                { "octopusDefaultWorkerPoolIdOrName", scope.WorkerPoolId },
+                { "octopusAccountIdOrName", "Accounts-1" },
+                { "octopusRoles", "discovery-role" },
+                { "updateIfExisting", bool.TrueString },
+                { "isDynamic", bool.TrueString },
+                { "awsUseWorkerCredentials", bool.FalseString },
+                { "awsAssumeRole", bool.FalseString }
+            };
+
+            if (scope.HealthCheckContainer != null)
+            {
+                serviceMessageProperties.Add("healthCheckContainerImageFeedIdOrName", scope.HealthCheckContainer.FeedIdOrName);
+                serviceMessageProperties.Add("healthCheckContainerImage", scope.HealthCheckContainer.ImageNameAndTag);
+            }
+
+            var expectedServiceMessage = new ServiceMessage(
+                KubernetesDiscoveryCommand.CreateKubernetesTargetServiceMessageName,
+                serviceMessageProperties);
+
+            Log.ServiceMessages.Should()
+                .ContainSingle(s => s.Properties["name"] == targetName)
+                .Which.Should()
+                .BeEquivalentTo(expectedServiceMessage);
+        }
+    }
+}
+#endif
--- source/Calamari.Tests/KubernetesFixtures/Terraform/Clusters/AKS/aks.tf
+++ source/Calamari.Tests/KubernetesFixtures/Terraform/Clusters/AKS/aks.tf
@@ -8,6 +8,7 @@ resource "azurerm_kubernetes_cluster" "default" {
   resource_group_name = azurerm_resource_group.default.name
   location            = "Australia East"
   dns_prefix          = "${random_pet.prefix.id}-k8s"
+  local_account_disabled = true
   
   tags = {
     octopus-environment = "Staging"
@@ -22,6 +23,17 @@ resource "azurerm_kubernetes_cluster" "default" {
   }
 
   role_based_access_control_enabled = true
+
+  azure_active_directory_role_based_access_control {
+    managed                = false
+#    tenant_id              = "3d13e379-e666-469e-ac38-ec6fd61c1166"
+#    admin_group_object_ids =  ["86bfdb0a-eab4-4082-80c2-dda667f6d169", "49ca57c3-a5ae-4d46-96f8-e50324a08cdd"]
+#    azure_rbac_enabled     = true
+
+    client_app_id = var.aks_client_id
+    server_app_id = var.aks_client_id
+    server_app_secret = var.aks_client_secret
+  }
 
   service_principal {
     client_id     = var.aks_client_id
