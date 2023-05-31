80ae48f1f75f34112dc81706a5ee52d7390567e3
veo.chen <veo.chen@octopus.com>
2023-05-31 14:09:45 +1200

    be explicit

--- source/Calamari.Tests/KubernetesFixtures/Terraform/Clusters/AKS/aks.tf
+++ source/Calamari.Tests/KubernetesFixtures/Terraform/Clusters/AKS/aks.tf
@@ -21,6 +21,7 @@ resource "azurerm_kubernetes_cluster" "default" {
     os_disk_size_gb = 30
   }
 
+  local_account_disabled            = false
   role_based_access_control_enabled = true
 
   service_principal {
