d50cbd7271332903e524b0ba0ca09621e3512f26
Scott Merchant <scott.merchant@octopus.com>
2023-06-05 17:35:20 +0930

    Fix build stuff

--- source/Calamari/Kubernetes/Commands/KubernetesDeploymentCommandBase.cs
+++ source/Calamari/Kubernetes/Commands/KubernetesDeploymentCommandBase.cs
@@ -65,8 +65,13 @@ namespace Calamari.Kubernetes.Commands
         protected virtual IEnumerable<IInstallConvention> CommandSpecificInstallConventions() =>
             Enumerable.Empty<IInstallConvention>();
 
+        /// <remarks>
+        /// This empty implementation uses Task.FromResult(new object()); instead of
+        /// Task.CompletedTask because Task.CompletedTask was only added in .NET 4.6.1
+        /// so it is not compatible with Calamari.
+        /// </remarks>
         protected virtual async Task ExecuteCommand(RunningDeployment runningDeployment) =>
-            await Task.CompletedTask;
+            await Task.FromResult(new object());
 
         public override int Execute(string[] commandLineArguments)
         {
