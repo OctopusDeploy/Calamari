c05945c8be5d72b6e5fdf519529f1a3ea1f95c96
Alastair Pitts <alastair.pitts@octopus.com>
2023-07-10 16:17:01 +1000

    Better handle errors when resource groups don't exist

--- source/Calamari.Azure/Kubernetes/Discovery/AzureKubernetesDiscoverer.cs
+++ source/Calamari.Azure/Kubernetes/Discovery/AzureKubernetesDiscoverer.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using Calamari.Common.Features.Discovery;
 using Calamari.Common.Plumbing.Logging;
+using Microsoft.Rest.Azure;
 
 namespace Calamari.Azure.Kubernetes.Discovery
 {
@@ -34,14 +36,44 @@ namespace Calamari.Azure.Kubernetes.Discovery
             Log.Verbose($"  Client ID: {account.ClientId}");
             var azureClient = account.CreateAzureClient();
 
-            return azureClient.KubernetesClusters
-                              .List()
-                              .Select(c => KubernetesCluster.CreateForAks(
-                                  $"aks/{account.SubscriptionNumber}/{c.ResourceGroupName}/{c.Name}",
-                                  c.Name,
-                                  c.ResourceGroupName,
-                                  authenticationDetails.AccountId,
-                                  c.Tags.ToTargetTags()));
+            var discoveredClusters = new List<KubernetesCluster>();
+
+            // There appears to be an issue where the azure client returns stale data
+            // We need to upgrade this to use the newer SDK, but we need to upgrade to .NET 4.6.2 to support that.
+            var resourceGroups = azureClient.ResourceGroups.List();
+            //we don't care about resource groups that are being deleted 
+            foreach (var resourceGroup in resourceGroups.Where(rg => rg.ProvisioningState != "Deleting"))
+            {
+                try
+                {
+                    // There appears to be an issue where the azure client returns stale data
+                    // to mitigate this, specifically for scenario's where the resource group doesn't exist anymore
+                    // we specifically list the clusters in each resource group
+                    var clusters = azureClient.KubernetesClusters.ListByResourceGroup(resourceGroup.Name);
+
+                    discoveredClusters.AddRange(
+                                                clusters
+                                                    .Select(c => KubernetesCluster.CreateForAks(
+                                                                                                $"aks/{account.SubscriptionNumber}/{c.ResourceGroupName}/{c.Name}",
+                                                                                                c.Name,
+                                                                                                c.ResourceGroupName,
+                                                                                                authenticationDetails.AccountId,
+                                                                                                c.Tags.ToTargetTags())));
+                }
+                catch (CloudException ex)
+                {
+                    Log.Verbose($"Failed to list kubernetes clusters for resource group {resourceGroup.Name}. Response message: {ex.Message}, Status code: {ex.Response.StatusCode}");
+                    
+                    // if the resource group was not found, we don't care and move on
+                    if (ex.Response.StatusCode == HttpStatusCode.NotFound && ex.Message.StartsWith("Resource group"))
+                        continue;
+
+                    //throw in all other scenario's
+                    throw;
+                }
+            }
+
+            return discoveredClusters;
         }
     }
 }
