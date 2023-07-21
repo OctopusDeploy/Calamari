72ebf0fd8a2f3abcf24acfa4fb466b72e90f0749
hnrkndrssn <henrik@henrikandersson.org>
2023-07-21 12:09:50 +1000

    chore(tests): attempt to only ignore failing proxy test for dotnet-script

--- source/Calamari.Tests/Fixtures/DotnetScript/DotnetScriptProxyFixture.cs
+++ source/Calamari.Tests/Fixtures/DotnetScript/DotnetScriptProxyFixture.cs
@@ -11,19 +11,18 @@ namespace Calamari.Tests.Fixtures.DotnetScript
     [RequiresDotNetCore]
     public class DotnetScriptProxyFixture : WindowsScriptProxyFixtureBase
     {
-        [SetUp]
-        public void Setup()
+        protected override CalamariResult RunScript()
+        {
+            return RunScript("Proxy.csx").result;
+        }
+
+        public override void Initialize_HasSystemProxy_UseSystemProxyWithExceptions()
         {
             // If the HTTP_PROXY environment variable have been set .NET Core will only take entries in the NO_PROXY environment variable into account,
             // it will not look at Exceptions listed under Proxy Settings in Internet Options on Windows.
             // As the NO_PROXY environment variable is set to a fixed list of addresses, the Initialize_HasSystemProxy_UseSystemProxyWithExceptions test fails
             // as the address we want to bypass the proxy doesn't isn't configured. 
             Assert.Ignore("Some proxy tests currently fail with dotnet-script, currently ignoring them until this has been addressed.");
-        }
-        
-        protected override CalamariResult RunScript()
-        {
-            return RunScript("Proxy.csx").result;
         }
 
         protected override bool TestWebRequestDefaultProxy => true;
