075905be1ce5aace4e6300d1f83763fdce636d45
Alastair Pitts <alastair.pitts@octopus.com>
2023-08-07 09:44:28 +1000

    Reduce CloudFormation name length

--- source/Calamari.Tests/AWS/CloudFormation/CloudFormationFixture.cs
+++ source/Calamari.Tests/AWS/CloudFormation/CloudFormationFixture.cs
@@ -12,7 +12,7 @@ namespace Calamari.Tests.AWS.CloudFormation
     [Category(TestCategory.RunOnceOnWindowsAndLinux)]
     public class CloudFormationFixture
     {
-        string GenerateStackName() => $"calamari-cloudformationfixture-{Guid.NewGuid():N}";
+        string GenerateStackName() => $"calamari-cloudformation-{Guid.NewGuid():N}";
 
         [Test]
         public async Task CreateOrUpdateCloudFormationTemplate()
