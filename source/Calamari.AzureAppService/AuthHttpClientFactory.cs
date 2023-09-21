23a120a1eff65402bf0fca03a2f7e30f9a63b923
Chris Kim <chris.kim@octopus.com>
2023-09-22 02:22:30 +1200

    Use default proxy settings when authenticating with MSAL

---
+++ source/Calamari.AzureAppService/AuthHttpClientFactory.cs
@@ -0,0 +1,27 @@
+using System.Net;
+using System.Net.Http;
+using Microsoft.Identity.Client;
+
+namespace Calamari.AzureAppService
+{
+    public class AuthHttpClientFactory : IMsalHttpClientFactory
+    {
+        static readonly HttpClient _httpClient;
+
+        static AuthHttpClientFactory()
+        {
+            var proxyHttpClientHandler = new HttpClientHandler
+            {
+                Proxy = WebRequest.DefaultWebProxy,
+                UseProxy = true,
+            };
+
+            _httpClient = new HttpClient(proxyHttpClientHandler);
+        }
+
+        public HttpClient GetHttpClient()
+        {
+            return _httpClient;
+        }
+    }
+}
