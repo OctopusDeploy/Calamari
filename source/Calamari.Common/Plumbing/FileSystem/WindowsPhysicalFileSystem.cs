fefa800693fbcbe55c3288535fd2bf32e06dd0b0
Scott Merchant <scott.merchant@octopus.com>
2023-11-17 14:55:06 +1030

    Remove AlphaFS

--- source/Calamari.Common/Plumbing/FileSystem/WindowsPhysicalFileSystem.cs
+++ source/Calamari.Common/Plumbing/FileSystem/WindowsPhysicalFileSystem.cs
@@ -5,32 +5,18 @@ namespace Calamari.Common.Plumbing.FileSystem
 {
     public class WindowsPhysicalFileSystem : CalamariPhysicalFileSystem
     {
-        public WindowsPhysicalFileSystem()
-        {
-#if USE_ALPHAFS_FOR_LONG_FILE_PATH_SUPPORT
-            File = new LongPathsFile();
-            Directory = new LongPathsDirectory();
-#endif
-        }
-
         [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool GetDiskFreeSpaceEx(string lpDirectoryName, out ulong lpFreeBytesAvailable, out ulong lpTotalNumberOfBytes, out ulong lpTotalNumberOfFreeBytes);
 
         public override bool GetDiskFreeSpace(string directoryPath, out ulong totalNumberOfFreeBytes)
         {
-            ulong freeBytesAvailable;
-            ulong totalNumberOfBytes;
-
-            return GetDiskFreeSpaceEx(directoryPath, out freeBytesAvailable, out totalNumberOfBytes, out totalNumberOfFreeBytes);
+            return GetDiskFreeSpaceEx(directoryPath, out var _, out var __, out totalNumberOfFreeBytes);
         }
 
         public override bool GetDiskTotalSpace(string directoryPath, out ulong totalNumberOfBytes)
         {
-            ulong freeBytesAvailable;
-            ulong totalNumberOfFreeBytes;
-
-            return GetDiskFreeSpaceEx(directoryPath, out freeBytesAvailable, out totalNumberOfBytes, out totalNumberOfFreeBytes);
+            return GetDiskFreeSpaceEx(directoryPath, out var _, out totalNumberOfBytes, out var __);
         }
     }
 }
