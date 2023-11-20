8d1a7626ca7bbbea8f1b7a1b211e9e987bbd4587
Scott Merchant <scott.merchant@octopus.com>
2023-11-20 13:23:04 +1030

    Fix build

--- source/Calamari.Common/Plumbing/Pipeline/IBehaviourExtensions.cs
+++ source/Calamari.Common/Plumbing/Pipeline/IBehaviourExtensions.cs
@@ -7,24 +7,7 @@
     {
         public static Task CompletedTask(this IBehaviour _)
         {
-#if NETSTANDARD
             return Task.CompletedTask;
-#elif NET452
-            return Task.FromResult(0);
-#else
-            return Net40CompletedTask;
-#endif
         }
-
-#if NET40
-        static readonly Task Net40CompletedTask = CreateNet40CompletedTask();
-
-        static Task<int> CreateNet40CompletedTask()
-        {
-            var taskCompletionSource = new TaskCompletionSource<int>();
-            taskCompletionSource.SetResult(0);
-            return taskCompletionSource.Task;
-        }
-#endif
     }
 }
