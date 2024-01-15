53c99eece42264f75a79498312a9daade0a4dc62
Scott Merchant <97430840+scme0@users.noreply.github.com>
2024-01-16 10:19:31 +1030

    Remove octopus diagnostics (#1211)

--- source/Calamari.Testing/LogParser/ScriptOutputFilter.cs
+++ source/Calamari.Testing/LogParser/ScriptOutputFilter.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Calamari.Common.Plumbing.ServiceMessages;
-using Octopus.Diagnostics;
 
 namespace Calamari.Testing.LogParser
 {
