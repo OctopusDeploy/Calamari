109d0ce3eaeca94c569a077d318f6863fa948045
Scott Merchant <97430840+scme0@users.noreply.github.com>
2024-05-22 14:31:13 +0930

    Update source/Calamari.Common/Plumbing/KubernetesEnvironmentVariables.cs
    Co-authored-by: Alastair Pitts <APErebus@users.noreply.github.com>

--- source/Calamari.Common/Plumbing/KubernetesEnvironmentVariables.cs
+++ source/Calamari.Common/Plumbing/KubernetesEnvironmentVariables.cs
@@ -3,7 +3,7 @@ namespace Calamari.Common.Plumbing
     public static class KubernetesEnvironmentVariables
     {
         public const string KubernetesAgentNamespace = "OCTOPUS__K8STENTACLE__NAMESPACE";
-        public const string KubernetesAgentVolumeFreeBytes = "OCTOPUS__K8STENTACLE__PERSISTENVOLUMEFREEBYTES";
-        public const string KubernetesAgentVolumeTotalBytes = "OCTOPUS__K8STENTACLE__PERSISTENVOLUMETOTALBYTES";
+        public const string KubernetesAgentVolumeFreeBytes = "OCTOPUS__K8STENTACLE__PERSISTENTVOLUMEFREEBYTES";
+        public const string KubernetesAgentVolumeTotalBytes = "OCTOPUS__K8STENTACLE__PERSISTENTVOLUMETOTALBYTES";
     }
 }
