d38642d91f85b04b7469faa3dac21b21a55e6d0e
Alastair Pitts <alastair.pitts@octopus.com>
2024-05-23 10:27:15 +1000

    Create the target directory if it doesn't exist before Tar extraction

--- source/Calamari.Common/Features/Packages/TarPackageExtractor.cs
+++ source/Calamari.Common/Features/Packages/TarPackageExtractor.cs
@@ -2,13 +2,11 @@
 using System.IO;
 using System.Text;
 using Calamari.Common.Plumbing.Logging;
+using Polly;
+using Polly.Retry;
 using SharpCompress.Common;
 using SharpCompress.Readers;
 using SharpCompress.Readers.Tar;
-#if !NET40
-using Polly;
-
-#endif
 
 namespace Calamari.Common.Features.Packages
 {
@@ -25,50 +23,53 @@ namespace Calamari.Common.Features.Packages
 
         public int Extract(string packageFile, string directory)
         {
+            EnsureTargetDirectoryExists(directory);
+
             var files = 0;
-            using (var inStream = new FileStream(packageFile, FileMode.Open, FileAccess.Read))
+            using var inStream = new FileStream(packageFile, FileMode.Open, FileAccess.Read);
+            var compressionStream = GetCompressionStream(inStream);
+            try
             {
-                var compressionStream = GetCompressionStream(inStream);
-                try
+                using var reader = TarReader.Open(compressionStream, new ReaderOptions { ArchiveEncoding = new ArchiveEncoding { Default = Encoding.UTF8 } });
+                while (reader.MoveToNextEntry())
                 {
-                    using (var reader = TarReader.Open(compressionStream, new ReaderOptions { ArchiveEncoding = new ArchiveEncoding { Default = Encoding.UTF8 } }))
-                    {
-                        while (reader.MoveToNextEntry())
-                        {
-                            ProcessEvent(ref files, reader.Entry);
-                            ExtractEntry(directory, reader);
-                        }
-                    }
+                    ProcessEvent(ref files, reader.Entry);
+                    ExtractEntry(directory, reader);
                 }
-                finally
-                {
-                    if (compressionStream != inStream)
-                        compressionStream.Dispose();
-                }
+            }
+            finally
+            {
+                if (compressionStream != inStream)
+                    compressionStream.Dispose();
             }
 
             return files;
         }
 
+        static void EnsureTargetDirectoryExists(string directory) => Directory.CreateDirectory(directory);
+
         void ExtractEntry(string directory, TarReader reader)
         {
-#if NET40
-            reader.WriteEntryToDirectory(directory, new PackageExtractionOptions(log));
-#else
-            var extractAttempts = 10;
-            Policy.Handle<IOException>()
-                .WaitAndRetry(
-                    extractAttempts,
-                    i => TimeSpan.FromMilliseconds(50),
-                    (ex, retry) =>
-                    {
-                        log.Verbose($"Failed to extract: {ex.Message}. Retry in {retry.Milliseconds} milliseconds.");
-                    })
-                .Execute(() =>
-                {
-                    reader.WriteEntryToDirectory(directory, new PackageExtractionOptions(log));
-                });
-#endif
+            var strategy = new ResiliencePipelineBuilder()
+                           .AddRetry(new RetryStrategyOptions
+                           {
+                               ShouldHandle = new PredicateBuilder().Handle<IOException>(),
+                               MaxRetryAttempts = 10,
+                               Delay = TimeSpan.FromMilliseconds(50),
+                               BackoffType = DelayBackoffType.Constant,
+                               OnRetry = args =>
+                                         {
+                                             if (args.Outcome.Exception != null)
+                                             {
+                                                 log.Verbose($"Failed to extract: {args.Outcome.Exception.Message}. Retry in {args.RetryDelay.Milliseconds} milliseconds.");
+                                             }
+
+                                             return default;
+                                         }
+                           })
+                           .Build();
+
+            strategy.Execute(() => reader.WriteEntryToDirectory(directory, new PackageExtractionOptions(log)));
         }
 
         protected virtual Stream GetCompressionStream(Stream stream)
@@ -76,7 +77,7 @@ namespace Calamari.Common.Features.Packages
             return stream;
         }
 
-        protected void ProcessEvent(ref int filesExtracted, IEntry entry)
+        void ProcessEvent(ref int filesExtracted, IEntry entry)
         {
             if (entry.IsDirectory)
                 return;
