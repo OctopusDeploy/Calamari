d7d75db993c7c29a5d0dc5a02541e224d061ba63
Alastair Pitts <alastair.pitts@octopus.com>
2024-06-03 14:34:34 +1000

    Randomly assign region for Azure WebApp container tests

---
+++ source/Calamari.AzureAppService.Tests/RandomAzureRegion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace Calamari.AzureAppService.Tests
+{
+    public static class RandomAzureRegion
+    {
+        static Random random = new Random();
+
+        static string[] regions = new[]
+        {
+            "southeastasia",
+            "centralus",
+            "eastus",
+            "eastus2",
+            "westus",
+            "westus2",
+            "westus3",
+            "australiaeast"
+        };
+
+        public static string GetRandomRegion(params string[] excludedRegions)
+        {
+            var possibleRegions = regions.Except(excludedRegions).ToArray();
+
+            return possibleRegions[random.Next(0, regions.Length)];
+        }
+    }
+}
