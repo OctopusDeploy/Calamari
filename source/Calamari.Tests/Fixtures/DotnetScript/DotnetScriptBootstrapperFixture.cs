b9ba3e8a3da5c8e1397a818618e4246a8aaa99d4
hnrkndrssn <henrik@henrikandersson.org>
2024-06-24 12:59:19 +1000

    chore(tests): add tests for passing arguments to dotnet-script

--- source/Calamari.Tests/Fixtures/DotnetScript/DotnetScriptBootstrapperFixture.cs
+++ source/Calamari.Tests/Fixtures/DotnetScript/DotnetScriptBootstrapperFixture.cs
@@ -1,7 +1,25 @@
+using System;
+using Calamari.Common.Features.Scripting.DotnetScript;
+using FluentAssertions;
+using JetBrains.Annotations;
+using NUnit.Framework;
+
 namespace Calamari.Tests.Fixtures.DotnetScript
 {
+    [TestFixture]
     public class DotnetScriptBootstrapperFixture
     {
-        
+        [TestCase(null, null, null)]
+        [TestCase("-- \"Parameter 1\" \"Parameter 2\"", null, "\"Parameter 1\" \"Parameter 2\"")]
+        [TestCase("\"Parameter 1\" \"Parameter 2\"", null, "\"Parameter 1\" \"Parameter 2\"")]
+        [TestCase("--isolated-load-context -- \"Parameter 1\" \"Parameter 2\"", "--isolated-load-context ", "\"Parameter 1\" \"Parameter 2\"")]
+        [TestCase("--isolated-load-context -d -- \"Parameter 1\" \"Parameter 2\"", "--isolated-load-context -d ", "\"Parameter 1\" \"Parameter 2\"")]
+        [TestCase("--isolated-load-context --verbosity debug -- \"Parameter 1\" \"Parameter 2\"", "--isolated-load-context --verbosity debug ", "\"Parameter 1\" \"Parameter 2\"")]
+        public void FormatCommandArgumentsTest([CanBeNull] string scriptParameters, [CanBeNull] string commandArguments, [CanBeNull] string scriptArguments)
+        {
+            var bootstrapFile = "Bootstrap." + Guid.NewGuid().ToString().Substring(10) + "." + "Script.csx";
+            var formattedCommandArgument = DotnetScriptBootstrapper.FormatCommandArguments(bootstrapFile, scriptParameters);
+            formattedCommandArgument.Should().Contain($"{commandArguments}\"{bootstrapFile}\" -- {scriptArguments}");
+        }
     }
 }
