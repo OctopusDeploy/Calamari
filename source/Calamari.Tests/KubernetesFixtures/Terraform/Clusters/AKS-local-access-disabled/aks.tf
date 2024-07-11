b8ec60d93a4c6951ea541ae66e3fc83afaab9918
Eddy Moulton <eddy.moulton@octopus.com>
2024-07-11 17:18:21 +1000

    Update AKS test config

--- source/Calamari.Tests/KubernetesFixtures/Terraform/Clusters/AKS-local-access-disabled/aks.tf
+++ source/Calamari.Tests/KubernetesFixtures/Terraform/Clusters/AKS-local-access-disabled/aks.tf
@@ -2,7 +2,7 @@ data "azurerm_resource_group" "default" {
   name = "${var.static_resource_prefix}-rg"
 }
 
-data "azurerm_kubernetes_cluster" "default" {
+data "azurerm_kubernetes_cluster" "local_access_disabled" {
   name                = "${var.static_resource_prefix}-aks-no-local"
   resource_group_name = data.azurerm_resource_group.default.name
 }
--- source/Calamari.Tests/KubernetesFixtures/Terraform/Clusters/AKS-local-access-disabled/outputs.tf
+++ source/Calamari.Tests/KubernetesFixtures/Terraform/Clusters/AKS-local-access-disabled/outputs.tf
@@ -1,38 +1,38 @@
 output "aks_cluster_host" {
   description = "Endpoint for AKS control plane."
-  value       = data.azurerm_kubernetes_cluster.default.kube_config.0.host
+  value       = data.azurerm_kubernetes_cluster.local_access_disabled.kube_config.0.host
   sensitive   = true
 }
 
 
 output "aks_cluster_username" {
-  value     = data.azurerm_kubernetes_cluster.default.kube_config.0.username
+  value     = data.azurerm_kubernetes_cluster.local_access_disabled.kube_config.0.username
   sensitive = true
 }
 
 output "aks_cluster_password" {
-  value     = data.azurerm_kubernetes_cluster.default.kube_config.0.password
+  value     = data.azurerm_kubernetes_cluster.local_access_disabled.kube_config.0.password
   sensitive = true
 }
 
 output "aks_cluster_client_certificate" {
-  value     = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)
+  value     = base64decode(data.azurerm_kubernetes_cluster.local_access_disabled.kube_config.0.client_certificate)
   sensitive = true
 }
 
 output "aks_cluster_client_key" {
-  value     = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.client_key)
+  value     = base64decode(data.azurerm_kubernetes_cluster.local_access_disabled.kube_config.0.client_key)
   sensitive = true
 }
 
 output "aks_cluster_ca_certificate" {
-  value     = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)
+  value     = base64decode(data.azurerm_kubernetes_cluster.local_access_disabled.kube_config.0.cluster_ca_certificate)
   sensitive = true
 }
 
 output "aks_cluster_name" {
   description = "AKS name."
-  value       = data.azurerm_kubernetes_cluster.default.name
+  value       = data.azurerm_kubernetes_cluster.local_access_disabled.name
 }
 
 output "aks_rg_name" {
