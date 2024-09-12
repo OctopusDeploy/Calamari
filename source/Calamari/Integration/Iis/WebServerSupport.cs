f53883ec11afad3479d0d4c1e4bf36881fd04511
Robert E <robert.erez@gmail.com>
2024-09-12 23:05:42 +1000

    update iis library to netcore

--- source/Calamari/Integration/Iis/WebServerSupport.cs
+++ source/Calamari/Integration/Iis/WebServerSupport.cs
@@ -1,5 +1,4 @@
-﻿#if IIS_SUPPORT
-using System;
+﻿using System;
 
 namespace Calamari.Integration.Iis
 {
@@ -9,27 +8,5 @@ namespace Calamari.Integration.Iis
         public abstract string GetHomeDirectory(string webSiteName, string virtualDirectoryPath);
         public abstract void DeleteWebSite(string webSiteName);
         public abstract bool ChangeHomeDirectory(string webSiteName, string virtualDirectoryPath, string newWebRootPath);
-
-        public static WebServerSupport Legacy()
-        {
-            return new WebServerSixSupport();            
-        }
-
-        public static WebServerSupport AutoDetect()
-        {
-            // Sources:
-            // http://support.microsoft.com/kb/224609
-            // http://msdn.microsoft.com/en-us/library/windows/desktop/ms724832(v=vs.85).aspx
-
-#pragma warning disable DE0009 // API is deprecated
-            if (Environment.OSVersion.Version.Major < 6)
-#pragma warning restore DE0009 // API is deprecated
-            {
-                return new WebServerSixSupport();
-            }
-
-            return new WebServerSevenSupport();
-        }
     }
 }
-#endif
