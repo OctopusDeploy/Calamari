338cc55a4831d1af409d4ec6293878ae39931509
Alastair Pitts <alastair.pitts@octopus.com>
2024-10-04 14:06:44 +1000

    No longer use deprecated Azure SDK's in Calamari.CloudAccounts

--- source/Calamari.Azure/AzureKnownEnvironment.cs
+++ source/Calamari.Azure/AzureKnownEnvironment.cs
@@ -1,59 +1,46 @@
 ﻿using System;
 using Azure.Identity;
 using Azure.ResourceManager;
-using Microsoft.Azure.Management.ResourceManager.Fluent;
 
 namespace Calamari.Azure
 {
     public sealed class AzureKnownEnvironment
-    { 
+    {
         /// <param name="environment">The environment name exactly matching the names defined in Azure SDK (see here https://github.com/Azure/azure-libraries-for-net/blob/master/src/ResourceManagement/ResourceManager/AzureEnvironment.cs)
         /// Other names are allowed in case this list is ever expanded/changed, but will likely result in an error at deployment time.
         /// </param>
         public AzureKnownEnvironment(string environment)
         {
             Value = environment;
-            
+
             if (string.IsNullOrEmpty(environment) || environment == "AzureCloud") // This environment name is defined in Sashimi.Azure.Accounts.AzureEnvironmentsListAction
-                Value = Global.Value;                                             // We interpret it as the normal Azure environment for historical reasons)
-
-            azureSdkEnvironment = AzureEnvironment.FromName(Value) ??
-                               throw new InvalidOperationException($"Unknown environment name {Value}");
+            {
+                Value = "AzureGlobalCloud"; // We interpret it as the normal Azure environment for historical reasons)
+            }
         }
 
-        private readonly AzureEnvironment azureSdkEnvironment;
         public string Value { get; }
-
-        public static readonly AzureKnownEnvironment Global = new AzureKnownEnvironment("AzureGlobalCloud");
-        public static readonly AzureKnownEnvironment AzureChinaCloud = new AzureKnownEnvironment("AzureChinaCloud");
-        public static readonly AzureKnownEnvironment AzureUSGovernment = new AzureKnownEnvironment("AzureUSGovernment");
-        public static readonly AzureKnownEnvironment AzureGermanCloud = new AzureKnownEnvironment("AzureGermanCloud");
-
-        public AzureEnvironment AsAzureSDKEnvironment()
-        {
-            return azureSdkEnvironment;
-        }
 
         public ArmEnvironment AsAzureArmEnvironment() => ToArmEnvironment(Value);
 
-        private static ArmEnvironment ToArmEnvironment(string name) => name switch
-        {
-            "AzureGlobalCloud" => ArmEnvironment.AzurePublicCloud,
-            "AzureChinaCloud" => ArmEnvironment.AzureChina,
-            "AzureGermanCloud" => ArmEnvironment.AzureGermany,
-            "AzureUSGovernment" => ArmEnvironment.AzureGovernment,
-            _ => throw  new InvalidOperationException($"ARM Environment {name} is not a known Azure Environment name.")
-        };
-
+        static ArmEnvironment ToArmEnvironment(string name) => name switch
+                                                               {
+                                                                   "AzureGlobalCloud" => ArmEnvironment.AzurePublicCloud,
+                                                                   "AzureChinaCloud" => ArmEnvironment.AzureChina,
+                                                                   "AzureGermanCloud" => ArmEnvironment.AzureGermany,
+                                                                   "AzureUSGovernment" => ArmEnvironment.AzureGovernment,
+                                                                   _ => throw new InvalidOperationException($"ARM Environment {name} is not a known Azure Environment name.")
+                                                               };
+        
         public Uri GetAzureAuthorityHost() => ToAzureAuthorityHost(Value);
 
-        private static Uri ToAzureAuthorityHost(string name) => name switch
-        {
-            "AzureGlobalCloud" => AzureAuthorityHosts.AzurePublicCloud,
-            "AzureChinaCloud" => AzureAuthorityHosts.AzureChina,
-            "AzureGermanCloud" => AzureAuthorityHosts.AzureGermany,
-            "AzureUSGovernment" => AzureAuthorityHosts.AzureGovernment,
-            _ => throw new InvalidOperationException($"ARM Environment {name} is not a known Azure Environment name.")
-        };
+        static Uri ToAzureAuthorityHost(string name) => name switch
+                                                        {
+                                                            "AzureGlobalCloud" => AzureAuthorityHosts.AzurePublicCloud,
+                                                            "AzureChinaCloud" => AzureAuthorityHosts.AzureChina,
+                                                            "AzureGermanCloud" => AzureAuthorityHosts.AzureGermany,
+                                                            "AzureUSGovernment" => AzureAuthorityHosts.AzureGovernment,
+                                                            _ => throw new InvalidOperationException($"ARM Environment {name} is not a known Azure Environment name.")
+                                                        };
     }
 }
--- source/Calamari.CloudAccounts/DefaultVariables.cs
+++ source/Calamari.CloudAccounts/DefaultVariables.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Calamari.CloudAccounts
 {
-    static class DefaultVariables
+    public static class DefaultAccountEndpoints
     {
         public const string ResourceManagementEndpoint = "https://management.azure.com/";
         public const string GraphManagementEndpoint = "https://graph.microsoft.com/";
