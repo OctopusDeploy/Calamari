b8ad8d01aaaba5344e8c236247e28b6029e1cf0f
Trent Mohay <trent.mohay@octopus.com>
2024-10-18 09:10:20 +1100

    Removed the azurecloudservice projects and update solution (#1361)

--- source/Calamari.ConsolidateCalamariPackages/MigratedCalamariFlavours.cs
+++ source/Calamari.ConsolidateCalamariPackages/MigratedCalamariFlavours.cs
@@ -11,7 +11,7 @@ namespace Calamari.ConsolidateCalamariPackages
         public static List<string> FullFrameworkOnlyFlavours = new()
         {
             "Calamari.AzureWebApp",
-            "Calamari.AzureCloudService",
+            "Calamari.",
         };
 
         public static List<string> CrossPlatformFlavours = new()
