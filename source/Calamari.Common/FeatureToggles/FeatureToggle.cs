4ac82196129425286f5178f4363c385ef5d2b7f2
Alastair Pitts <alastair.pitts@octopus.com>
2024-10-21 15:15:34 +1100

    Remove unnecessary change

--- source/Calamari.Common/FeatureToggles/FeatureToggle.cs
+++ source/Calamari.Common/FeatureToggles/FeatureToggle.cs
@@ -14,6 +14,6 @@
         AzureRMDeprecationFeatureToggle,
         PreventHelmV2DeploymentsFeatureToggle,
         KubernetesLiveObjectStatusFeatureToggle,
-        KubernetesAuthAwsCliWithExecFeatureToggle,
+        KubernetesAuthAwsCliWithExecFeatureToggle
     }
 }
