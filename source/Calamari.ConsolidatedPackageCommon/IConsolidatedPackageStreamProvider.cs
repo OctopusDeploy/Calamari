c7a1932a8d12827865e646eab1713f38f3a47650
Trent Mohay <trent.mohay@octopus.com>
2025-01-15 12:05:42 +1100

    remove cruft

--- source/Calamari.ConsolidatedPackageCommon/IConsolidatedPackageStreamProvider.cs
+++
@@ -1,9 +0,0 @@
-using System.IO;
-
-namespace Octopus.Server.Orchestration.Targets.Common.BundledPackages.Transferrable
-{
-    public interface IConsolidatedPackageStreamProvider
-    {
-        Stream OpenStream();
-    }
-}
