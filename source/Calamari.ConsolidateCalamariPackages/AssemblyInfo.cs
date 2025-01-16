895e03309a798cbc37f1a9aa4a673b293e135ce3
Trent Mohay <trent.mohay@octopus.com>
2025-01-16 12:35:14 +1100

    not sure - think its getting there

---
+++ source/Calamari.ConsolidateCalamariPackages/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Octopus.Tentacle.Tests")]
+[assembly: InternalsVisibleTo("Octopus.Tentacle.Client.Tests")]
+[assembly: InternalsVisibleTo("Octopus.Tentacle.Tests.Integration")]
+[assembly: InternalsVisibleTo("Octopus.Tentacle.Tests.Integration.Common")]
+[assembly: InternalsVisibleTo("Octopus.Tentacle.Kubernetes.Tests.Integration")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
