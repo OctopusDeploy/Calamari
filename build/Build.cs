987d36c24a9510e2d8e969249408c3502cf9da97
Trent Mohay <trent.mohay@octopus.com>
2025-01-17 14:07:11 +1100

    Fix up the MigratedCalamariPackages

--- build/Build.cs
+++ build/Build.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Calamari.ConsolidateCalamariPackages;
 using NuGet.Packaging;
 using Nuke.Common;
 using Nuke.Common.CI.TeamCity;
--- source/Calamari.ConsolidateCalamariPackages/CalamariPackages.cs
+++ source/Calamari.ConsolidateCalamariPackages/CalamariPackages.cs
@@ -4,23 +4,18 @@ using System.Linq;
 
 namespace Octopus.Calamari.ConsolidatedPackage
 {
-    public class MigratedCalamariFlavours
+    public class CalamariPackages
     {
-        public static List<string> Flavours => FullFrameworkOnlyFlavours.Concat(CrossPlatformFlavours).ToList();
+        public static List<string> Flavours => CrossPlatformPackages;
 
-        public static List<string> FullFrameworkOnlyFlavours = new()
-        {
-            "Calamari.AzureWebApp",
-            "Calamari.",
-        };
-
-        public static List<string> CrossPlatformFlavours = new()
+        static List<string> CrossPlatformPackages = new()
         {
             "Calamari.AzureServiceFabric",
             "Calamari.AzureAppService",
             "Calamari.AzureResourceGroup",
             "Calamari.GoogleCloudScripting",
             "Calamari.AzureScripting",
+            "Calamari.AzureWebApp",
             "Calamari.Terraform"
         };
     }
