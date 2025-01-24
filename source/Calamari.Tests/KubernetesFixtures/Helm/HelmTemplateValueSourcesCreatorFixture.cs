d2ae6f31b186232ae49f78eb93ace251bbdab6e3
Kevin Tchang <kevin.tchang@octopus.com>
2025-01-24 15:06:58 +1100

    fix line ending issue with test

--- source/Calamari.Tests/KubernetesFixtures/Helm/HelmTemplateValueSourcesCreatorFixture.cs
+++ source/Calamari.Tests/KubernetesFixtures/Helm/HelmTemplateValueSourcesCreatorFixture.cs
@@ -254,6 +254,11 @@ secondary.Development.yaml"
             // Assert
             using (var _ = new AssertionScope())
             {
+                // defined as verbatim text to avoid line ending issues when tests are run on both Windows & Linux
+                var expectedKeyValueContent = @"key-1: value-1
+KeyWithNumberValue: 3
+";
+                
                 var inlineTvsFilename = Path.Combine(RootDir, InlineYamlValuesFileWriter.GetFileName(0));
                 var chartTvsFilename = Path.Combine(RootDir, "values/dev.yaml");
                 var keyValuesTvsFilename = Path.Combine(RootDir, KeyValuesValuesFileWriter.GetFileName(2));
@@ -262,7 +267,7 @@ secondary.Development.yaml"
                 filenames.Should().BeEquivalentTo(inlineTvsFilename, chartTvsFilename, keyValuesTvsFilename, packageTvsFilename);
                 
                 fileSystem.Received().WriteAllText(inlineTvsFilename, @"colors: [""red"",""green"",""blue""]");
-                fileSystem.Received().WriteAllText(keyValuesTvsFilename, "key-1: value-1\nKeyWithNumberValue: 3\n");
+                fileSystem.Received().WriteAllText(keyValuesTvsFilename, expectedKeyValueContent);
             }
         }
 
