ea30662581cd4c7ac151e08c331882ad9878b996
Alastair Pitts <alastair.pitts@octopus.com>
2025-01-29 14:19:01 +1100

    Add test for PV

--- source/Calamari/Kubernetes/ResourceStatus/Resources/PersistentVolume.cs
+++ source/Calamari/Kubernetes/ResourceStatus/Resources/PersistentVolume.cs
@@ -8,19 +8,19 @@ namespace Calamari.Kubernetes.ResourceStatus.Resources
 
         public string ReclaimPolicy { get; }
 
-        public string StorageCapacity { get; }
+        public string Capacity { get; }
 
         public PersistentVolume(JObject json, Options options) : base(json, options)
         {
             Status = Field("$.status.phase");
             ReclaimPolicy = Field("$.spec.persistentVolumeReclaimPolicy");
-            StorageCapacity = Field("$.spec.capacity.storage");
+            Capacity = Field("$.spec.capacity.storage");
         }
 
         public override bool HasUpdate(Resource lastStatus)
         {
             var last = CastOrThrow<PersistentVolume>(lastStatus);
-            return last.Status != Status && last.ReclaimPolicy != ReclaimPolicy && last.StorageCapacity != StorageCapacity;
+            return last.Status != Status && last.ReclaimPolicy != ReclaimPolicy && last.Capacity != Capacity;
         }
     }
 }
