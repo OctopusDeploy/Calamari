546b02e1166a414684d1ab96c36470fcd2d03c44
Will Thomson <43223637+wlthomson@users.noreply.github.com>
2025-05-28 13:39:07 +1200

    Strip spaces from script modules (#1494)

---
+++ source/Calamari.Tests/Fixtures/Variables/ScriptVariablesFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Calamari.Common.Plumbing.Variables;
+using Calamari.Common.Plumbing.FileSystem;
+using Calamari.Testing.Helpers;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Calamari.Tests.Fixtures.Variables
+{
+    [TestFixture]
+    public class ScriptVariablesFixture
+    {
+        [Test]
+        [TestCase("MyScriptModule", "MyScriptModule")]
+        [TestCase("My-Script-Module", "MyScriptModule")]
+        [TestCase("My Script Module", "MyScriptModule")]
+        [TestCase("My_Script_Module", "My_Script_Module")]
+        [TestCase("My-Script_ Module", "MyScript_Module")]
+        public void FormatScriptName_ShouldCorrectlyFormatFilenames(string input, string expected)
+        {
+            var result = ScriptVariables.FormatScriptName(input);
+            result.Should().Be(expected);
+        }
+    }
+}
