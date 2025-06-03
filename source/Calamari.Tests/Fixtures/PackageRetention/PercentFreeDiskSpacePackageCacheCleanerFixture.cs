e2c7e2434be1580b4a59f4ce7bddf885e916690e
bec-callow-oct <bec.callow@octopus.com>
2025-06-03 11:50:43 +1000

    Add algorithm to clear package cache based on quantity to keep (#1486)
    * Add quantity to keep calculation for package cache retention
    
    * Quantity to keep 0 means keep all
    
    * Implement least recently used sort
    
    * Add configuration for quantity of versions to keep
    
    * Separate remove package by ID and remove by version logic
    
    * Remove packages by percent free disk space when quantity of versions to keep is zero or null
    
    * Log when no packages are removed
    
    * Refactor to separate cache cleaner logic
    
    * Use cache retention strategy to select which cleaner to run
    
    * Update enum to match server
    
    * Fix cache cleaner run conditions
    
    * Improve logging
    
    ---------
    
    Co-authored-by: Hideaki Murakami <hideaki.murakami@octopus.com>

---
+++ source/Calamari.Tests/Fixtures/PackageRetention/PercentFreeDiskSpacePackageCacheCleanerFixture.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Calamari.Common.Plumbing.Deployment.PackageRetention;
+using Calamari.Common.Plumbing.Variables;
+using Calamari.Deployment.PackageRetention.Caching;
+using Calamari.Deployment.PackageRetention.Model;
+using Calamari.Testing.Helpers;
+using FluentAssertions;
+using NUnit.Framework;
+using Octopus.Versioning.Semver;
+
+namespace Calamari.Tests.Fixtures.PackageRetention
+{
+    [TestFixture]
+    public class PercentFreeDiskSpacePackageCacheCleanerFixture
+    {
+        [OneTimeSetUp]
+        public void TestFixtureSetUp()
+        {
+            Environment.SetEnvironmentVariable("TentacleHome", TestEnvironment.GetTestPath());
+        }
+
+        [OneTimeTearDown]
+        public void TestFixtureTearDown()
+        {
+            Environment.SetEnvironmentVariable("TentacleHome", null);
+        }
+        
+        [Test]
+        [TestCase("0")]
+        [TestCase(null)]
+        public void WhenQuantityOfVersionsToKeepIsNotSet_FindPackagesToRemoveByPercentFreeDiskSpace(string quantityOfVersionsToKeep)
+        {
+            var variables = new CalamariVariables
+            {
+                { "MachinePackageCacheRetentionQuantityOfVersionsToKeep", quantityOfVersionsToKeep },
+                { "MachinePackageCacheRetentionStrategy", "FreeSpace" }
+            };
+
+            var fileSystem = new FileSystemThatHasSpace(500, 5000);
+            var log = new InMemoryLog();
+            var subject = new PercentFreeDiskSpacePackageCacheCleaner(fileSystem, new FirstInFirstOutJournalEntrySort(), variables, log);
+            var result = subject.GetPackagesToRemove(MakeSomeJournalEntries());
+            result.Count().Should().Be(1);
+        }
+        
+        [Test]
+        public void WhenMachinePackageCacheStrategyIsNotFreeSpace_ReturnNoPackages()
+        {
+            var variables = new CalamariVariables
+            {
+                { "MachinePackageCacheRetentionStrategy", "Quantities" },
+            };
+
+            var fileSystem = new FileSystemThatHasSpace(500, 5000);
+            var log = new InMemoryLog();
+            var subject = new PercentFreeDiskSpacePackageCacheCleaner(fileSystem, new FirstInFirstOutJournalEntrySort(), variables, log);
+            var result = subject.GetPackagesToRemove(MakeSomeJournalEntries());
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void WhenThereIsEnoughFreeSpace_NothingIsRemoved()
+        {
+            var variables = new CalamariVariables();
+            var fileSystem = new FileSystemThatHasSpace(1000, 1000);
+            var log = new InMemoryLog();
+            var subject = new PercentFreeDiskSpacePackageCacheCleaner(fileSystem, new FirstInFirstOutJournalEntrySort(), variables, log);
+            var result = subject.GetPackagesToRemove(MakeSomeJournalEntries());
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void WhenFreeingUpAPackageWorthOfSpace_OnePackageIsMarkedForRemoval()
+        {
+            var variables = new CalamariVariables();
+            var fileSystem = new FileSystemThatHasSpace(500, 5000);
+            var log = new InMemoryLog();
+            var subject = new PercentFreeDiskSpacePackageCacheCleaner(fileSystem, new FirstInFirstOutJournalEntrySort(), variables, log);
+            var result = subject.GetPackagesToRemove(MakeSomeJournalEntries());
+            result.Count().Should().Be(1);
+        }
+
+        [Test]
+        public void WhenFreeingUpTwoPackagesWorthOfSpace_TwoPackagesAreMarkedForRemoval()
+        {
+            var variables = new CalamariVariables();
+            var fileSystem = new FileSystemThatHasSpace(1000, 10000);
+            var log = new InMemoryLog();
+            var subject = new PercentFreeDiskSpacePackageCacheCleaner(fileSystem, new FirstInFirstOutJournalEntrySort(), variables, log);
+            var result = subject.GetPackagesToRemove(MakeSomeJournalEntries());
+            result.Count().Should().Be(2);
+        }
+
+        static IEnumerable<JournalEntry> MakeSomeJournalEntries()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                var packageIdentity = new PackageIdentity(new PackageId("HelloWorld"), new SemanticVersion(1, 0, i), new PackagePath($"C:\\{i}.zip"));
+                var packageUsages = new PackageUsages { new UsageDetails(new ServerTaskId($"Deployments-{i}"), new CacheAge(i), DateTime.Now.AddDays(i)) };
+                yield return new JournalEntry(packageIdentity, 1000, packageUsages: packageUsages);
+            }
+        }
+    }
+}
---
+++ source/Calamari.Tests/Fixtures/PackageRetention/UsageDetailsBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Calamari.Common.Plumbing.Deployment.PackageRetention;
+using Calamari.Deployment.PackageRetention.Model;
+
+namespace Calamari.Tests.Fixtures.PackageRetention
+{
+    class UsageDetailsBuilder
+    {
+        CacheAge cacheAgeAtUsage;
+        DateTime dateTime;
+        ServerTaskId deploymentTaskId;
+
+        public UsageDetails Build()
+        {
+            return new UsageDetails(deploymentTaskId, cacheAgeAtUsage, dateTime);
+        }
+
+        public UsageDetailsBuilder WithCacheAgeAtUsage(CacheAge cacheAge)
+        {
+            cacheAgeAtUsage = cacheAge;
+            return this;
+        }
+
+        public UsageDetailsBuilder WithDateTime(DateTime usageDateTime)
+        {
+            dateTime = usageDateTime;
+            return this;
+        }
+
+        public UsageDetailsBuilder WithDeploymentTaskId(ServerTaskId taskId)
+        {
+            deploymentTaskId = taskId;
+            return this;
+        }
+    }
+}
