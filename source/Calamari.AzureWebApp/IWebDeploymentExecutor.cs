32e2bab6b9925339563f2570e01665c92350d0ae
Alastair Pitts <APErebus@users.noreply.github.com>
2025-07-21 21:22:59 +1000

    Update Calamari.AzureWebApp to support .NET 6.0 (again) (#1490)
    * Mark all packages as CrossPlatform now
    
    * Port relevant changes from #1355
    
    * Fix build
    
    * Fix merge
    
    * Remove duplicate line

---
+++ source/Calamari.AzureWebApp/IWebDeploymentExecutor.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Calamari.Azure.AppServices;
+using Calamari.AzureWebApp.Integration.Websites.Publishing;
+using Calamari.Common.Commands;
+using Calamari.Common.Plumbing.Variables;
+
+namespace Calamari.AzureWebApp
+{
+    public interface IWebDeploymentExecutor
+    {
+        Task ExecuteDeployment(RunningDeployment deployment,
+                               AzureTargetSite targetSite,
+                               IVariables variables,
+                               WebDeployPublishSettings publishSettings);
+    }
+}
