46e079e85e8c9f0f598e61527a6fd5592e95f1b0
Alastair Pitts <alastair.pitts@octopus.com>
2025-08-06 13:51:02 +1000

    Upgrade to .NET 8.0

--- build/Frameworks.cs
+++ build/Frameworks.cs
@@ -6,5 +6,6 @@ namespace Calamari.Build
     {
         public const string Net462 = "net462";
         public const string Net60 = "net6.0";
+        public const string Net80 = "net8.0";
     }
 }
