d026fb1ce52753054b5b1ff221326e1c5cc3ec3c
Alastair Pitts <alastair.pitts@octopus.com>
2025-08-12 14:15:45 +1000

    Support previous parameters due to V2

--- source/Calamari.Common/Plumbing/Commands/CommonOptions.cs
+++ source/Calamari.Common/Plumbing/Commands/CommonOptions.cs
@@ -33,6 +33,19 @@ namespace Calamari.Common.Plumbing.Commands
                       .Add("variablesPassword=", "Password used to decrypt variables.", v => options.InputVariables.VariablesPassword = v)
                       .Add("outputVariables=", "Path to a encrypted JSON file containing output variables from previous executions.", v => options.InputVariables.OutputVariablesFile = v)
                       .Add("outputVariablesPassword=", "Password used to decrypt output variables", v => options.InputVariables.OutputVariablesPassword = v);
+
+            //these are legacy options to support the V2 pipeline
+            set.Add("sensitiveVariables=", "(DEPRECATED) Path to a encrypted JSON file containing sensitive variables.", v => options.InputVariables.VariableFiles.Add(v))
+               .Add("sensitiveVariablesPassword=",
+                    "(DEPRECATED) Password used to decrypt sensitive variables. Will only be used if variablesPassword is not set.",
+                    v =>
+                    {
+                        if (string.IsNullOrEmpty(options.InputVariables.VariablesPassword))
+                        {
+                            options.InputVariables.VariablesPassword = v;
+                        }
+                    });
+
             options.RemainingArguments = set.Parse(args.Skip(1));
 
             return options;
