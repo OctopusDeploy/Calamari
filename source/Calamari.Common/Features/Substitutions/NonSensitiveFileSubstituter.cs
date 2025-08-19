cb28143a755099d2c275bc2fb82f62d121a219ac
Alastair Pitts <alastair.pitts@octopus.com>
2025-08-19 11:29:38 +1000

    Add ability to substitute files with non-sensitive variables only

---
+++ source/Calamari.Common/Features/Substitutions/NonSensitiveFileSubstituter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Calamari.Common.Commands;
+using Calamari.Common.Plumbing.FileSystem;
+using Calamari.Common.Plumbing.Logging;
+using Calamari.Common.Plumbing.Variables;
+
+namespace Calamari.Common.Features.Substitutions
+{
+    public class NonSensitiveFileSubstituter : FileSubstituter, INonSensitiveFileSubstituter
+    {
+        public NonSensitiveFileSubstituter(ILog log, ICalamariFileSystem fileSystem, INonSensitiveVariables variables)
+            : base(log, fileSystem, variables)
+        {
+        }
+
+        protected override void PerformSubstitutionAndUpdateFile(string sourceFile, string targetFile, bool throwOnError)
+        {
+            try
+            {
+                //We always want to throw when substitution fails
+                base.PerformSubstitutionAndUpdateFile(sourceFile, targetFile, true);
+            }
+            catch (Exception e)
+            {
+                throw new CommandException($"{e.Message}{Environment.NewLine}This may be due to sensitive variables in use");
+            }
+        }
+    }
+}
