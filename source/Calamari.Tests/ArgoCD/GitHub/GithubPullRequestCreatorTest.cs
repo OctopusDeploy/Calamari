41fc712a10a3acd53ebc5c7864499af3e86d8cf4
Trent Mohay <trent.mohay@octopus.com>
2025-08-29 10:55:44 +1000

    Port Create PR Logic from server in commit-to-git step (#1540)

---
+++ source/Calamari.Tests/ArgoCD/GitHub/GithubPullRequestCreatorTest.cs
@@ -0,0 +1,38 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Calamari.ArgoCD.Git;
+using Calamari.ArgoCD.GitHub;
+using Calamari.Testing.Helpers;
+using Calamari.Tests.ArgoCD.Git;
+using NUnit.Framework;
+
+namespace Calamari.Tests.ArgoCD.GitHub
+{
+    //Reintroduce this if you want to test the actual GH PR creation
+    //[TestFixture]
+    public class GithubPullRequestCreatorTest
+    {
+        InMemoryLog log = new InMemoryLog();
+        
+        //[Test]
+        public async Task CreatePrInRepo()
+        {
+            GitHubClientFactory gitHubClientFactory = new GitHubClientFactory();
+            GitHubPullRequestCreator prCreator = new GitHubPullRequestCreator(gitHubClientFactory);
+
+            var connection = new GitConnection(
+                                              "yourGithubUsername",
+                                              "ADD_PAT_HERE",
+                                              "https://github.com/rain-on/PopulatedArgoCd.git",
+                                              new GitBranchName("NOT_USED_IN_THIS_TEST"));
+
+            await prCreator.CreatePullRequest(log,
+                                        connection,
+                                        "The Title",
+                                        "The body",
+                                        new GitBranchName("blork"),
+                                        new GitBranchName("main"),
+                                        CancellationToken.None);
+        }
+    }
+}
