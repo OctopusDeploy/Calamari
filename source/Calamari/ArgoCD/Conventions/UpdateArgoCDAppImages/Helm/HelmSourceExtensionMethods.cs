c8c91b381dce5abb7e2e3176d9989d02eacea87c
Trent Mohay <trent.mohay@octopus.com>
2025-09-16 18:08:36 +1000

    sorted the helm extension stuff

--- source/Calamari/ArgoCD/Conventions/UpdateArgoCDAppImages/Helm/HelmSourceExtensionMethods.cs
+++ source/Calamari/ArgoCD/Conventions/UpdateArgoCDAppImages/Helm/HelmSourceExtensionMethods.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,9 +11,17 @@ namespace Calamari.ArgoCD.Conventions.UpdateArgoCDAppImages.Helm
     {
         public static string GenerateInlineValuesAbsolutePath(this HelmSource helmSource, string fileName)
         {
-            var pathExtension = $"{helmSource.TargetRevision}/{helmSource.Path}/{fileName}";
-            var path = new Uri(helmSource.RepoUrl, pathExtension);
-            return path.AbsoluteUri;
+            // var path = helmSource.RepositoryUrl.AddPath(helmSource.TargetRevision).AddPath(helmSource.Path).AddPath(fileName);
+            var uriAsString = helmSource.RepoUrl.ToString(); 
+            var repoURL = uriAsString.EndsWith("/") ? uriAsString : $"{uriAsString}/";
+
+            var path = helmSource.Path.TrimStart('.').TrimStart('/');
+            
+            var builder = new UriBuilder(repoURL);
+            builder.Path += Path.Combine(helmSource.TargetRevision.Trim('/'), path, fileName.Trim('/'));
+            builder.Port = -1; //TODO(tmm): Had to manually unser this :/
+
+            return builder.ToString();
         }
 
         public static IEnumerable<string> GenerateValuesFilePaths(this HelmSource helmSource)
