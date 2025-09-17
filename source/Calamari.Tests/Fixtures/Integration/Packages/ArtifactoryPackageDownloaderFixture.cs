c913070765f74713aa58b9b1369f4290db560d95
Matt Richardson <matt.richardson@octopus.com>
2025-09-18 09:29:20 +1000

    More spots

--- source/Calamari.Tests/Fixtures/Integration/Packages/ArtifactoryPackageDownloaderFixture.cs
+++ source/Calamari.Tests/Fixtures/Integration/Packages/ArtifactoryPackageDownloaderFixture.cs
@@ -41,7 +41,8 @@ namespace Calamari.Tests.Fixtures.Integration.Packages
                     cacheDirectory = Path.Combine(currentDirectory, testDirectory);
                     fileSystem.EnsureDirectoryExists(cacheDirectory);
 
-                    feedUri = new Uri("https://octopusdeploy.jfrog.io");
+                    var authFeedUri = await ExternalVariables.Get(ExternalVariable.ArtifactoryUrl, cancellationToken);
+                    feedUri = new Uri(authFeedUri);
                     var sensitiveValue = await ExternalVariables.Get(ExternalVariable.ArtifactoryAdminToken, cancellationToken);
                     feedCredentials = new NetworkCredential("", sensitiveValue.ToString());
         
