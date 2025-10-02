196c857e0e3080b0fe9531223df7418d7ea614b5
Alastair Pitts <alastair.pitts@octopus.com>
2025-10-03 09:54:06 +1000

    Fix compile issue

--- source/Calamari/ArgoCD/Git/RepositoryWrapper.cs
+++ source/Calamari/ArgoCD/Git/RepositoryWrapper.cs
@@ -54,7 +54,7 @@ namespace Calamari.ArgoCD.Git
         public void RecursivelyStageFilesForRemoval(string subPath)
         {
             var cleansedSubPath = subPath.StartsWith("./") ? subPath.Substring(2) : subPath;
-            if(!cleansedSubPath.EndsWith("/") && !cleansedSubPath.IsNullOrEmpty())
+            if(!cleansedSubPath.EndsWith("/") && !string.IsNullOrEmpty(cleansedSubPath))
             {
                 cleansedSubPath += "/";
             } 
