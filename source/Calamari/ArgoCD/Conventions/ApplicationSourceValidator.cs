8ab5ee5b2b0feaa89fd903b7439bfccfe9c6e289
Frank Lin <frank.lin@octopus.com>
2025-10-20 14:56:29 +1000

    use tinytype

--- source/Calamari/ArgoCD/Conventions/ApplicationSourceValidator.cs
+++ source/Calamari/ArgoCD/Conventions/ApplicationSourceValidator.cs
@@ -1,18 +1,19 @@
 using System.Linq;
 using Calamari.ArgoCD.Domain;
+using Calamari.ArgoCD.Models;
 using Calamari.Common.Commands;
 
 namespace Calamari.ArgoCD.Conventions
 {
-    public class ApplicationSourceValidator
+    public static class ApplicationSourceValidator
     {
         public static void ValidateApplicationSources(Application applicationFromYaml)
         {
-            var groupedByName = applicationFromYaml.Spec.Sources.GroupBy(s => s.Name ?? string.Empty);
+            var groupedByName = applicationFromYaml.Spec.Sources.GroupBy(s => s.Name.ToApplicationSourceName());
 
             foreach (var group in groupedByName)
             {
-                if (!string.IsNullOrWhiteSpace(group.Key) && group.Count() > 1)
+                if (group.Key != null && group.Count() > 1)
                 {
                     throw new CommandException($"Application {applicationFromYaml.Metadata.Name} has multiples sources with the name '{group.Key}'. Please ensure all sources have unique names.");                   
                 }
