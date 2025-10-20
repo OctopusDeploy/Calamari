12efe846e8b53d29b05b0f173006b4c845c17cef
Frank Lin <frank.lin@octopus.com>
2025-10-20 15:19:38 +1000

    fds

--- source/Calamari/ArgoCD/Conventions/ApplicationSourceValidator.cs
+++ source/Calamari/ArgoCD/Conventions/ApplicationSourceValidator.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using Calamari.ArgoCD.Domain;
 using Calamari.ArgoCD.Models;
@@ -7,17 +9,54 @@ namespace Calamari.ArgoCD.Conventions
 {
     public static class ApplicationSourceValidator
     {
-        public static void ValidateApplicationSources(Application applicationFromYaml)
+        public static void ValidateApplicationSources(Application application)
         {
-            var groupedByName = applicationFromYaml.Spec.Sources.GroupBy(s => s.Name.ToApplicationSourceName());
+            var groupedByName = application.Spec.Sources.GroupBy(s => s.Name.ToApplicationSourceName());
 
             foreach (var group in groupedByName)
             {
                 if (group.Key != null && group.Count() > 1)
                 {
-                    throw new CommandException($"Application {applicationFromYaml.Metadata.Name} has multiples sources with the name '{group.Key}'. Please ensure all sources have unique names.");                   
+                    throw new CommandException($"Application {application.Metadata.Name} has multiples sources with the name '{group.Key}'. Please ensure all sources have unique names.");                   
                 }
             }
+        }
+
+        public static IEnumerable<KeyValuePair<string, string>> GetUnnamedAnnotations(IDictionary<string, string> annotations)
+        {
+            var unprocessedKeys = new []
+            {
+                ArgoCDConstants.Annotations.OctopusProjectAnnotationKey(null),
+                ArgoCDConstants.Annotations.OctopusEnvironmentAnnotationKey(null),
+                ArgoCDConstants.Annotations.OctopusTenantAnnotationKey(null)
+            };
+            
+            return annotations.Where(a => unprocessedKeys.Contains(a.Key));
+        }
+
+        public static bool ContainsMultipleSourcesAndUnnamedAnnotations(Application application)
+        {
+            if (application.Spec.Sources.Count <= 1)
+            {
+                return false;
+            }
+
+            if (application.Metadata.Annotations.ContainsKey(ArgoCDConstants.Annotations.OctopusProjectAnnotationKey(null)))
+            {
+                return true;
+            }
+            
+            if (application.Metadata.Annotations.ContainsKey(ArgoCDConstants.Annotations.OctopusEnvironmentAnnotationKey(null)))
+            {
+                return true;
+            }
+
+            if (application.Metadata.Annotations.ContainsKey(ArgoCDConstants.Annotations.OctopusTenantAnnotationKey(null)))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
