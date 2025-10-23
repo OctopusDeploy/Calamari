fa6e314ea07f375ef79b2bdb51fbfd8092fb56f9
Alastair Pitts <alastair.pitts@octopus.com>
2025-10-23 16:59:03 +1100

    Add new test build targets

--- build/Build.NetCoreTesting.cs
+++ build/Build.NetCoreTesting.cs
@@ -27,4 +27,63 @@ partial class Build
                             _ => throw new ProcessException(process)
                         }));
             });
+
+
+    [PublicAPI]
+    Target OncePerWindowsOrLinuxTesting =>
+        target => target
+            .Executes(() =>
+                      {
+                          const string testFilter = "(TestCategory != Windows & TestCategory != PlatformAgnostic) | TestCategory = RunOnceOnWindowsAndLinux";
+
+                          DotNetTasks.DotNetTest(settings => settings
+                                                             .SetProjectFile("Binaries/Calamari.Tests.dll")
+                                                             .SetFilter(testFilter)
+                                                             .SetLoggers("trx")
+                                                             .SetProcessExitHandler(process => process.ExitCode switch
+                                                                                               {
+                                                                                                   0 => null, //successful
+                                                                                                   1 => null, //some tests failed
+                                                                                                   _ => throw new ProcessException(process)
+                                                                                               }));
+                      });
+    
+    
+    [PublicAPI]
+    Target OncePerWindowsTesting =>
+        target => target
+            .Executes(() =>
+                      {
+                          const string testFilter = "TestCategory != macOs & TestCategory != Nix & TestCategory != PlatformAgnostic & TestCategory != nixMacOS & TestCategory != RunOnceOnWindowsAndLinux & TestCategory != ModifiesSystemProxy";
+
+                          DotNetTasks.DotNetTest(settings => settings
+                                                             .SetProjectFile("Binaries/Calamari.Tests.dll")
+                                                             .SetFilter(testFilter)
+                                                             .SetLoggers("trx")
+                                                             .SetProcessExitHandler(process => process.ExitCode switch
+                                                                                               {
+                                                                                                   0 => null, //successful
+                                                                                                   1 => null, //some tests failed
+                                                                                                   _ => throw new ProcessException(process)
+                                                                                               }));
+                      });
+    
+    [PublicAPI]
+    Target WindowsSystemProxyTesting =>
+        target => target
+            .Executes(() =>
+                      {
+                          const string testFilter = "TestCategory = Windows & TestCategory = ModifiesSystemProxy";
+
+                          DotNetTasks.DotNetTest(settings => settings
+                                                             .SetProjectFile("Binaries/Calamari.Tests.dll")
+                                                             .SetFilter(testFilter)
+                                                             .SetLoggers("trx")
+                                                             .SetProcessExitHandler(process => process.ExitCode switch
+                                                                                               {
+                                                                                                   0 => null, //successful
+                                                                                                   1 => null, //some tests failed
+                                                                                                   _ => throw new ProcessException(process)
+                                                                                               }));
+                      });
 }
