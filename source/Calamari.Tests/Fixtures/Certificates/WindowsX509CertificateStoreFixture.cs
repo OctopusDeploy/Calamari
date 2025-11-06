9e1e06c3f8e6318d3656129d5f3820fe958debdc
JT <justin.taylor@octopus.com>
2025-11-07 10:50:05 +1100

    style: clean up using

--- source/Calamari.Tests/Fixtures/Certificates/WindowsX509CertificateStoreFixture.cs
+++ source/Calamari.Tests/Fixtures/Certificates/WindowsX509CertificateStoreFixture.cs
@@ -3,7 +3,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Security.AccessControl;
 using System.Security.Cryptography.X509Certificates;
 using System.Security.Principal;
 using System.Threading;
