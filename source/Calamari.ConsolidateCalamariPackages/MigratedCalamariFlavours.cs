014398bebad5e9f58bad5da5e8b501d7ded0cfb4
JT <justin.taylor@octopus.com>
2025-11-13 14:53:43 +1100

    Push AzureServiceFabric into FullFrameworkOnlyFlvours

--- source/Calamari.ConsolidateCalamariPackages/MigratedCalamariFlavours.cs
+++ source/Calamari.ConsolidateCalamariPackages/MigratedCalamariFlavours.cs
@@ -4,19 +4,19 @@ using System.Linq;
 
 namespace Octopus.Calamari.ConsolidatedPackage
 {
-    public class MigratedCalamariFlavours
+    public static class MigratedCalamariFlavours
     {
         public static List<string> Flavours => FullFrameworkOnlyFlavours.Concat(CrossPlatformFlavours).ToList();
 
         public static List<string> FullFrameworkOnlyFlavours = new()
         {
             "Calamari.AzureWebApp",
+            "Calamari.AzureServiceFabric",
             "Calamari.",
         };
 
         public static List<string> CrossPlatformFlavours = new()
         {
-            "Calamari.AzureServiceFabric",
             "Calamari.AzureAppService",
             "Calamari.AzureResourceGroup",
             "Calamari.GoogleCloudScripting",
