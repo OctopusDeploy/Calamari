d99464f146a8f0811a38e983dfa568dd31802b19
Alastair Pitts <alastair.pitts@octopus.com>
2025-11-27 14:30:24 +1100

    Ignore win7-x86 rids

--- build/Build.cs
+++ build/Build.cs
@@ -530,6 +530,7 @@ partial class Build : NukeBuild
         var allRuntimes = Solution.AllProjects
                                   .SelectMany(p => p.GetRuntimeIdentifiers() ?? Array.Empty<string>())
                                   .Distinct()
+                                  .Where(rid => rid != "win7-x86") //I have no idea where this is coming from, but let's ignore it. My theory is it's coming from the netstandard libs
                                   .ToHashSet();
 
         if (!string.IsNullOrWhiteSpace(TargetRuntime))
