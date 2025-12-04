99f199eb320e1a1199385cacab5d5bc9f8de616d
Alastair Pitts <alastair.pitts@octopus.com>
2025-12-05 09:47:01 +1100

    Add nuke platform agnostic and fix calamari flavour test reporting

--- build/Build.TestCalamariFlavourProject.cs
+++ build/Build.TestCalamariFlavourProject.cs
@@ -1,9 +1,8 @@
-using System.IO;
 using JetBrains.Annotations;
 using Nuke.Common;
-using Nuke.Common.IO;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.CI.TeamCity;
 using Serilog;
 
 namespace Calamari.Build;
@@ -14,47 +13,28 @@ partial class Build
     [Parameter(Name = "VSTest_TestCaseFilter")] readonly string? CalamariFlavourTestCaseFilter;
 
     [PublicAPI]
-    Target TestCalamariFlavourProject => target => target.Executes(async () =>
-    {
-        var testProject = $"Calamari.{CalamariFlavourToTest}.Tests";
+    Target TestCalamariFlavourProject =>
+        target => target
+            .Executes(() =>
+                      {
+                          var testProject = $"Calamari.{CalamariFlavourToTest}.Tests";
 
-        var affectedProjectFile = RootDirectory / "affected.proj";
-        bool isAffected;
-        if (affectedProjectFile.FileExists())
-        {
-            Log.Information("Affected projects analysis found; checking to see if {TestProject} is affected",
-                testProject);
-            var contents = await File.ReadAllTextAsync(affectedProjectFile);
+                          Log.Verbose("{TestProject} tests will be executed", testProject);
 
-            isAffected = contents.Contains(testProject);
-        }
-        else
-        {
-            Log.Information("Affected projects analysis not found; assuming {TestProject} *is* affected", testProject);
-            isAffected = true;
-        }
+                          var runningInTeamCity = TeamCity.Instance is not null;
+                          var outputDirectory = RootDirectory / "outputs";
 
-        if (isAffected)
-        {
-            Log.Verbose("{TestProject} tests will be executed", testProject);
-
-            DotNetTasks.DotNetTest(settings => settings
-                .SetProjectFile($"CalamariTests/{testProject}.dll")
-                .SetFilter(CalamariFlavourTestCaseFilter)
-                .SetProcessExitHandler(
-                    process => process.ExitCode switch
-                    {
-                        0 => null, //successful
-                        1 => null, //some tests failed
-                        _ => throw new ProcessException(process)
-                    }));
-        }
-        else
-        {
-            Log.Information("{TestProject} is not affected, so no tests will be executed", testProject);
-            Log.Information(
-                $"##teamcity[testStarted name='{testProject}-NoTests' captureStandardOutput='false']");
-            Log.Information($"##teamcity[testFinished name='{testProject}-NoTests' duration='0']");
-        }
-    });
+                          DotNetTasks.DotNetTest(settings => settings
+                                                             .SetProjectFile($"CalamariTests/{testProject}.dll")
+                                                             .SetFilter(CalamariFlavourTestCaseFilter)
+                                                             .SetProcessExitHandler(process => process.ExitCode switch
+                                                                                               {
+                                                                                                   0 => null, //successful
+                                                                                                   1 => null, //some tests failed
+                                                                                                   _ => throw new ProcessException(process)
+                                                                                               })
+                                                             .SetTestAdapterPath(outputDirectory)
+                                                             .AddLoggers("console;verbosity=normal")
+                                                             .When(runningInTeamCity, x => x.EnableTeamCityTestLogger(outputDirectory)));
+                      });
 }
