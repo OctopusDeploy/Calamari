00f054d0038540e48bfed02d13a942aadb7735fb
Alastair Pitts <alastair.pitts@octopus.com>
2025-12-08 16:29:10 +1100

    Add support for TeamCity parallel tests

--- build/Build.NetCoreTesting.cs
+++ build/Build.NetCoreTesting.cs
@@ -1,4 +1,9 @@
-﻿using JetBrains.Annotations;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using JetBrains.Annotations;
 using Nuke.Common;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
@@ -14,23 +19,31 @@ partial class Build
             .Executes(() =>
                       {
                           const string testFilter = "TestCategory = PlatformAgnostic";
-                          
+
                           var runningInTeamCity = TeamCity.Instance is not null;
                           var outputDirectory = RootDirectory / "outputs";
 
-                          DotNetTasks.DotNetTest(settings => settings
-                                                             .SetProjectFile("Binaries/Calamari.Tests.dll")
-                                                             .SetFilter(testFilter)
-                                                             .SetProcessExitHandler(process => process.ExitCode switch
-                                                                                               {
-                                                                                                   0 => null, //successful
-                                                                                                   1 => null, //some tests failed
-                                                                                                   _ => throw new ProcessException(process)
-                                                                                               })
-                                                             .SetTestAdapterPath(outputDirectory)
-                                                             .AddLoggers("console;verbosity=normal")
-                                                             .When(runningInTeamCity, x => x.EnableTeamCityTestLogger(outputDirectory)));
+                          var settingsFilePath = TryBuildExcludedTestsSettingsFile();
+
+                          DotNetTasks.DotNetTest(settings =>
+                                                 {
+                                                     settings = settings.SetProjectFile("Binaries/Calamari.Tests.dll");
+
+                                                     settings = settingsFilePath is not null ? settings.SetSettingsFile(settingsFilePath) : settings.SetFilter(testFilter);
+
+                                                     return settings
+                                                            .SetProcessExitHandler(process => process.ExitCode switch
+                                                                                              {
+                                                                                                  0 => null, //successful
+                                                                                                  1 => null, //some tests failed
+                                                                                                  _ => throw new ProcessException(process)
+                                                                                              })
+                                                            .SetTestAdapterPath(outputDirectory)
+                                                            .AddLoggers("console;verbosity=normal")
+                                                            .When(runningInTeamCity, x => x.EnableTeamCityTestLogger(outputDirectory));
+                                                 });
                       });
+
 
     [PublicAPI]
     Target LinuxSpecificTesting =>
@@ -41,19 +54,28 @@ partial class Build
 
                           var runningInTeamCity = TeamCity.Instance is not null;
                           var outputDirectory = RootDirectory / "outputs";
+                          
+                          var settingsFilePath = TryBuildExcludedTestsSettingsFile();
 
-                          DotNetTasks.DotNetTest(settings => settings
-                                                             .SetProjectFile("Binaries/Calamari.Tests.dll")
-                                                             .SetFilter(testFilter)
-                                                             .SetProcessExitHandler(process => process.ExitCode switch
-                                                                                               {
-                                                                                                   0 => null, //successful
-                                                                                                   1 => null, //some tests failed
-                                                                                                   _ => throw new ProcessException(process)
-                                                                                               })
-                                                             .SetTestAdapterPath(outputDirectory)
-                                                             .AddLoggers("console;verbosity=normal")
-                                                             .When(runningInTeamCity, x => x.EnableTeamCityTestLogger(outputDirectory)));
+                          DotNetTasks.DotNetTest(settings =>
+                                                 {
+                                                     settings = settings.SetProjectFile("Binaries/Calamari.Tests.dll");
+
+                                                     settings = settingsFilePath is not null ? settings.SetSettingsFile(settingsFilePath) : settings.SetFilter(testFilter);
+
+                                                     return settings
+                                                            .SetProjectFile("Binaries/Calamari.Tests.dll")
+                                                            .SetFilter(testFilter)
+                                                            .SetProcessExitHandler(process => process.ExitCode switch
+                                                                                              {
+                                                                                                  0 => null, //successful
+                                                                                                  1 => null, //some tests failed
+                                                                                                  _ => throw new ProcessException(process)
+                                                                                              })
+                                                            .SetTestAdapterPath(outputDirectory)
+                                                            .AddLoggers("console;verbosity=normal")
+                                                            .When(runningInTeamCity, x => x.EnableTeamCityTestLogger(outputDirectory));
+                                                 });
                       });
 
     [PublicAPI]
@@ -65,19 +87,28 @@ partial class Build
 
                           var runningInTeamCity = TeamCity.Instance is not null;
                           var outputDirectory = RootDirectory / "outputs";
+                          
+                          var settingsFilePath = TryBuildExcludedTestsSettingsFile();
 
-                          DotNetTasks.DotNetTest(settings => settings
-                                                             .SetProjectFile("Binaries/Calamari.Tests.dll")
-                                                             .SetFilter(testFilter)
-                                                             .SetProcessExitHandler(process => process.ExitCode switch
-                                                                                               {
-                                                                                                   0 => null, //successful
-                                                                                                   1 => null, //some tests failed
-                                                                                                   _ => throw new ProcessException(process)
-                                                                                               })
-                                                             .SetTestAdapterPath(outputDirectory)
-                                                             .AddLoggers("console;verbosity=normal")
-                                                             .When(runningInTeamCity, x => x.EnableTeamCityTestLogger(outputDirectory)));
+                          DotNetTasks.DotNetTest(settings =>
+                                                 {
+                                                     settings = settings.SetProjectFile("Binaries/Calamari.Tests.dll");
+
+                                                     settings = settingsFilePath is not null ? settings.SetSettingsFile(settingsFilePath) : settings.SetFilter(testFilter);
+
+                                                     return settings
+                                                            .SetProjectFile("Binaries/Calamari.Tests.dll")
+                                                            .SetFilter(testFilter)
+                                                            .SetProcessExitHandler(process => process.ExitCode switch
+                                                                                              {
+                                                                                                  0 => null, //successful
+                                                                                                  1 => null, //some tests failed
+                                                                                                  _ => throw new ProcessException(process)
+                                                                                              })
+                                                            .SetTestAdapterPath(outputDirectory)
+                                                            .AddLoggers("console;verbosity=normal")
+                                                            .When(runningInTeamCity, x => x.EnableTeamCityTestLogger(outputDirectory));
+                                                 });
                       });
 
     [PublicAPI]
@@ -89,19 +120,28 @@ partial class Build
 
                           var runningInTeamCity = TeamCity.Instance is not null;
                           var outputDirectory = RootDirectory / "outputs";
+                          
+                          var settingsFilePath = TryBuildExcludedTestsSettingsFile();
 
-                          DotNetTasks.DotNetTest(settings => settings
-                                                             .SetProjectFile("Binaries/Calamari.Tests.dll")
-                                                             .SetFilter(testFilter)
-                                                             .SetProcessExitHandler(process => process.ExitCode switch
-                                                                                               {
-                                                                                                   0 => null, //successful
-                                                                                                   1 => null, //some tests failed
-                                                                                                   _ => throw new ProcessException(process)
-                                                                                               })
-                                                             .SetTestAdapterPath(outputDirectory)
-                                                             .AddLoggers("console;verbosity=normal")
-                                                             .When(runningInTeamCity, x => x.EnableTeamCityTestLogger(outputDirectory)));
+                          DotNetTasks.DotNetTest(settings =>
+                                                 {
+                                                     settings = settings.SetProjectFile("Binaries/Calamari.Tests.dll");
+
+                                                     settings = settingsFilePath is not null ? settings.SetSettingsFile(settingsFilePath) : settings.SetFilter(testFilter);
+
+                                                     return settings
+                                                            .SetProjectFile("Binaries/Calamari.Tests.dll")
+                                                            .SetFilter(testFilter)
+                                                            .SetProcessExitHandler(process => process.ExitCode switch
+                                                                                              {
+                                                                                                  0 => null, //successful
+                                                                                                  1 => null, //some tests failed
+                                                                                                  _ => throw new ProcessException(process)
+                                                                                              })
+                                                            .SetTestAdapterPath(outputDirectory)
+                                                            .AddLoggers("console;verbosity=normal")
+                                                            .When(runningInTeamCity, x => x.EnableTeamCityTestLogger(outputDirectory));
+                                                 });
                       });
 
     [PublicAPI]
@@ -113,18 +153,63 @@ partial class Build
 
                           var runningInTeamCity = TeamCity.Instance is not null;
                           var outputDirectory = RootDirectory / "outputs";
+                          
+                          var settingsFilePath = TryBuildExcludedTestsSettingsFile();
 
-                          DotNetTasks.DotNetTest(settings => settings
-                                                             .SetProjectFile("Binaries/Calamari.Tests.dll")
-                                                             .SetFilter(testFilter)
-                                                             .SetProcessExitHandler(process => process.ExitCode switch
-                                                                                               {
-                                                                                                   0 => null, //successful
-                                                                                                   1 => null, //some tests failed
-                                                                                                   _ => throw new ProcessException(process)
-                                                                                               })
-                                                             .SetTestAdapterPath(outputDirectory)
-                                                             .AddLoggers("console;verbosity=normal")
-                                                             .When(runningInTeamCity, x => x.EnableTeamCityTestLogger(outputDirectory)));
+                          DotNetTasks.DotNetTest(settings =>
+                                                 {
+                                                     settings = settings.SetProjectFile("Binaries/Calamari.Tests.dll");
+
+                                                     settings = settingsFilePath is not null ? settings.SetSettingsFile(settingsFilePath) : settings.SetFilter(testFilter);
+
+                                                     return settings
+                                                            .SetFilter(testFilter)
+                                                            .SetProcessExitHandler(process => process.ExitCode switch
+                                                                                              {
+                                                                                                  0 => null, //successful
+                                                                                                  1 => null, //some tests failed
+                                                                                                  _ => throw new ProcessException(process)
+                                                                                              })
+                                                            .SetTestAdapterPath(outputDirectory)
+                                                            .AddLoggers("console;verbosity=normal")
+                                                            .When(runningInTeamCity, x => x.EnableTeamCityTestLogger(outputDirectory));
+                                                 });
                       });
+    
+    
+    static string? TryBuildExcludedTestsSettingsFile()
+    {
+        var excludedTestsFile = Environment.GetEnvironmentVariable("TeamCityTestExclusionFilePath");
+        if (!string.IsNullOrEmpty(excludedTestsFile))
+        {
+            if (File.Exists(excludedTestsFile))
+            {
+                var testSet = new HashSet<string>();
+
+                using var filestream = File.OpenRead(excludedTestsFile);
+                using var streamReader = new StreamReader(filestream);
+                while (streamReader.ReadLine() is { } line)
+                    if (!line.StartsWith('#'))
+                    {
+                        testSet.Add(line);
+                    }
+
+                var exclusionWhere = string.Join(" and ",
+                                                 testSet.Select(test => $"test != \"{test}\""));
+                var runSettingsFile = $"""
+                                       <RunSettings>
+                                           <NUnit>
+                                               <Where>{exclusionWhere}</Where>
+                                           </NUnit>
+                                       </RunSettings> 
+                                       """;
+
+                var filePath = RootDirectory / "excluded.runSettings";
+                File.WriteAllText(filePath, runSettingsFile);
+                return filePath;
+            }
+        }
+
+        return null;
+    }
 }
