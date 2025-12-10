4d93c4262a131a47da91416bd8fd4edb62508556
Alastair Pitts <alastair.pitts@octopus.com>
2025-12-10 15:22:43 +1100

    Consolidate test settings into single builder

---
+++ build/Build.CalamariTesting.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using JetBrains.Annotations;
+using Nuke.Common;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.CI.TeamCity;
+
+namespace Calamari.Build;
+
+partial class Build
+{
+    [PublicAPI]
+    Target PlatformAgnosticTesting =>
+        target => target
+            .Executes(async () =>
+                      {
+                          var dotnetPath = await LocateOrInstallDotNetSdk();
+
+                          CreateTestRun("Binaries/Calamari.Tests.dll")
+                              .WithDotNetPath(dotnetPath)
+                              .WithFilter("TestCategory = PlatformAgnostic")
+                              .Execute();
+                      });
+
+    [PublicAPI]
+    Target LinuxSpecificTesting =>
+        target => target
+            .Executes(async () =>
+                      {
+                          var dotnetPath = await LocateOrInstallDotNetSdk();
+
+                          CreateTestRun("Binaries/Calamari.Tests.dll")
+                              .WithDotNetPath(dotnetPath)
+                              .WithFilter("TestCategory != Windows & TestCategory != PlatformAgnostic & TestCategory != RunOnceOnWindowsAndLinux")
+                              .Execute();
+                      });
+
+    [PublicAPI]
+    Target OncePerWindowsOrLinuxTesting =>
+        target => target
+            .Executes(async () =>
+                      {
+                          var dotnetPath = await LocateOrInstallDotNetSdk();
+
+                          CreateTestRun("Binaries/Calamari.Tests.dll")
+                              .WithDotNetPath(dotnetPath)
+                              .WithFilter("(TestCategory != Windows & TestCategory != PlatformAgnostic) | TestCategory = RunOnceOnWindowsAndLinux")
+                              .Execute();
+                      });
+
+    [PublicAPI]
+    Target OncePerWindowsTesting =>
+        target => target
+            .Executes(async () =>
+                      {
+                          var dotnetPath = await LocateOrInstallDotNetSdk();
+
+                          CreateTestRun("Binaries/Calamari.Tests.dll")
+                              .WithDotNetPath(dotnetPath)
+                              .WithFilter("TestCategory != macOs & TestCategory != Nix & TestCategory != PlatformAgnostic & TestCategory != nixMacOS & TestCategory != RunOnceOnWindowsAndLinux & TestCategory != ModifiesSystemProxy")
+                              .Execute();
+                      });
+
+    [PublicAPI]
+    Target WindowsSystemProxyTesting =>
+        target => target
+            .Executes(async () =>
+                      {
+                          var dotnetPath = await LocateOrInstallDotNetSdk();
+
+                          CreateTestRun("Binaries/Calamari.Tests.dll")
+                              .WithDotNetPath(dotnetPath)
+                              .WithFilter("TestCategory = Windows & TestCategory = ModifiesSystemProxy")
+                              .Execute();
+                      });
+}
