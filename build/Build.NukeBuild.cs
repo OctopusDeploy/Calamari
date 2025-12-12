c409e49005bb00377ad79c32a76c735b06c0c1fc
Alastair Pitts <alastair.pitts@octopus.com>
2025-12-12 12:31:48 +1100

    Continue refactoring the calamari builds
    # Conflicts:
    #	build/Build.cs

---
+++ build/Build.NukeBuild.cs
@@ -0,0 +1,23 @@
+﻿using Calamari.Build.Utilities;
+
+namespace Calamari.Build;
+
+public partial class Build
+{
+    Target PublishNukeBuild =>
+        d =>
+            d.Executes(() =>
+                       {
+                           const string runtime = "win-x64";
+                           var nukeBuildOutputDirectory = BuildDirectory / "outputs" / runtime / "nukebuild";
+                           nukeBuildOutputDirectory.CreateOrCleanDirectory();
+                           
+                           DotNetPublish(p => p
+                                              .SetProject(RootDirectory / "build" / "_build.csproj")
+                                              .SetConfiguration(Configuration)
+                                              .SetRuntime(runtime)
+                                              .EnableSelfContained());
+                           
+                           Ci.ZipFolderAndUploadArtifact(nukeBuildOutputDirectory, ArtifactsDirectory / $"nukebuild.{runtime}.zip");
+                       }); 
+}
