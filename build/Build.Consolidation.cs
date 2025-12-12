538e68ed8820d3a10b3c484630f5db58ca9b12d6
Alastair Pitts <alastair.pitts@octopus.com>
2025-12-12 12:31:48 +1100

    Continue refactoring the calamari builds

---
+++ build/Build.Consolidation.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.IO.Compression;
+using System.Text.RegularExpressions;
+using JetBrains.Annotations;
+using NuGet.Packaging;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.NuGet;
+using Octopus.Calamari.ConsolidatedPackage;
+
+namespace Calamari.Build;
+
+public partial class Build
+{
+    static readonly List<string> NuGetPackagesToExcludeFromConsolidation =
+    [
+        "Octopus.Calamari.CloudAccounts",
+        "Octopus.Calamari.Common",
+        "Octopus.Calamari.ConsolidateCalamariPackages",
+        "Octopus.Calamari.ConsolidatedPackage",
+        "Octopus.Calamari.ConsolidatedPackage.Api"
+    ];
+    
+    static AbsolutePath ConsolidateCalamariPackagesProject => KnownPaths.SourceDirectory / "Calamari.ConsolidateCalamariPackages.Tests" / "Calamari.ConsolidateCalamariPackages.Tests.csproj";
+    static AbsolutePath ConsolidatedPackageDirectory => KnownPaths.ArtifactsDirectory / "consolidated";
+
+    Target PackageConsolidatedCalamariZip =>
+        d =>
+            d.Executes(() =>
+                       {
+                           var artifacts = Directory.GetFiles(ArtifactsDirectory, "*.nupkg")
+                                                    .Where(a => !NuGetPackagesToExcludeFromConsolidation.Any(a.Contains));
+
+                           var packageReferences = new List<BuildPackageReference>();
+                           foreach (var artifact in artifacts)
+                           {
+                               using var zip = ZipFile.OpenRead(artifact);
+                               var nuspecFileStream = zip.Entries.First(e => e.Name.EndsWith(".nuspec")).Open();
+                               var nuspecReader = new NuspecReader(nuspecFileStream);
+                               var metadata = nuspecReader.GetMetadata().ToList();
+                               packageReferences.Add(new BuildPackageReference
+                               {
+                                   Name = Regex.Replace(metadata.Where(kvp => kvp.Key == "id").Select(i => i.Value).First(), @"^Octopus\.", ""),
+                                   Version = metadata.Where(kvp => kvp.Key == "version").Select(i => i.Value).First(),
+                                   PackagePath = artifact
+                               });
+                           }
+
+                           foreach (var flavour in GetCalamariFlavours())
+                           {
+                               if (Solution.GetProject(flavour) != null)
+                               {
+                                   packageReferences.Add(new BuildPackageReference
+                                   {
+                                       Name = flavour,
+                                       Version = NugetVersion.Value,
+                                       PackagePath = ArtifactsDirectory / $"{flavour}.zip"
+                                   });
+                               }
+                           }
+
+                           Directory.CreateDirectory(ConsolidatedPackageDirectory);
+                           var (result, packageFilename) = new Consolidate(Log.Logger).Execute(ConsolidatedPackageDirectory, packageReferences);
+
+                           if (!result)
+                               throw new Exception("Failed to consolidate calamari Packages");
+
+                           ConsolidatedPackagePath = packageFilename;
+                           Log.Information("Created consolidated package zip: {PackageFilename}", packageFilename);
+                       });
+
+    Target CalamariConsolidationVerification =>
+        d =>
+            d.DependsOn(PackageConsolidatedCalamariZip)
+             .OnlyWhenDynamic(() => string.IsNullOrEmpty(TargetRuntime), "TargetRuntime is not restricted")
+             .Executes(() =>
+                       {
+                           Environment.SetEnvironmentVariable("CONSOLIDATED_ZIP", ConsolidatedPackagePath);
+                           Environment.SetEnvironmentVariable("EXPECTED_VERSION", NugetVersion.Value);
+                           Environment.SetEnvironmentVariable("IS_WINDOWS", OperatingSystem.IsWindows().ToString());
+
+                           DotNetTest(s => s
+                                           .SetProjectFile(ConsolidateCalamariPackagesProject)
+                                           .SetConfiguration(Configuration)
+                                           .SetProcessArgumentConfigurator(args =>
+                                                                               args.Add("--logger:\"console;verbosity=detailed\"")
+                                                                                   .Add("--")
+                                                                                   .Add("NUnit.ShowInternalProperties=true")));
+                       });
+
+    [PublicAPI]
+    Target PackCalamariConsolidatedNugetPackage =>
+        d =>
+            d.DependsOn(CalamariConsolidationVerification)
+             .Executes(() =>
+                       {
+                           NuGetPack(s => 
+                                         s.SetTargetPath(BuildDirectory / "Calamari.Consolidated.nuspec")
+                                           .SetBasePath(BuildDirectory)
+                                           .SetVersion(NugetVersion.Value)
+                                           .SetOutputDirectory(ArtifactsDirectory));
+                       });
+}
--- build/KnownPaths.cs
+++ build/KnownPaths.cs
@@ -6,6 +6,15 @@ namespace Calamari.Build;
 public static class KnownPaths
 {
     public static AbsolutePath RootDirectory => NukeBuild.RootDirectory;
+    
+    public static AbsolutePath SourceDirectory => RootDirectory / "source";
+
+    public static AbsolutePath BuildDirectory => RootDirectory / "builds";
+
+    public static AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+
+    public static AbsolutePath PublishDirectory => RootDirectory / "publish";
+    
     public static AbsolutePath OutputsDirectory => RootDirectory / "outputs";
 
 }
