2ae09dd7cd17c5ae6b9ef8838ea851aec12ec26a
Frank Lin <frank.lin@octopus.com>
2026-01-05 09:30:19 +1000

    fds

--- source/Calamari/ArgoCD/Conventions/UpdateArgoCDAppImagesInstallConvention.cs
+++ source/Calamari/ArgoCD/Conventions/UpdateArgoCDAppImagesInstallConvention.cs
@@ -84,10 +84,8 @@ namespace Calamari.ArgoCD.Conventions
                                                        applicationFromYaml.Spec.Sources.Count(s => ScopingAnnotationReader.GetScopeForApplicationSource(s.Name.ToApplicationSourceName(), applicationFromYaml.Metadata.Annotations, containsMultipleSources) == deploymentScope)));
 
                 var didUpdateSomething = false;
-                for (var index = 0; index < applicationFromYaml.Spec.Sources.Count; index++)
+                foreach (var applicationSource in applicationFromYaml.Spec.Sources)
                 {
-                    var applicationSource = applicationFromYaml.Spec.Sources[index];
-
                     var annotatedScope = ScopingAnnotationReader.GetScopeForApplicationSource(applicationSource.Name.ToApplicationSourceName(), applicationFromYaml.Metadata.Annotations, containsMultipleSources);
                     log.LogApplicationSourceScopeStatus(annotatedScope, applicationSource.Name.ToApplicationSourceName(), deploymentScope);
                     if (annotatedScope == deploymentScope)
