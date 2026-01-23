6481c4c3b461f696124558dc587b87adc92ef748
kube89 <34094131+kube89@users.noreply.github.com>
2026-01-23 23:05:10 +1100

    add Octopus.Action.Script.PreloadScriptModules for selective Bash module preloading

---
+++ source/Calamari.Common/Features/Scripting/Bash/BashScriptVariables.cs
@@ -0,0 +1,7 @@
+namespace Calamari.Common.Features.Scripting.Bash
+{
+    public static class BashScriptVariables
+    {
+        public static readonly string PreloadScriptModules = "Octopus.Action.Script.PreloadScriptModules";
+    }
+}
