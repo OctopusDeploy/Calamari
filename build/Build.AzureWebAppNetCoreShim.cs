f3bd75acfec9d09b2631ce0a7b32fc8886870499
Alastair Pitts <APErebus@users.noreply.github.com>
2026-01-28 14:02:12 +1100

    Split Build.cs into separate files (#1725)
    * Split Build.cs into separate files
    
    * Fix build directory path
    
    * Use KnownPaths everywhere

---
+++ build/Build.AzureWebAppNetCoreShim.cs
@@ -0,0 +1,41 @@
+﻿namespace Calamari.Build;
+
+public partial class Build
+{
+    Target PublishAzureWebAppNetCoreShim =>
+        _ => _.DependsOn(RestoreSolution)
+              .DependsOn(GetCalamariFlavourProjectsToPublish)
+              //we only build the net core shim when there is the AzureWebApp project is being built
+              .OnlyWhenDynamic(() => CalamariProjects.Any(p => p.Name == "Calamari.AzureWebApp"))
+              .Executes(() =>
+                        {
+                            if (!OperatingSystem.IsWindows())
+                            {
+                                Log.Warning("Unable to build Calamari.AzureWebApp.NetCoreShim as it's a Full Framework application and can only be compiled on Windows");
+                                return;
+                            }
+
+                            var project = Solution.GetProject("Calamari.AzureWebApp.NetCoreShim");
+                            if (project is null)
+                            {
+                                Log.Error("Failed to find Calamari.AzureWebApp.NetCoreShim project");
+                                return;
+                            }
+
+                            var outputPath = KnownPaths.PublishDirectory / project.Name;
+
+                            //as this is the only Net 4.6.2 application left, we do a build and restore here
+                            DotNetPublish(s => s
+                                               .SetConfiguration(Configuration)
+                                               .SetProject(project.Path)
+                                               .SetFramework("net462")
+                                               .SetVersion(NugetVersion.Value)
+                                               .SetInformationalVersion(OctoVersionInfo.Value?.InformationalVersion)
+                                               .SetOutput(outputPath));
+
+                            var archivePath = KnownPaths.SourceDirectory / "Calamari.AzureWebApp" / "netcoreshim" / "netcoreshim.zip";
+                            archivePath.DeleteFile();
+
+                            outputPath.CompressTo(archivePath);
+                        });
+}
---
+++ build/Build.LocalPackages.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Calamari.Build;
+
+public partial class Build
+{
+    static AbsolutePath LocalPackagesDirectory => KnownPaths.RootDirectory / ".." / "LocalPackages";
+    
+    Target CopyToLocalPackages =>
+        d =>
+            d.Requires(() => IsLocalBuild)
+             .DependsOn(PublishCalamariProjects)
+             .Executes(() =>
+                       {
+                           Directory.CreateDirectory(LocalPackagesDirectory);
+                           foreach (AbsolutePath file in Directory.GetFiles(KnownPaths.ArtifactsDirectory, "Octopus.Calamari.*.nupkg"))
+                           {
+                               var target = LocalPackagesDirectory / Path.GetFileName(file);
+                               file.Copy(target, ExistsPolicy.FileOverwrite);
+                           }
+                       });
+
+    Target UpdateCalamariVersionOnOctopusServer =>
+        d =>
+            d.OnlyWhenStatic(() => SetOctopusServerVersion)
+             .Requires(() => IsLocalBuild)
+             .DependsOn(CopyToLocalPackages)
+             .Executes(() =>
+                       {
+                           var serverProjectFile = KnownPaths.RootDirectory / ".." / "OctopusDeploy" / "source" / "Octopus.Server" / "Octopus.Server.csproj";
+                           if (File.Exists(serverProjectFile))
+                           {
+                               Log.Information("Setting Calamari version in Octopus Server "
+                                               + "project {ServerProjectFile} to {NugetVersion}",
+                                               serverProjectFile, NugetVersion.Value);
+                               SetOctopusServerCalamariVersion(serverProjectFile);
+                           }
+                           else
+                           {
+                               Log.Warning("Could not set Calamari version in Octopus Server project "
+                                           + "{ServerProjectFile} to {NugetVersion} as could not find "
+                                           + "project file",
+                                           serverProjectFile, NugetVersion.Value);
+                           }
+                       });
+    void SetOctopusServerCalamariVersion(string projectFile)
+    {
+        var text = File.ReadAllText(projectFile);
+        text = Regex.Replace(text, @"<BundledCalamariVersion>([\S])+</BundledCalamariVersion>",
+                             $"<BundledCalamariVersion>{NugetVersion.Value}</BundledCalamariVersion>");
+        File.WriteAllText(projectFile, text);
+    }
+}
