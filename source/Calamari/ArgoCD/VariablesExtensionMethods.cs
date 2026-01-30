d97d7515c35da70ea8a9cb129718bf579a24b441
Trent Mohay <trent.mohay@octopus.com>
2026-01-30 11:17:17 +1100

    removed package purpose from package filtering

--- source/Calamari/ArgoCD/VariablesExtensionMethods.cs
+++ source/Calamari/ArgoCD/VariablesExtensionMethods.cs
@@ -14,7 +14,6 @@ namespace Calamari.ArgoCD
             var packageReferences = (from packageIndex in packageIndexes
                                      let image = variables.Get(PackageVariables.IndexedImage(packageIndex), string.Empty)
                                      let purpose = variables.Get(PackageVariables.IndexedPackagePurpose(packageIndex), string.Empty)
-                                     where purpose.Equals("DockerImageReference", StringComparison.Ordinal)
                                      select image)
                 .ToList();
 
