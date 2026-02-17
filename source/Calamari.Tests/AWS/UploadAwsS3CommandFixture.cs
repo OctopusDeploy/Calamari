d8f2661eba8d2bb946f99051f57d73b0f0d48f22
JT <justin.taylor@octopus.com>
2026-02-17 14:06:41 +1100

    test: Fix outdated assert

--- source/Calamari.Tests/AWS/UploadAwsS3CommandFixture.cs
+++ source/Calamari.Tests/AWS/UploadAwsS3CommandFixture.cs
@@ -268,10 +268,7 @@ namespace Calamari.Tests.AWS
                     CannedAcl = "private",
                     BucketKeyBehaviour = BucketKeyBehaviourType.Filename,
                     Metadata = specialHeaders.Concat(userDefinedMetadata).ToList(),
-                    Tags = new List<KeyValuePair<string, string>>()
-                    {
-                        new KeyValuePair<string, string>("Environment", "Test")
-                    }
+                    Tags = [new KeyValuePair<string, string>("Environment", "Test")]
                 }
             };
 
@@ -287,10 +284,7 @@ namespace Calamari.Tests.AWS
                                {
                                    if (specialHeader.Key == "Expires")
                                    {
-                                       //There's a serialization bug in Json.Net that ends up changing the time to local.
-                                       //Fix this assertion once that's done.
-                                       var expectedDate = DateTime.Parse(specialHeader.Value.TrimEnd('Z')).ToUniversalTime();
-                                       response.Expiration.Should().Be(expectedDate);
+                                       response.ExpiresString.Should().Be(specialHeader.Value);
                                    }
                                    else if (specialHeader.Key == "x-amz-website-redirect-location")
                                    {
