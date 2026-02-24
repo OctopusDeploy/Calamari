b00797958902500730dd660d7ba6b96bf42d09a2
Trent Mohay <trent.mohay@octopus.com>
2026-02-24 12:54:23 +1100

    unrequired change removed

--- source/Calamari/Kubernetes/SpecialVariables.cs
+++ source/Calamari/Kubernetes/SpecialVariables.cs
@@ -75,8 +75,6 @@ namespace Calamari.Kubernetes
             public static readonly string InputPath = "Octopus.Action.ArgoCD.InputPath";
 
             public static readonly string PurgeOutput = "Octopus.Action.ArgoCD.PurgeOutputFolder";
-            
-            public static readonly string UseHelmValueYamlPathFromStep = "Octopus.Action.ArgoCD.UseHelmValueYamlPathFromStep";
 
             public static class PullRequest
             {
