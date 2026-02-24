b98f597a68d3a2f18b71a9b1c49521a92319e4c2
IsaacCalligeros95 <Isaac.Calligeros@octopus.com>
2026-02-24 13:01:14 +1030

    Tidy-up

--- source/Calamari.Tests/Fixtures/Bash/Scripts/no-octopus-parameters.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script that deliberately does NOT use octopus_parameters
-# Used to test that the array is not loaded when not needed
-
-# Use get_octopusvariable instead (uses the switch statement, not the array)
-name=$(get_octopusvariable "PerfSentinel")
-echo "ScriptRan=true"
-echo "SentinelValue=$name"
