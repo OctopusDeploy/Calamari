06f5f43f11e75dcfddf3a530a63d3882f94b41e5
Alastair Pitts <alastair.pitts@octopus.com>
2026-02-26 16:45:20 +1100

    Add TestPlatforms code

---
+++ source/Calamari.Testing/TestPlatformsAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace Calamari.Testing;
+
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+public class TestPlatformsAttribute(TestPlatforms supportedPlatforms) : TestAttribute, ITestAction
+{
+    readonly TestPlatforms supportedPlatforms = supportedPlatforms;
+
+    string GetSkipReason() =>
+        $"This test only runs on: {string.Join(", ", TestPlatformsHelpers.GetSpecificPlatforms(supportedPlatforms))}";
+
+    public void BeforeTest(ITest test)
+    {
+        var currentPlatform = TestPlatformsHelpers.GetCurrentPlatform();
+
+        if ((currentPlatform & supportedPlatforms) == 0)
+        {
+            Assert.Ignore(GetSkipReason());
+        }
+    }
+
+    public void AfterTest(ITest test)
+    {
+    }
+
+    ActionTargets ITestAction.Targets { get; }
+}
---
+++ source/Calamari.Testing/TestPlatformsHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Calamari.Testing;
+
+//Directly taken from the OctopusDeploy repo
+public static class TestPlatformsHelpers
+{
+    static readonly TestPlatforms[] SpecificPlatforms = [TestPlatforms.Windows, TestPlatforms.Linux, TestPlatforms.MacOs];
+
+    /// <summary>Enumerate the specific platforms set in the given <see cref="TestPlatforms" /> bit flags.</summary>
+    public static IEnumerable<TestPlatforms> GetSpecificPlatforms(TestPlatforms flags) =>
+        SpecificPlatforms.Where(platform => (platform & flags) != 0);
+
+    public static TestPlatforms GetCurrentPlatform() =>
+        OperatingSystem.IsLinux() ? TestPlatforms.Linux :
+        OperatingSystem.IsWindows() ? TestPlatforms.Windows :
+        OperatingSystem.IsMacOS() ? TestPlatforms.MacOs :
+        throw new InvalidOperationException("Unknown test platform.");
+}
