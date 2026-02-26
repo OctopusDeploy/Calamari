d89c0206766ef3626c527bae4722db715fc59ee7
JT <justin.taylor@octopus.com>
2026-02-26 15:25:38 +1100

    fix: stop Windows version attributes from excluding non-windows platforms (#1806)

--- source/Calamari.Testing/Requirements/RequiresWindowsServer2012OrAboveAttribute.cs
+++ source/Calamari.Testing/Requirements/RequiresWindowsServer2012OrAboveAttribute.cs
@@ -9,6 +9,11 @@ public class RequiresWindowsServer2012OrAboveAttribute : TestAttribute, ITestAct
 {
     public void BeforeTest(ITest testDetails)
     {
+        // If a test truly requires Windows only, the [WindowsTest] Attribute should also be used
+        if (!OperatingSystem.IsWindows())
+        {
+            return;
+        }
         if (!OperatingSystem.IsWindowsVersionAtLeast(6, 2, 9200))
         {
             Assert.Ignore("Requires Windows Server 2012 or above");
