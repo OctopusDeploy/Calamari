ebabd6c57dbd45b0ffed729fc2ae6fedd3161e30
Rhys Parry <rhys.parry@octopus.com>
2026-03-12 12:01:30 +1000

    More specific IOException handling

--- source/Calamari.Common/Features/Processes/ScriptIsolation/FileLock.cs
+++ source/Calamari.Common/Features/Processes/ScriptIsolation/FileLock.cs
@@ -14,10 +14,34 @@ public static class FileLock
             var fileStream = lockOptions.LockFile.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite, fileShareMode);
             return new LockHandle(fileStream);
         }
-        catch (IOException e)
+        catch (IOException e) when (IsFileLocked(e))
         {
             throw new LockRejectedException(e);
         }
+    }
+
+    const int WindowsErrorSharingViolation = unchecked((int)0x80070020); // ERROR_SHARING_VIOLATION
+    const int LinuxErrorAgainWouldBlock = 11; // EAGAIN / EWOULDBLOCK
+    const int MacOsErrorAgainWouldBlock = 35; // EAGAIN / EWOULDBLOCK
+
+    static bool IsFileLocked(IOException ioException)
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            return ioException.HResult == WindowsErrorSharingViolation;
+        }
+
+        if (OperatingSystem.IsLinux())
+        {
+            return ioException.HResult == LinuxErrorAgainWouldBlock;
+        }
+
+        if (OperatingSystem.IsMacOS())
+        {
+            return ioException.HResult == MacOsErrorAgainWouldBlock;
+        }
+
+        return false;
     }
 
     static FileShare GetFileShareMode(LockType isolationLevel)
