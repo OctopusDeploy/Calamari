e07004d6714f8ba67456dfbe9845b5d16351ac16
Trent Mohay <trent.mohay@octopus.com>
2026-03-27 16:13:27 +1100

    sort that out

---
+++ source/Calamari/Deployment/Conventions/CommitToGitConvention.cs
@@ -0,0 +1,12 @@
+using System;
+using Calamari.Common.Commands;
+
+namespace Calamari.Deployment.Conventions;
+
+public class CommitToGitConvention : IInstallConvention
+{
+    public void Install(RunningDeployment deployment)
+    {
+        
+    }
+}
