a035b4479cc8d61446bb5b7561a6495b4bc3a17d
Trent Mohay <trent.mohay@octopus.com>
2026-04-02 14:30:43 +1100

    what did I do wrong?

--- source/Calamari/ArgoCD/Conventions/ManifestTemplating/ApplicationSourceUpdater.cs
+++ source/Calamari/ArgoCD/Conventions/ManifestTemplating/ApplicationSourceUpdater.cs
@@ -55,7 +55,7 @@ public class ApplicationSourceUpdater
         var applicationSource = sourceWithMetadata.Source;
         log.Info($"Writing files to repository '{applicationSource.OriginalRepoUrl}' for '{applicationFromYaml.Metadata.Name}'");
 
-        applicationFromYaml.Metadata.Annotations.TryGetValue(ArgoCDConstants.Annotations.OctopusProjectAnnotationKey(applicationSource.Name.ToApplicationSourceName()), out var pathOverrideFromAnnotation); 
+        applicationFromYaml.Metadata.Annotations.TryGetValue(ArgoCDConstants.Annotations.OctopusPathAnnotationKey(applicationSource.Name.ToApplicationSourceName()), out var pathOverrideFromAnnotation); 
         
         var sourceUpdater = new CopyTemplatesSourceUpdater(packageFiles, log, fileSystem, deploymentConfig.PurgeOutputDirectory, pathOverrideFromAnnotation);
 
