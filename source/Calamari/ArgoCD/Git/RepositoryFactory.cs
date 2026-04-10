665e0add7b3ee3eebc82c05576e5485d70c100e1
Trent Mohay <trent.mohay@octopus.com>
2026-04-10 19:09:49 +1000

    Force git basic auth

--- source/Calamari/ArgoCD/Git/RepositoryFactory.cs
+++ source/Calamari/ArgoCD/Git/RepositoryFactory.cs
@@ -1,7 +1,9 @@
 using System;
 using System.IO;
-using Calamari.ArgoCD.Git.GitVendorApiAdapters;
 using System.Linq;
+using System.Text;
+using System.Threading;
+using Calamari.ArgoCD.Git.PullRequests;
 using Calamari.Common.Commands;
 using Calamari.Common.Plumbing.Extensions;
 using Calamari.Common.Plumbing.FileSystem;
@@ -21,17 +23,25 @@ namespace Calamari.ArgoCD.Git
         readonly ILog log;
         readonly ICalamariFileSystem fileSystem;
         readonly string repositoryParentDirectory;
-        readonly IGitVendorAgnosticApiAdapterFactory vendorAgnosticApiAdapterFactory;
+        readonly IGitVendorPullRequestClientResolver gitVendorPullRequestClientResolver;
         readonly IClock clock;
 
-        public RepositoryFactory(ILog log, ICalamariFileSystem fileSystem, string repositoryParentDirectory, IGitVendorAgnosticApiAdapterFactory vendorAgnosticApiAdapterFactory,
+        public RepositoryFactory(ILog log, ICalamariFileSystem fileSystem, string repositoryParentDirectory, IGitVendorPullRequestClientResolver gitVendorPullRequestClientResolver,
                                  IClock clock)
         {
             this.log = log;
             this.fileSystem = fileSystem;
             this.repositoryParentDirectory = repositoryParentDirectory;
-            this.vendorAgnosticApiAdapterFactory = vendorAgnosticApiAdapterFactory;
+            this.gitVendorPullRequestClientResolver = gitVendorPullRequestClientResolver;
             this.clock = clock;
+
+            // Calamari runs as a single-purpose process per deployment step and always receives
+            // explicit credentials. Clear the search paths for all global config levels so libgit2
+            // cannot load ~/.gitconfig or /etc/gitconfig and pick up a credential helper (e.g.
+            // osxkeychain) that would silently override or bypass the credentials we provide.
+            GlobalSettings.SetConfigSearchPaths(ConfigurationLevel.Global, []);
+            GlobalSettings.SetConfigSearchPaths(ConfigurationLevel.System, []);
+            GlobalSettings.SetConfigSearchPaths(ConfigurationLevel.Xdg, []);
         }
 
         public RepositoryWrapper CloneRepository(string repositoryName, IGitConnection gitConnection)
@@ -83,6 +93,8 @@ namespace Calamari.ArgoCD.Git
 
             var repo = new Repository(repoPath);
 
+            try
+            {
             //this is required to handle the issue around "HEAD"
             var branchToCheckout = repo.GetBranchName(gitConnection.GitReference);
             var remoteBranch = repo.Branches.First(f => f.IsRemote && f.UpstreamBranchCanonicalName == branchToCheckout.Value);
@@ -97,8 +109,14 @@ namespace Calamari.ArgoCD.Git
             }
             
             LibGit2Sharp.Commands.Checkout(repo, branchToCheckout.ToFriendlyName());
-            
-            var gitVendorApiAdapter = vendorAgnosticApiAdapterFactory.TryCreateGitVendorApiAdaptor(gitConnection);
+            }
+            catch (LibGit2SharpException e)
+            {
+                throw new CommandException($"Failed to checkout branch '{gitConnection.GitReference}' in repository at {gitConnection.Url}. Error: {e.Message}", e);
+            }
+
+            //TODO(tmm): Make this function (and all callers async).
+            var gitVendorApiAdapter = gitVendorPullRequestClientResolver.TryResolve(gitConnection, log, CancellationToken.None).Result;
             return new RepositoryWrapper(repo,
                                          fileSystem,
                                          checkoutPath,
