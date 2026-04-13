fd6db8b978ac6ce74459dba57695fc981c3a49b4
Trent Mohay <trent.mohay@octopus.com>
2026-04-13 10:50:12 +1000

    Back porting "Enforce Basic Auth for git operations" (#1875)

--- source/Calamari/ArgoCD/ArgoCDModule.cs
+++ source/Calamari/ArgoCD/ArgoCDModule.cs
@@ -3,11 +3,26 @@ using Calamari.ArgoCD.Conventions;
 using Calamari.ArgoCD.Git;
 using Calamari.ArgoCD.Git.GitVendorApiAdapters;
 using Calamari.ArgoCD.GitHub;
+using LibGit2Sharp;
 
 namespace Calamari.ArgoCD
 {
     public class ArgoCDModule : Module
     {
+        static ArgoCDModule()
+        {
+            // Note this cannot be set in the RepositoryFactory as it causes tests to fail, due to the following issue.
+            
+            // LibGit2Sharp custom sub-transports are registered by calling a static registration
+            // method on GlobalSettings. Additionally, if you try and register a multiple transports
+            // with the same scheme, it throws an exception. It's not ideal, but it's what we've got
+            // to work with.
+            //
+            // Using the type constructor to make sure that these methods are only called once.
+            GlobalSettings.RegisterSmartSubtransport<GitHttpSmartSubTransport>("http");
+            GlobalSettings.RegisterSmartSubtransport<GitHttpSmartSubTransport>("https");
+        }
+
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterType<DeploymentConfigFactory>().AsSelf().InstancePerLifetimeScope();
@@ -22,5 +37,5 @@ namespace Calamari.ArgoCD
             builder.RegisterType<ArgoCDManifestsFileMatcher>().As<IArgoCDManifestsFileMatcher>().InstancePerLifetimeScope();
             builder.RegisterType<ArgoCDFilesUpdatedReporter>().As<IArgoCDFilesUpdatedReporter>().InstancePerLifetimeScope();
         }
+        }
     }
-}
