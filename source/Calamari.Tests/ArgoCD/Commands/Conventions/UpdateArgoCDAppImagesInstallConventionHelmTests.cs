605e9581bda358830912148f9edf1a959e154305
scme0 <scott.merchant@octopus.com>
2026-04-17 11:55:19 +0200

    Undo previous fix because root cause has been addressed

--- source/Calamari.Tests/ArgoCD/Commands/Conventions/UpdateArgoCDAppImagesInstallConventionHelmTests.cs
+++ source/Calamari.Tests/ArgoCD/Commands/Conventions/UpdateArgoCDAppImagesInstallConventionHelmTests.cs
@@ -1558,7 +1558,7 @@ image:
             AssertNotUpdatedWithExpectedPatch(getCapturedResults,
                 expectedPatchPointer: "/0/image/tag",
                 expectedPatchValue: "1.27.1",
-                expectedPatchedFilePath: Path.Combine("otherRepoPath", "values.yaml"));
+                expectedPatchedFilePath: "otherRepoPath/values.yaml");
         }
 
         [Test]
