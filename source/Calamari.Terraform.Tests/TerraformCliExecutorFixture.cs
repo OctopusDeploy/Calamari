c3aad7e79098eb1cd37b506d2b64f43b97a8d335
Huy Nguyen <162080607+HuyPhanNguyen@users.noreply.github.com>
2026-04-21 18:34:41 +1000

    Retry terraform init on transient 502/503 errors (#1840)
    Co-authored-by: Trent Mohay <trent.mohay@octopus.com>

--- source/Calamari.Terraform.Tests/TerraformCliExecutorFixture.cs
+++ source/Calamari.Terraform.Tests/TerraformCliExecutorFixture.cs
@@ -51,5 +51,91 @@ namespace Calamari.Terraform.Tests
             variables.Get(TerraformSpecialVariables.Action.Terraform.VarFiles).Returns("foo\nbar\r\nbaz");
             cliExecutor.TerraformVariableFiles.Should().Be("-var-file=\"foo\" -var-file=\"bar\" -var-file=\"baz\"");
         }
+
+        [TestCase("│ Error while installing dnsimple/dnsimple v2.0.1: unsuccessful request to\n│ https://github.com/...\n│ 502 Bad Gateway", true)]
+        [TestCase("│ Error while installing hashicorp/azurerm v4.63.0: unsuccessful request to\n│ https://releases.hashicorp.com/...\n│ 503 Service Unavailable", true)]
+        [TestCase("Error configuring backend: 502 Bad Gateway", true)]
+        [TestCase("Error: Invalid backend configuration", false)]
+        [TestCase("Error: No valid credential sources found", false)]
+        [TestCase("", false)]
+        [TestCase(null, false)]
+        public void IsTransientInitError_MatchesExpectedPatterns(string errorText, bool expected)
+        {
+            TerraformCliExecutor.IsTransientInitError(errorText).Should().Be(expected);
+        }
+
+        [Test]
+        public void InitializePlugins_RetriesOnTransient502Error()
+        {
+            var testVariables = Substitute.For<IVariables>();
+            testVariables.GetStrings(KnownVariables.EnabledFeatureToggles).Returns(new List<string>());
+            var commandLineRunner = Substitute.For<ICommandLineRunner>();
+            var callCount = 0;
+            commandLineRunner.Execute(Arg.Do<CommandLineInvocation>(invocation =>
+            {
+                callCount++;
+                if (callCount == 1)
+                    invocation.AdditionalInvocationOutputSink.WriteInfo("Terraform v0.15.0");
+            })).Returns(_ =>
+            {
+                if (callCount == 2)
+                    return new CommandResult("terraform init", 1, "│ Error while installing dnsimple/dnsimple v2.0.1: unsuccessful request to\n│ https://github.com/...\n│ 502 Bad Gateway");
+                return new CommandResult("foo", 0);
+            });
+
+            var executor = new TerraformCliExecutor(Substitute.For<ILog>(), Substitute.For<ICalamariFileSystem>(), commandLineRunner, new RunningDeployment("blah", testVariables), new Dictionary<string, string>());
+
+            commandLineRunner.Received(3).Execute(Arg.Any<CommandLineInvocation>());
+        }
+
+        [Test]
+        public void InitializePlugins_DoesNotRetryNonTransientError()
+        {
+            var testVariables = Substitute.For<IVariables>();
+            testVariables.GetStrings(KnownVariables.EnabledFeatureToggles).Returns(new List<string>());
+            var commandLineRunner = Substitute.For<ICommandLineRunner>();
+            var callCount = 0;
+            commandLineRunner.Execute(Arg.Do<CommandLineInvocation>(invocation =>
+            {
+                callCount++;
+                if (callCount == 1)
+                    invocation.AdditionalInvocationOutputSink.WriteInfo("Terraform v0.15.0");
+            })).Returns(_ =>
+            {
+                if (callCount == 2)
+                    return new CommandResult("terraform init", 1, "Error: Invalid backend configuration");
+                return new CommandResult("foo", 0);
+            });
+
+            var act = () => new TerraformCliExecutor(Substitute.For<ILog>(), Substitute.For<ICalamariFileSystem>(), commandLineRunner, new RunningDeployment("blah", testVariables), new Dictionary<string, string>());
+
+            act.Should().Throw<CommandLineException>();
+            commandLineRunner.Received(2).Execute(Arg.Any<CommandLineInvocation>());
+        }
+
+        [Test]
+        public void InitializePlugins_ThrowsAfterRetriesExhausted()
+        {
+            var testVariables = Substitute.For<IVariables>();
+            testVariables.GetStrings(KnownVariables.EnabledFeatureToggles).Returns(new List<string>());
+            var commandLineRunner = Substitute.For<ICommandLineRunner>();
+            var callCount = 0;
+            commandLineRunner.Execute(Arg.Do<CommandLineInvocation>(invocation =>
+            {
+                callCount++;
+                if (callCount == 1)
+                    invocation.AdditionalInvocationOutputSink.WriteInfo("Terraform v0.15.0");
+            })).Returns(_ =>
+            {
+                if (callCount >= 2)
+                    return new CommandResult("terraform init", 1, "│ Error while installing dnsimple/dnsimple v2.0.1: unsuccessful request to\n│ https://github.com/...\n│ 502 Bad Gateway");
+                return new CommandResult("foo", 0);
+            });
+
+            var act = () => new TerraformCliExecutor(Substitute.For<ILog>(), Substitute.For<ICalamariFileSystem>(), commandLineRunner, new RunningDeployment("blah", testVariables), new Dictionary<string, string>());
+
+            act.Should().Throw<CommandLineException>();
+            commandLineRunner.Received(5).Execute(Arg.Any<CommandLineInvocation>());
+        }
     }
 }
