68dd79e3bcad6b79420ed8f21eb0f9c26cdb463e
Orion Edwards <orion.edwards@gmail.com>
2026-05-05 10:50:49 +1200

    Use dictionary to avoid repeated linear-scans of ZipArchive.Entries list

--- source/Calamari.ConsolidateCalamariPackages/ConsolidatedPackage.cs
+++ source/Calamari.ConsolidateCalamariPackages/ConsolidatedPackage.cs
@@ -28,21 +28,16 @@ namespace Octopus.Calamari.ConsolidatedPackage
                 throw new Exception($"Could not find platform {platform} for {calamariFlavour}");
             }
 
-            using (var sourceStream = packageStreamProvider.OpenStream())
+            var platformFilesLookup = platformFiles.ToDictionary(f => f.Source);
+
+            using var sourceStream = packageStreamProvider.OpenStream();
+            using var source = new ZipArchive(sourceStream, ZipArchiveMode.Read);
+            foreach (var sourceEntry in source.Entries)
             {
-                using (var source = new ZipArchive(sourceStream, ZipArchiveMode.Read))
-                {
-                    foreach (var fileTransfer in platformFiles)
-                    {
-                        var sourceEntry = source.Entries.FirstOrDefault(e => e.FullName.Equals(fileTransfer.Source));
-                        if(sourceEntry is null) continue;
-                    
-                        using (var sourceEntryStream = sourceEntry.Open())
-                        {
-                            yield return (fileTransfer.Destination, sourceEntry.Length, sourceEntryStream);
-                        }
-                    }
-                }
+                if (!platformFilesLookup.TryGetValue(sourceEntry.FullName, out var fileTransfer)) continue;
+
+                using var sourceEntryStream = sourceEntry.Open();
+                yield return (fileTransfer.Destination, sourceEntry.Length, sourceEntryStream);
             }
         }
     }
