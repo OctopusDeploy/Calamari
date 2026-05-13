efbf1bd436c82b04f64464a556a0e438afc6abd4
JT <justin.taylor@octopus.com>
2026-05-13 16:31:10 +1000

    feat: a little syntactical sugar

---
+++ source/Calamari.Tests/Common/Util/TypeExtensionMethodsFixture.cs
@@ -0,0 +1,46 @@
+using Calamari.Common.Commands;
+using Calamari.Common.Util;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Calamari.Tests.Common.Util;
+
+[TestFixture]
+public class TypeExtensionMethodsFixture
+{
+    public class GetCommandNameFromAttribute
+    {
+        [Test]
+        public void WhenAttributeNotPresent_ReturnsEmptyString()
+        {
+            var input = new object();
+            
+            var result = input.GetType().GetCommandNameFromAttribute();
+            
+            result.Should().BeEmpty();
+        }
+
+        [Test]
+        public void WhenAttributePresent_ReturnsCommandName()
+        {
+            
+
+            var input = new InputClass();
+            
+            var result = input.GetType().GetCommandNameFromAttribute();
+
+            result.Should().Be("command-name");
+        }
+    }
+    
+    
+    [Command("command-name") ]
+    class InputClass : ICommand
+    {
+        public int Execute()
+        {
+            return 0;
+        }
+    }
+    
+}
