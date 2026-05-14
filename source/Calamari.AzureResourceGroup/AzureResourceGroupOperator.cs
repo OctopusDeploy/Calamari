4665bb6e41dd4f96899905a82791cdcfa3ea10ae
JT <justin.taylor@octopus.com>
2026-05-14 17:29:09 +1000

    Handles null + cleanup code

--- source/Calamari.AzureResourceGroup/AzureResourceGroupOperator.cs
+++ source/Calamari.AzureResourceGroup/AzureResourceGroupOperator.cs
@@ -14,126 +14,126 @@ using Newtonsoft.Json.Linq;
 using Polly;
 using Polly.Timeout;
 
-namespace Calamari.AzureResourceGroup
+namespace Calamari.AzureResourceGroup;
+
+class AzureResourceGroupOperator(ILog log)
 {
-    class AzureResourceGroupOperator
+    public async Task<ArmOperation<ArmDeploymentResource>> CreateDeployment(ResourceGroupResource resourceGroupResource,
+                                                                            string deploymentName,
+                                                                            ArmDeploymentMode deploymentMode,
+                                                                            string template,
+                                                                            string? parameters)
     {
-        readonly ILog log;
+        log.Verbose($"Template:\n{template}\n");
+        if (parameters != null)
+            log.Verbose($"Parameters:\n{parameters}\n");
 
-        public AzureResourceGroupOperator(ILog log)
+        try
         {
-            this.log = log;
-        }
-
-        public async Task<ArmOperation<ArmDeploymentResource>> CreateDeployment(ResourceGroupResource resourceGroupResource,
-                                                                                string deploymentName,
-                                                                                ArmDeploymentMode deploymentMode,
-                                                                                string template,
-                                                                                string? parameters)
-        {
-            log.Verbose($"Template:\n{template}\n");
-            if (parameters != null)
-                log.Verbose($"Parameters:\n{parameters}\n");
-
-            try
+            var deploymentContent = new ArmDeploymentContent(new ArmDeploymentProperties(deploymentMode)
             {
-                var deploymentContent = new ArmDeploymentContent(new ArmDeploymentProperties(deploymentMode)
-                {
-                    Template = BinaryData.FromString(template),
-                    Parameters = parameters != null ? BinaryData.FromString(parameters) : null
-                });
-                var createDeploymentResult = await resourceGroupResource.GetArmDeployments().CreateOrUpdateAsync(WaitUntil.Started, deploymentName, deploymentContent);
+                Template = BinaryData.FromString(template),
+                Parameters = parameters != null ? BinaryData.FromString(parameters) : null
+            });
+            var createDeploymentResult = await resourceGroupResource.GetArmDeployments().CreateOrUpdateAsync(WaitUntil.Started, deploymentName, deploymentContent);
 
-                log.Info($"Deployment {deploymentName} created.");
+            log.Info($"Deployment {deploymentName} created.");
 
-                return createDeploymentResult;
-            }
-            catch
+            return createDeploymentResult;
+        }
+        catch
+        {
+            log.Error("Error submitting deployment");
+            throw;
+        }
+    }
+
+    public async Task PollForCompletionWithTimeout(ArmOperation<ArmDeploymentResource> deploymentOperation, IVariables variables)
+    {
+        var pollingTimeout = GetPollingTimeout(variables);
+        var asyncResourceGroupPollingTimeoutPolicy = Policy.TimeoutAsync(pollingTimeout, TimeoutStrategy.Optimistic);
+        await asyncResourceGroupPollingTimeoutPolicy.ExecuteAsync(ct => Poll(deploymentOperation, ct), CancellationToken.None);
+    }
+
+    public async Task PollForCompletion(ArmOperation<ArmDeploymentResource> deploymentOperation)
+    {
+        await Poll(deploymentOperation, CancellationToken.None);
+    }
+
+    async Task Poll(ArmOperation<ArmDeploymentResource> deploymentOperation, CancellationToken cancellationToken)
+    {
+        log.Info("Polling for deployment completion...");
+        try
+        {
+            var delayStrategy = DelayStrategy.CreateExponentialDelayStrategy(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(30));
+            var response = await deploymentOperation.WaitForCompletionAsync(delayStrategy, cancellationToken);
+            log.Info($"Deployment completed with status: {response.Value?.Data.Properties?.ProvisioningState}");
+        }
+        catch
+        {
+            log.Error("Error polling for deployment completion");
+            throw;
+        }
+    }
+
+    public async Task FinalizeDeployment(ArmOperation<ArmDeploymentResource> operation, IVariables variables)
+    {
+        await LogOperationResults(operation);
+        CaptureOutputs(operation.Value.Data.Properties.Outputs?.ToString(), variables);
+    }
+
+    async Task LogOperationResults(ArmOperation<ArmDeploymentResource> operation)
+    {
+        if (!operation.HasValue || !operation.HasCompleted)
+            return;
+
+        var sb = new StringBuilder("Operations details:\n");
+        await foreach (var op in operation.Value.GetDeploymentOperationsAsync())
+        {
+            var properties = op.Properties;
+            sb.AppendLine($"Resource: {properties.TargetResource?.ResourceName}");
+            sb.AppendLine($"Type: {properties.TargetResource?.ResourceType}");
+            sb.AppendLine($"Timestamp: {properties.Timestamp?.ToLocalTime():s}");
+            sb.AppendLine($"Deployment operation: {op.Id}");
+            sb.AppendLine($"Status: {properties.StatusCode}");
+            sb.AppendLine($"Provisioning State: {properties.ProvisioningState}");
+            if (properties.StatusMessage != null)
+                sb.AppendLine($"Status Message: {JsonConvert.SerializeObject(properties.StatusMessage)}");
+            sb.Append(" \n");
+        }
+
+        log.Info(sb.ToString());
+    }
+
+    void CaptureOutputs(string? outputsJson, IVariables variables)
+    {
+        if (string.IsNullOrWhiteSpace(outputsJson))
+            return;
+
+        log.Verbose("Deployment Outputs:");
+        log.Verbose(outputsJson);
+
+        var outputs = JObject.Parse(outputsJson);
+
+
+            
+        foreach (var output in outputs)
+        {
+            if (output.Value?["value"] is not null)
             {
-                log.Error("Error submitting deployment");
-                throw;
+                log.SetOutputVariable($"AzureRmOutputs[{output.Key}]", output.Value["value"]!.ToString(), variables);
             }
         }
+                
+    }
 
-        public async Task PollForCompletionWithTimeout(ArmOperation<ArmDeploymentResource> deploymentOperation, IVariables variables)
+    static TimeSpan GetPollingTimeout(IVariables variables)
+    {
+        var pollingTimeoutVariableValue = variables.GetInt32(SpecialVariables.Action.Azure.ArmDeploymentTimeout);
+        if (pollingTimeoutVariableValue.HasValue && pollingTimeoutVariableValue.Value > 0)
         {
-            var pollingTimeout = GetPollingTimeout(variables);
-            var asyncResourceGroupPollingTimeoutPolicy = Policy.TimeoutAsync(pollingTimeout, TimeoutStrategy.Optimistic);
-            await asyncResourceGroupPollingTimeoutPolicy.ExecuteAsync(ct => Poll(deploymentOperation, ct), CancellationToken.None);
+            return TimeSpan.FromMinutes(pollingTimeoutVariableValue.Value);
         }
-
-        public async Task PollForCompletion(ArmOperation<ArmDeploymentResource> deploymentOperation)
-        {
-            await Poll(deploymentOperation, CancellationToken.None);
-        }
-
-        async Task Poll(ArmOperation<ArmDeploymentResource> deploymentOperation, CancellationToken cancellationToken)
-        {
-            log.Info("Polling for deployment completion...");
-            try
-            {
-                var delayStrategy = DelayStrategy.CreateExponentialDelayStrategy(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(30));
-                var response = await deploymentOperation.WaitForCompletionAsync(delayStrategy, cancellationToken);
-                log.Info($"Deployment completed with status: {response.Value?.Data.Properties?.ProvisioningState}");
-            }
-            catch
-            {
-                log.Error("Error polling for deployment completion");
-                throw;
-            }
-        }
-
-        public async Task FinalizeDeployment(ArmOperation<ArmDeploymentResource> operation, IVariables variables)
-        {
-            await LogOperationResults(operation);
-            CaptureOutputs(operation.Value.Data.Properties.Outputs?.ToString(), variables);
-        }
-
-        async Task LogOperationResults(ArmOperation<ArmDeploymentResource> operation)
-        {
-            if (!operation.HasValue || !operation.HasCompleted)
-                return;
-
-            var sb = new StringBuilder("Operations details:\n");
-            await foreach (var op in operation.Value.GetDeploymentOperationsAsync())
-            {
-                var properties = op.Properties;
-                sb.AppendLine($"Resource: {properties.TargetResource?.ResourceName}");
-                sb.AppendLine($"Type: {properties.TargetResource?.ResourceType}");
-                sb.AppendLine($"Timestamp: {properties.Timestamp?.ToLocalTime():s}");
-                sb.AppendLine($"Deployment operation: {op.Id}");
-                sb.AppendLine($"Status: {properties.StatusCode}");
-                sb.AppendLine($"Provisioning State: {properties.ProvisioningState}");
-                if (properties.StatusMessage != null)
-                    sb.AppendLine($"Status Message: {JsonConvert.SerializeObject(properties.StatusMessage)}");
-                sb.Append(" \n");
-            }
-
-            log.Info(sb.ToString());
-        }
-
-        void CaptureOutputs(string? outputsJson, IVariables variables)
-        {
-            if (string.IsNullOrWhiteSpace(outputsJson))
-                return;
-
-            log.Verbose("Deployment Outputs:");
-            log.Verbose(outputsJson);
-
-            var outputs = JObject.Parse(outputsJson);
-
-            foreach (var output in outputs)
-                log.SetOutputVariable($"AzureRmOutputs[{output.Key}]", output.Value["value"].ToString(), variables);
-        }
-
-        static TimeSpan GetPollingTimeout(IVariables variables)
-        {
-            var pollingTimeoutVariableValue = variables.GetInt32(SpecialVariables.Action.Azure.ArmDeploymentTimeout);
-            if (pollingTimeoutVariableValue.HasValue && pollingTimeoutVariableValue.Value > 0)
-            {
-                return TimeSpan.FromMinutes(pollingTimeoutVariableValue.Value);
-            }
-            return TimeSpan.FromMinutes(30);
-        }
+        return TimeSpan.FromMinutes(30);
     }
 }
