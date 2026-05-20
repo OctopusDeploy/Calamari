df41432c8624b166cc7e48f82f313e390d6be760
JT <justin.taylor@octopus.com>
2026-05-20 18:54:51 +1000

    Add Serilization extensions. for variables.

---
+++ source/Calamari.Common/Plumbing/Variables/VariableDeserilizationExtensions.cs
@@ -0,0 +1,41 @@
+using System;
+using Calamari.Common.Commands;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Calamari.Common.Plumbing.Variables;
+
+public static class CalamariVariableDeserilisationExtensions
+{
+    public static readonly JsonSerializerSettings DefaultSerializationSettings = new()
+    {
+        Formatting = Formatting.None,
+        ContractResolver = new CamelCasePropertyNamesContractResolver(),
+        MissingMemberHandling = MissingMemberHandling.Error,
+    };
+    
+    public static T GetValueDeserilisedAs<T>(this CalamariVariables variables, string name)
+    {
+
+        var variableJson = variables.Get(name);
+
+        if (string.IsNullOrEmpty(variableJson))
+        {
+            throw new CommandException($"Variable {name} was not supplied");
+        }
+
+        try
+        {
+            var output = JsonConvert.DeserializeObject<T>(variableJson, DefaultSerializationSettings);
+            return output ?? throw new CommandException($"Variable {name} was deserialized as null ");
+        }
+        catch (JsonSerializationException)
+        {
+            throw new CommandException($"Variable {name} could not be deserialized as type {typeof(T).FullName}");
+        }
+        catch (JsonReaderException)
+        {
+            throw new CommandException($"Variable {name} was not valid JSON or could not be deserialized");
+        }
+    }
+}
