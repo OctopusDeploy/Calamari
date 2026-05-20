98fad5c07689c56837331247d70bcd709bf72773
Eddy Moulton <8491021+eddymoulton@users.noreply.github.com>
2026-05-20 13:25:24 +1000

    Use Octopus.LibGit2Sharp (#1949)

--- source/Calamari.Tests/ArgoCD/Commands/Conventions/UpdateArgoCDApplicationManifestsInstallConventionTests.cs
+++ source/Calamari.Tests/ArgoCD/Commands/Conventions/UpdateArgoCDApplicationManifestsInstallConventionTests.cs
@@ -28,7 +28,7 @@ using Octopus.Calamari.Contracts.ArgoCD;
 namespace Calamari.Tests.ArgoCD.Commands.Conventions
 {
     [TestFixture]
-    [Category(TestCategory.RequiresOpenSsl3)]
+    [Category(TestCategory.RequiresOpenSsl1_1OrOpenSsl3)]
     public class UpdateArgoCDApplicationManifestsInstallConventionTests
     {
         const string ProjectSlug = "TheProject";
