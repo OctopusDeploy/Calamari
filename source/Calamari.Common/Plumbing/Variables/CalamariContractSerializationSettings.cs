b59460c1dca41b6f07707b9c7b5c799c4e58d2c7
JT <justin.taylor@octopus.com>
2026-05-21 12:21:30 +1000

    Typing

--- source/Calamari.Common/Plumbing/Variables/CalamariContractSerializationSettings.cs
+++ source/Calamari.Common/Plumbing/Variables/CalamariContractSerializationSettings.cs
@@ -8,7 +8,7 @@ public static class CalamariContractSerializationSettings
 
     // NOTE: Values here should match Octopus.Variables/CalamariContractSerializationSettings.cs
     // changes in one should be reflected in the other
-    public static JsonSerializerSettings Default => new JsonSerializerSettings
+    public static JsonSerializerSettings Default => new()
     {
         Formatting = Formatting.None,
         NullValueHandling = NullValueHandling.Ignore,
