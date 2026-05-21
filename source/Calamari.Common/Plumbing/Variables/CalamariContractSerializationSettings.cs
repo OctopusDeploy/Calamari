dc58ffb1ac4d79aa98b698246f852b4b3d1294c6
JT <justin.taylor@octopus.com>
2026-05-21 12:10:22 +1000

    Add default contract serialisation settings

---
+++ source/Calamari.Common/Plumbing/Variables/CalamariContractSerializationSettings.cs
@@ -0,0 +1,17 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Calamari.Common.Plumbing.Variables;
+
+public static class CalamariContractSerializationSettings
+{
+
+    // NOTE: Values here should match Octopus.Variables/CalamariContractSerializationSettings.cs
+    // changes in one should be reflected in the other
+    public static JsonSerializerSettings Default => new JsonSerializerSettings
+    {
+        Formatting = Formatting.None,
+        NullValueHandling = NullValueHandling.Ignore,
+        ContractResolver = new CamelCasePropertyNamesContractResolver()
+    };
+}
