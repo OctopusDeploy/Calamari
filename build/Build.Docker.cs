b8dbcdbd01532ae98e72a983b23dbbf6ad4e02f6
Alastair Pitts <alastair.pitts@octopus.com>
2026-05-27 11:04:49 +1000

    Rename BuildDockerImages

--- build/Build.Docker.cs
+++ build/Build.Docker.cs
@@ -6,54 +6,54 @@ namespace Calamari.Build;
 public partial class Build
 {
     [PublicAPI]
-    public Target BuildDockerImage =>
-        _ => _
-            .Executes(() =>
-                      {
-                          var flavours = GetCalamariFlavours();
-                          string[] supportedPlatforms = ["linux/amd64", "linux/arm64", "linux/arm"];
-                          var dockerBuildPlatform = string.Join(",", supportedPlatforms);
+    Target BuildDockerImages =>
+        d =>
+            d.Executes(() =>
+                       {
+                           var flavours = GetCalamariFlavours();
+                           string[] supportedPlatforms = ["linux/amd64", "linux/arm64", "linux/arm"];
+                           var dockerBuildPlatform = string.Join(",", supportedPlatforms);
 
-                          var dockerFile = KnownPaths.DockerDirectory / "Dockerfile";
+                           var dockerFile = KnownPaths.DockerDirectory / "Dockerfile";
 
-                          foreach (var flavour in flavours)
-                          {
-                              var compressedArtifactPath = KnownPaths.OutputsDirectory / $"{flavour}.zip";
-                              compressedArtifactPath.UncompressTo(KnownPaths.OutputsDirectory / flavour);
+                           foreach (var flavour in flavours)
+                           {
+                               var compressedArtifactPath = KnownPaths.OutputsDirectory / $"{flavour}.zip";
+                               compressedArtifactPath.UncompressTo(KnownPaths.OutputsDirectory / flavour);
 
-                              //Rename any `linux-x64` folders to `linux-amd64`
-                              Directory.Move(KnownPaths.OutputsDirectory / flavour / "linux-x64",
-                                             KnownPaths.OutputsDirectory / flavour / "linux-amd64");
+                               //Rename any `linux-x64` folders to `linux-amd64`
+                               Directory.Move(KnownPaths.OutputsDirectory / flavour / "linux-x64",
+                                              KnownPaths.OutputsDirectory / flavour / "linux-amd64");
 
-                              var tag = $"octopusdeploy/{flavour}:{NugetVersion.Value}".ToLowerInvariant();
+                               var tag = $"octopusdeploy/{flavour}:{NugetVersion.Value}".ToLowerInvariant();
 
-                              //build the docker image for this flavour
-                              DockerTasks.DockerBuildxBuild(settings =>
-                                                            {
-                                                                settings = settings
-                                                                           .AddBuildArg($"SRC_FOLDER={KnownPaths.OutputsDirectory}", $"DEST_FOLDER={flavour}")
-                                                                           .SetPlatform(dockerBuildPlatform)
-                                                                           .SetTag(tag)
-                                                                           .SetFile(dockerFile)
-                                                                           .SetPath(KnownPaths.RootDirectory);
+                               //build the docker image for this flavour
+                               DockerTasks.DockerBuildxBuild(settings =>
+                                                             {
+                                                                 settings = settings
+                                                                            .AddBuildArg($"SRC_FOLDER={KnownPaths.OutputsDirectory}", $"DEST_FOLDER={flavour}")
+                                                                            .SetPlatform(dockerBuildPlatform)
+                                                                            .SetTag(tag)
+                                                                            .SetFile(dockerFile)
+                                                                            .SetPath(KnownPaths.RootDirectory);
 
-                                                                return settings;
-                                                            });
+                                                                 return settings;
+                                                             });
 
-                              var sanitizedTag = tag.Replace("/", "-");
-                              var outputFile = KnownPaths.PublishDirectory / $"{sanitizedTag}.tar";
+                               var sanitizedTag = tag.Replace("/", "-");
+                               var outputFile = KnownPaths.PublishDirectory / $"{sanitizedTag}.tar";
 
-                              //save the docker image to a tar file
-                              DockerTasks.DockerImageSave(_ => _
-                                                               .SetImages(tag)
-                                                               .SetOutput(outputFile));
+                               //save the docker image to a tar file
+                               DockerTasks.DockerImageSave(_ => _
+                                                                .SetImages(tag)
+                                                                .SetOutput(outputFile));
 
-                              //compress with gzip
-                              var compressedZipPath = $"{outputFile}.gz";
-                              outputFile.CompressTo(compressedZipPath);
-                              
-                              // This file is then uploaded to OctopusDeploy to perform the release process
-                              TeamCity.Instance.PublishArtifacts(compressedZipPath);
-                          }
-                      });
+                               //compress with gzip
+                               var compressedZipPath = $"{outputFile}.gz";
+                               outputFile.CompressTo(compressedZipPath);
+
+                               // This file is then uploaded to OctopusDeploy to perform the release process
+                               TeamCity.Instance.PublishArtifacts(compressedZipPath);
+                           }
+                       });
 }
