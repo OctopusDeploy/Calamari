fe16a2257a5cf18b6d183111ddc9be4388625b44
JT <justin.taylor@octopus.com>
2026-05-29 12:08:25 +1000

    Refactor convention

--- source/Calamari.Aws/Deployment/Conventions/DeployEcsServiceConvention.cs
+++ source/Calamari.Aws/Deployment/Conventions/DeployEcsServiceConvention.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using Amazon.CloudFormation;
 using Amazon.CloudFormation.Model;
 using Calamari.Aws.Inputs.Ecs;
@@ -17,49 +16,42 @@ using Calamari.Deployment.Conventions;
 namespace Calamari.Aws.Deployment.Conventions;
 
 // Currently a thin wrapper over existing template deployment process with the goal to swapping it out for native ECS API solution in the future.
-public class DeployEcsServiceConvention: IInstallConvention
+public class DeployEcsServiceConvention(DeployEcsCommandInputs commandInputs, AwsEnvironmentGeneration awsEnvironment, ILog log, IVariables variables)
+    : IInstallConvention
 {
-    readonly DeployAwsCloudFormationConvention encapsulatedConvention;
-
-    public DeployEcsServiceConvention(DeployEcsCommandInputs commandInputs, AwsEnvironmentGeneration awsEnvironment, ILog log, IVariables variables) 
+    public void Install(RunningDeployment deployment)
     {
+        var template = EcsDeployTemplateGenerator.GenerateTemplate(commandInputs);
         var stackEventLogger = new StackEventLogger(log);
 
-        encapsulatedConvention = new DeployAwsCloudFormationConvention(ClientFactory, 
-            TemplateFactory, 
-            stackEventLogger, 
-            StackProvider, 
+        var deployCloudFormationConvention = new DeployAwsCloudFormationConvention(ClientFactory,
+            TemplateFactory,
+            stackEventLogger,
+            StackProvider,
             commandInputs.ShouldWaitForDeploymentCompletion,
             commandInputs.CfStackName,
             awsEnvironment,
             log,
             commandInputs.WaitOption.GetTimeoutSpan()
-            );
+        );
+        deployCloudFormationConvention.Install(deployment);
         return;
 
         StackArn StackProvider(RunningDeployment _) => commandInputs.CfStackArn;
         IAmazonCloudFormation ClientFactory() => ClientHelpers.CreateCloudFormationClient(awsEnvironment);
-        
+
         ICloudFormationRequestBuilder TemplateFactory()
         {
-            return new CloudFormationTemplate(() => EcsDeployTemplateGenerator.GenerateTemplate(commandInputs),
+            return new CloudFormationTemplate(() => template,
                 new EmptyTemplateInputs<Parameter>(),
                 commandInputs.CfStackName,
                 ["CAPABILITY_NAMED_IAM"],
-                true,
+                false,
                 null,
                 commandInputs.Tags,
                 commandInputs.CfStackArn,
                 ClientFactory,
                 variables);
         }
-        
     }
-    
-    public void Install(RunningDeployment deployment)
-    {
-        encapsulatedConvention.Install(deployment);
-    }
-
 }
-
