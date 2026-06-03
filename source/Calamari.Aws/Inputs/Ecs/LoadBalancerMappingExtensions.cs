4d18bdcdc4b828885d32d20ea34c60a9c9615ff7
JT <justin.taylor@octopus.com>
2026-06-03 11:03:53 +1000

    fix int parse

--- source/Calamari.Aws/Inputs/Ecs/LoadBalancerMappingExtensions.cs
+++ source/Calamari.Aws/Inputs/Ecs/LoadBalancerMappingExtensions.cs
@@ -13,7 +13,7 @@ public static class LoadBalancerMappingExtensions
         var mappings = loadBalancerMappings.Select(lbm => new Cfn.LoadBalancer
         {
             ContainerName  = lbm.ContainerName,
-            ContainerPort  = lbm.ContainerPort.ConvertedOrDefault<int?>(s => int.Parse(s, CultureInfo.InvariantCulture)),
+            ContainerPort  = lbm.ContainerPort.ConvertedOrDefault<int?>(s => int.TryParse(s, out var result) ? result : null),
             TargetGroupArn = lbm.TargetGroupArn
         }).ToArray();
 
