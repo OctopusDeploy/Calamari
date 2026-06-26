c8b89ae89df19af77ac5c2b46c18a9b958a69a19
robert <robert.erez@gmail.com>
2026-06-26 13:34:59 +1000

    Extract IBicepTemplateBuilder and unit-test the Bicep deploy behaviour
    Move the az-cli bicep compile behind IBicepTemplateBuilder and the resource-group
    create+deploy behind IAzureResourceGroupOperator.DeployCreatingResourceGroup, so
    DeployBicepTemplateBehaviour's source resolution and wiring is unit-tested without
    Azure or the az cli.

--- source/Calamari.AzureResourceGroup/AzureResourceGroupOperator.cs
+++ source/Calamari.AzureResourceGroup/AzureResourceGroupOperator.cs
@@ -40,7 +40,44 @@ class AzureResourceGroupOperator(ILog log) : IAzureResourceGroupOperator
         await FinalizeDeployment(deploymentOperation, variables);
     }
 
-    public async Task<ArmOperation<ArmDeploymentResource>> CreateDeployment(ResourceGroupResource resourceGroupResource,
+    // Used by the Bicep deploy behaviour: creates the resource group first if it does not already exist.
+    public async Task DeployCreatingResourceGroup(IAzureAccount account,
+                                                  string subscriptionId,
+                                                  string resourceGroupName,
+                                                  string resourceGroupLocation,
+                                                  string deploymentName,
+                                                  ArmDeploymentMode deploymentMode,
+                                                  string template,
+                                                  string? parameters,
+                                                  IVariables variables)
+    {
+        var armClient = account.CreateArmClient();
+        var resourceGroupResource = await GetOrCreateResourceGroup(armClient, subscriptionId, resourceGroupName, resourceGroupLocation);
+
+        var deploymentOperation = await CreateDeployment(resourceGroupResource, deploymentName, deploymentMode, template, parameters);
+        await PollForCompletion(deploymentOperation);
+        await FinalizeDeployment(deploymentOperation, variables);
+    }
+
+    async Task<ResourceGroupResource> GetOrCreateResourceGroup(ArmClient armClient, string subscriptionId, string resourceGroupName, string location)
+    {
+        var subscription = armClient.GetSubscriptionResource(SubscriptionResource.CreateResourceIdentifier(subscriptionId));
+
+        var resourceGroups = subscription.GetResourceGroups();
+        var existing = await resourceGroups.GetIfExistsAsync(resourceGroupName);
+
+        if (existing.HasValue && existing.Value != null)
+            return existing.Value;
+
+        log.Info($"The resource group with the name {resourceGroupName} does not exist");
+        log.Info($"Creating resource group {resourceGroupName} in location {location}");
+
+        var resourceGroupData = new ResourceGroupData(location);
+        var armOperation = await resourceGroups.CreateOrUpdateAsync(WaitUntil.Completed, resourceGroupName, resourceGroupData);
+        return armOperation.Value;
+    }
+
+    async Task<ArmOperation<ArmDeploymentResource>> CreateDeployment(ResourceGroupResource resourceGroupResource,
                                                                             string deploymentName,
                                                                             ArmDeploymentMode deploymentMode,
                                                                             string template,
@@ -70,14 +107,14 @@ class AzureResourceGroupOperator(ILog log) : IAzureResourceGroupOperator
         }
     }
 
-    public async Task PollForCompletionWithTimeout(ArmOperation<ArmDeploymentResource> deploymentOperation, IVariables variables)
+    async Task PollForCompletionWithTimeout(ArmOperation<ArmDeploymentResource> deploymentOperation, IVariables variables)
     {
         var pollingTimeout = GetPollingTimeout(variables);
         var asyncResourceGroupPollingTimeoutPolicy = Policy.TimeoutAsync(pollingTimeout, TimeoutStrategy.Optimistic);
         await asyncResourceGroupPollingTimeoutPolicy.ExecuteAsync(ct => Poll(deploymentOperation, ct), CancellationToken.None);
     }
 
-    public async Task PollForCompletion(ArmOperation<ArmDeploymentResource> deploymentOperation)
+    async Task PollForCompletion(ArmOperation<ArmDeploymentResource> deploymentOperation)
     {
         await Poll(deploymentOperation, CancellationToken.None);
     }
@@ -98,7 +135,7 @@ class AzureResourceGroupOperator(ILog log) : IAzureResourceGroupOperator
         }
     }
 
-    public async Task FinalizeDeployment(ArmOperation<ArmDeploymentResource> operation, IVariables variables)
+    async Task FinalizeDeployment(ArmOperation<ArmDeploymentResource> operation, IVariables variables)
     {
         await LogOperationResults(operation);
         CaptureOutputs(operation.Value.Data.Properties.Outputs?.ToString(), variables);
