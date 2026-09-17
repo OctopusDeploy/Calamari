323056974fb78219bad9df1cf019204978dbaeac
Alastair Pitts <APErebus@users.noreply.github.com>
2026-09-17 11:29:45 +1000

    Update WindowsFxVersion for Azure App Service tests

--- source/Calamari.AzureAppService.Tests/AzureAppServiceDeployContainerBehaviourFixture.cs
+++ source/Calamari.AzureAppService.Tests/AzureAppServiceDeployContainerBehaviourFixture.cs
@@ -57,7 +57,7 @@ namespace Calamari.AzureAppService.Tests
                                                                        {
                                                                            SiteConfig = new SiteConfigProperties
                                                                            {
-                                                                               WindowsFxVersion = "DOCKER|mcr.microsoft.com/dotnet/samples:aspnetapp",
+                                                                               WindowsFxVersion = "DOCKER|mcr.microsoft.com/dotnet/framework/samples:aspnetapp-windowsservercore-ltsc2022",
                                                                                IsAlwaysOn = true,
                                                                                AppSettings = new List<AppServiceNameValuePair>
                                                                                {
